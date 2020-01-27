be02335f3ad822c4b911e75b60dc53d3eeca4f2a
Robert Fratto <robert.fratto@grafana.com>
2020-01-27 13:25:16 -0500

    add instance recovery
    if an prometheus instance exits unexpectedly, the agent will try to
    restart it to recover.

--- pkg/prometheus/agent.go
+++ pkg/prometheus/agent.go
@@ -4,7 +4,11 @@
 package prometheus
 
 import (
+	"sync"
+	"time"
+
 	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
 	"github.com/prometheus/prometheus/config"
 )
 
@@ -12,6 +16,7 @@ import (
 // instances, along with a global configuration.
 type Config struct {
 	Global  config.GlobalConfig `yaml:"global"`
+	WALDir  string              `yaml:"wal_directory"`
 	Configs []InstanceConfig    `yaml:"configs,omitempty"`
 }
 
@@ -48,29 +53,92 @@ func zeroGlobalConfig(c config.GlobalConfig) bool {
 // and WAL components of Prometheus. It is broken down into a series
 // of Instances, each of which perform metric collection.
 type Agent struct {
-	cfg       Config
-	logger    log.Logger
-	instances []*instance
+	cfg    Config
+	logger log.Logger
+
+	instanceMtx sync.Mutex
+	instances   []*instance
 }
 
 // New creates and starts a new Agent.
-func New(cfg Config, logger log.Logger) *Agent {
-	// TODO(rfratto): validate config here or have the invoker be responsible?
+func New(cfg Config, logger log.Logger) (*Agent, error) {
+	if err := cfg.Validate(); err != nil {
+		return nil, err
+	}
+
 	a := &Agent{
 		cfg:    cfg,
 		logger: log.With(logger, "agent", "prometheus"),
 	}
 
 	for _, c := range cfg.Configs {
-		a.instances = append(a.instances, newInstance(cfg.Global, c, a.logger))
+		inst, err := newInstance(cfg.Global, c, cfg.WALDir, a.logger)
+		if err != nil {
+			return nil, err
+		}
+		a.instances = append(a.instances, inst)
 	}
 
-	return a
+	go a.run()
+	return a, nil
 }
 
-// Stop stops the agent.
+func (a *Agent) run() {
+	// This function watches all instances for abnormal shutdowns and restarts them
+	// whenever that's detected. This function only exits when all instances
+	// shutdown normally, which can only happen when Stop is called on the agent.
+	a.forAllInstances(func(i int, _ *instance) {
+		for {
+			inst := a.instances[i]
+			<-inst.exited
+
+			if err := inst.Err(); err != nil && err != instanceStoppedNormallyErr {
+				// TODO(rfratto): metric for abnormal instance exits
+				level.Error(a.logger).Log("msg", "instance stopped abnormally. restarting in 5 sec...", "err", err)
+				time.Sleep(time.Second * 5)
+			} else {
+				level.Info(a.logger).Log("msg", "agent stopped normally")
+				return
+			}
+
+			// Try to recreate the instance.
+			inst, err := newInstance(a.cfg.Global, a.cfg.Configs[i], a.cfg.WALDir, a.logger)
+			if err != nil {
+				level.Error(a.logger).Log("msg", "failed to recreate instance", "err", err)
+				// TODO(rfratto): should this be a panic? if we let the function return here
+				// then that's an entire agent instance that's lost and won't recover until
+				// the entire process is restarted.
+				return
+			}
+
+			a.instanceMtx.Lock()
+			a.instances[i] = inst
+			a.instanceMtx.Unlock()
+		}
+	})
+}
+
+// Stop stops the agent and all its instances.
 func (a *Agent) Stop() {
-	for _, i := range a.instances {
-		i.Stop()
+	a.forAllInstances(func(idx int, inst *instance) {
+		inst.Stop()
+	})
+}
+
+// forAllInstances runs f in parallel for all provided instances. Only returns when
+// all f exit.
+func (a *Agent) forAllInstances(f func(idx int, inst *instance)) {
+	var wg sync.WaitGroup
+	wg.Add(len(a.instances))
+
+	a.instanceMtx.Lock()
+	for idx, inst := range a.instances {
+		go func(idx int, inst *instance) {
+			f(idx, inst)
+			wg.Done()
+		}(idx, inst)
 	}
+	a.instanceMtx.Unlock()
+
+	wg.Wait()
 }
