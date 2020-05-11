b0525e48699e1212b4c2805685bb7ac1af137155
Robert Fratto <robert.fratto@grafana.com>
2020-05-11 15:38:22 -0400

    Duplicate RemoteWriteConfig without the Secret type (#67)
    * Duplicate RemoteWriteConfig without the Secret type
    
    This commit completely removes the Secret type to allow
    RemoteWriteConfigs to be arbitrarily marshalled back and forth between
    YAML. Sanitization now happens on demand rather than being forced by the
    marshaling.
    
    Closes #62
    
    * fix lint error

--- pkg/prometheus/ha/codec.go
+++ pkg/prometheus/ha/codec.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/cortexproject/cortex/pkg/ring/kv/codec"
 	"github.com/grafana/agent/pkg/prometheus/instance"
-	"github.com/prometheus/common/config"
 	"gopkg.in/yaml.v2"
 )
 
@@ -31,33 +30,26 @@ func (*yamlCodec) Decode(bb []byte) (interface{}, error) {
 		return nil, err
 	}
 
-	var codecConfig codecConfig
-	if err := yaml.NewDecoder(r).Decode(&codecConfig); err != nil {
+	var inst instance.Config
+	if err := yaml.NewDecoder(r).Decode(&inst); err != nil {
 		return nil, err
 	}
-	restoreConfig(&codecConfig)
-	return codecConfig.Config, nil
+	return &inst, nil
 }
 
 func (*yamlCodec) Encode(v interface{}) ([]byte, error) {
 	var buf bytes.Buffer
 
-	var configToWrite *instance.Config
-
-	switch val := v.(type) {
-	case instance.Config:
-		configToWrite = &val
-	case *instance.Config:
-		configToWrite = val
+	switch v.(type) {
+	case instance.Config, *instance.Config:
+		break
 	default:
 		panic(fmt.Sprintf("unexpected type %T passed to yamlCodec.Encode", v))
 	}
 
-	codecConfig := newCodecConfig(configToWrite)
-
 	w := gzip.NewWriter(&buf)
 	yamlEncoder := yaml.NewEncoder(w)
-	if err := yamlEncoder.Encode(codecConfig); err != nil {
+	if err := yamlEncoder.Encode(v); err != nil {
 		return nil, err
 	}
 
@@ -67,46 +59,4 @@ func (*yamlCodec) Encode(v interface{}) ([]byte, error) {
 
 func (*yamlCodec) CodecID() string {
 	return "agentConfig/yaml"
-}
-
-type codecConfig struct {
-	Config  *instance.Config `yaml:"config"`
-	Secrets []configSecrets  `yaml:"secrets"`
-}
-
-// newCodecConfig creates a new codecConfig that is ready for marshaling as YAML
-// and storing in a KV store. newCodecConfig must extract secrets stored in the
-// instance.Config and store them elsewhere as strings as the MarshalYAML function
-// on the Prometheus Secret type replaces the contents of the secret with
-// the text "<secret>".
-func newCodecConfig(c *instance.Config) *codecConfig {
-	var secrets []configSecrets
-	for _, rwr := range c.RemoteWrite {
-		var s configSecrets
-		if rwr.HTTPClientConfig.BasicAuth != nil {
-			s.BasicAuthPassword = string(rwr.HTTPClientConfig.BasicAuth.Password)
-		}
-		s.BearerToken = string(rwr.HTTPClientConfig.BearerToken)
-		secrets = append(secrets, s)
-	}
-	return &codecConfig{
-		Config:  c,
-		Secrets: secrets,
-	}
-}
-
-type configSecrets struct {
-	BasicAuthPassword string `yaml:"basic_auth_password"`
-	BearerToken       string `yaml:"bearer_token"`
-}
-
-// restoreConfig restores the instance config stored in a codec config by copying
-// extracted secrets to it.
-func restoreConfig(c *codecConfig) {
-	for i, rwr := range c.Config.RemoteWrite {
-		if rwr.HTTPClientConfig.BasicAuth != nil {
-			rwr.HTTPClientConfig.BasicAuth.Password = config.Secret(c.Secrets[i].BasicAuthPassword)
-		}
-		rwr.HTTPClientConfig.BearerToken = config.Secret(c.Secrets[i].BearerToken)
-	}
 }
