8bcb3c0199ac84c7fb12f5a855491ae8337a0a71
Robert Fratto <robert.fratto@grafana.com>
2020-09-01 09:55:56 -0400

    Make subsystems optional (#171)
    * make subsystems optional
    
    - Prometheus subsystem will only be loaded if prometheus: key is present
    - Loki subsystem will only be loaded if loki: key is present
    - Integrations subsystem will only be loaded if integrations: key is present
    
    If Integrations is enabled, Prometheus is forced to be enabled as well
    since Integrations depend on Prometheus.
    
    * log when prometheus subsystem is forced to be enabled
    
    * changelog
    
    * reword changelog
    
    * note how this won't work for loki integrations

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -25,20 +25,35 @@ type Config struct {
 
 // ApplyDefaults sets default values in the config
 func (c *Config) ApplyDefaults() error {
-	if err := c.Prometheus.ApplyDefaults(); err != nil {
-		return err
+	// The integration subsystem depends on Prometheus; so if it's enabled, force Prometheus
+	// to be enabled.
+	//
+	// TODO(rfratto): when Loki integrations are added, this line will no longer work; each
+	// integration will then have to be associated with a subsystem.
+	if c.Integrations.Enabled && !c.Prometheus.Enabled {
+		fmt.Println("NOTE: enabling Prometheus subsystem as Integrations are enabled")
+		c.Prometheus.Enabled = true
 	}
 
-	// The default port exposed to the lifecycler should be the gRPC listen
-	// port since the agents will use gRPC for notifying other agents of
-	// resharding.
-	c.Prometheus.ServiceConfig.Lifecycler.ListenPort = c.Server.GRPCListenPort
-	c.Integrations.ListenPort = &c.Server.HTTPListenPort
+	if c.Prometheus.Enabled {
+		if err := c.Prometheus.ApplyDefaults(); err != nil {
+			return err
+		}
 
-	// Apply the defaults for integrations *after* manual defaults are applied
-	// (like the c.Integrations.ListenPort) above.
-	if err := c.Integrations.ApplyDefaults(); err != nil {
-		return err
+		// The default port exposed to the lifecycler should be the gRPC listen
+		// port since the agents will use gRPC for notifying other agents of
+		// resharding.
+		c.Prometheus.ServiceConfig.Lifecycler.ListenPort = c.Server.GRPCListenPort
+	}
+
+	if c.Integrations.Enabled {
+		c.Integrations.ListenPort = &c.Server.HTTPListenPort
+
+		// Apply the defaults for integrations *after* manual defaults are applied
+		// (like the c.Integrations.ListenPort) above.
+		if err := c.Integrations.ApplyDefaults(); err != nil {
+			return err
+		}
 	}
 
 	return nil
