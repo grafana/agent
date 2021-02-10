b29e21bf8537a9eefc389c67bbb4f589453009ca
Robert Fratto <robertfratto@gmail.com>
2021-02-10 12:19:42 -0500

    bind mount /etc/machine-id

--- example/k3d/scripts/create.bash
+++ example/k3d/scripts/create.bash
@@ -4,4 +4,5 @@ k3d create \
   --name agent-k3d \
   --publish 30080:30080 \
   --api-port 50443 \
-  -v /var/lib/k3d/agent-k3d/storage/:/var/lib/rancher/k3s/storage/
+  -v /var/lib/k3d/agent-k3d/storage/:/var/lib/rancher/k3s/storage/ \
+  -v /etc/machine-id:/etc/machine-id
