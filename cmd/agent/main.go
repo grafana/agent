92464dc719e1eb6746e2feb2ec25b2a23a9de348
matt durham <mattdurham@ppog.org>
2021-03-04 09:47:48 -0500

    Fix error return issues

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -43,7 +43,7 @@ func main() {
 	// After this point we can use util_log.Logger and stop using the log package
 	util_log.InitLogger(&cfg.Server)
 	logger := util_log.Logger
-	srv, _ := NewAgent(logger, cfg)
+	srv := NewAgent(logger, cfg)
 	if err := srv.srv.Run(); err != nil {
 		level.Error(logger).Log("msg", "error running agent", "err", err)
 		// Don't os.Exit here; we want to do cleanup by stopping promMetrics
