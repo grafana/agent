51e17d50eab5690f8db6aa93045e64a2e07cdf45
matt durham <mattdurham@ppog.org>
2021-03-24 13:56:40 -0400

    Change to explicit setting of map

--- pkg/integrations/windows_exporter/config.go
+++ pkg/integrations/windows_exporter/config.go
@@ -53,80 +53,128 @@ func (c *Config) NewIntegration(l log.Logger) (integrations.Integration, error) 
 // using the windows_exporter key name 'collector.iis.site-whitelist' for example.
 func (c *Config) ConvertToMap() map[string]string {
 	configMap := make(map[string]string)
-	mapToConfig(c, configMap)
+	translateConfig(c.Exchange, configMap)
+	translateConfig(c.IIS, configMap)
+	translateConfig(c.LogicalDisk, configMap)
+	translateConfig(c.MSMQ, configMap)
+	translateConfig(c.MSSQL, configMap)
+	translateConfig(c.Network, configMap)
+	translateConfig(c.Process, configMap)
+	translateConfig(c.Service, configMap)
+	translateConfig(c.SMTP, configMap)
+	translateConfig(c.TextFile, configMap)
 	return configMap
 }
 
+type translatableConfig interface {
+	translate(cm map[string]string)
+}
+
+func translateConfig(c translatableConfig, cm map[string]string) {
+	if c == nil || reflect.ValueOf(c).IsNil() {
+		return
+	}
+	c.translate(cm)
+}
+
 type ExchangeConfig struct {
-	EnabledList *string `yaml:"enabled_list" exporter:"collectors.exchange.enabled"`
+	EnabledList *string `yaml:"enabled_list"`
+}
+
+func (c *ExchangeConfig) translate(cm map[string]string) {
+	setIfNotNil(cm, "collectors.exchange.enabled", c.EnabledList)
 }
 
 type IISConfig struct {
-	SiteWhiteList *string `yaml:"site_whitelist" exporter:"collector.iis.site-whitelist"`
-	SiteBlackList *string `yaml:"site_blacklist" exporter:"collector.iis.site-blacklist"`
-	AppWhiteList  *string `yaml:"app_whitelist" exporter:"collector.iis.app-whitelist"`
-	AppBlackList  *string `yaml:"app_blacklist" exporter:"collector.iis.app-blacklist"`
+	SiteWhiteList *string `yaml:"site_whitelist"`
+	SiteBlackList *string `yaml:"site_blacklist"`
+	AppWhiteList  *string `yaml:"app_whitelist"`
+	AppBlackList  *string `yaml:"app_blacklist"`
+}
+
+func (c *IISConfig) translate(cm map[string]string) {
+	setIfNotNil(cm, "collector.iis.site-whitelist", c.SiteWhiteList)
+	setIfNotNil(cm, "collector.iis.site-blacklist", c.SiteBlackList)
+	setIfNotNil(cm, "collector.iis.app-whitelist", c.AppWhiteList)
+	setIfNotNil(cm, "collector.iis.app-blacklist", c.AppBlackList)
 }
 
 type TextFileConfig struct {
-	TextFileDirectory *string `yaml:"text_file_directory" exporter:"collector.textfile.directory"`
+	TextFileDirectory *string `yaml:"text_file_directory"`
+}
+
+func (c *TextFileConfig) translate(cm map[string]string) {
+	setIfNotNil(cm, "collector.textfile.directory", c.TextFileDirectory)
 }
 
 type SMTPConfig struct {
-	WhiteList *string `yaml:"whitelist" exporter:"collector.smtp.server-whitelist"`
-	BlackList *string `yaml:"blacklist" exporter:"collector.smtp.server-blacklist"`
+	WhiteList *string `yaml:"whitelist"`
+	BlackList *string `yaml:"blacklist"`
+}
+
+func (c *SMTPConfig) translate(cm map[string]string) {
+	setIfNotNil(cm, "collector.smtp.server-whitelist", c.WhiteList)
+	setIfNotNil(cm, "collector.smtp.server-blacklist", c.BlackList)
 }
 
 type ServiceConfig struct {
-	Where *string `yaml:"where_clause" exporter:"collector.service.services-where"`
+	Where *string `yaml:"where_clause"`
+}
+
+func (c *ServiceConfig) translate(cm map[string]string) {
+	setIfNotNil(cm, "collector.service.services-where", c.Where)
 }
 
 type ProcessConfig struct {
-	WhiteList *string `yaml:"whitelist" exporter:"collector.process.whitelist"`
-	BlackList *string `yaml:"blacklist" exporter:"collector.process.blacklist"`
+	WhiteList *string `yaml:"whitelist"`
+	BlackList *string `yaml:"blacklist"`
+}
+
+func (c *ProcessConfig) translate(cm map[string]string) {
+	setIfNotNil(cm, "collector.process.whitelist", c.WhiteList)
+	setIfNotNil(cm, "collector.process.blacklist", c.BlackList)
 }
 
 type NetworkConfig struct {
-	WhiteList *string `yaml:"whitelist" exporter:"collector.net.nic-whitelist"`
-	BlackList *string `yaml:"blacklist" exporter:"collector.net.nic-blacklist"`
+	WhiteList *string `yaml:"whitelist"`
+	BlackList *string `yaml:"blacklist"`
+}
+
+func (c *NetworkConfig) translate(cm map[string]string) {
+	setIfNotNil(cm, "collector.net.nic-whitelist", c.WhiteList)
+	setIfNotNil(cm, "collector.net.nic-blacklist", c.BlackList)
 }
 
 type MSSQLConfig struct {
-	EnabledClasses *string `yaml:"enabled_classes" exporter:"collectors.mssql.classes-enabled"`
+	EnabledClasses *string `yaml:"enabled_classes"`
+}
+
+func (c *MSSQLConfig) translate(cm map[string]string) {
+	setIfNotNil(cm, "collectors.mssql.classes-enabled", c.EnabledClasses)
 }
 
 type MSMQConfig struct {
-	Where *string `yaml:"where_clause" exporter:"collector.msmq.msmq-where"`
+	Where *string `yaml:"where_clause"`
+}
+
+func (c *MSMQConfig) translate(cm map[string]string) {
+	setIfNotNil(cm, "collector.msmq.msmq-where", c.Where)
 }
 
 type LogicalDiskConfig struct {
-	WhiteList *string `yaml:"whitelist" exporter:"collector.logical_disk.volume-whitelist"`
-	BlackList *string `yaml:"blacklist" exporter:"collector.logical_disk.volume-blacklist"`
+	WhiteList *string `yaml:"whitelist"`
+	BlackList *string `yaml:"blacklist"`
 }
 
-// This is to create a map from the config using the exporter struct tag as the key. This is to be ONLY used on pointer configs
-// and pointer strings. This is because we need to know if a value was set so that we don't overwrite defaults.
-func mapToConfig(config interface{}, cm map[string]string) {
-	if config == nil || (reflect.ValueOf(config).Kind() == reflect.Ptr && reflect.ValueOf(config).IsNil()) {
+func (c *LogicalDiskConfig) translate(cm map[string]string) {
+	setIfNotNil(cm, "collector.logical_disk.volume-whitelist", c.WhiteList)
+	setIfNotNil(cm, "collector.logical_disk.volume-blacklist", c.BlackList)
+
+}
+
+func setIfNotNil(cm map[string]string, key string, value *string) {
+	if value == nil {
 		return
 	}
-	t := reflect.TypeOf(config)
-	v := reflect.ValueOf(config)
-	if t.Kind() != reflect.Ptr {
-		return
-	}
-	fieldCount := t.Elem().NumField()
-	for i := 0; i < fieldCount; i++ {
-		valueField := v.Elem().Field(i)
-		typeField := t.Elem().Field(i)
-		en := typeField.Tag.Get("exporter")
-		// Currently agent only support translating string pointer vars
-		if en != "" && valueField.Kind() == reflect.Ptr && valueField.Elem().Kind() == reflect.String {
-			cm[en] = valueField.Elem().String()
-			continue
-		}
-		if valueField.Kind() == reflect.Ptr {
-			mapToConfig(valueField.Interface(), cm)
-		}
-	}
+	cm[key] = *value
 }
