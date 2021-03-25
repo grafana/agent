41a7e77dc810b4dd7ef39989ab04468fa88d4778
Robert Fratto <robertfratto@gmail.com>
2021-03-25 17:14:03 -0400

    document the RW mutex in util.Logger better

--- pkg/util/logger.go
+++ pkg/util/logger.go
@@ -13,10 +13,11 @@ import (
 // Logger implements Go Kit's log.Logger interface. It supports being
 // dynamically updated at runtime.
 type Logger struct {
+	// mut protects against race conditions accessing l, which can be modified
+	// and accessed concurrently if ApplyConfig and Log are called at the same
+	// time.
 	mut sync.RWMutex
-
-	// l is the constructed log.Logger.
-	l log.Logger
+	l   log.Logger
 
 	// makeLogger will default to defaultLogger. It's a struct
 	// member to make testing work properly.
