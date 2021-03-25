d468deb87d96569a3cea623a35b9caf8eab442e4
matt durham <mattdurham@ppog.org>
2021-03-25 15:10:04 -0400

    Cross platform support

--- pkg/integrations/windows_exporter/config.go
+++ pkg/integrations/windows_exporter/config.go
@@ -3,7 +3,6 @@ import (
 	"github.com/go-kit/kit/log"
 	"github.com/grafana/agent/pkg/integrations"
 	"github.com/grafana/agent/pkg/integrations/config"
-	"github.com/prometheus-community/windows_exporter/collector"
 )
 
 // Config controls the windows_exporter integration.
@@ -49,14 +48,6 @@ type ExchangeConfig struct {
 	EnabledList string `yaml:"enabled_list"`
 }
 
-func (c *ExchangeConfig) sync(v interface{}) bool {
-	other, ok := v.(*collector.ExchangeConfig)
-	if ok {
-		setStringIfNotEmpty(c.EnabledList, &other.Enabled)
-	}
-	return ok
-}
-
 // IISConfig handles settings for the windows_exporter IIS collector
 type IISConfig struct {
 	SiteWhiteList string `yaml:"site_whitelist"`
@@ -65,28 +56,9 @@ type IISConfig struct {
 	AppBlackList  string `yaml:"app_blacklist"`
 }
 
-func (c *IISConfig) sync(v interface{}) bool {
-	other, ok := v.(*collector.IISConfig)
-	if ok {
-		setStringIfNotEmpty(c.SiteWhiteList, &other.SiteWhiteList)
-		setStringIfNotEmpty(c.SiteBlackList, &other.SiteBlackList)
-		setStringIfNotEmpty(c.AppWhiteList, &other.AppWhiteList)
-		setStringIfNotEmpty(c.AppBlackList, &other.AppBlackList)
-	}
-	return ok
-}
-
 // TextFileConfig handles settings for the windows_exporter Text File collector
 type TextFileConfig struct {
 	TextFileDirectory string `yaml:"text_file_directory"`
-}
-
-func (c *TextFileConfig) sync(v interface{}) bool {
-	other, ok := v.(*collector.TextFileConfig)
-	if ok {
-		setStringIfNotEmpty(c.TextFileDirectory, &other.TextFileDirectory)
-	}
-	return ok
 }
 
 // SMTPConfig handles settings for the windows_exporter SMTP collector
@@ -95,26 +67,9 @@ type SMTPConfig struct {
 	BlackList string `yaml:"blacklist"`
 }
 
-func (c *SMTPConfig) sync(v interface{}) bool {
-	other, ok := v.(*collector.SMTPConfig)
-	if ok {
-		setStringIfNotEmpty(c.WhiteList, &other.ServerWhiteList)
-		setStringIfNotEmpty(c.BlackList, &other.ServerBlackList)
-	}
-	return ok
-}
-
 // ServiceConfig handles settings for the windows_exporter service collector
 type ServiceConfig struct {
 	Where string `yaml:"where_clause"`
-}
-
-func (c *ServiceConfig) sync(v interface{}) bool {
-	other, ok := v.(*collector.ServiceConfig)
-	if ok {
-		setStringIfNotEmpty(c.Where, &other.ServiceWhereClause)
-	}
-	return ok
 }
 
 // ProcessConfig handles settings for the windows_exporter process collector
@@ -123,28 +78,10 @@ type ProcessConfig struct {
 	BlackList string `yaml:"blacklist"`
 }
 
-func (c *ProcessConfig) sync(v interface{}) bool {
-	other, ok := v.(*collector.ProcessConfig)
-	if ok {
-		setStringIfNotEmpty(c.WhiteList, &other.ProcessWhiteList)
-		setStringIfNotEmpty(c.BlackList, &other.ProcessBlackList)
-	}
-	return ok
-}
-
 // NetworkConfig handles settings for the windows_exporter network collector
 type NetworkConfig struct {
 	WhiteList string `yaml:"whitelist"`
 	BlackList string `yaml:"blacklist"`
-}
-
-func (c *NetworkConfig) sync(v interface{}) bool {
-	other, ok := v.(*collector.NetworkConfig)
-	if ok {
-		setStringIfNotEmpty(c.WhiteList, &other.NICWhiteList)
-		setStringIfNotEmpty(c.BlackList, &other.NICBlackList)
-	}
-	return ok
 }
 
 // MSSQLConfig handles settings for the windows_exporter SQL server collector
@@ -152,50 +89,13 @@ type MSSQLConfig struct {
 	EnabledClasses string `yaml:"enabled_classes"`
 }
 
-func (c *MSSQLConfig) sync(v interface{}) bool {
-	other, ok := v.(*collector.MSSQLConfig)
-	if ok {
-		setStringIfNotEmpty(c.EnabledClasses, &other.MSSQLEnabledCollectors)
-	}
-	return ok
-}
-
 // MSMQConfig handles settings for the windows_exporter MSMQ collector
 type MSMQConfig struct {
 	Where string `yaml:"where_clause"`
-}
-
-func (c *MSMQConfig) sync(v interface{}) bool {
-	other, ok := v.(*collector.MSMQConfig)
-	if ok {
-		setStringIfNotEmpty(c.Where, &other.MSMQWhereClause)
-	}
-	return ok
 }
 
 // LogicalDiskConfig handles settings for the windows_exporter logical disk collector
 type LogicalDiskConfig struct {
 	WhiteList string `yaml:"whitelist"`
 	BlackList string `yaml:"blacklist"`
-}
-
-func (c *LogicalDiskConfig) sync(v interface{}) bool {
-	other, ok := v.(*collector.LogicalDiskConfig)
-	if ok {
-		setStringIfNotEmpty(c.WhiteList, &other.VolumeWhiteList)
-		setStringIfNotEmpty(c.BlackList, &other.VolumeBlackList)
-	}
-	return ok
-}
-
-type translatableConfig interface {
-	sync(v interface{}) bool
-}
-
-// This only works because "" is not a reasonable valid choice for any configurable option currently in windows_exporter
-func setStringIfNotEmpty(source string, destination *string) {
-	if source == "" {
-		return
-	}
-	*destination = source
 }
--- pkg/integrations/windows_exporter/config_windows.go
+++ pkg/integrations/windows_exporter/config_windows.go
@@ -33,3 +33,104 @@ func (c *Config) applyConfig(exporterConfigs map[string]collector.Config) {
 		}
 	}
 }
+
+// The sync functions are specifically not with their types since they contain windows specific code
+
+func (c *IISConfig) sync(v interface{}) bool {
+	other, ok := v.(*collector.IISConfig)
+	if ok {
+		setStringIfNotEmpty(c.SiteWhiteList, &other.SiteWhiteList)
+		setStringIfNotEmpty(c.SiteBlackList, &other.SiteBlackList)
+		setStringIfNotEmpty(c.AppWhiteList, &other.AppWhiteList)
+		setStringIfNotEmpty(c.AppBlackList, &other.AppBlackList)
+	}
+	return ok
+}
+
+func (c *ExchangeConfig) sync(v interface{}) bool {
+	other, ok := v.(*collector.ExchangeConfig)
+	if ok {
+		setStringIfNotEmpty(c.EnabledList, &other.Enabled)
+	}
+	return ok
+}
+
+func (c *TextFileConfig) sync(v interface{}) bool {
+	other, ok := v.(*collector.TextFileConfig)
+	if ok {
+		setStringIfNotEmpty(c.TextFileDirectory, &other.TextFileDirectory)
+	}
+	return ok
+}
+
+func (c *SMTPConfig) sync(v interface{}) bool {
+	other, ok := v.(*collector.SMTPConfig)
+	if ok {
+		setStringIfNotEmpty(c.WhiteList, &other.ServerWhiteList)
+		setStringIfNotEmpty(c.BlackList, &other.ServerBlackList)
+	}
+	return ok
+}
+
+func (c *ServiceConfig) sync(v interface{}) bool {
+	other, ok := v.(*collector.ServiceConfig)
+	if ok {
+		setStringIfNotEmpty(c.Where, &other.ServiceWhereClause)
+	}
+	return ok
+}
+
+func (c *ProcessConfig) sync(v interface{}) bool {
+	other, ok := v.(*collector.ProcessConfig)
+	if ok {
+		setStringIfNotEmpty(c.WhiteList, &other.ProcessWhiteList)
+		setStringIfNotEmpty(c.BlackList, &other.ProcessBlackList)
+	}
+	return ok
+}
+
+func (c *NetworkConfig) sync(v interface{}) bool {
+	other, ok := v.(*collector.NetworkConfig)
+	if ok {
+		setStringIfNotEmpty(c.WhiteList, &other.NICWhiteList)
+		setStringIfNotEmpty(c.BlackList, &other.NICBlackList)
+	}
+	return ok
+}
+
+func (c *MSSQLConfig) sync(v interface{}) bool {
+	other, ok := v.(*collector.MSSQLConfig)
+	if ok {
+		setStringIfNotEmpty(c.EnabledClasses, &other.MSSQLEnabledCollectors)
+	}
+	return ok
+}
+
+func (c *MSMQConfig) sync(v interface{}) bool {
+	other, ok := v.(*collector.MSMQConfig)
+	if ok {
+		setStringIfNotEmpty(c.Where, &other.MSMQWhereClause)
+	}
+	return ok
+}
+
+func (c *LogicalDiskConfig) sync(v interface{}) bool {
+	other, ok := v.(*collector.LogicalDiskConfig)
+	if ok {
+		setStringIfNotEmpty(c.WhiteList, &other.VolumeWhiteList)
+		setStringIfNotEmpty(c.BlackList, &other.VolumeBlackList)
+	}
+	return ok
+}
+
+type translatableConfig interface {
+	sync(v interface{}) bool
+}
+
+// This only works because "" is not a reasonable valid choice for any configurable option currently in windows_exporter
+func setStringIfNotEmpty(source string, destination *string) {
+	if source == "" {
+		return
+	}
+	*destination = source
+}
