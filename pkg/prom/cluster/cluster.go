a2cb6a6aa84a8f7447f0305a6418fb684361e5fd
Robert Fratto <robertfratto@gmail.com>
2021-04-23 11:45:40 -0400

    tests

--- pkg/prom/cluster/cluster.go
+++ pkg/prom/cluster/cluster.go
@@ -14,22 +14,6 @@ import (
 	"github.com/grafana/agent/pkg/prom/instance/configstore"
 	"github.com/grafana/agent/pkg/util"
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/common/config"
-	"github.com/prometheus/prometheus/discovery"
-	"github.com/prometheus/prometheus/discovery/azure"
-	"github.com/prometheus/prometheus/discovery/consul"
-	"github.com/prometheus/prometheus/discovery/digitalocean"
-	"github.com/prometheus/prometheus/discovery/dns"
-	"github.com/prometheus/prometheus/discovery/dockerswarm"
-	"github.com/prometheus/prometheus/discovery/ec2"
-	"github.com/prometheus/prometheus/discovery/eureka"
-	"github.com/prometheus/prometheus/discovery/hetzner"
-	"github.com/prometheus/prometheus/discovery/kubernetes"
-	"github.com/prometheus/prometheus/discovery/marathon"
-	"github.com/prometheus/prometheus/discovery/openstack"
-	"github.com/prometheus/prometheus/discovery/scaleway"
-	"github.com/prometheus/prometheus/discovery/triton"
-	"github.com/prometheus/prometheus/discovery/zookeeper"
 	"google.golang.org/grpc"
 )
 
@@ -117,107 +101,7 @@ func (c *Cluster) storeValidate(cfg *instance.Config) error {
 
 	// If configs aren't allowed to read from the store, we need to make sure no
 	// configs coming in from the API set files for passwords.
-	for i, rw := range cfg.RemoteWrite {
-		if err := validateNoFiles(&rw.HTTPClientConfig); err != nil {
-			return fmt.Errorf("failed to validate remote_write at index %d: %w", i, err)
-		}
-	}
-
-	for i, sc := range cfg.ScrapeConfigs {
-		if err := validateNoFiles(&sc.HTTPClientConfig); err != nil {
-			return fmt.Errorf("failed to validate scrape_config at index %d: %w", i, err)
-		}
-
-		for j, disc := range sc.ServiceDiscoveryConfigs {
-			if err := validateDiscoveryNoFiles(disc); err != nil {
-				return fmt.Errorf("failed to validate service discovery at index %d withini scrape_config at index %d: %w", j, i, err)
-			}
-		}
-	}
-
-	return nil
-}
-
-func validateNoFiles(cfg *config.HTTPClientConfig) error {
-	checks := []struct {
-		name  string
-		check func() bool
-	}{
-		{"bearer_token_file", func() bool { return cfg.BearerTokenFile != "" }},
-		{"password_file", func() bool { return cfg.BasicAuth != nil && cfg.BasicAuth.PasswordFile != "" }},
-		{"credentials_file", func() bool { return cfg.Authorization != nil && cfg.Authorization.CredentialsFile != "" }},
-		{"ca_file", func() bool { return cfg.TLSConfig.CAFile != "" }},
-		{"cert_file", func() bool { return cfg.TLSConfig.CertFile != "" }},
-		{"key_file", func() bool { return cfg.TLSConfig.KeyFile != "" }},
-	}
-	for _, check := range checks {
-		if check.check() {
-			return fmt.Errorf("%s must be empty unless dangerous_allow_reading_files is set", check.name)
-		}
-	}
-	return nil
-}
-
-func validateDiscoveryNoFiles(disc discovery.Config) error {
-	switch d := disc.(type) {
-	case *azure.SDConfig:
-		// no-op
-	case *consul.SDConfig:
-		if err := validateNoFiles(&config.HTTPClientConfig{TLSConfig: d.TLSConfig}); err != nil {
-			return err
-		}
-	case *digitalocean.SDConfig:
-		if err := validateNoFiles(&d.HTTPClientConfig); err != nil {
-			return err
-		}
-	case *dns.SDConfig:
-		// no-op
-	case *dockerswarm.SDConfig:
-		if err := validateNoFiles(&d.HTTPClientConfig); err != nil {
-			return err
-		}
-	case *ec2.SDConfig:
-		// no-op
-	case *eureka.SDConfig:
-		if err := validateNoFiles(&d.HTTPClientConfig); err != nil {
-			return err
-		}
-	case *hetzner.SDConfig:
-		if err := validateNoFiles(&d.HTTPClientConfig); err != nil {
-			return err
-		}
-	case *kubernetes.SDConfig:
-		if err := validateNoFiles(&d.HTTPClientConfig); err != nil {
-			return err
-		}
-	case *marathon.SDConfig:
-		if err := validateNoFiles(&d.HTTPClientConfig); err != nil {
-			return err
-		}
-		if d.AuthTokenFile != "" {
-			return fmt.Errorf("auth_token_file must be empty unless dangerous_allow_reading_files is set")
-		}
-	case *openstack.SDConfig:
-		if err := validateNoFiles(&config.HTTPClientConfig{TLSConfig: d.TLSConfig}); err != nil {
-			return err
-		}
-	case *scaleway.SDConfig:
-		if err := validateNoFiles(&d.HTTPClientConfig); err != nil {
-			return err
-		}
-	case *triton.SDConfig:
-		if err := validateNoFiles(&config.HTTPClientConfig{TLSConfig: d.TLSConfig}); err != nil {
-			return err
-		}
-	case *zookeeper.NerveSDConfig:
-		// no-op
-	case *zookeeper.ServersetSDConfig:
-		// no-op
-	default:
-		return fmt.Errorf("unknown service discovery %s; rejecting config for safety. set dangerous_allow_reading_files to ignore", d.Name())
-	}
-
-	return nil
+	return validateNofiles(cfg)
 }
 
 // Reshard implements agentproto.ScrapingServiceServer, and syncs the state of
