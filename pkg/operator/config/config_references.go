444f711618501501d241896e47b032b2666de2ce
Robert Fratto <robertfratto@gmail.com>
2021-05-28 13:49:17 -0400

    DRY up config_references.go

--- pkg/operator/config/config_references.go
+++ pkg/operator/config/config_references.go
@@ -39,16 +39,8 @@ func (d *Deployment) AssetReferences() []AssetReference {
 		// Retrieve from ServiceMonitors
 		for _, monitor := range inst.ServiceMonitors {
 			for _, endpoint := range monitor.Spec.Endpoints {
-				if endpoint.BasicAuth != nil {
-					res = append(res, AssetReference{
-						Namespace: monitor.Namespace,
-						Reference: prom.SecretOrConfigMap{Secret: &endpoint.BasicAuth.Username},
-					})
-					res = append(res, AssetReference{
-						Namespace: monitor.Namespace,
-						Reference: prom.SecretOrConfigMap{Secret: &endpoint.BasicAuth.Password},
-					})
-				}
+				res = append(res, basicAuthAssetReferences(monitor.Namespace, endpoint.BasicAuth)...)
+
 				if endpoint.TLSConfig != nil {
 					res = append(res, tlsConfigReferences(monitor.Namespace, endpoint.TLSConfig)...)
 				}
@@ -62,16 +54,8 @@ func (d *Deployment) AssetReferences() []AssetReference {
 		// Retrieve from PodMonitors
 		for _, monitor := range inst.PodMonitors {
 			for _, endpoint := range monitor.Spec.PodMetricsEndpoints {
-				if endpoint.BasicAuth != nil {
-					res = append(res, AssetReference{
-						Namespace: monitor.Namespace,
-						Reference: prom.SecretOrConfigMap{Secret: &endpoint.BasicAuth.Username},
-					})
-					res = append(res, AssetReference{
-						Namespace: monitor.Namespace,
-						Reference: prom.SecretOrConfigMap{Secret: &endpoint.BasicAuth.Password},
-					})
-				}
+				res = append(res, basicAuthAssetReferences(monitor.Namespace, endpoint.BasicAuth)...)
+
 				if endpoint.TLSConfig != nil {
 					res = append(res, tlsConfigReferences(monitor.Namespace, &prom.TLSConfig{
 						SafeTLSConfig: endpoint.TLSConfig.SafeTLSConfig,
@@ -86,16 +70,8 @@ func (d *Deployment) AssetReferences() []AssetReference {
 
 		// Retrieve from Probes
 		for _, probe := range inst.Probes {
-			if probe.Spec.BasicAuth != nil {
-				res = append(res, AssetReference{
-					Namespace: probe.Namespace,
-					Reference: prom.SecretOrConfigMap{Secret: &probe.Spec.BasicAuth.Username},
-				})
-				res = append(res, AssetReference{
-					Namespace: probe.Namespace,
-					Reference: prom.SecretOrConfigMap{Secret: &probe.Spec.BasicAuth.Password},
-				})
-			}
+			res = append(res, basicAuthAssetReferences(probe.Namespace, probe.Spec.BasicAuth)...)
+
 			if probe.Spec.TLSConfig != nil {
 				res = append(res, tlsConfigReferences(probe.Namespace, &prom.TLSConfig{
 					SafeTLSConfig: probe.Spec.TLSConfig.SafeTLSConfig,
@@ -114,16 +90,7 @@ func (d *Deployment) AssetReferences() []AssetReference {
 func apiServerAssetReferences(namespace string, apiServer *prom.APIServerConfig) []AssetReference {
 	var res []AssetReference
 
-	if apiServer.BasicAuth != nil {
-		res = append(res, AssetReference{
-			Namespace: namespace,
-			Reference: prom.SecretOrConfigMap{Secret: &apiServer.BasicAuth.Username},
-		})
-		res = append(res, AssetReference{
-			Namespace: namespace,
-			Reference: prom.SecretOrConfigMap{Secret: &apiServer.BasicAuth.Password},
-		})
-	}
+	res = append(res, basicAuthAssetReferences(namespace, apiServer.BasicAuth)...)
 
 	if apiServer.TLSConfig != nil {
 		res = append(res, tlsConfigReferences(namespace, apiServer.TLSConfig)...)
@@ -132,19 +99,24 @@ func apiServerAssetReferences(namespace string, apiServer *prom.APIServerConfig)
 	return filterEmptyReferences(res)
 }
 
+func basicAuthAssetReferences(namespace string, ba *prom.BasicAuth) []AssetReference {
+	if ba == nil {
+		return nil
+	}
+
+	return []AssetReference{{
+		Namespace: namespace,
+		Reference: prom.SecretOrConfigMap{Secret: &ba.Username},
+	}, {
+		Namespace: namespace,
+		Reference: prom.SecretOrConfigMap{Secret: &ba.Password},
+	}}
+}
+
 func remoteWriteAssetReferences(namespace string, rw *grafana.RemoteWriteSpec) []AssetReference {
 	var res []AssetReference
 
-	if rw.BasicAuth != nil {
-		res = append(res, AssetReference{
-			Namespace: namespace,
-			Reference: prom.SecretOrConfigMap{Secret: &rw.BasicAuth.Username},
-		})
-		res = append(res, AssetReference{
-			Namespace: namespace,
-			Reference: prom.SecretOrConfigMap{Secret: &rw.BasicAuth.Password},
-		})
-	}
+	res = append(res, basicAuthAssetReferences(namespace, rw.BasicAuth)...)
 
 	if rw.SigV4 != nil {
 		res = append(res, AssetReference{
@@ -179,15 +151,19 @@ func tlsConfigReferences(namespace string, cfg *prom.TLSConfig) []AssetReference
 func filterEmptyReferences(refs []AssetReference) []AssetReference {
 	res := make([]AssetReference, 0, len(refs))
 
+Outer:
 	for _, ref := range refs {
-		if ref.Reference.ConfigMap == nil && ref.Reference.Secret == nil {
-			continue
-		}
-		if ref.Reference.ConfigMap != nil && ref.Reference.ConfigMap.LocalObjectReference.Name == "" {
-			continue
-		}
-		if ref.Reference.Secret != nil && ref.Reference.Secret.LocalObjectReference.Name == "" {
-			continue
+		var (
+			configMap = ref.Reference.ConfigMap
+			secret    = ref.Reference.Secret
+		)
+		switch {
+		case configMap == nil && secret == nil:
+			continue Outer
+		case configMap != nil && configMap.LocalObjectReference.Name == "":
+			continue Outer
+		case secret != nil && secret.LocalObjectReference.Name == "":
+			continue Outer
 		}
 		res = append(res, ref)
 	}
