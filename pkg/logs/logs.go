d33d79ca93805f4dd8b022f3f3370ed640d84fb7
Robert Fratto <robertfratto@gmail.com>
2021-07-20 13:56:16 -0400

    defer logs fix for a future PR

--- pkg/logs/logs.go
+++ pkg/logs/logs.go
@@ -4,7 +4,6 @@ package logs
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 	"sync"
 	"time"
 
@@ -56,6 +55,13 @@ func (l *Logs) ApplyConfig(c *Config) error {
 
 	if c == nil {
 		c = &Config{}
+	}
+
+	if c.PositionsDirectory != "" {
+		err := os.MkdirAll(c.PositionsDirectory, 0700)
+		if err != nil {
+			level.Warn(l.l).Log("msg", "failed to create the positions directory. logs may be unable to save their position", "path", c.PositionsDirectory, "err", err)
+		}
 	}
 
 	newInstances := make(map[string]*Instance, len(c.Configs))
@@ -141,12 +147,6 @@ func NewInstance(reg prometheus.Registerer, c *InstanceConfig, l log.Logger) (*I
 func (i *Instance) ApplyConfig(c *InstanceConfig) error {
 	i.mut.Lock()
 	defer i.mut.Unlock()
-
-	positionsDir := filepath.Dir(c.PositionsConfig.PositionsFile)
-	err := os.MkdirAll(positionsDir, 0700)
-	if err != nil {
-		level.Warn(i.log).Log("msg", "failed to create the positions directory. logs may be unable to save their position", "path", positionsDir, "err", err)
-	}
 
 	// No-op if the configs haven't changed.
 	if util.CompareYAML(c, i.cfg) {
