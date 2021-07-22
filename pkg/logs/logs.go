b10bcf09df4d5f58d85c7752309bd4d431859052
Robert Fratto <robertfratto@gmail.com>
2021-07-22 09:49:58 -0400

    undo base rebase change

--- pkg/logs/logs.go
+++ pkg/logs/logs.go
@@ -4,6 +4,7 @@ package logs
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"sync"
 	"time"
 
@@ -55,13 +56,6 @@ func (l *Logs) ApplyConfig(c *Config) error {
 
 	if c == nil {
 		c = &Config{}
-	}
-
-	if c.PositionsDirectory != "" {
-		err := os.MkdirAll(c.PositionsDirectory, 0700)
-		if err != nil {
-			level.Warn(l.l).Log("msg", "failed to create the positions directory. logs may be unable to save their position", "path", c.PositionsDirectory, "err", err)
-		}
 	}
 
 	newInstances := make(map[string]*Instance, len(c.Configs))
