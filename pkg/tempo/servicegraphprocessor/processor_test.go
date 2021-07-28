1043fb81424a9beb67d96fe6a0ba6cb6822f1865
Mario Rodriguez <mariorvinas@gmail.com>
2021-07-28 10:28:04 +0200

    Fix tests

--- pkg/tempo/servicegraphprocessor/processor_test.go
+++ pkg/tempo/servicegraphprocessor/processor_test.go
@@ -119,38 +119,70 @@ func (m *mockConsumer) ConsumeTraces(context.Context, pdata.Traces) error { retu
 
 const (
 	happyCaseExpectedMetrics = `
-		# HELP tempo_service_graph_request_seconds Time for a request between two nodes
-		# TYPE tempo_service_graph_request_seconds histogram
-		tempo_service_graph_request_seconds_bucket{client="app",server="db",le="0.01"} 0
-		tempo_service_graph_request_seconds_bucket{client="app",server="db",le="0.02"} 0
-		tempo_service_graph_request_seconds_bucket{client="app",server="db",le="0.04"} 0
-		tempo_service_graph_request_seconds_bucket{client="app",server="db",le="0.08"} 0
-		tempo_service_graph_request_seconds_bucket{client="app",server="db",le="0.16"} 0
-		tempo_service_graph_request_seconds_bucket{client="app",server="db",le="0.32"} 0
-		tempo_service_graph_request_seconds_bucket{client="app",server="db",le="0.64"} 0
-		tempo_service_graph_request_seconds_bucket{client="app",server="db",le="1.28"} 1
-		tempo_service_graph_request_seconds_bucket{client="app",server="db",le="2.56"} 3
-		tempo_service_graph_request_seconds_bucket{client="app",server="db",le="5.12"} 3
-		tempo_service_graph_request_seconds_bucket{client="app",server="db",le="10.24"} 3
-		tempo_service_graph_request_seconds_bucket{client="app",server="db",le="20.48"} 3
-		tempo_service_graph_request_seconds_bucket{client="app",server="db",le="+Inf"} 3
-		tempo_service_graph_request_seconds_sum{client="app",server="db"} 5
-		tempo_service_graph_request_seconds_count{client="app",server="db"} 3
-		tempo_service_graph_request_seconds_bucket{client="lb",server="app",le="0.01"} 0
-		tempo_service_graph_request_seconds_bucket{client="lb",server="app",le="0.02"} 0
-		tempo_service_graph_request_seconds_bucket{client="lb",server="app",le="0.04"} 0
-		tempo_service_graph_request_seconds_bucket{client="lb",server="app",le="0.08"} 0
-		tempo_service_graph_request_seconds_bucket{client="lb",server="app",le="0.16"} 0
-		tempo_service_graph_request_seconds_bucket{client="lb",server="app",le="0.32"} 0
-		tempo_service_graph_request_seconds_bucket{client="lb",server="app",le="0.64"} 0
-		tempo_service_graph_request_seconds_bucket{client="lb",server="app",le="1.28"} 1
-		tempo_service_graph_request_seconds_bucket{client="lb",server="app",le="2.56"} 2
-		tempo_service_graph_request_seconds_bucket{client="lb",server="app",le="5.12"} 3
-		tempo_service_graph_request_seconds_bucket{client="lb",server="app",le="10.24"} 3
-		tempo_service_graph_request_seconds_bucket{client="lb",server="app",le="20.48"} 3
-		tempo_service_graph_request_seconds_bucket{client="lb",server="app",le="+Inf"} 3
-		tempo_service_graph_request_seconds_sum{client="lb",server="app"} 6.2
-		tempo_service_graph_request_seconds_count{client="lb",server="app"} 3
+		# HELP tempo_service_graph_request_client_seconds Time for a request between two nodes as seen from the client
+		# TYPE tempo_service_graph_request_client_seconds histogram
+		tempo_service_graph_request_client_seconds_bucket{client="app",server="db",le="0.01"} 0
+		tempo_service_graph_request_client_seconds_bucket{client="app",server="db",le="0.02"} 0
+		tempo_service_graph_request_client_seconds_bucket{client="app",server="db",le="0.04"} 0
+		tempo_service_graph_request_client_seconds_bucket{client="app",server="db",le="0.08"} 0
+		tempo_service_graph_request_client_seconds_bucket{client="app",server="db",le="0.16"} 0
+		tempo_service_graph_request_client_seconds_bucket{client="app",server="db",le="0.32"} 0
+		tempo_service_graph_request_client_seconds_bucket{client="app",server="db",le="0.64"} 0
+		tempo_service_graph_request_client_seconds_bucket{client="app",server="db",le="1.28"} 2
+		tempo_service_graph_request_client_seconds_bucket{client="app",server="db",le="2.56"} 2
+		tempo_service_graph_request_client_seconds_bucket{client="app",server="db",le="5.12"} 3
+		tempo_service_graph_request_client_seconds_bucket{client="app",server="db",le="10.24"} 3
+		tempo_service_graph_request_client_seconds_bucket{client="app",server="db",le="20.48"} 3
+		tempo_service_graph_request_client_seconds_bucket{client="app",server="db",le="+Inf"} 3
+		tempo_service_graph_request_client_seconds_sum{client="app",server="db"} 4.6
+		tempo_service_graph_request_client_seconds_count{client="app",server="db"} 3
+		tempo_service_graph_request_client_seconds_bucket{client="lb",server="app",le="0.01"} 0
+		tempo_service_graph_request_client_seconds_bucket{client="lb",server="app",le="0.02"} 0
+		tempo_service_graph_request_client_seconds_bucket{client="lb",server="app",le="0.04"} 0
+		tempo_service_graph_request_client_seconds_bucket{client="lb",server="app",le="0.08"} 0
+		tempo_service_graph_request_client_seconds_bucket{client="lb",server="app",le="0.16"} 0
+		tempo_service_graph_request_client_seconds_bucket{client="lb",server="app",le="0.32"} 0
+		tempo_service_graph_request_client_seconds_bucket{client="lb",server="app",le="0.64"} 0
+		tempo_service_graph_request_client_seconds_bucket{client="lb",server="app",le="1.28"} 0
+		tempo_service_graph_request_client_seconds_bucket{client="lb",server="app",le="2.56"} 2
+		tempo_service_graph_request_client_seconds_bucket{client="lb",server="app",le="5.12"} 3
+		tempo_service_graph_request_client_seconds_bucket{client="lb",server="app",le="10.24"} 3
+		tempo_service_graph_request_client_seconds_bucket{client="lb",server="app",le="20.48"} 3
+		tempo_service_graph_request_client_seconds_bucket{client="lb",server="app",le="+Inf"} 3
+		tempo_service_graph_request_client_seconds_sum{client="lb",server="app"} 7.8
+		tempo_service_graph_request_client_seconds_count{client="lb",server="app"} 3
+		# HELP tempo_service_graph_request_server_seconds Time for a request between two nodes as seen from the server
+		# TYPE tempo_service_graph_request_server_seconds histogram
+		tempo_service_graph_request_server_seconds_bucket{client="app",server="db",le="0.01"} 0
+		tempo_service_graph_request_server_seconds_bucket{client="app",server="db",le="0.02"} 0
+		tempo_service_graph_request_server_seconds_bucket{client="app",server="db",le="0.04"} 0
+		tempo_service_graph_request_server_seconds_bucket{client="app",server="db",le="0.08"} 0
+		tempo_service_graph_request_server_seconds_bucket{client="app",server="db",le="0.16"} 0
+		tempo_service_graph_request_server_seconds_bucket{client="app",server="db",le="0.32"} 0
+		tempo_service_graph_request_server_seconds_bucket{client="app",server="db",le="0.64"} 0
+		tempo_service_graph_request_server_seconds_bucket{client="app",server="db",le="1.28"} 1
+		tempo_service_graph_request_server_seconds_bucket{client="app",server="db",le="2.56"} 3
+		tempo_service_graph_request_server_seconds_bucket{client="app",server="db",le="5.12"} 3
+		tempo_service_graph_request_server_seconds_bucket{client="app",server="db",le="10.24"} 3
+		tempo_service_graph_request_server_seconds_bucket{client="app",server="db",le="20.48"} 3
+		tempo_service_graph_request_server_seconds_bucket{client="app",server="db",le="+Inf"} 3
+		tempo_service_graph_request_server_seconds_sum{client="app",server="db"} 5
+		tempo_service_graph_request_server_seconds_count{client="app",server="db"} 3
+		tempo_service_graph_request_server_seconds_bucket{client="lb",server="app",le="0.01"} 0
+		tempo_service_graph_request_server_seconds_bucket{client="lb",server="app",le="0.02"} 0
+		tempo_service_graph_request_server_seconds_bucket{client="lb",server="app",le="0.04"} 0
+		tempo_service_graph_request_server_seconds_bucket{client="lb",server="app",le="0.08"} 0
+		tempo_service_graph_request_server_seconds_bucket{client="lb",server="app",le="0.16"} 0
+		tempo_service_graph_request_server_seconds_bucket{client="lb",server="app",le="0.32"} 0
+		tempo_service_graph_request_server_seconds_bucket{client="lb",server="app",le="0.64"} 0
+		tempo_service_graph_request_server_seconds_bucket{client="lb",server="app",le="1.28"} 1
+		tempo_service_graph_request_server_seconds_bucket{client="lb",server="app",le="2.56"} 2
+		tempo_service_graph_request_server_seconds_bucket{client="lb",server="app",le="5.12"} 3
+		tempo_service_graph_request_server_seconds_bucket{client="lb",server="app",le="10.24"} 3
+		tempo_service_graph_request_server_seconds_bucket{client="lb",server="app",le="20.48"} 3
+		tempo_service_graph_request_server_seconds_bucket{client="lb",server="app",le="+Inf"} 3
+		tempo_service_graph_request_server_seconds_sum{client="lb",server="app"} 6.2
+		tempo_service_graph_request_server_seconds_count{client="lb",server="app"} 3
 		# HELP tempo_service_graph_request_total Total count of requests between two nodes
 		# TYPE tempo_service_graph_request_total counter
 		tempo_service_graph_request_total{client="app",server="db"} 3
