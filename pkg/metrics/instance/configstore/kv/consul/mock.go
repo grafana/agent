0272e292f1b3717062bb76d89e9d86c7dc70502c
Robert Fratto <robertfratto@gmail.com>
2021-09-22 14:16:40 -0400

    fix more incorrect mock consul behavior

--- pkg/metrics/instance/configstore/kv/consul/mock.go
+++ pkg/metrics/instance/configstore/kv/consul/mock.go
@@ -181,8 +181,7 @@ func (m *mockKV) List(prefix string, q *consul.QueryOptions) (consul.KVPairs, *c
 
 	result := consul.KVPairs{}
 	for _, kvp := range m.kvps {
-		if strings.HasPrefix(kvp.Key, prefix) && kvp.ModifyIndex >= q.WaitIndex {
-			// unfortunately real consul doesn't do index check and returns everything with given prefix.
+		if strings.HasPrefix(kvp.Key, prefix) {
 			result = append(result, copyKVPair(kvp))
 		}
 	}
