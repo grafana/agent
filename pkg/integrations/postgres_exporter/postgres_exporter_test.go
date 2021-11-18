540a0424675ef9695becbd8f54545279d11c024f
Dharma Saputra <mail.dharma.saputra@gmail.com>
2021-11-18 22:00:52 +0700

    Update Implementation and fix unit test

--- pkg/integrations/postgres_exporter/postgres_exporter_test.go
+++ pkg/integrations/postgres_exporter/postgres_exporter_test.go
@@ -8,12 +8,12 @@ import (
 func Test_ParsePostgresURL(t *testing.T) {
 	dsn := "postgresql://linus:42secret@localhost:5432/postgres?sslmode=disable"
 	expected := map[string]string{
-		"dbname":   "'postgres'",
-		"host":     "'localhost'",
-		"password": "'42secret'",
-		"port":     "'5432'",
-		"sslmode":  "'disable'",
-		"user":     "'linus'",
+		"dbname":   "postgres",
+		"host":     "localhost",
+		"password": "42secret",
+		"port":     "5432",
+		"sslmode":  "disable",
+		"user":     "linus",
 	}
 
 	actual, _ := parsePostgresURL(dsn)
