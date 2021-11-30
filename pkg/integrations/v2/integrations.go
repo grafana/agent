79b83ab795b7c6501d5f7bb702b5d7a8e90784a0
Robert Fratto <robertfratto@gmail.com>
2021-11-30 09:02:07 -0500

    wip: prototype new integrations subsystem

---
+++ pkg/integrations/v2/integrations.go
@@ -0,0 +1,141 @@
+// Package integrations provides a way to run and manage Grafana Agent
+// "integrations," which integrate some external system (such as MySQL) to
+// Grafana Agent's existing metrics, logging, and tracing subsystems.
+//
+// Integrations are implemented in sub-packages. Every integration must
+// have an implementation of Config that configures the integration. The Config
+// interface is then used to instantiate an instance of the Integration
+// interface.
+//
+// Implementations of integrations implement extra functionality by
+// implementing interface extensions. The Integration interface is the most
+// basic interface that all integrations must implement. Extensions like
+// the MetricsIntegration interface define an integration that supports
+// metrics.
+//
+// Extension interfaces are used by the integrations subsystem to enable
+// common use cases. New behaviors can be implemented by manually using
+// the other subsystems of the agent provided in IntegrationOptions.
+package integrations
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/go-kit/log"
+	"github.com/grafana/agent/pkg/logs"
+	"github.com/grafana/agent/pkg/metrics"
+	"github.com/grafana/agent/pkg/traces"
+	prom_config "github.com/prometheus/prometheus/config"
+	"github.com/prometheus/prometheus/discovery"
+	"github.com/prometheus/prometheus/discovery/targetgroup"
+)
+
+// Config provides a configuration and constructor for an integration.
+type Config interface {
+	// Name returns the YAML field name of the integration. Name is used
+	// when unmarshaling the Config from YAML.
+	Name() string
+
+	// Identifier returns a string to uniquely identify this Integration.
+	// Identifier must be unique for each integration that shares the same
+	// Name.
+	//
+	// If there is no reasonable identifier to use for an integration,
+	// IntegrationOptions.AgentIdentifier may be used by default.
+	Identifier(IntegrationOptions) (string, error)
+
+	// NewIntegration should return a new Integration using the provided
+	// IntegrationOptions to help initialize the Integration.
+	//
+	// NewIntegration should do any initialization of an Integration, but it
+	// should not be run until Integration.Run is invoked.
+	NewIntegration(IntegrationOptions) (Integration, error)
+}
+
+// IntegrationOptions are used to pass around subsystems that empower
+// integrations.
+type IntegrationOptions struct {
+	// Logger to use for logging. Logs sent to the logger will inject
+	// a field for integration name and instance key.
+	Logger log.Logger
+
+	// AgentIdentifier provides an identifier for the running agent. This can
+	// be used for labelling whenever appropriate.
+	//
+	// AgentIdentifier will be set to the hostname:port of the running agent.
+	// TODO(rfratto): flag to override identifier at agent level?
+	AgentIdentifier string
+
+	// Some integrations may wish to interact with various subsystems for their
+	// implementation if the desired behavior is not supported natively by the
+	// integration manager.
+
+	Metrics *metrics.Agent // Metrics subsystem
+	Logs    *logs.Logs     // Logs subsystem
+	Tracing *traces.Traces // Traces subsystem
+}
+
+// An Integration integrates some external system with Grafana Agent's existing
+// subsystems.
+//
+// All integrations must at least implement this interface. More behaviors
+// can be added by implementing additional *Integration interfaces, such
+// as HTTPIntegration.
+type Integration interface {
+	// Run starts the integration and performs background tasks. Run must not
+	// return until ctx is canceled, even if there is no work to do.
+	Run(ctx context.Context) error
+}
+
+// ConfigurableIntegration is an Integration whose config can be updated
+// dynamically. Integrations that do not implement this interface will be shut
+// down and re-instantiated with the new Config.
+type UpdateIntegration interface {
+	Integration
+
+	// ApplyConfig should apply the config c to the integration. An error can be
+	// returned if the Config is invalid.
+	ApplyConfig(c Config) error
+}
+
+// HTTPIntegration is an integration that exposes an HTTP handler.
+//
+// Integrations are given a unique base path prefix where HTTP requests will be
+// routed. The prefix chosen for an integration is not guaranteed to be
+// predictable.
+type HTTPIntegration interface {
+	Integration
+
+	// Handler returns an http.Handler. Handler will be invoked for any endpoint
+	// under prefix. If Handler returns nil, nothing will be called.
+	//
+	// prefix will not be removed from the HTTP request by default.
+	Handler(prefix string) (http.Handler, error)
+}
+
+// MetricsIntegration is an integration that exposes Prometheus scrape targets.
+//
+// It is assumed, but not required, that HTTPIntegration is also implemented
+// to expose metrics. See HTTPIntegration for more information about how
+// HTTP works with integrations.
+type MetricsIntegration interface {
+	Integration
+
+	// Targets should return a channel that emits the full set of targets
+	// whenever the targets exposed by an integration changes. prefix
+	// can be used to create a __metrics_path__ label for targets that
+	// exist from an HTTPIntegration.
+	//
+	// Targets will only be called once whenever a config is loaded.
+	Targets(prefix string) chan<- []*targetgroup.Group
+
+	// ScrapeConfigs configures automatic scraping of targets. ScrapeConfigs
+	// is optional if an integration should not scrape itself.
+	//
+	// Use the provided discovery.Configs to discover the targets exposed by this
+	// integration.
+	//
+	// ScrapeConfigs will only be called once whenever a config is loaded.
+	ScrapeConfigs(discovery.Configs) []*prom_config.ScrapeConfig
+}
