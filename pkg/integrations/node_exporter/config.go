5c606a645820251d541927973fb5c4c2b60e8a82
Robbie Lankford <robert.lankford@grafana.com>
2021-12-09 12:30:20 -0600

    add DarwinConfig for macos_exporter integration

--- pkg/integrations/node_exporter/config.go
+++ pkg/integrations/node_exporter/config.go
@@ -321,3 +321,19 @@ func (f *flags) addBools(m map[*bool]string) {
 		}
 	}
 }
+
+func init() {
+	// Register macos_exporter
+	integrations.RegisterIntegration(&DarwinConfig{})
+}
+
+// DarwinConfig extends the Config struct and overrides the name of
+// the integration to avoid conflicts with node_exporter integration.
+type DarwinConfig struct {
+	Config
+}
+
+// Name returns the name of the integration that this config represents.
+func (*DarwinConfig) Name() string {
+	return "macos_exporter"
+}
