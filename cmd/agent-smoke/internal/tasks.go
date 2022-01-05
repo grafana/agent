5c81a9aec881de31ec87ec9a5352685238b000ae
Robbie Lankford <robert.lankford@grafana.com>
2022-01-04 21:37:20 -0600

    refactor task interface

--- cmd/agent-smoke/internal/tasks.go
+++ cmd/agent-smoke/internal/tasks.go
@@ -6,65 +6,69 @@ import (
 	"math/rand"
 	"time"
 
+	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 )
 
-// The Task interface represents units of work performed by some function on
-// some frequency interval by the smoke framework.
+// The Task interface represents some unit of work performed concurrently.
 type Task interface {
-	Task() (func(context.Context, *Smoke) error, time.Duration)
+	Run(context.Context) error
+}
+
+type repeatingTask struct {
+	Task
+	frequency time.Duration
 }
 
 type deletePodTask struct {
+	logger    log.Logger
+	clientset *kubernetes.Clientset
 	namespace string
 	pod       string
-	duration  time.Duration
 }
 
 var _zero int64
 
-func (t *deletePodTask) Task() (func(context.Context, *Smoke) error, time.Duration) {
-	return func(ctx context.Context, s *Smoke) error {
-		if err := s.clientset.CoreV1().Pods(t.namespace).Delete(ctx, t.pod, metav1.DeleteOptions{
-			GracePeriodSeconds: &_zero,
-		}); err != nil {
-			const msg = "failed to delete %s pod"
-			level.Error(s.logger).Log("msg", fmt.Sprintf(msg, t.pod), "err", err)
-		}
-		return nil
-	}, t.duration
+func (t *deletePodTask) Run(ctx context.Context) error {
+	if err := t.clientset.CoreV1().Pods(t.namespace).Delete(ctx, t.pod, metav1.DeleteOptions{
+		GracePeriodSeconds: &_zero,
+	}); err != nil {
+		const msg = "failed to delete %s pod"
+		level.Error(t.logger).Log("msg", fmt.Sprintf(msg, t.pod), "err", err)
+	}
+	return nil
 }
 
 type scaleDeploymentTask struct {
+	logger      log.Logger
+	clientset   *kubernetes.Clientset
 	namespace   string
 	deployment  string
 	maxReplicas int
 	minReplicas int
-	duration    time.Duration
 }
 
-func (t *scaleDeploymentTask) Task() (func(context.Context, *Smoke) error, time.Duration) {
-	return func(ctx context.Context, s *Smoke) error {
-		newReplicas := rand.Intn(t.maxReplicas-t.minReplicas) + t.minReplicas
-		const msg = "scaling %s replicas"
-		level.Debug(s.logger).Log("msg", fmt.Sprintf(msg, t.deployment), "replicas", newReplicas)
+func (t *scaleDeploymentTask) Run(ctx context.Context) error {
+	newReplicas := rand.Intn(t.maxReplicas-t.minReplicas) + t.minReplicas
+	const msg = "scaling %s replicas"
+	level.Debug(t.logger).Log("msg", fmt.Sprintf(msg, t.deployment), "replicas", newReplicas)
 
-		scale, err := s.clientset.AppsV1().Deployments(t.namespace).
-			GetScale(ctx, t.deployment, metav1.GetOptions{})
-		if err != nil {
-			const msg = "failed to get autoscalingv1.Scale object for %s deployment"
-			level.Error(s.logger).Log("msg", fmt.Sprintf(msg, t.deployment), "err", err)
-		}
+	scale, err := t.clientset.AppsV1().Deployments(t.namespace).
+		GetScale(ctx, t.deployment, metav1.GetOptions{})
+	if err != nil {
+		const msg = "failed to get autoscalingv1.Scale object for %s deployment"
+		level.Error(t.logger).Log("msg", fmt.Sprintf(msg, t.deployment), "err", err)
+	}
 
-		sc := *scale
-		sc.Spec.Replicas = int32(newReplicas)
-		_, err = s.clientset.AppsV1().Deployments(t.namespace).
-			UpdateScale(ctx, t.deployment, &sc, metav1.UpdateOptions{})
-		if err != nil {
-			const msg = "failed to scale %s deployment"
-			level.Error(s.logger).Log("msg", fmt.Sprintf(msg, t.deployment), "err", err)
-		}
-		return nil
-	}, t.duration
+	sc := *scale
+	sc.Spec.Replicas = int32(newReplicas)
+	_, err = t.clientset.AppsV1().Deployments(t.namespace).
+		UpdateScale(ctx, t.deployment, &sc, metav1.UpdateOptions{})
+	if err != nil {
+		const msg = "failed to scale %s deployment"
+		level.Error(t.logger).Log("msg", fmt.Sprintf(msg, t.deployment), "err", err)
+	}
+	return nil
 }
