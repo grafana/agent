97f9e11a290d29555ea740430027665901ae8b0f
Robbie Lankford <robert.lankford@grafana.com>
2022-01-06 22:33:11 -0600

    refactor smoke Options to Config

--- tools/smoke/internal/smoke.go
+++ tools/smoke/internal/smoke.go
@@ -2,6 +2,7 @@ package smoke
 
 import (
 	"context"
+	"flag"
 	"time"
 
 	"github.com/go-kit/log"
@@ -16,26 +17,41 @@ type Smoke struct {
 	tasks  []repeatingTask
 }
 
-// Options struct to pass configuration to the Smoke constructor.
-type Options struct {
+// Config struct to pass configuration to the Smoke constructor.
+type Config struct {
 	Kubeconfig        string
 	Namespace         string
 	ChaosFrequency    time.Duration
 	MutationFrequency time.Duration
 }
 
-// NewSmokeTest is the constructor for a Smoke object.
-func NewSmokeTest(logger log.Logger, opts Options) (*Smoke, error) {
+// RegisterFlags registers flags for the config to the given FlagSet.
+func (c *Config) RegisterFlags(f *flag.FlagSet) {
+	f.StringVar(&c.Namespace, "namespace", DefaultConfig.Namespace, "namespace smoke test should run in")
+	f.StringVar(&c.Kubeconfig, "kubeconfig", DefaultConfig.Kubeconfig, "absolute path to the kubeconfig file")
+	f.DurationVar(&c.ChaosFrequency, "chaos-frequency", DefaultConfig.ChaosFrequency, "chaos frequency duration")
+	f.DurationVar(&c.MutationFrequency, "mutation-frequency", DefaultConfig.MutationFrequency, "mutation frequency duration")
+}
+
+// DefaultConfig holds defaults for Smoke settings.
+var DefaultConfig = Config{
+	Kubeconfig:        "",
+	Namespace:         "default",
+	ChaosFrequency:    30 * time.Minute,
+	MutationFrequency: 5 * time.Minute,
+}
+
+// New is the constructor for a Smoke object.
+func New(logger log.Logger, cfg Config) (*Smoke, error) {
 	s := &Smoke{
 		logger: logger,
 	}
-
 	if s.logger == nil {
 		s.logger = log.NewNopLogger()
 	}
 
 	// use the current context in kubeconfig. this falls back to in-cluster config if kubeconfig is empty
-	config, err := clientcmd.BuildConfigFromFlags("", opts.Kubeconfig)
+	config, err := clientcmd.BuildConfigFromFlags("", cfg.Kubeconfig)
 	if err != nil {
 		return nil, err
 	}
@@ -51,30 +67,30 @@ func NewSmokeTest(logger log.Logger, opts Options) (*Smoke, error) {
 			Task: &deletePodTask{
 				logger:    log.With(s.logger, "task", "delete_pod", "pod", "grafana-agent-0"),
 				clientset: clientset,
-				namespace: opts.Namespace,
+				namespace: cfg.Namespace,
 				pod:       "grafana-agent-0",
 			},
-			frequency: opts.ChaosFrequency,
+			frequency: cfg.ChaosFrequency,
 		},
 		repeatingTask{
 			Task: &deletePodBySelectorTask{
 				logger:    log.With(s.logger, "task", "delete_pod_by_selector"),
 				clientset: clientset,
-				namespace: opts.Namespace,
+				namespace: cfg.Namespace,
 				selector:  "name=grafana-agent-cluster",
 			},
-			frequency: opts.ChaosFrequency,
+			frequency: cfg.ChaosFrequency,
 		},
 		repeatingTask{
 			Task: &scaleDeploymentTask{
 				logger:      log.With(s.logger, "task", "scale_deployment", "deployment", "avalanche"),
 				clientset:   clientset,
-				namespace:   opts.Namespace,
+				namespace:   cfg.Namespace,
 				deployment:  "avalanche",
 				maxReplicas: 11,
 				minReplicas: 2,
 			},
-			frequency: opts.MutationFrequency,
+			frequency: cfg.MutationFrequency,
 		})
 
 	return s, nil
--- tools/smoke/main.go
+++ tools/smoke/main.go
@@ -14,19 +14,16 @@ import (
 
 func main() {
 	var (
-		opts        smoke.Options
+		cfg         smoke.Config
 		logLevel    logging.Level
 		logFormat   logging.Format
 		withTimeout time.Duration
 	)
 
+	cfg.RegisterFlags(flag.CommandLine)
 	logLevel.RegisterFlags(flag.CommandLine)
 	logFormat.RegisterFlags(flag.CommandLine)
-	flag.StringVar(&opts.Namespace, "namespace", "agent-smoke-test", "namespace smoke test should run in")
-	flag.StringVar(&opts.Kubeconfig, "kubeconfig", "", "absolute path to the kubeconfig file")
 	flag.DurationVar(&withTimeout, "duration", time.Duration(0), "timeout after duration, example 3h")
-	flag.DurationVar(&opts.ChaosFrequency, "chaos-frequency", 30*time.Minute, "chaos frequency duration")
-	flag.DurationVar(&opts.MutationFrequency, "mutation-frequency", 5*time.Minute, "mutation frequency duration")
 	flag.Parse()
 
 	logger := util.NewLoggerFromLevel(logLevel, logFormat)
@@ -39,7 +36,7 @@ func main() {
 		level.Debug(logger).Log("msg", "running with duration", "duration", withTimeout.String())
 	}
 
-	smokeTest, err := smoke.NewSmokeTest(logger, opts)
+	smokeTest, err := smoke.New(logger, cfg)
 	if err != nil {
 		level.Error(logger).Log("msg", "error constructing smoke test", "err", err)
 		os.Exit(1)
