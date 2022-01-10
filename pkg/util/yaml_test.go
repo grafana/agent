grafana/agent

commit 6598a87eb7e1de2044973cc0d174a4edbc93c507

pkg/integrations: decouple common config from integration-specific configs (#1192)
* pkg/integrations: decouple common config from integrations

* fix linting issues

pkg/util/yaml.go
@@ -1,0 +1,120 @@
+package util
+
+import (
+	"errors"
+	"regexp"
+	"sort"
+
+	"gopkg.in/yaml.v2"
+)
+
+// RawYAML is similar to json.RawMessage and allows for deferred YAML decoding.
+type RawYAML []byte
+
+// UnmarshalYAML implements yaml.Unmarshaler.
+func (r *RawYAML) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var ms yaml.MapSlice
+	if err := unmarshal(&ms); err != nil {
+		return err
+	}
+	bb, err := yaml.Marshal(ms)
+	if err != nil {
+		return err
+	}
+	*r = bb
+	return nil
+}
+
+// MarshalYAML implements yaml.Marshaler.
+func (r RawYAML) MarshalYAML() (interface{}, error) {
+	return r.Map()
+}
+
+// Map converts the raw YAML into a yaml.MapSlice.
+func (r RawYAML) Map() (yaml.MapSlice, error) {
+	var ms yaml.MapSlice
+	if err := yaml.Unmarshal(r, &ms); err != nil {
+		return nil, err
+	}
+	return ms, nil
+}
+
+// MarshalYAMLMerged marshals all values from vv into a single object.
+func MarshalYAMLMerged(vv ...interface{}) ([]byte, error) {
+	var full yaml.MapSlice
+	for _, v := range vv {
+		bb, err := yaml.Marshal(v)
+		if err != nil {
+			return nil, err
+		}
+		ms, err := RawYAML(bb).Map()
+		if err != nil {
+			return nil, err
+		}
+		full = append(full, ms...)
+	}
+	return yaml.Marshal(full)
+}
+
+// UnmarshalYAMLMerged performs a strict unmarshal of bb into all values from
+// vv.
+func UnmarshalYAMLMerged(bb []byte, vv ...interface{}) error {
+	var typeErrors []yaml.TypeError
+
+	for _, v := range vv {
+		// Perform a non-strict unmarshal first to populate the object completely.
+		if err := yaml.Unmarshal(bb, v); err != nil {
+			return err
+		}
+
+		// Then, perform a strict unmarshal. This is likely to fail with type
+		// errors for missing fields that may have been consumed by another object
+		// in vv.
+		var te *yaml.TypeError
+		if err := yaml.UnmarshalStrict(bb, v); errors.As(err, &te) {
+			typeErrors = append(typeErrors, *te)
+		} else if err != nil {
+			return err
+		}
+	}
+
+	var (
+		addedErrors    = map[string]struct{}{}
+		notFoundErrors = map[string]int{}
+	)
+
+	// Do an initial pass over our errors, separating errors for not found fields.
+	// Other errors are "real" and should be returned.
+	for _, te := range typeErrors {
+		for _, msg := range te.Errors {
+			notFound := notFoundErrRegex.FindStringSubmatch(msg)
+			if notFound != nil {
+				// Track the invalid field error. Use the first capture group which
+				// excludes the type, which will be unique per v.
+				notFoundErrors[notFound[1]]++
+				continue
+			}
+			addedErrors[msg] = struct{}{}
+		}
+	}
+
+	// Iterate over our errors for not found fields. The field truly isn't defined
+	// if it was reported len(vv) times (i.e., no v consumed it).
+	for msg, count := range notFoundErrors {
+		if count == len(vv) {
+			addedErrors[msg] = struct{}{}
+		}
+	}
+
+	if len(addedErrors) > 0 {
+		realErrors := make([]string, 0, len(addedErrors))
+		for msg := range addedErrors {
+			realErrors = append(realErrors, msg)
+		}
+		sort.Strings(realErrors)
+		return &yaml.TypeError{Errors: realErrors}
+	}
+	return nil
+}
+
+var notFoundErrRegex = regexp.MustCompile(`^(line \d+: field .* not found) in type .*$`)

pkg/integrations/agent/agent.go
@@ -14,18 +14,11 @@ import (
 )
 
 // Config controls the Agent integration.
-type Config struct {
-	Common config.Common `yaml:",inline"`
-}
+type Config struct{}
 
 // Name returns the name of the integration that this config represents.
 func (c *Config) Name() string {
 	return "agent"
-}
-
-// CommonConfig returns the common settings shared across all integrations.
-func (c *Config) CommonConfig() config.Common {
-	return c.Common
 }
 
 // InstanceKey returns the hostname of the machine.

pkg/integrations/cadvisor/common.go
@@ -4,7 +4,6 @@ import (
 	"time"
 
 	"github.com/grafana/agent/pkg/integrations"
-	"github.com/grafana/agent/pkg/integrations/config"
 )
 
 const name = "cadvisor"
@@ -34,8 +33,6 @@ var DefaultConfig Config = Config{
 
 // Config controls cadvisor
 type Config struct {
-	Common config.Common `yaml:",inline"`
-
 	// Common cadvisor config options
 	// StoreContainerLabels converts container labels and environment variables into labels on prometheus metrics for each container. If false, then only metrics exported are container name, first alias, and image name.
 	StoreContainerLabels bool `yaml:"store_container_labels,omitempty"`
@@ -104,12 +101,6 @@ func (c *Config) UnmarshalYAML(unmarshal func(interface{}) error) error {
 // Name returns the name of the integration that this config represents.
 func (c *Config) Name() string {
 	return name
-}
-
-// CommonConfig returns the common settings shared across all configs for
-// integrations.
-func (c *Config) CommonConfig() config.Common {
-	return c.Common
 }
 
 // InstanceKey returns the agentKey

pkg/integrations/consul_exporter/consul_exporter.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/go-kit/log"
 	"github.com/grafana/agent/pkg/integrations"
-	"github.com/grafana/agent/pkg/integrations/config"
 	consul_api "github.com/hashicorp/consul/api"
 	"github.com/prometheus/consul_exporter/pkg/exporter"
 )
@@ -24,8 +23,6 @@ var DefaultConfig = Config{
 
 // Config controls the consul_exporter integration.
 type Config struct {
-	Common config.Common `yaml:",inline"`
-
 	Server             string        `yaml:"server,omitempty"`
 	CAFile             string        `yaml:"ca_file,omitempty"`
 	CertFile           string        `yaml:"cert_file,omitempty"`
@@ -53,11 +50,6 @@ func (c *Config) UnmarshalYAML(unmarshal func(interface{}) error) error {
 // Name returns the name of the integration.
 func (c *Config) Name() string {
 	return "consul_exporter"
-}
-
-// CommonConfig returns the common set of settings for this integration.
-func (c *Config) CommonConfig() config.Common {
-	return c.Common
 }
 
 // InstanceKey returns the hostname:port of the Consul server.

pkg/integrations/dnsmasq_exporter/dnsmasq_exporter.go
@@ -5,7 +5,6 @@ import (
 	"github.com/go-kit/log"
 	"github.com/google/dnsmasq_exporter/collector"
 	"github.com/grafana/agent/pkg/integrations"
-	"github.com/grafana/agent/pkg/integrations/config"
 	"github.com/miekg/dns"
 )
 
@@ -17,8 +16,6 @@ var DefaultConfig Config = Config{
 
 // Config controls the dnsmasq_exporter integration.
 type Config struct {
-	Common config.Common `yaml:",inline"`
-
 	// DnsmasqAddress is the address of the dnsmasq server (host:port).
 	DnsmasqAddress string `yaml:"dnsmasq_address,omitempty"`
 
@@ -29,11 +26,6 @@ type Config struct {
 // Name returns the name of the integration that this config is for.
 func (c *Config) Name() string {
 	return "dnsmasq_exporter"
-}
-
-// CommonConfig returns the set of common settings shared across all integrations.
-func (c *Config) CommonConfig() config.Common {
-	return c.Common
 }
 
 // InstanceKey returns the address of the dnsmasq server.

pkg/integrations/elasticsearch_exporter/elasticsearch_exporter.go
@@ -12,7 +12,6 @@ import (
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
 	"github.com/grafana/agent/pkg/integrations"
-	"github.com/grafana/agent/pkg/integrations/config"
 	"github.com/prometheus/client_golang/prometheus"
 
 	"github.com/prometheus-community/elasticsearch_exporter/collector"
@@ -30,10 +29,6 @@ var DefaultConfig = Config{
 
 // Config controls the elasticsearch_exporter integration.
 type Config struct {
-	Common config.Common `yaml:",inline"`
-
-	// Exporter configuration
-
 	// HTTP API address of an Elasticsearch node.
 	Address string `yaml:"address,omitempty"`
 	// Timeout for trying to get stats from Elasticsearch.
@@ -75,12 +70,6 @@ func (c *Config) UnmarshalYAML(unmarshal func(interface{}) error) error {
 // Name returns the name of the integration that this config represents.
 func (c *Config) Name() string {
 	return "elasticsearch_exporter"
-}
-
-// CommonConfig returns the common settings shared across all configs for
-// integrations.
-func (c *Config) CommonConfig() config.Common {
-	return c.Common
 }
 
 // InstanceKey returns the hostname:port of the elasticsearch node being queried.

pkg/integrations/github_exporter/github_exporter.go
@@ -7,7 +7,6 @@ import (
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
 	"github.com/grafana/agent/pkg/integrations"
-	"github.com/grafana/agent/pkg/integrations/config"
 	gh_config "github.com/infinityworks/github-exporter/config"
 	"github.com/infinityworks/github-exporter/exporter"
 	config_util "github.com/prometheus/common/config"
@@ -20,8 +19,6 @@ var DefaultConfig Config = Config{
 
 // Config controls github_exporter
 type Config struct {
-	Common config.Common `yaml:",inline"`
-
 	// URL for the github API
 	APIURL string `yaml:"api_url,omitempty"`
 
@@ -52,12 +49,6 @@ func (c *Config) UnmarshalYAML(unmarshal func(interface{}) error) error {
 // Name returns the name of the integration that this config represents.
 func (c *Config) Name() string {
 	return "github_exporter"
-}
-
-// CommonConfig returns the common settings shared across all configs for
-// integrations.
-func (c *Config) CommonConfig() config.Common {
-	return c.Common
 }
 
 // InstanceKey returns the hostname:port of the GitHub API server.

pkg/integrations/integration.go
@@ -14,10 +14,6 @@ type Config interface {
 	// pull the configuration from the Agent config YAML.
 	Name() string
 
-	// CommonConfig returns the set of common configuration values present across
-	// all integrations.
-	CommonConfig() config.Common
-
 	// InstanceKey should return the key the reprsents the config, which will be
 	// used to populate the value of the `instance` label for metrics.
 	//
@@ -25,8 +21,8 @@ type Config interface {
 	// may be used if the integration being configured applies to an entire
 	// machine.
 	//
-	// This method is only used if the common config does not have an override for
-	// InstanceKey.
+	// This method may not be invoked if the instance key for a Config is
+	// overridden.
 	InstanceKey(agentKey string) (string, error)
 
 	// NewIntegration returns an integration for the given with the given logger.

commit 8c2f41d1aaeb2f7c805248275b8f92c8b4e38092

pkg/util: support custom yaml.Unmarshaler implementations for util.UnmarshalYAMLMerged
It's common for config types to have implement yaml.Unmarshaler for:

* Applying defaults
* Applying extra logic post-unmarshal

If these config types were unmarshaled through util.UnmarshalYAMLMerged,
the yaml.Unmarshaler implementation would never complete successfully,
preventing the post-unmarshal logic from running.

This issue was introduced in #1192, but went unnoticed until #1228
implemented yaml.Unmarshaler to perform field migrations. #1240 reported
the issue.

This commit fixes the bug by performing a second non-strict unmarshal to
ensure that all input values unmarshal successfully, with the exception
of unmarshal errors unrelated to unrecognized field names.

This is hacky, but it's worthwhile noting that util.UnmarshalYAMLMerged
is a temporary workaround needed for the integrations-next migration,
and will eventually be removed.

pkg/util/yaml.go
@@ -70,6 +70,17 @@ func UnmarshalYAMLMerged(bb []byte, vv ...interface{}) error {
 		} else if err != nil {
 			return err
 		}
+
+		// It's common for custom yaml.Unmarshaler implementations to use
+		// UnmarshalYAML to apply default values both before and after calling the
+		// unmarshal method passed to them.
+		//
+		// We *must* do a second non-strict unmarshal *after* the strict unmarshal
+		// to ensure that every v was able to complete its unmarshal to completion,
+		// ignoring type errors from unrecognized fields.
+		if err := yaml.Unmarshal(bb, v); err != nil {
+			return err
+		}
 	}
 
 	var (

pkg/util/yaml_test.go
@@ -1,0 +1,47 @@
+package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// TestUnmarshalYAMLMerged_CustomUnmarshal checks to see that
+// UnmarshalYAMLMerged works with merging types that have custom unmarshal
+// methods which do extra checks after calling unmarshal.
+func TestUnmarshalYAMLMerged_CustomUnmarshal(t *testing.T) {
+	in := `
+  fieldA: foo
+  fieldB: bar
+  `
+
+	var (
+		val1 typeOne
+		val2 typeTwo
+	)
+
+	err := UnmarshalYAMLMerged([]byte(in), &val1, &val2)
+	require.NoError(t, err)
+
+	require.Equal(t, "foo", val1.FieldA)
+	require.Equal(t, "bar", val2.FieldB)
+	require.True(t, val2.Unmarshaled)
+}
+
+type typeOne struct {
+	FieldA string `yaml:"fieldA"`
+}
+
+type typeTwo struct {
+	FieldB      string `yaml:"fieldB"`
+	Unmarshaled bool   `yaml:"-"`
+}
+
+func (t *typeTwo) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	type rawType typeTwo
+	if err := unmarshal((*rawType)(t)); err != nil {
+		return err
+	}
+	t.Unmarshaled = true
+	return nil
+}