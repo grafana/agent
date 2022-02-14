8d3457f4949db35f3001c236a71049919bb41095
Christopher Knutson <christopher.knutson@gmail.com>
2022-02-14 14:05:49 -0600

    fix godoc comments

--- pkg/util/strings.go
+++ pkg/util/strings.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 )
 
-// helper function for comverting CamelCase to Snake Case
+// CamelToSnake is a helper function for converting CamelCase to Snake Case
 func CamelToSnake(str string) string {
 	var matchFirstCap = regexp.MustCompile("(.)([A-Z][a-z]+)")
 	var matchAllCap = regexp.MustCompile("([a-z0-9])([A-Z])")
