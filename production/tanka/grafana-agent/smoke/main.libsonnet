grafana/agent

commit b0c1e45c4ab884fd51c85687f51142ef85d2ca5d

pkg/server: replace pkg/util/server with pkg/server (#1476)
* pkg/server: replace pkg/util/server with pkg/server

pkg/server is a new top-level subsystem which exposes an HTTP and gRPC
server. It was forked off of weaveworks/common/server with direct
support for reloading settings we wish to keep as dynamic.

The following YAML fields are intended to be dynamically updatable:

* server.log_level
* server.log_format
* server.http_tls_config
* server.grpc_tls_config

Other fields are intended to be static and should only be set via
command line flag. Their YAML counterparts are now DEPRECATED and will
be removed in v0.26.0. As of this commit, reloading the config file will
fail if a non-dynamic field has changed in the server block.

Closes grafana/agent#1336.

Flag changes
------------

Many flag names corresponding to the server have changed.

The following flags are NEW:

* `-server.http.enable-tls`
* `-server.grpc.enable-tls`
* `-server.http.address`
* `-server.grpc.address`

The `-server.*-enable-tls` flags must now be passed to enable TLS for
HTTP and gRPC respectively. This is a change from the previous behavior
of dynamically enabling TLS when a certificate pair is provided.

`-server.http.address` and `-server.grpc.address` are intended to be
`host:port` addresses on which to listen for HTTP and gRPC traffic. They
replace the `-server.*-listen-address` and `-server.*-listen-port` flags.

The following flags have been RENAMED:

* `-server.log.source-ips.enabled` (renamed from `-server.log-source-ips-enabled`)
* `-server.log.source-ips.header` (renamed from `-server.log-source-ips-header`)
* `-server.log.source-ips.regex` (renamed from `-server.log-source-ips-regex`)
* `-server.http.network` (renamed from `-server.http-listen-network`)
* `-server.http.conn-limit` (renamed from `-server.http-conn-limit`)
* `-server.http.read-timeout` (renamed from `-server.http-read-timeout`)

production/tanka/grafana-agent/smoke/main.libsonnet
@@ -45,7 +45,6 @@ local util = k.util;
         containerPort.newNamed(name='remote-write', containerPort=19090),
       ]) +
       container.withArgsMixin(k.util.mapToFlags({
-        'log.level': 'debug',
         namespace: namespace,
         'mutation-frequency': this._config.mutationFrequency,
         'chaos-frequency': this._config.chaosFrequency,

production/tanka/grafana-agent/v1/main.libsonnet
@@ -59,7 +59,6 @@ local service = k.core.v1.service;
     config:: {
       server: {
         log_level: 'info',
-        http_listen_port: 8080,
       },
     } + (
       if has_metrics_config

commit fe0ecdbd6368b388c0081caf2ff06c751a0eca25

Partially revert b0c1e45c
b0c1e45c4ab884fd51c85687f51142ef85d2ca5d accidentally removed setting
`-log.level` in the smoke binary as part of removing the deprecated
`-log.level` field from agent code.

production/tanka/grafana-agent/smoke/main.libsonnet
@@ -45,6 +45,7 @@ local util = k.util;
         containerPort.newNamed(name='remote-write', containerPort=19090),
       ]) +
       container.withArgsMixin(k.util.mapToFlags({
+        'log.level': 'debug',
         namespace: namespace,
         'mutation-frequency': this._config.mutationFrequency,
         'chaos-frequency': this._config.chaosFrequency,