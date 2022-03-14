83794d9cfa083101fba837ff9090fdeb51e4fb60
Paschalis Tsilias <paschalis.tsilias@grafana.com>
2022-03-14 17:32:22 +0200

    Attempt no.3 to fix non-linux stub
    Signed-off-by: Paschalis Tsilias <paschalis.tsilias@grafana.com>

--- pkg/integrations/v2/ebpf/integration_stub.go
+++ pkg/integrations/v2/ebpf/integration_stub.go
@@ -7,6 +7,7 @@ import (
 	"github.com/grafana/agent/pkg/integrations/v2"
 
 	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
 )
 
 func init() {
@@ -17,7 +18,8 @@ type config struct{}
 
 func (c *config) ApplyDefaults(globals integrations.Globals) error        { return nil }
 func (c *config) Identifier(globals integrations.Globals) (string, error) { return c.Name(), nil }
-func (c *config) Name() string                                            { return "noop-ebpf" }
+func (c *config) Name() string                                            { return "ebpf" }
 func (c *config) NewIntegration(l log.Logger, globals integrations.Globals) (integrations.Integration, error) {
+	level.Warn(l).Log("msg", "the ebpf integration only works on linux; enabling it on other platforms will do nothing")
 	return integrations.NoOpIntegration, nil
 }
