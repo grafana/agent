388ba5e185a938be572c50f4ca18e7aef413a4ed
Robert Fratto <robertfratto@gmail.com>
2022-03-22 12:49:41 -0400

    metrics/instance: use native Prometheus capabilities to get rw timestamp
    Prometheus Agent extended the API of remote.Storage to natively find the
    lowest sent timestamp across all queues. Now that we're embedding a new
    enough version of Prometheus, we can remove our old hack of collecting
    metrics to find the timestamp.

--- pkg/metrics/instance/instance_test.go
+++ pkg/metrics/instance/instance_test.go
@@ -291,64 +291,6 @@ func TestInstance_Recreate(t *testing.T) {
 	})
 }
 
-func TestMetricValueCollector(t *testing.T) {
-	r := prometheus.NewRegistry()
-	vc := NewMetricValueCollector(r, "this_should_be_tracked")
-
-	shouldTrack := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "this_should_be_tracked",
-		ConstLabels: prometheus.Labels{
-			"foo": "bar",
-		},
-	})
-
-	shouldTrack.Set(12345)
-
-	shouldNotTrack := prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "this_should_not_be_tracked",
-	})
-
-	r.MustRegister(shouldTrack, shouldNotTrack)
-
-	vals, err := vc.GetValues("foo", "bar")
-	require.NoError(t, err)
-	require.Equal(t, []float64{12345}, vals)
-}
-
-func TestRemoteWriteMetricInterceptor_AllValues(t *testing.T) {
-	r := prometheus.NewRegistry()
-	vc := NewMetricValueCollector(r, "track")
-
-	valueA := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "this_should_be_tracked",
-		ConstLabels: prometheus.Labels{
-			"foo": "bar",
-		},
-	})
-	valueA.Set(12345)
-
-	valueB := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "track_this_too",
-		ConstLabels: prometheus.Labels{
-			"foo": "bar",
-		},
-	})
-	valueB.Set(67890)
-
-	shouldNotReturn := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "track_this_but_label_does_not_match",
-		ConstLabels: prometheus.Labels{
-			"foo": "nope",
-		},
-	})
-
-	r.MustRegister(valueA, valueB, shouldNotReturn)
-
-	vals, err := vc.GetValues("foo", "bar")
-	require.NoError(t, err)
-	require.Equal(t, []float64{12345, 67890}, vals)
-}
-
 func getTestServer(t *testing.T) (addr string, closeFunc func()) {
 	t.Helper()
 
