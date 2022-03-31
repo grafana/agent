f5b73a3ebd056b85425f7645b677dbec322933d4
Robert Fratto <robertfratto@gmail.com>
2022-03-31 11:56:32 -0400

    pkg/config: use -config.file.type flag to derive file type (#1539)
    Introduce a -config.file.type which acts as a hint for the contents of
    the config file being loaded. This then replaces the
    -config.dynamic-config-path flag with -config.file.type=dynamic.
    
    Having a dedicated flag for file should be useful for avoiding having
    too many flags which load a config file and allows us to change the
    default type in the future (i.e., default to `-config.file.type=flow`).

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -39,6 +39,13 @@ var (
 	}
 )
 
+var (
+	fileTypeYAML    = "yaml"
+	fileTypeDynamic = "dynamic"
+
+	fileTypes = []string{fileTypeYAML, fileTypeDynamic}
+)
+
 // DefaultConfig holds default settings for all the subsystems.
 var DefaultConfig = Config{
 	// All subsystems with a DefaultConfig should be listed here.
@@ -307,34 +314,47 @@ func getenv(name string) string {
 // to the flagset before parsing them with the values specified by
 // args.
 func Load(fs *flag.FlagSet, args []string) (*Config, error) {
-	return load(fs, args, func(url string, expand bool, c *Config) error {
-		if features.Enabled(fs, featRemoteConfigs) {
-			return LoadRemote(url, expand, c)
+	return load(fs, args, func(path, fileType string, expandArgs bool, c *Config) error {
+		switch fileType {
+		case fileTypeYAML:
+			if features.Enabled(fs, featRemoteConfigs) {
+				return LoadRemote(path, expandArgs, c)
+			}
+			return LoadFile(path, expandArgs, c)
+		case fileTypeDynamic:
+			if !features.Enabled(fs, featDynamicConfig) {
+				return fmt.Errorf("feature %q must be enabled to use file type %s", featDynamicConfig, fileTypeDynamic)
+			} else if !features.Enabled(fs, featIntegrationsNext) {
+				return fmt.Errorf("feature %q must be enabled to use file type %s", featIntegrationsNext, fileTypeDynamic)
+			} else if features.Enabled(fs, featRemoteConfigs) {
+				return fmt.Errorf("feature %q can not be enabled with file type %s", featRemoteConfigs, fileTypeDynamic)
+			} else if expandArgs {
+				return fmt.Errorf("-config.expand-env can not be used with file type %s", fileTypeDynamic)
+			}
+
+			return LoadDynamicConfiguration(path, expandArgs, c)
+		default:
+			return fmt.Errorf("unknown file type %q. accepted values: %s", fileType, strings.Join(fileTypes, ", "))
 		}
-		if features.Enabled(fs, featDynamicConfig) && !features.Enabled(fs, featIntegrationsNext) {
-			return fmt.Errorf("integrations-next must be enabled for dynamic configuration to work")
-		} else if features.Enabled(fs, featDynamicConfig) {
-			return LoadDynamicConfiguration(url, expand, c)
-		}
-		return LoadFile(url, expand, c)
 	})
 }
 
+type loaderFunc func(path string, fileType string, expandArgs bool, target *Config) error
+
 // load allows for tests to inject a function for retrieving the config file that
 // doesn't require having a literal file on disk.
-func load(fs *flag.FlagSet, args []string, loader func(string, bool, *Config) error) (*Config, error) {
+func load(fs *flag.FlagSet, args []string, loader loaderFunc) (*Config, error) {
 	var (
 		cfg = DefaultConfig
 
-		printVersion      bool
-		file              string
-		dynamicConfigPath string
-		configExpandEnv   bool
+		printVersion    bool
+		file            string
+		fileType        string
+		configExpandEnv bool
 	)
 
 	fs.StringVar(&file, "config.file", "", "configuration file to load")
-	fs.StringVar(&dynamicConfigPath, "config.dynamic-config-path", "", "dynamic configuration path that points to a single configuration file supports file:// or s3:// protocols. Must be enabled by -enable-features=dynamic-config,integrations-next")
-
+	fs.StringVar(&fileType, "config.file.type", "yaml", fmt.Sprintf("Type of file pointed to by -config.file flag. Supported values: %s. %s requires dynamic-config and integrations-next features to be enabled.", strings.Join(fileTypes, ", "), fileTypeDynamic))
 	fs.BoolVar(&printVersion, "version", false, "Print this build's version information")
 	fs.BoolVar(&configExpandEnv, "config.expand-env", false, "Expands ${var} in config according to the values of the environment variables.")
 	cfg.RegisterFlags(fs)
@@ -349,15 +369,9 @@ func load(fs *flag.FlagSet, args []string, loader func(string, bool, *Config) er
 		os.Exit(0)
 	}
 
-	if features.Enabled(fs, featDynamicConfig) {
-		if dynamicConfigPath == "" {
-			return nil, fmt.Errorf("-config.dynamic-config-path flag required when using dynamic configuration")
-		} else if err := loader(dynamicConfigPath, configExpandEnv, &cfg); err != nil {
-			return nil, fmt.Errorf("error loading dynamic configuration file %s: %w", dynamicConfigPath, err)
-		}
-	} else if file == "" {
+	if file == "" {
 		return nil, fmt.Errorf("-config.file flag required")
-	} else if err := loader(file, configExpandEnv, &cfg); err != nil {
+	} else if err := loader(file, fileType, configExpandEnv, &cfg); err != nil {
 		return nil, fmt.Errorf("error loading config file %s: %w", file, err)
 	}
 
