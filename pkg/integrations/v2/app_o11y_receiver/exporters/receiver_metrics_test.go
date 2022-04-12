17275222b3094de2db3b5740874f2ae917d308cd
Domas <domasx2@gmail.com>
2022-04-12 18:28:32 +0300

    Apply suggestions from code review
    Co-authored-by: Robert Fratto <robertfratto@gmail.com>

--- pkg/integrations/v2/app_o11y_receiver/exporters/receiver_metrics_test.go
+++ pkg/integrations/v2/app_o11y_receiver/exporters/receiver_metrics_test.go
@@ -48,21 +48,9 @@ func testcase(t *testing.T, payload models.Payload, assertions []metricAssertion
 
 func TestReceiverMetricsExport(t *testing.T) {
 	var payload models.Payload
-	payload.Logs = []models.Log{
-		{},
-		{},
-	}
-	payload.Measurements = []models.Measurement{
-		{},
-		{},
-		{},
-	}
-	payload.Exceptions = []models.Exception{
-		{},
-		{},
-		{},
-		{},
-	}
+        payload.Logs = make([]models.Log, 2)
+        payload.Measurements = make([]models.Measurement, 3)
+        payload.Exceptions = make([]models.Exception, 4)
 	testcase(t, payload, []metricAssertion{
 		{
 			name:  "app_o11y_receiver_total_logs",
