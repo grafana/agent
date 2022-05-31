94b6afcaa9e59e4e36cd69ba2adb4fc1ee1213e8
Domas <domasx2@gmail.com>
2022-05-31 18:32:52 +0300

    fix bug with unmarshalling app agent receiver config multiple times

--- pkg/integrations/v2/app_agent_receiver/config_test.go
+++ pkg/integrations/v2/app_agent_receiver/config_test.go
@@ -45,3 +45,35 @@ server:
 	require.Equal(t, 142.0, cfg.Server.RateLimiting.RPS)
 	require.Equal(t, 50, cfg.Server.RateLimiting.Burstiness)
 }
+
+func TestConfig_MultipleUnmarshals(t *testing.T) {
+	var cfg1 Config
+	cb1 := `
+sourcemaps:
+  download_origins: ["one"]
+logs_labels:
+  app: frontend
+  one: two`
+	var cfg2 Config
+	cb2 := `
+logs_labels:
+  app: backend
+  bar: baz`
+
+	err := yaml.UnmarshalStrict([]byte(cb1), &cfg1)
+	require.NoError(t, err)
+	err = yaml.UnmarshalStrict([]byte(cb2), &cfg2)
+	require.NoError(t, err)
+
+	require.Equal(t, map[string]string{
+		"app": "frontend",
+		"one": "two",
+	}, cfg1.LogsLabels)
+	require.Equal(t, []string{"one"}, cfg1.SourceMaps.DownloadFromOrigins)
+
+	require.Equal(t, map[string]string{
+		"app": "backend",
+		"bar": "baz",
+	}, cfg2.LogsLabels)
+	require.Equal(t, []string{"*"}, cfg2.SourceMaps.DownloadFromOrigins)
+}
