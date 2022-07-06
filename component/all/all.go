d5b5dcf5c847c308d21b2e2fdad77339728f13f3
mattdurham <mattdurham@ppog.org>
2022-07-06 10:21:51 -0400

    Metric Working Group (#1814)
    * adding existing components
    
    * Push basic metric usage
    
    * Push example
    
    * mapping shenanigans
    
    * Update basic_scraper.flow
    
    * test
    
    * Mostly working example of appender
    
    * added todos
    
    * Rollback wal changes and added additional tests.
    
    * Add ability to remove stale markers
    
    * Fix linting
    
    * Feedback from PR. Renaming and checking to see if the localid is also set.
    
    * Normalize naming
    
    * Remove scraper, since it is being added in another PR
    
    * No linting for appendable
    
    * Linting
    
    * Fix linting

--- component/all/all.go
+++ component/all/all.go
@@ -2,6 +2,7 @@
 package all
 
 import (
-	_ "github.com/grafana/agent/component/local/file"     // Import local.file
-	_ "github.com/grafana/agent/component/targets/mutate" // Import targets.mutate
+	_ "github.com/grafana/agent/component/local/file"          // Import local.file
+	_ "github.com/grafana/agent/component/metrics/remotewrite" // Import metrics.remotewrite
+	_ "github.com/grafana/agent/component/targets/mutate"      // Import targets.mutate
 )
