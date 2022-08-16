ef654c9c4298bf9422b65d3e0f188ad0dba005f9
Robert Fratto <robertfratto@gmail.com>
2022-08-16 12:34:05 -0400

    wip: implement graph view

--- web/ui/src/pages/DAG.tsx
+++ web/ui/src/pages/DAG.tsx
@@ -1,8 +1,13 @@
 import { faDiagramProject } from '@fortawesome/free-solid-svg-icons';
+import ComponentGraph from '../components/ComponentGraph';
 import Page from '../components/Page';
 
 function DAG() {
-  return <Page name="DAG" desc="Relationships between components" icon={faDiagramProject} />;
+  return (
+    <Page name="DAG" desc="Relationships between components" icon={faDiagramProject}>
+      <ComponentGraph />
+    </Page>
+  );
 }
 
 export default DAG;
