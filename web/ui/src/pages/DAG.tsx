5c50efa6b6806f319ba45f132be6d43da75e1c84
Robert Fratto <robertfratto@gmail.com>
2022-08-18 14:00:34 -0400

    hook in API for component list and dag

--- web/ui/src/pages/DAG.tsx
+++ web/ui/src/pages/DAG.tsx
@@ -1,11 +1,33 @@
 import { faDiagramProject } from '@fortawesome/free-solid-svg-icons';
-import ComponentGraph from '../features/component/ComponentGraph';
+import { useEffect, useState } from 'react';
+import { usePathPrefix } from '../contexts/PathPrefixContext';
+import { ComponentGraph } from '../features/component/ComponentGraph';
+import { ComponentInfo } from '../features/component/types';
 import Page from '../features/layout/Page';
 
 function DAG() {
+  const pathPrefix = usePathPrefix();
+
+  const [components, setComponents] = useState<ComponentInfo[]>([]);
+
+  useEffect(
+    function () {
+      const worker = async () => {
+        const resp = await fetch(pathPrefix + 'api/v0/web/components');
+        const jsonResp = await resp.json();
+
+        console.log(jsonResp);
+        setComponents(jsonResp);
+      };
+
+      worker().catch(console.error);
+    },
+    [pathPrefix]
+  );
+
   return (
-    <Page name="DAG" desc="Relationships between components" icon={faDiagramProject}>
-      <ComponentGraph />
+    <Page name="DAG" desc="Relationships between defined components" icon={faDiagramProject}>
+      {components.length > 0 && <ComponentGraph components={components} />}
     </Page>
   );
 }
