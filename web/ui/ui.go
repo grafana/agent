91e776cbf32d82421a8fdf9a95cbdeaa0044e23d
Robert Fratto <robertfratto@gmail.com>
2022-08-18 17:42:14 -0400

    support embedding UI into binary

--- web/ui/ui.go
+++ web/ui/ui.go
@@ -21,6 +21,12 @@ import (
 // Grafana Agent Flow UI. The UI will be served relative to pathPrefix. If no
 // pathPrefix is specified, the UI will be served at root.
 //
+// By default, the UI is retreived from the ./web/ui/build directory relative
+// to working directory, assuming that the Agent is run from the repo root.
+// However, if the builtinassets Go tag is present, the built UI will be
+// embedded into the binary; run go generate -tags builtinassets for this
+// package to generate the assets to embed.
+//
 // RegisterRoutes catches all requests from pathPrefix and so should only be
 // invoked after all other routes have been registered.
 //
