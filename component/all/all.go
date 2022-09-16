0381c69c251becf0b784dd57d587db83b4ded313
Paschalis Tsilias <tpaschalis@users.noreply.github.com>
2022-09-16 18:50:45 +0300

    flow: use singular noun for prometheus.integrations namespace (#2169)
    Signed-off-by: Paschalis Tsilias <paschalis.tsilias@grafana.com>

--- component/all/all.go
+++ component/all/all.go
@@ -2,12 +2,12 @@
 package all
 
 import (
-	_ "github.com/grafana/agent/component/discovery/kubernetes"                  // Import discovery.kubernetes
-	_ "github.com/grafana/agent/component/discovery/relabel"                     // Import discovery.relabel
-	_ "github.com/grafana/agent/component/local/file"                            // Import local.file
-	_ "github.com/grafana/agent/component/prometheus/integrations/node_exporter" // Import prometheus.integration.node_exporter
-	_ "github.com/grafana/agent/component/prometheus/relabel"                    // Import prometheus.relabel
-	_ "github.com/grafana/agent/component/prometheus/remotewrite"                // Import prometheus.remote_write
-	_ "github.com/grafana/agent/component/prometheus/scrape"                     // Import prometheus.scrape
-	_ "github.com/grafana/agent/component/remote/s3"                             // Import s3.file
+	_ "github.com/grafana/agent/component/discovery/kubernetes"                 // Import discovery.kubernetes
+	_ "github.com/grafana/agent/component/discovery/relabel"                    // Import discovery.relabel
+	_ "github.com/grafana/agent/component/local/file"                           // Import local.file
+	_ "github.com/grafana/agent/component/prometheus/integration/node_exporter" // Import prometheus.integration.node_exporter
+	_ "github.com/grafana/agent/component/prometheus/relabel"                   // Import prometheus.relabel
+	_ "github.com/grafana/agent/component/prometheus/remotewrite"               // Import prometheus.remote_write
+	_ "github.com/grafana/agent/component/prometheus/scrape"                    // Import prometheus.scrape
+	_ "github.com/grafana/agent/component/remote/s3"                            // Import s3.file
 )
