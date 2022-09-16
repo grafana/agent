ab29fd09e1e1b7397cde56ad968962b698e0bc81
Paschalis Tsilias <paschalis.tsilias@grafana.com>
2022-09-16 17:21:19 +0300

    Offset error reporting to label; fix error message to not mention singleton concept
    Signed-off-by: Paschalis Tsilias <paschalis.tsilias@grafana.com>

--- pkg/flow/internal/controller/loader.go
+++ pkg/flow/internal/controller/loader.go
@@ -168,9 +168,9 @@ func (l *Loader) populateGraph(g *dag.Graph, blocks []*ast.BlockStmt) diag.Diagn
 			if registration.Singleton && block.Label != "" {
 				diags.Add(diag.Diagnostic{
 					Severity: diag.SeverityLevelError,
-					Message:  fmt.Sprintf("Singleton component %q must not have a label", componentName),
-					StartPos: block.NamePos.Position(),
-					EndPos:   block.NamePos.Add(len(componentName) - 1).Position(),
+					Message:  fmt.Sprintf("Component %q does not support labels", componentName),
+					StartPos: block.NamePos.Add(len(componentName) + 1).Position(),
+					EndPos:   block.NamePos.Add(len(componentName) + 1).Add(len(block.Label) + 1).Position(),
 				})
 				continue
 			}
