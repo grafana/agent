51dc11e02a59f5440792c420aa398bb21411453d
Robert Fratto <robertfratto@gmail.com>
2022-10-03 10:13:48 -0400

    component/otelcol/receiver: create receiver component abstraction (#2254)
    This commit introduces a new package, component/otelcol/receiver, which
    exposes a generic Flow component implementation which can run
    OpenTelemetry Collector receiver.
    
    Like #2227, it leaves some work unfinished for future PRs:
    
    * A Zap logging adapter needs to be created to correctly process logs
      from OpenTelemetry Collector components.
    * Component-specific metrics are currently ignored.
    * Component-specific traces are currently ignored.
    
    As of this commit, there are no registered `otelcol.receiver.*`
    components. Implementations for OpenTelemetry Collector Flow components
    will be done in future PRs.
    
    Related to #2213.

---
+++ component/otelcol/receiver/receiver.go
@@ -0,0 +1,166 @@
+// Package receiver utilities to create a Flow component from OpenTelemetry
+// Collector receivers.
+package receiver
+
+import (
+	"context"
+	"errors"
+	"os"
+
+	"github.com/grafana/agent/component"
+	"github.com/grafana/agent/component/otelcol"
+	"github.com/grafana/agent/component/otelcol/internal/fanoutconsumer"
+	"github.com/grafana/agent/component/otelcol/internal/scheduler"
+	"github.com/grafana/agent/pkg/build"
+	otelcomponent "go.opentelemetry.io/collector/component"
+	otelconfig "go.opentelemetry.io/collector/config"
+	"go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/trace"
+	"go.uber.org/zap"
+)
+
+// Arguments is an extension of component.Arguments which contains necessary
+// settings for OpenTelemetry Collector receivers.
+type Arguments interface {
+	component.Arguments
+
+	// Convert converts the Arguments into an OpenTelemetry Collector exporter
+	// configuration.
+	Convert() otelconfig.Receiver
+
+	// Extensions returns the set of extensions that the configured component is
+	// allowed to use.
+	Extensions() map[otelconfig.ComponentID]otelcomponent.Extension
+
+	// Exporters returns the set of exporters that are exposed to the configured
+	// component.
+	Exporters() map[otelconfig.DataType]map[otelconfig.ComponentID]otelcomponent.Exporter
+
+	// NextConsumers returns the set of consumers to send data to.
+	NextConsumers() *otelcol.ConsumerArguments
+}
+
+// Receiver is a Flow component shim which manages an OpenTelemetry Collector
+// receiver component.
+type Receiver struct {
+	ctx    context.Context
+	cancel context.CancelFunc
+
+	opts    component.Options
+	factory otelcomponent.ReceiverFactory
+
+	sched *scheduler.Scheduler
+}
+
+var (
+	_ component.Component       = (*Receiver)(nil)
+	_ component.HealthComponent = (*Receiver)(nil)
+)
+
+// New creates a new Flow component which encapsulates an OpenTelemetry
+// Collector receiver. args must hold a value of the argument type registered
+// with the Flow component.
+//
+// If the registered Flow component registers exported fields, it is the
+// responsibility of the caller to export values when needed; the Receiver
+// component never exports any values.
+func New(opts component.Options, f otelcomponent.ReceiverFactory, args Arguments) (*Receiver, error) {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	r := &Receiver{
+		ctx:    ctx,
+		cancel: cancel,
+
+		opts:    opts,
+		factory: f,
+
+		sched: scheduler.New(opts.Logger),
+	}
+	if err := r.Update(args); err != nil {
+		return nil, err
+	}
+	return r, nil
+}
+
+// Run starts the Receiver component.
+func (r *Receiver) Run(ctx context.Context) error {
+	defer r.cancel()
+	return r.sched.Run(ctx)
+}
+
+// Update implements component.Component. It will convert the Arguments into
+// configuration for OpenTelemetry Collector receiver configuration and manage
+// the underlying OpenTelemetry Collector receiver.
+func (r *Receiver) Update(args component.Arguments) error {
+	rargs := args.(Arguments)
+
+	host := scheduler.NewHost(
+		r.opts.Logger,
+		scheduler.WithHostExtensions(rargs.Extensions()),
+		scheduler.WithHostExporters(rargs.Exporters()),
+	)
+
+	settings := otelcomponent.ReceiverCreateSettings{
+		TelemetrySettings: otelcomponent.TelemetrySettings{
+			// TODO(rfratto): create an adapter from zap -> go-kit/log
+			Logger: zap.NewNop(),
+
+			// TODO(rfratto): expose tracing and logging statistics.
+			//
+			// We may want to put off tracing until we have native tracing
+			// instrumentation from Flow, but metrics should come sooner since we're
+			// already set up for supporting component-specific metrics.
+			TracerProvider: trace.NewNoopTracerProvider(),
+			MeterProvider:  metric.NewNoopMeterProvider(),
+		},
+
+		BuildInfo: otelcomponent.BuildInfo{
+			Command:     os.Args[0],
+			Description: "Grafana Agent",
+			Version:     build.Version,
+		},
+	}
+
+	receiverConfig := rargs.Convert()
+
+	var (
+		next        = rargs.NextConsumers()
+		nextTraces  = fanoutconsumer.Traces(next.Traces)
+		nextMetrics = fanoutconsumer.Metrics(next.Metrics)
+		nextLogs    = fanoutconsumer.Logs(next.Logs)
+	)
+
+	// Create instances of the receiver from our factory for each of our
+	// supported telemetry signals.
+	var components []otelcomponent.Component
+
+	tracesReceiver, err := r.factory.CreateTracesReceiver(r.ctx, settings, receiverConfig, nextTraces)
+	if err != nil && !errors.Is(err, otelcomponent.ErrDataTypeIsNotSupported) {
+		return err
+	} else if tracesReceiver != nil {
+		components = append(components, tracesReceiver)
+	}
+
+	metricsReceiver, err := r.factory.CreateMetricsReceiver(r.ctx, settings, receiverConfig, nextMetrics)
+	if err != nil && !errors.Is(err, otelcomponent.ErrDataTypeIsNotSupported) {
+		return err
+	} else if metricsReceiver != nil {
+		components = append(components, metricsReceiver)
+	}
+
+	logsReceiver, err := r.factory.CreateLogsReceiver(r.ctx, settings, receiverConfig, nextLogs)
+	if err != nil && !errors.Is(err, otelcomponent.ErrDataTypeIsNotSupported) {
+		return err
+	} else if logsReceiver != nil {
+		components = append(components, logsReceiver)
+	}
+
+	// Schedule the components to run once our component is running.
+	r.sched.Schedule(host, components...)
+	return nil
+}
+
+// CurrentHealth implements component.HealthComponent.
+func (r *Receiver) CurrentHealth() component.Health {
+	return r.sched.CurrentHealth()
+}
