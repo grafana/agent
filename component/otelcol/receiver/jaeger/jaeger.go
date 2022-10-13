9d58218aa3c11ee92748d8247dd06177f6bcf093
Robert Fratto <robertfratto@gmail.com>
2022-10-13 11:28:02 -0400

    otelcol.receiver.jaeger: new component
    Introduce an `otelcol.receiver.jaeger` component which wraps around the
    otelcol-contrib jaeger reciever.
    
    Closes #2290.

---
+++ component/otelcol/receiver/jaeger/jaeger.go
@@ -0,0 +1,205 @@
+// Package jaeger provides an otelcol.receiver.jaeger component.
+package jaeger
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/alecthomas/units"
+	"github.com/grafana/agent/component"
+	"github.com/grafana/agent/component/otelcol"
+	"github.com/grafana/agent/component/otelcol/receiver"
+	"github.com/grafana/agent/pkg/river"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/jaegerreceiver"
+	otelcomponent "go.opentelemetry.io/collector/component"
+	otelconfig "go.opentelemetry.io/collector/config"
+)
+
+func init() {
+	component.Register(component.Registration{
+		Name: "otelcol.receiver.jaeger",
+		Args: Arguments{},
+
+		Build: func(opts component.Options, args component.Arguments) (component.Component, error) {
+			fact := jaegerreceiver.NewFactory()
+			return receiver.New(opts, fact, args.(Arguments))
+		},
+	})
+}
+
+// Arguments configures the otelcol.receiver.jaeger component.
+type Arguments struct {
+	Protocols      ProtocolsArguments       `river:"protocols,block"`
+	RemoteSampling *RemoteSamplingArguments `river:"remote_sampling,block,optional"`
+
+	// Output configures where to send received data. Required.
+	Output *otelcol.ConsumerArguments `river:"output,block"`
+}
+
+var (
+	_ river.Unmarshaler  = (*Arguments)(nil)
+	_ receiver.Arguments = Arguments{}
+)
+
+// DefaultArguments provides default settings for Arguments. All protocols are
+// configured with defaults and then set to nil in UnmarshalRiver if they were
+// not defined in the source config.
+var DefaultArguments = Arguments{
+	Protocols: ProtocolsArguments{
+		GRPC: &otelcol.GRPCServerArguments{
+			Endpoint:  "0.0.0.0:14250",
+			Transport: "tcp",
+		},
+		ThriftHTTP: &otelcol.HTTPServerArguments{
+			Endpoint: "0.0.0.0:14268",
+		},
+		ThriftBinary: &ProtocolUDP{
+			Endpoint:      "0.0.0.0:6831",
+			QueueSize:     1_000,
+			MaxPacketSize: 65 * units.KiB,
+			Workers:       10,
+		},
+		ThriftCompact: &ProtocolUDP{
+			Endpoint:      "0.0.0.0:6832",
+			QueueSize:     1_000,
+			MaxPacketSize: 65 * units.KiB,
+			Workers:       10,
+		},
+	},
+}
+
+// UnmarshalRiver implements river.Unmarshaler.
+func (args *Arguments) UnmarshalRiver(f func(interface{}) error) error {
+	*args = DefaultArguments
+
+	type arguments Arguments
+
+	// Unmarshal into a temporary struct so we can detect which protocols were
+	// actually enabled by the user.
+	var temp arguments
+	if err := f(&temp); err != nil {
+		return err
+	}
+
+	// Remove protocols from args if they weren't provided by the user.
+	if temp.Protocols.GRPC == nil {
+		args.Protocols.GRPC = nil
+	}
+	if temp.Protocols.ThriftHTTP == nil {
+		args.Protocols.ThriftHTTP = nil
+	}
+	if temp.Protocols.ThriftBinary == nil {
+		args.Protocols.ThriftBinary = nil
+	}
+	if temp.Protocols.ThriftCompact == nil {
+		args.Protocols.ThriftCompact = nil
+	}
+
+	// Finally, unmarshal into the real struct.
+	if err := f((*arguments)(args)); err != nil {
+		return err
+	}
+	return args.Validate()
+}
+
+// Validate returns an error if args is invalid.
+func (args *Arguments) Validate() error {
+	if args.Protocols.GRPC == nil &&
+		args.Protocols.ThriftHTTP == nil &&
+		args.Protocols.ThriftBinary == nil &&
+		args.Protocols.ThriftCompact == nil {
+
+		return fmt.Errorf("at least one protocol must be enabled")
+	}
+
+	return nil
+}
+
+// Convert implements receiver.Arguments.
+func (args Arguments) Convert() otelconfig.Receiver {
+	return &jaegerreceiver.Config{
+		ReceiverSettings: otelconfig.NewReceiverSettings(otelconfig.NewComponentID("jaeger")),
+		Protocols: jaegerreceiver.Protocols{
+			GRPC:          args.Protocols.GRPC.Convert(),
+			ThriftHTTP:    args.Protocols.ThriftHTTP.Convert(),
+			ThriftBinary:  args.Protocols.ThriftBinary.Convert(),
+			ThriftCompact: args.Protocols.ThriftCompact.Convert(),
+		},
+		RemoteSampling: args.RemoteSampling.Convert(),
+	}
+}
+
+// Extensions implements receiver.Arguments.
+func (args Arguments) Extensions() map[otelconfig.ComponentID]otelcomponent.Extension {
+	if args.RemoteSampling == nil {
+		return nil
+	}
+	return args.RemoteSampling.Client.Extensions()
+}
+
+// Exporters implements receiver.Arguments.
+func (args Arguments) Exporters() map[otelconfig.DataType]map[otelconfig.ComponentID]otelcomponent.Exporter {
+	return nil
+}
+
+// NextConsumers implements receiver.Arguments.
+func (args Arguments) NextConsumers() *otelcol.ConsumerArguments {
+	return args.Output
+}
+
+// ProtocolsArguments configures protocols for otelcol.receiver.jaeger to
+// listen on.
+type ProtocolsArguments struct {
+	GRPC          *otelcol.GRPCServerArguments `river:"grpc,block,optional"`
+	ThriftHTTP    *otelcol.HTTPServerArguments `river:"thrift_http,block,optional"`
+	ThriftBinary  *ProtocolUDP                 `river:"thrift_binary,block,optional"`
+	ThriftCompact *ProtocolUDP                 `river:"thrift_compact,block,optional"`
+}
+
+// ProtocolUDP configures a UDP server.
+type ProtocolUDP struct {
+	Endpoint         string           `river:"endpoint,attr,optional"`
+	QueueSize        int              `river:"queue_size,attr,optional"`
+	MaxPacketSize    units.Base2Bytes `river:"max_packet_size,attr,optional"`
+	Workers          int              `river:"workers,attr,optional"`
+	SocketBufferSize units.Base2Bytes `river:"socket_buffer_size,attr,optional"`
+}
+
+// Convert converts proto into the upstream type.
+func (proto *ProtocolUDP) Convert() *jaegerreceiver.ProtocolUDP {
+	if proto == nil {
+		return nil
+	}
+
+	return &jaegerreceiver.ProtocolUDP{
+		Endpoint: proto.Endpoint,
+		ServerConfigUDP: jaegerreceiver.ServerConfigUDP{
+			QueueSize:        proto.QueueSize,
+			MaxPacketSize:    int(proto.MaxPacketSize),
+			Workers:          proto.Workers,
+			SocketBufferSize: int(proto.SocketBufferSize),
+		},
+	}
+}
+
+// RemoteSamplingArguments configures remote sampling settings.
+type RemoteSamplingArguments struct {
+	HostEndpoint               string                      `river:"host_endpoint,attr"`
+	StrategyFile               string                      `river:"strategy_file,attr"`
+	StrategyFileReloadInterval time.Duration               `river:"strategy_file_reload_interval,attr"`
+	Client                     otelcol.GRPCClientArguments `river:"client,block"`
+}
+
+// Convert converts args into the upstream type.
+func (args *RemoteSamplingArguments) Convert() *jaegerreceiver.RemoteSamplingConfig {
+	if args == nil {
+		return nil
+	}
+
+	return &jaegerreceiver.RemoteSamplingConfig{
+		HostEndpoint:               args.HostEndpoint,
+		StrategyFile:               args.StrategyFile,
+		StrategyFileReloadInterval: args.StrategyFileReloadInterval,
+		GRPCClientSettings:         *args.Client.Convert(),
+	}
+}
---
+++ component/otelcol/receiver/jaeger/jaeger_test.go
@@ -0,0 +1,120 @@
+package jaeger_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/grafana/agent/component/otelcol/receiver/jaeger"
+	"github.com/grafana/agent/pkg/flow/componenttest"
+	"github.com/grafana/agent/pkg/river"
+	"github.com/grafana/agent/pkg/util"
+	"github.com/phayes/freeport"
+	"github.com/stretchr/testify/require"
+)
+
+// Test ensures that otelcol.receiver.jaeger can start successfully.
+func Test(t *testing.T) {
+	httpAddr := getFreeAddr(t)
+
+	ctx := componenttest.TestContext(t)
+	l := util.TestLogger(t)
+
+	ctrl, err := componenttest.NewControllerFromID(l, "otelcol.receiver.jaeger")
+	require.NoError(t, err)
+
+	cfg := fmt.Sprintf(`
+		protocols {
+			grpc {
+				endpoint = "%s"
+			}
+		}
+
+		output { /* no-op */ }
+	`, httpAddr)
+	var args jaeger.Arguments
+	require.NoError(t, river.Unmarshal([]byte(cfg), &args))
+
+	go func() {
+		err := ctrl.Run(ctx, args)
+		require.NoError(t, err)
+	}()
+
+	require.NoError(t, ctrl.WaitRunning(time.Second))
+
+	// TODO(rfratto): is it worthwhile trying to make sure we can send data over
+	// the client or can we trust that getting the component to run successfully
+	// is enough?
+	time.Sleep(100 * time.Millisecond)
+}
+
+func TestArguments_UnmarshalRiver(t *testing.T) {
+	t.Run("grpc", func(t *testing.T) {
+		in := `
+			protocols { grpc {} }
+			output {}
+		`
+
+		var args jaeger.Arguments
+		require.NoError(t, river.Unmarshal([]byte(in), &args))
+
+		require.Equal(t, jaeger.DefaultArguments.Protocols.GRPC, args.Protocols.GRPC)
+		require.Nil(t, args.Protocols.ThriftHTTP)
+		require.Nil(t, args.Protocols.ThriftBinary)
+		require.Nil(t, args.Protocols.ThriftCompact)
+	})
+
+	t.Run("thrift_http", func(t *testing.T) {
+		in := `
+			protocols { thrift_http {} }
+			output {} 
+		`
+
+		var args jaeger.Arguments
+		require.NoError(t, river.Unmarshal([]byte(in), &args))
+
+		require.Nil(t, args.Protocols.GRPC)
+		require.Equal(t, jaeger.DefaultArguments.Protocols.ThriftHTTP, args.Protocols.ThriftHTTP)
+		require.Nil(t, args.Protocols.ThriftBinary)
+		require.Nil(t, args.Protocols.ThriftCompact)
+	})
+
+	t.Run("thrift_binary", func(t *testing.T) {
+		in := `
+			protocols { thrift_binary {} }
+			output {}
+		`
+
+		var args jaeger.Arguments
+		require.NoError(t, river.Unmarshal([]byte(in), &args))
+
+		require.Nil(t, args.Protocols.GRPC)
+		require.Nil(t, args.Protocols.ThriftHTTP)
+		require.Equal(t, jaeger.DefaultArguments.Protocols.ThriftBinary, args.Protocols.ThriftBinary)
+		require.Nil(t, args.Protocols.ThriftCompact)
+	})
+
+	t.Run("thrift_compact", func(t *testing.T) {
+		in := `
+			protocols { thrift_compact {} }
+			output {}
+		`
+
+		var args jaeger.Arguments
+		require.NoError(t, river.Unmarshal([]byte(in), &args))
+
+		require.Nil(t, args.Protocols.GRPC)
+		require.Nil(t, args.Protocols.ThriftHTTP)
+		require.Nil(t, args.Protocols.ThriftBinary)
+		require.Equal(t, jaeger.DefaultArguments.Protocols.ThriftCompact, args.Protocols.ThriftCompact)
+	})
+}
+
+func getFreeAddr(t *testing.T) string {
+	t.Helper()
+
+	portNumber, err := freeport.GetFreePort()
+	require.NoError(t, err)
+
+	return fmt.Sprintf("localhost:%d", portNumber)
+}
