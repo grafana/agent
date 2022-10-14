8baa37c768bb24b84a2364fabbb3ea211d1545a9
Robert Fratto <robertfratto@gmail.com>
2022-10-14 15:21:36 -0400

    river/internal/value: change representation of objects and maps (#2369)
    This commit changes the representation of objects and maps from
    []interface{} and map[string]interface{} to use []value.Value and
    map[string]value.Value respectively.
    
    This fixes #2362, an issue where interface-based capsule values where
    losing type information and being coerced to a type based on the actual
    value.
    
    This also greatly improves the performance of creating arrays and
    objects, which no longer need to be inidividually copied in a loop:
    
    ```
    name                   old time/op    new time/op    delta
    Object/Non-capsule-10    1.84µs ± 8%    0.00µs ± 0%   -99.89%  (p=0.000 n=19+19)
    Object/Capsule-10        1.82µs ± 4%    0.00µs ± 0%   -99.89%  (p=0.000 n=20+19)
    Array/Non-capsule-10      389ns ± 2%      19ns ± 2%   -95.20%  (p=0.000 n=19+18)
    Array/Capsule-10          389ns ± 1%      19ns ± 0%   -95.19%  (p=0.000 n=20+20)
    
    name                   old alloc/op   new alloc/op   delta
    Object/Non-capsule-10    1.88kB ± 0%    0.00kB       -100.00%  (p=0.000 n=20+20)
    Object/Capsule-10        1.88kB ± 0%    0.00kB       -100.00%  (p=0.000 n=20+20)
    Array/Non-capsule-10       344B ± 0%       24B ± 0%   -93.02%  (p=0.000 n=20+20)
    Array/Capsule-10           344B ± 0%       24B ± 0%   -93.02%  (p=0.000 n=20+20)
    
    name                   old allocs/op  new allocs/op  delta
    Object/Non-capsule-10      42.0 ± 0%       0.0       -100.00%  (p=0.000 n=20+20)
    Object/Capsule-10          42.0 ± 0%       0.0       -100.00%  (p=0.000 n=20+20)
    Array/Non-capsule-10       2.00 ± 0%      1.00 ± 0%   -50.00%  (p=0.000 n=20+20)
    Array/Capsule-10           2.00 ± 0%      1.00 ± 0%   -50.00%  (p=0.000 n=20+20)
    ```
    
    As part of this change, the logic used for the concat function has been
    greatly simplified. This has seemingly no impact on performance, as the
    benchmark results below show:
    
    ```
    name       old time/op    new time/op    delta
    Concat-10    1.10µs ± 4%    1.09µs ± 1%   ~     (p=0.735 n=18+19)
    
    name       old alloc/op   new alloc/op   delta
    Concat-10    1.30kB ± 0%    1.30kB ± 0%   ~     (all equal)
    
    name       old allocs/op  new allocs/op  delta
    Concat-10      20.0 ± 0%      20.0 ± 0%   ~     (all equal)
    ```

--- pkg/river/internal/stdlib/stdlib.go
+++ pkg/river/internal/stdlib/stdlib.go
@@ -4,12 +4,9 @@ package stdlib
 import (
 	"encoding/json"
 	"os"
-	"reflect"
 
 	"github.com/grafana/agent/pkg/river/internal/value"
 )
-
-var goAny = reflect.TypeOf((*interface{})(nil)).Elem()
 
 // Functions returns the list of stdlib functions by name. The interface{}
 // value is always a River-compatible function value, where functions have at
@@ -53,39 +50,14 @@ var Functions = map[string]interface{}{
 			return args[0], nil
 		}
 
-		// If the imcoming Go slices have the same type, we can have our resulting
-		// slice use the same type. This will allow decoding to use the direct
-		// assignment optimization.
-		//
-		// However, if the types don't match, then we're forced to fall back to
-		// returning []interface{}.
-		//
-		// TODO(rfratto): This could fall back to checking the elements if the
-		// array/slice types don't match. It would be slower, but the direct
-		// assignment optimization probably justifies it.
-		useType := args[0].Reflect().Type()
-		for i := 1; i < len(args); i++ {
-			if args[i].Reflect().Type() != useType {
-				useType = reflect.SliceOf(goAny)
-				break
-			}
-		}
-
-		// Build out the final array.
-		raw := reflect.MakeSlice(useType, finalSize, finalSize)
-
-		var argNum int
+		raw := make([]value.Value, 0, finalSize)
 		for _, arg := range args {
 			for i := 0; i < arg.Len(); i++ {
-				elem := arg.Index(i)
-				if elem.Type() != value.TypeNull {
-					raw.Index(argNum).Set(elem.Reflect())
-				}
-				argNum++
+				raw = append(raw, arg.Index(i))
 			}
 		}
 
-		return value.Encode(raw.Interface()), nil
+		return value.Array(raw...), nil
 	}),
 
 	"json_decode": func(in string) (interface{}, error) {
