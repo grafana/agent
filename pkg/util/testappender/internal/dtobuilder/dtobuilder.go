51ed0fde9355bf402c4600788a798c505c1f15ed
Robert Fratto <robertfratto@gmail.com>
2022-11-02 12:08:57 -0400

    Add utilities for testing code which send data to storage.Appenders (#2445)
    `util/testappender` is a new package which introduces utilities for
    testing code which write metrics to storage.Appender implementations.
    
    `testappender.Appender` implements the `storage.Appender` interface and
    generates a slice of Prometheus `*dto.MetricFamily` upon calling Commit.
    The resulting slice of families can be retrieved through the
    MetricFamilies method.
    
    Then, `testappender.Compare` introduces a utility method to compare a
    set of metric families against an expected string. The string can be
    reprsenting either Prometheus' or OpenMetrics' text exposition format.

---
+++ pkg/util/testappender/internal/dtobuilder/dtobuilder.go
@@ -0,0 +1,408 @@
+package dtobuilder
+
+import (
+	"math"
+	"sort"
+	"strconv"
+	"time"
+
+	dto "github.com/prometheus/client_model/go"
+	"github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/model/exemplar"
+	"github.com/prometheus/prometheus/model/labels"
+	"github.com/prometheus/prometheus/model/metadata"
+	"github.com/prometheus/prometheus/model/textparse"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"k8s.io/utils/pointer"
+)
+
+// Sample represents an individually written sample to a storage.Appender.
+type Sample struct {
+	Labels         labels.Labels
+	Timestamp      int64
+	Value          float64
+	PrintTimestamp bool
+}
+
+// SeriesExemplar represents an individually written exemplar to a
+// storage.Appender.
+type SeriesExemplar struct {
+	// Labels is the labels of the series exposing the exemplar, not the labels
+	// on the exemplar itself.
+	Labels   labels.Labels
+	Exemplar exemplar.Exemplar
+}
+
+// Build converts a series of written samples, exemplars, and metadata into a
+// slice of *dto.MetricFamily.
+func Build(
+	samples map[string]Sample,
+	exemplars map[string]SeriesExemplar,
+	metadata map[string]metadata.Metadata,
+) []*dto.MetricFamily {
+
+	b := builder{
+		Samples:   samples,
+		Exemplars: exemplars,
+		Metadata:  metadata,
+
+		familyLookup: make(map[string]*dto.MetricFamily),
+	}
+	return b.Build()
+}
+
+type builder struct {
+	Samples   map[string]Sample
+	Exemplars map[string]SeriesExemplar
+	Metadata  map[string]metadata.Metadata
+
+	families     []*dto.MetricFamily
+	familyLookup map[string]*dto.MetricFamily
+}
+
+// Build converts the dtoBuilder's Samples, Exemplars, and Metadata into a set
+// of []*dto.MetricFamily.
+func (b *builder) Build() []*dto.MetricFamily {
+	// *dto.MetricFamily represents a set of samples for a given family of
+	// metrics. All metrics with the same __name__ belong to the same family.
+	//
+	// Each *dto.MetricFamily has a set of *dto.Metric, which contain individual
+	// samples within that family. The *dto.Metric is where non-__name__ labels
+	// are kept.
+	//
+	// *dto.Metrics can represent counters, gauges, summaries, histograms, and
+	// untyped values.
+	//
+	// In the case of a summary, the *dto.Metric contains multiple samples,
+	// holding each quantile, the _count, and the _sum. Similarly for histograms,
+	// the *dto.Metric contains each bucket, the _count, and the _sum.
+	//
+	// Because *dto.Metrics for summaries and histograms contain multiple
+	// samples, Build must roll up individually recorded samples into the
+	// appropriate *dto.Metric. See buildMetricsFromSamples for more information.
+
+	// We *must* do things in the following order:
+	//
+	// 1. Populate the families from metadata so we know what fields in
+	//    *dto.Metric to set.
+	// 2. Populate *dto.Metric values from provided samples.
+	// 3. Assign exemplars to *dto.Metrics as appropriate.
+	b.buildFamiliesFromMetadata()
+	b.buildMetricsFromSamples()
+	b.injectExemplars()
+
+	// Sort all the data before returning.
+	sortMetricFamilies(b.families)
+	return b.families
+}
+
+// buildFamiliesFromMetadata populates the list of families based on the
+// metadata known to the dtoBuilder. familyLookup will be updated for all
+// metrics which map to the same family.
+//
+// In the case of summaries and instograms, multiple metrics map to the same
+// family (the bucket/quantile, the _sum, and the _count metrics).
+func (b *builder) buildFamiliesFromMetadata() {
+	for familyName, m := range b.Metadata {
+		mt := textParseToMetricType(m.Type)
+		mf := &dto.MetricFamily{
+			Name: pointer.String(familyName),
+			Type: &mt,
+		}
+		if m.Help != "" {
+			mf.Help = pointer.String(m.Help)
+		}
+
+		b.families = append(b.families, mf)
+
+		// Determine how to populate the lookup table.
+		switch mt {
+		case dto.MetricType_SUMMARY:
+			// Summaries include metrics with the family name (for quantiles),
+			// followed by _sum and _count suffixes.
+			b.familyLookup[familyName] = mf
+			b.familyLookup[familyName+"_sum"] = mf
+			b.familyLookup[familyName+"_count"] = mf
+		case dto.MetricType_HISTOGRAM:
+			// Histograms include metrics for _bucket, _sum, and _count suffixes.
+			b.familyLookup[familyName+"_bucket"] = mf
+			b.familyLookup[familyName+"_sum"] = mf
+			b.familyLookup[familyName+"_count"] = mf
+		default:
+			// Everything else matches the family name exactly.
+			b.familyLookup[familyName] = mf
+		}
+	}
+}
+
+func textParseToMetricType(tp textparse.MetricType) dto.MetricType {
+	switch tp {
+	case textparse.MetricTypeCounter:
+		return dto.MetricType_COUNTER
+	case textparse.MetricTypeGauge:
+		return dto.MetricType_GAUGE
+	case textparse.MetricTypeHistogram:
+		return dto.MetricType_HISTOGRAM
+	case textparse.MetricTypeSummary:
+		return dto.MetricType_SUMMARY
+	default:
+		// There are other values for m.Type, but they're all
+		// OpenMetrics-specific and we're only converting into the Prometheus
+		// exposition format.
+		return dto.MetricType_UNTYPED
+	}
+}
+
+// buildMetricsFromSamples populates *dto.Metrics. If the MetricFamily doesn't
+// exist for a given sample, a new one is created.
+func (b *builder) buildMetricsFromSamples() {
+	for _, sample := range b.Samples {
+		// Get or create the metric family.
+		metricName := sample.Labels.Get(model.MetricNameLabel)
+		mf := b.getOrCreateMetricFamily(metricName)
+
+		// Retrieve the *dto.Metric based on labels.
+		m := getOrCreateMetric(mf, sample.Labels)
+		if sample.PrintTimestamp {
+			m.TimestampMs = pointer.Int64(sample.Timestamp)
+		}
+
+		switch familyType(mf) {
+		case dto.MetricType_COUNTER:
+			m.Counter = &dto.Counter{
+				Value: pointer.Float64(sample.Value),
+			}
+
+		case dto.MetricType_GAUGE:
+			m.Gauge = &dto.Gauge{
+				Value: pointer.Float64(sample.Value),
+			}
+
+		case dto.MetricType_SUMMARY:
+			if m.Summary == nil {
+				m.Summary = &dto.Summary{}
+			}
+
+			switch {
+			case metricName == mf.GetName()+"_count":
+				val := uint64(sample.Value)
+				m.Summary.SampleCount = &val
+			case metricName == mf.GetName()+"_sum":
+				m.Summary.SampleSum = pointer.Float64(sample.Value)
+			case metricName == mf.GetName():
+				quantile, err := strconv.ParseFloat(sample.Labels.Get(model.QuantileLabel), 64)
+				if err != nil {
+					continue
+				}
+
+				m.Summary.Quantile = append(m.Summary.Quantile, &dto.Quantile{
+					Quantile: &quantile,
+					Value:    pointer.Float64(sample.Value),
+				})
+			}
+
+		case dto.MetricType_UNTYPED:
+			m.Untyped = &dto.Untyped{
+				Value: pointer.Float64(sample.Value),
+			}
+
+		case dto.MetricType_HISTOGRAM:
+			if m.Histogram == nil {
+				m.Histogram = &dto.Histogram{}
+			}
+
+			switch {
+			case metricName == mf.GetName()+"_count":
+				val := uint64(sample.Value)
+				m.Histogram.SampleCount = &val
+			case metricName == mf.GetName()+"_sum":
+				m.Histogram.SampleSum = pointer.Float64(sample.Value)
+			case metricName == mf.GetName()+"_bucket":
+				boundary, err := strconv.ParseFloat(sample.Labels.Get(model.BucketLabel), 64)
+				if err != nil {
+					continue
+				}
+
+				count := uint64(sample.Value)
+
+				m.Histogram.Bucket = append(m.Histogram.Bucket, &dto.Bucket{
+					UpperBound:      &boundary,
+					CumulativeCount: &count,
+				})
+			}
+		}
+	}
+}
+
+func (b *builder) getOrCreateMetricFamily(familyName string) *dto.MetricFamily {
+	mf, ok := b.familyLookup[familyName]
+	if ok {
+		return mf
+	}
+
+	mt := dto.MetricType_UNTYPED
+	mf = &dto.MetricFamily{
+		Name: &familyName,
+		Type: &mt,
+	}
+	b.families = append(b.families, mf)
+	b.familyLookup[familyName] = mf
+	return mf
+}
+
+func getOrCreateMetric(mf *dto.MetricFamily, l labels.Labels) *dto.Metric {
+	metricLabels := toLabelPairs(familyType(mf), l)
+
+	for _, check := range mf.Metric {
+		if labelPairsEqual(check.Label, metricLabels) {
+			return check
+		}
+	}
+
+	m := &dto.Metric{
+		Label: metricLabels,
+	}
+	mf.Metric = append(mf.Metric, m)
+	return m
+}
+
+// toLabelPairs converts labels.Labels into []*dto.LabelPair. The __name__
+// label is always dropped, since the metric name is retrieved from the family
+// name instead.
+//
+// The quantile label is dropped for summaries, and the le label is dropped for
+// histograms.
+func toLabelPairs(mt dto.MetricType, ls labels.Labels) []*dto.LabelPair {
+	res := make([]*dto.LabelPair, 0, len(ls))
+	for _, l := range ls {
+		if l.Name == model.MetricNameLabel {
+			continue
+		} else if l.Name == model.QuantileLabel && mt == dto.MetricType_SUMMARY {
+			continue
+		} else if l.Name == model.BucketLabel && mt == dto.MetricType_HISTOGRAM {
+			continue
+		}
+
+		res = append(res, &dto.LabelPair{
+			Name:  pointer.String(l.Name),
+			Value: pointer.String(l.Value),
+		})
+	}
+
+	sort.Slice(res, func(i, j int) bool {
+		switch {
+		case *res[i].Name < *res[j].Name:
+			return true
+		case *res[i].Value < *res[j].Value:
+			return true
+		default:
+			return false
+		}
+	})
+	return res
+}
+
+func labelPairsEqual(a, b []*dto.LabelPair) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := 0; i < len(a); i++ {
+		if *a[i].Name != *b[i].Name || *a[i].Value != *b[i].Value {
+			return false
+		}
+	}
+
+	return true
+}
+
+func familyType(mf *dto.MetricFamily) dto.MetricType {
+	ty := mf.Type
+	if ty == nil {
+		return dto.MetricType_UNTYPED
+	}
+	return *ty
+}
+
+// injectExemplars populates the exemplars in the various *dto.Metric
+// instances. Exemplars are ignored if the parent *dto.MetricFamily doesn't
+// support exeplars based on metric type.
+func (b *builder) injectExemplars() {
+	for _, e := range b.Exemplars {
+		// Get or create the metric family.
+		exemplarName := e.Labels.Get(model.MetricNameLabel)
+
+		mf, ok := b.familyLookup[exemplarName]
+		if !ok {
+			// No metric family, which means no corresponding sample; ignore.
+			continue
+		}
+
+		m := getMetric(mf, e.Labels)
+		if m == nil {
+			continue
+		}
+
+		// Only counters and histograms support exemplars.
+		switch familyType(mf) {
+		case dto.MetricType_COUNTER:
+			if m.Counter == nil {
+				// Sample never added; ignore.
+				continue
+			}
+			m.Counter.Exemplar = convertExemplar(dto.MetricType_COUNTER, e.Exemplar)
+		case dto.MetricType_HISTOGRAM:
+			if m.Histogram == nil {
+				// Sample never added; ignore.
+				continue
+			}
+
+			switch {
+			case exemplarName == mf.GetName()+"_bucket":
+				boundary, err := strconv.ParseFloat(e.Labels.Get(model.BucketLabel), 64)
+				if err != nil {
+					continue
+				}
+				bucket := findBucket(m.Histogram, boundary)
+				if bucket == nil {
+					continue
+				}
+				bucket.Exemplar = convertExemplar(dto.MetricType_HISTOGRAM, e.Exemplar)
+			}
+		}
+	}
+}
+
+func getMetric(mf *dto.MetricFamily, l labels.Labels) *dto.Metric {
+	metricLabels := toLabelPairs(familyType(mf), l)
+
+	for _, check := range mf.Metric {
+		if labelPairsEqual(check.Label, metricLabels) {
+			return check
+		}
+	}
+
+	return nil
+}
+
+func convertExemplar(mt dto.MetricType, e exemplar.Exemplar) *dto.Exemplar {
+	res := &dto.Exemplar{
+		Label: toLabelPairs(mt, e.Labels),
+		Value: &e.Value,
+	}
+	if e.HasTs {
+		res.Timestamp = timestamppb.New(time.UnixMilli(e.Ts))
+	}
+	return res
+}
+
+func findBucket(h *dto.Histogram, bound float64) *dto.Bucket {
+	for _, b := range h.GetBucket() {
+		// If it's close enough, use the bucket.
+		if math.Abs(b.GetUpperBound()-bound) < 1e-9 {
+			return b
+		}
+	}
+
+	return nil
+}
---
+++ pkg/util/testappender/testappender_test.go
@@ -0,0 +1,298 @@
+package testappender_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/grafana/agent/pkg/util/testappender"
+	"github.com/prometheus/prometheus/model/exemplar"
+	"github.com/prometheus/prometheus/model/labels"
+	"github.com/prometheus/prometheus/model/metadata"
+	"github.com/prometheus/prometheus/model/textparse"
+	"github.com/stretchr/testify/require"
+)
+
+func Example() {
+	var app testappender.Appender
+	app.Append(0, labels.FromStrings("__name__", "example_metric", "foo", "bar"), 60, 1234)
+	app.UpdateMetadata(0, labels.FromStrings("__name__", "example_metric"), metadata.Metadata{
+		Type: textparse.MetricTypeGauge,
+	})
+
+	expect := `
+		# TYPE example_metric gauge
+		example_metric{foo="bar"} 1234 60
+	`
+
+	_ = app.Commit()
+	families, _ := app.MetricFamilies()
+
+	err := testappender.Compare(families, expect)
+	if err != nil {
+		fmt.Println("Metrics do not match:", err)
+	} else {
+		fmt.Println("Metrics match!")
+	}
+	// Output: Metrics match!
+}
+
+// TestAppender_NoOp asserts that not doing anything results in no data.
+func TestAppender_NoOp(t *testing.T) {
+	var app testappender.Appender
+	requireAppenderData(t, &app, "", false)
+}
+
+func TestAppender_Metadata(t *testing.T) {
+	t.Run("No metadata", func(t *testing.T) {
+		var app testappender.Appender
+		app.Append(0, labels.FromStrings("__name__", "example_metric", "foo", "bar"), 60, 1234)
+
+		expect := `
+		# TYPE example_metric untyped
+		example_metric{foo="bar"} 1234 60
+	`
+		requireAppenderData(t, &app, expect, false)
+	})
+
+	t.Run("Has metadata", func(t *testing.T) {
+		var app testappender.Appender
+		app.Append(0, labels.FromStrings("__name__", "example_metric", "foo", "bar"), 60, 1234)
+		app.UpdateMetadata(0, labels.FromStrings("__name__", "example_metric"), metadata.Metadata{
+			Type: textparse.MetricTypeGauge,
+			Help: "example metric",
+		})
+
+		expect := `
+		# HELP example_metric example metric
+		# TYPE example_metric gauge
+		example_metric{foo="bar"} 1234 60
+		`
+		requireAppenderData(t, &app, expect, false)
+	})
+
+	t.Run("No help field", func(t *testing.T) {
+		var app testappender.Appender
+		app.Append(0, labels.FromStrings("__name__", "example_metric", "foo", "bar"), 60, 1234)
+		app.UpdateMetadata(0, labels.FromStrings("__name__", "example_metric"), metadata.Metadata{
+			Type: textparse.MetricTypeGauge,
+		})
+
+		expect := `
+		# TYPE example_metric gauge
+		example_metric{foo="bar"} 1234 60
+		`
+		requireAppenderData(t, &app, expect, false)
+	})
+}
+
+func TestAppender_Types(t *testing.T) {
+	t.Run("Untyped", func(t *testing.T) {
+		var app testappender.Appender
+		app.Append(0, labels.FromStrings("__name__", "example_metric", "foo", "bar"), 60, 1234)
+		app.UpdateMetadata(0, labels.FromStrings("__name__", "example_metric"), metadata.Metadata{
+			Type: textparse.MetricTypeUnknown,
+		})
+
+		expect := `
+		# TYPE example_metric untyped
+		example_metric{foo="bar"} 1234 60
+	`
+		requireAppenderData(t, &app, expect, false)
+	})
+
+	t.Run("Counter", func(t *testing.T) {
+		var app testappender.Appender
+		app.Append(0, labels.FromStrings("__name__", "example_metric", "foo", "bar"), 60, 1234)
+		app.UpdateMetadata(0, labels.FromStrings("__name__", "example_metric"), metadata.Metadata{
+			Type: textparse.MetricTypeCounter,
+		})
+
+		expect := `
+		# TYPE example_metric counter
+		example_metric{foo="bar"} 1234 60
+	`
+		requireAppenderData(t, &app, expect, false)
+	})
+
+	t.Run("Gauge", func(t *testing.T) {
+		var app testappender.Appender
+		app.Append(0, labels.FromStrings("__name__", "example_metric", "foo", "bar"), 60, 1234)
+		app.UpdateMetadata(0, labels.FromStrings("__name__", "example_metric"), metadata.Metadata{
+			Type: textparse.MetricTypeGauge,
+		})
+
+		expect := `
+		# TYPE example_metric gauge
+		example_metric{foo="bar"} 1234 60
+	`
+		requireAppenderData(t, &app, expect, false)
+	})
+
+	t.Run("Summary", func(t *testing.T) {
+		var app testappender.Appender
+
+		// Summaries have quantilies from 0 to 1, counts, and sums. Append the
+		// metadata first and then append all the various samples.
+		app.UpdateMetadata(0, labels.FromStrings("__name__", "example_metric"), metadata.Metadata{
+			Type: textparse.MetricTypeSummary,
+		})
+
+		app.Append(0, labels.FromStrings("__name__", "example_metric", "foo", "bar", "quantile", "0"), 10, 10)
+		app.Append(0, labels.FromStrings("__name__", "example_metric", "foo", "bar", "quantile", "0.25"), 10, 10)
+		app.Append(0, labels.FromStrings("__name__", "example_metric", "foo", "bar", "quantile", "0.50"), 10, 10)
+		app.Append(0, labels.FromStrings("__name__", "example_metric", "foo", "bar", "quantile", "0.75"), 10, 10)
+		app.Append(0, labels.FromStrings("__name__", "example_metric", "foo", "bar", "quantile", "1"), 10, 10)
+		app.Append(0, labels.FromStrings("__name__", "example_metric_count", "foo", "bar"), 10, 5)
+		app.Append(0, labels.FromStrings("__name__", "example_metric_sum", "foo", "bar"), 10, 50)
+
+		expect := `
+		# TYPE example_metric summary
+		example_metric{foo="bar",quantile="0"} 10 10
+		example_metric{foo="bar",quantile="0.25"} 10 10
+		example_metric{foo="bar",quantile="0.5"} 10 10
+		example_metric{foo="bar",quantile="0.75"} 10 10
+		example_metric{foo="bar",quantile="1"} 10 10
+		example_metric_sum{foo="bar"} 50 10
+		example_metric_count{foo="bar"} 5 10
+	`
+		requireAppenderData(t, &app, expect, false)
+	})
+
+	t.Run("Histogram", func(t *testing.T) {
+		var app testappender.Appender
+
+		// Histograms have buckets, counts, and sums. Append the metadata first and
+		// then append all the various samples.
+		app.UpdateMetadata(0, labels.FromStrings("__name__", "example_metric"), metadata.Metadata{
+			Type: textparse.MetricTypeHistogram,
+		})
+
+		app.Append(0, labels.FromStrings("__name__", "example_metric_bucket", "foo", "bar", "le", "0.5"), 10, 10)
+		app.Append(0, labels.FromStrings("__name__", "example_metric_bucket", "foo", "bar", "le", "0.75"), 10, 20)
+		app.Append(0, labels.FromStrings("__name__", "example_metric_bucket", "foo", "bar", "le", "1"), 10, 30)
+		app.Append(0, labels.FromStrings("__name__", "example_metric_bucket", "foo", "bar", "le", "+Inf"), 10, 40)
+		app.Append(0, labels.FromStrings("__name__", "example_metric_count", "foo", "bar"), 10, 4)
+		app.Append(0, labels.FromStrings("__name__", "example_metric_sum", "foo", "bar"), 10, 100)
+
+		expect := `
+		# TYPE example_metric histogram
+		example_metric_bucket{foo="bar",le="0.5"} 10 10
+		example_metric_bucket{foo="bar",le="0.75"} 20 10
+		example_metric_bucket{foo="bar",le="1"} 30 10
+		example_metric_bucket{foo="bar",le="+Inf"} 40 10
+		example_metric_sum{foo="bar"} 100 10
+		example_metric_count{foo="bar"} 4 10
+	`
+		requireAppenderData(t, &app, expect, false)
+	})
+}
+
+func TestAppender_Exemplars(t *testing.T) {
+	// These tests are the only tests where we expliclty test the OpenMetrics
+	// exposition format since OpenMetrics is the only text exposition format
+	// that supports exemplars.
+
+	t.Run("Counter", func(t *testing.T) {
+		var app testappender.Appender
+		app.Append(0, labels.FromStrings("__name__", "example_metric_total", "foo", "bar"), 60, 1234)
+		app.UpdateMetadata(0, labels.FromStrings("__name__", "example_metric_total"), metadata.Metadata{
+			Type: textparse.MetricTypeCounter,
+		})
+		app.AppendExemplar(0, labels.FromStrings("__name__", "example_metric_total", "foo", "bar"), exemplar.Exemplar{
+			Labels: labels.FromStrings("hello", "world"),
+			Value:  1337,
+			Ts:     30,
+			HasTs:  true,
+		})
+
+		expect := `
+		# TYPE example_metric counter
+		example_metric_total{foo="bar"} 1234.0 0.06 # {hello="world"} 1337.0 0.03
+		`
+		requireAppenderData(t, &app, expect, true)
+	})
+
+	t.Run("Histogram", func(t *testing.T) {
+		var app testappender.Appender
+
+		// Histograms have buckets, counts, and sums. Append the metadata first and
+		// then append all the various samples.
+		app.UpdateMetadata(0, labels.FromStrings("__name__", "example_metric"), metadata.Metadata{
+			Type: textparse.MetricTypeHistogram,
+		})
+
+		app.Append(0, labels.FromStrings("__name__", "example_metric_bucket", "foo", "bar", "le", "0.5"), 10, 10)
+		app.Append(0, labels.FromStrings("__name__", "example_metric_bucket", "foo", "bar", "le", "0.75"), 10, 20)
+		app.Append(0, labels.FromStrings("__name__", "example_metric_bucket", "foo", "bar", "le", "1"), 10, 30)
+		app.Append(0, labels.FromStrings("__name__", "example_metric_bucket", "foo", "bar", "le", "+Inf"), 10, 40)
+		app.Append(0, labels.FromStrings("__name__", "example_metric_count", "foo", "bar"), 10, 4)
+		app.Append(0, labels.FromStrings("__name__", "example_metric_sum", "foo", "bar"), 10, 100)
+
+		// Examplars must be attached to a specific bucket.
+		app.AppendExemplar(0, labels.FromStrings("__name__", "example_metric_bucket", "foo", "bar", "le", "0.75"), exemplar.Exemplar{
+			Labels: labels.FromStrings("hello", "world"),
+			Value:  1337,
+			Ts:     30,
+			HasTs:  true,
+		})
+
+		expect := `
+		# TYPE example_metric histogram
+		example_metric_bucket{foo="bar",le="0.5"} 10 0.01
+		example_metric_bucket{foo="bar",le="0.75"} 20 0.01 # {hello="world"} 1337.0 0.03
+		example_metric_bucket{foo="bar",le="1.0"} 30 0.01
+		example_metric_bucket{foo="bar",le="+Inf"} 40 0.01
+		example_metric_sum{foo="bar"} 100.0 0.01
+		example_metric_count{foo="bar"} 4 0.01
+	`
+		requireAppenderData(t, &app, expect, true)
+	})
+}
+
+// TestAppender_MultipleMetrics tests that multiple metrics, where some have
+// metadata and others don't, works as expected.
+func TestAppender_MultipleMetrics(t *testing.T) {
+	var app testappender.Appender
+
+	app.UpdateMetadata(0, labels.FromStrings("__name__", "example_metric_a"), metadata.Metadata{
+		Type: textparse.MetricTypeCounter,
+	})
+	app.UpdateMetadata(0, labels.FromStrings("__name__", "example_metric_b"), metadata.Metadata{
+		Type: textparse.MetricTypeGauge,
+	})
+
+	app.Append(0, labels.FromStrings("__name__", "example_metric_a", "foo", "bar"), 10, 10)
+	app.Append(0, labels.FromStrings("__name__", "example_metric_a", "foo", "rab"), 10, 20)
+	app.Append(0, labels.FromStrings("__name__", "example_metric_b", "fizz", "buzz"), 10, 30)
+	app.Append(0, labels.FromStrings("__name__", "example_metric_b", "fizz", "zzub"), 10, 40)
+	app.Append(0, labels.FromStrings("__name__", "example_metric_c", "hello", "world"), 10, 50)
+	app.Append(0, labels.FromStrings("__name__", "example_metric_c", "hello", "dlrow"), 10, 60)
+
+	expect := `
+		# TYPE example_metric_a counter
+		example_metric_a{foo="bar"} 10 10
+		example_metric_a{foo="rab"} 20 10
+		# TYPE example_metric_b gauge
+		example_metric_b{fizz="buzz"} 30 10
+		example_metric_b{fizz="zzub"} 40 10
+		# TYPE example_metric_c untyped
+		example_metric_c{hello="dlrow"} 60 10
+		example_metric_c{hello="world"} 50 10
+	`
+	requireAppenderData(t, &app, expect, false)
+}
+
+// requireAppenderData commits the appender and asserts that its resulting data
+// matches the Prometheus Exposition Format string specified by expect.
+func requireAppenderData(t *testing.T, app *testappender.Appender, expect string, openMetrics bool) {
+	t.Helper()
+
+	require.NoError(t, app.Commit(), "commit should not have failed")
+
+	families, err := app.MetricFamilies()
+	require.NoError(t, err, "failed to get metric families")
+
+	var c testappender.Comparer
+	c.OpenMetrics = openMetrics
+	require.NoError(t, c.Compare(families, expect))
+}
