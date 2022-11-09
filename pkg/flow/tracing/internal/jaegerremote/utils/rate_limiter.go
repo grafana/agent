867f3a31352800b7e8970f0650d0c74b80983d44
Robert Fratto <robertfratto@gmail.com>
2022-11-09 15:58:32 -0500

    disable linter checks in fork

--- pkg/flow/tracing/internal/jaegerremote/utils/rate_limiter.go
+++ pkg/flow/tracing/internal/jaegerremote/utils/rate_limiter.go
@@ -14,7 +14,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package utils // import "go.opentelemetry.io/contrib/samplers/jaegerremote/internal/utils"
+//nolint:all
+package utils
 
 import (
 	"sync"
