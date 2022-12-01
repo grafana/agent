2a9c2a2f4d14e68c8dd1131f4449f8b17f21216d
Mischa Thompson <mischabear@gmail.com>
2022-12-01 15:24:46 -0800

    Make yaml comparison generic and move to utils

--- pkg/util/compare_yaml.go
+++ pkg/util/compare_yaml.go
@@ -2,6 +2,8 @@ package util
 
 import (
 	"bytes"
+	"fmt"
+	"io"
 
 	"gopkg.in/yaml.v2"
 )
@@ -18,4 +20,36 @@ func CompareYAML(a, b interface{}) bool {
 		return false
 	}
 	return bytes.Equal(aBytes, bBytes)
+}
+
+// CompareYAMLWithHook marshals both a and b to YAML and checks the results
+// for equality, allowing for a hook to define custom marshaling behavior.
+// If either Marshal fails, CompareYAMLWithHook returns false.
+func CompareYAMLWithHook(a, b interface{}, hook func(in interface{}) (ok bool, out interface{}, err error)) bool {
+	aBytes, err := marshalWithHook(a, hook)
+	aString := string(aBytes)
+	fmt.Println(aString)
+	if err != nil {
+		return false
+	}
+	bBytes, err := marshalWithHook(b, hook)
+	bString := string(bBytes)
+	fmt.Println(bString)
+	if err != nil {
+		return false
+	}
+	return bytes.Equal(aBytes, bBytes)
+}
+
+func marshalWithHook(i interface{}, hook func(in interface{}) (ok bool, out interface{}, err error)) ([]byte, error) {
+	var buf bytes.Buffer
+	err := marshalToWriterWithHook(i, &buf, hook)
+	return buf.Bytes(), err
+}
+
+func marshalToWriterWithHook(i interface{}, w io.Writer, hook func(in interface{}) (ok bool, out interface{}, err error)) error {
+	enc := yaml.NewEncoder(w)
+	defer enc.Close()
+	enc.SetHook(hook)
+	return enc.Encode(i)
 }
