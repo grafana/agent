39d9a4749a3adba9b41a0a3401a136a25b1288db
Patrick Oyarzun <patrick.oyarzun@grafana.com>
2022-12-02 16:54:06 -0600

    Initial implementation pulling from CRDs

--- component/mimir/rules/rules.go
+++ component/mimir/rules/rules.go
@@ -2,14 +2,23 @@ package rules
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
 	"github.com/grafana/agent/component"
-	"github.com/grafana/agent/pkg/mimir/client"
+	"github.com/grafana/agent/pkg/flow/rivertypes"
+	mimirClient "github.com/grafana/agent/pkg/mimir/client"
 	"github.com/grafana/dskit/crypto/tls"
-	v1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
+	promv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime"
+	controller "sigs.k8s.io/controller-runtime"
+	k8sClient "sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
 func init() {
@@ -26,16 +35,30 @@ func init() {
 type Arguments struct {
 	ClientParams ClientArguments `river:"client,block"`
 	SyncInterval time.Duration   `river:"sync_interval,attr,optional"`
+
+	RuleSelector          LabelSelector `river:"rule_selector,block,optional"`
+	RuleNamespaceSelector LabelSelector `river:"rule_namespace_selector,block,optional"`
+}
+
+type LabelSelector struct {
+	MatchLabels      map[string]string `river:"match_labels,attr"`
+	MatchExpressions []MatchExpression `river:"match_expressions,attr"`
+}
+
+type MatchExpression struct {
+	Key      string   `river:"key,attr"`
+	Operator string   `river:"operator,attr"`
+	Values   []string `river:"values,attr"`
 }
 
 type ClientArguments struct {
-	User            string       `river:"user,attr,optional"`
-	Key             string       `river:"key,attr,optional"`
-	Address         string       `river:"address,attr"`
-	ID              string       `river:"id,attr,optional"`
-	TLS             TLSArguments `river:"tls,block,optional"`
-	UseLegacyRoutes bool         `river:"use_legacy_routes,attr,optional"`
-	AuthToken       string       `river:"auth_token,attr,optional"`
+	User            string            `river:"user,attr,optional"`
+	Key             rivertypes.Secret `river:"key,attr,optional"`
+	Address         string            `river:"address,attr"`
+	ID              string            `river:"id,attr,optional"`
+	TLS             TLSArguments      `river:"tls,block,optional"`
+	UseLegacyRoutes bool              `river:"use_legacy_routes,attr,optional"`
+	AuthToken       rivertypes.Secret `river:"auth_token,attr,optional"`
 }
 
 type TLSArguments struct {
@@ -56,11 +79,16 @@ type Component struct {
 	opts component.Options
 	args Arguments
 
-	client *client.MimirClient
-	ticker *time.Ticker
+	mimirClient *mimirClient.MimirClient
+	k8sClient   k8sClient.Client
+	ticker      *time.Ticker
+
+	namespaceSelector labels.Selector
+	ruleSelector      labels.Selector
 }
 
 var _ component.Component = (*Component)(nil)
+var _ reconcile.Reconciler = (*Component)(nil)
 
 func NewComponent(o component.Options, c Arguments) (*Component, error) {
 	return &Component{
@@ -86,15 +114,36 @@ func (c *Component) Update(newConfig component.Arguments) error {
 	return c.init()
 }
 
+func (c *Component) Reconcile(ctx context.Context, req reconcile.Request) (reconcile.Result, error) {
+	return reconcile.Result{}, nil
+}
+
 func (c *Component) init() error {
+
+	// TODO: allow overriding some stuff in RestConfig and k8s client options?
+	restConfig := controller.GetConfigOrDie()
+
+	scheme := runtime.NewScheme()
+	err := corev1.AddToScheme(scheme)
+	if err != nil {
+		return fmt.Errorf("failed to add prometheus operator scheme: %w", err)
+	}
+	err = promv1.AddToScheme(scheme)
+	if err != nil {
+		return fmt.Errorf("failed to add prometheus operator scheme: %w", err)
+	}
+
+	c.k8sClient, err = k8sClient.New(restConfig, k8sClient.Options{
+		Scheme: scheme,
+	})
+
 	if c.args.SyncInterval == 0 {
 		c.args.SyncInterval = 30 * time.Second
 	}
 
-	var err error
-	c.client, err = client.New(client.Config{
+	c.mimirClient, err = mimirClient.New(mimirClient.Config{
 		User:    c.args.ClientParams.User,
-		Key:     c.args.ClientParams.Key,
+		Key:     string(c.args.ClientParams.Key),
 		Address: c.args.ClientParams.Address,
 		ID:      c.args.ClientParams.ID,
 		TLS: tls.ClientConfig{
@@ -107,7 +156,7 @@ func (c *Component) init() error {
 			MinVersion:         c.args.ClientParams.TLS.MinVersion,
 		},
 		UseLegacyRoutes: c.args.ClientParams.UseLegacyRoutes,
-		AuthToken:       c.args.ClientParams.AuthToken,
+		AuthToken:       string(c.args.ClientParams.AuthToken),
 	})
 	if err != nil {
 		return err
@@ -115,14 +164,28 @@ func (c *Component) init() error {
 
 	c.ticker = time.NewTicker(c.args.SyncInterval)
 
+	c.namespaceSelector, err = convertSelectorToListOptions(c.args.RuleNamespaceSelector)
+	if err != nil {
+		return err
+	}
+
+	c.ruleSelector, err = convertSelectorToListOptions(c.args.RuleSelector)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (c *Component) start(ctx context.Context) {
+	err := c.syncRules(ctx)
+	if err != nil {
+		level.Error(c.log).Log("msg", "failed to sync rules", "err", err)
+	}
+
 	for {
 		select {
 		case <-c.ticker.C:
-			level.Info(c.log).Log("msg", "syncing rules")
 			err := c.syncRules(ctx)
 			if err != nil {
 				level.Error(c.log).Log("msg", "failed to sync rules", "err", err)
@@ -135,6 +198,8 @@ func (c *Component) start(ctx context.Context) {
 }
 
 func (c *Component) syncRules(ctx context.Context) error {
+	level.Info(c.log).Log("msg", "syncing rules")
+
 	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
 
@@ -155,18 +220,58 @@ func (c *Component) syncRules(ctx context.Context) error {
 	return c.applyChanges(ctx, diff)
 }
 
-func (c *Component) discoverRuleCRDs(ctx context.Context) ([]v1.PrometheusRule, error) {
-	return nil, nil
+func convertSelectorToListOptions(selector LabelSelector) (labels.Selector, error) {
+	matchExpressions := []metav1.LabelSelectorRequirement{}
+
+	for _, me := range selector.MatchExpressions {
+		matchExpressions = append(matchExpressions, metav1.LabelSelectorRequirement{
+			Key:      me.Key,
+			Operator: metav1.LabelSelectorOperator(me.Operator),
+			Values:   me.Values,
+		})
+	}
+
+	return metav1.LabelSelectorAsSelector(&metav1.LabelSelector{
+		MatchLabels:      selector.MatchLabels,
+		MatchExpressions: matchExpressions,
+	})
 }
 
-func (c *Component) loadActiveRules(ctx context.Context) (map[string][]client.RuleGroup, error) {
-	return c.client.ListRules(ctx, "")
+func (c *Component) discoverRuleCRDs(ctx context.Context) ([]*promv1.PrometheusRule, error) {
+	// List namespaces
+	var namespaces corev1.NamespaceList
+	err := c.k8sClient.List(ctx, &namespaces, &k8sClient.ListOptions{
+		LabelSelector: c.namespaceSelector,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	var crds []*promv1.PrometheusRule
+	// List rules in each namespace
+	for _, namespace := range namespaces.Items {
+		var crdList promv1.PrometheusRuleList
+		err := c.k8sClient.List(ctx, &crdList, &k8sClient.ListOptions{
+			LabelSelector: c.ruleSelector,
+			Namespace:     namespace.Name,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		crds = append(crds, crdList.Items...)
+	}
+	return crds, nil
+}
+
+func (c *Component) loadActiveRules(ctx context.Context) (map[string][]mimirClient.RuleGroup, error) {
+	return c.mimirClient.ListRules(ctx, "")
 }
 
 type RuleGroupDiff struct {
 }
 
-func (c *Component) diffRuleStates(desired []v1.PrometheusRule, actual map[string][]client.RuleGroup) []RuleGroupDiff {
+func (c *Component) diffRuleStates(desired []*promv1.PrometheusRule, actual map[string][]mimirClient.RuleGroup) []RuleGroupDiff {
 	return nil
 }
 
