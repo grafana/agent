ed1b4ab9a1211fbb6c905e06cc518dc49fe81005
Daniele Ferru <daniele.ferru@grafana.com>
2022-12-05 12:48:32 +0100

    removing cancelable context with short timeout

--- pkg/integrations/gcp_metrics_exporter/gcp_metrics_exporter.go
+++ pkg/integrations/gcp_metrics_exporter/gcp_metrics_exporter.go
@@ -3,8 +3,6 @@ package gcp_metrics_exporter
 import (
 	"context"
 	"fmt"
-	integrations_v2 "github.com/grafana/agent/pkg/integrations/v2"
-	"github.com/grafana/agent/pkg/integrations/v2/metricsutils"
 	"net/http"
 	"time"
 
@@ -16,6 +14,8 @@ import (
 	"google.golang.org/api/option"
 
 	"github.com/grafana/agent/pkg/integrations"
+	integrations_v2 "github.com/grafana/agent/pkg/integrations/v2"
+	"github.com/grafana/agent/pkg/integrations/v2/metricsutils"
 )
 
 func init() {
@@ -53,10 +53,7 @@ func (c *Config) InstanceKey(agentKey string) (string, error) {
 }
 
 func (c *Config) NewIntegration(l log.Logger) (integrations.Integration, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	svc, err := createMonitoringService(ctx, c.ClientTimeout)
+	svc, err := createMonitoringService(context.Background(), c.ClientTimeout)
 	if err != nil {
 		return nil, err
 	}
