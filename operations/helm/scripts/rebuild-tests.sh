f9978115e94dc6019c502be612d1af5e0c699278
Robert Fratto <robertfratto@gmail.com>
2023-01-09 11:19:53 -0500

    operations/helm: initial Helm chart for grafana-agent
    This commit introduces a Helm chart for Grafana Agent which primarily
    focuses on Grafana Agent Flow. It is upstreamed from
    rfratto/agent-helm-chart.
    
    This initial commit brings in just the chart itself. There are some
    additional TODO items:
    
    - [ ] Generate the chart README
    - [ ] Generate the tests
    - [ ] Update the build image to include additional needed tooling
    - [ ] Port over the GitHub Action automation for CI & releasing
    - [ ] Sign the Helm charts

---
+++ operations/helm/scripts/rebuild-tests.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+CHART_PATH="operations/helm/charts/grafana-agent"
+OUTPUT_PATH="operations/helm/tests"
+
+rm -rf $OUTPUT_PATH
+
+CHART_NAME=$(basename $CHART_PATH)
+TESTS=$(find "${CHART_PATH}/tests" -name "*.values.yaml")
+
+for FILEPATH in $TESTS; do
+  FILENAME=$(basename $FILEPATH)
+  TESTNAME=${FILENAME%.values.yaml}
+
+  helm template --namespace default --debug ${CHART_NAME} ${CHART_PATH} -f ${FILEPATH} --output-dir ${OUTPUT_PATH}/${TESTNAME}
+done
