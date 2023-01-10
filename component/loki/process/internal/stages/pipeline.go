8dc860a2db4631cc59b0e78af5dc86124a22100d
Paschalis Tsilias <paschalis.tsilias@grafana.com>
2023-01-10 17:37:37 +0200

    Move TODO comment inside of struct definition
    Signed-off-by: Paschalis Tsilias <paschalis.tsilias@grafana.com>

--- component/loki/process/internal/stages/pipeline.go
+++ component/loki/process/internal/stages/pipeline.go
@@ -19,12 +19,12 @@ import (
 // StageConfig defines a single stage in a processing pipeline.
 // We define these as pointers types so we can use reflection to check that
 // exactly one is set.
-// TODO (@tpaschalis) The fact that this type belongs to an internal package
-// _and_ is part of the loki.process component Arguments, means that an
-// external caller will not be able to construct the component Arguments by
-// hand. This will be fixed once we've gained confidence in the ported
-// processing stages and the package is made non-internal.
 type StageConfig struct {
+	// TODO (@tpaschalis) The fact that this type belongs to an internal package
+	// _and_ is part of the loki.process component Arguments, means that an
+	// external caller will not be able to construct the component Arguments by
+	// hand. This will be fixed once we've gained confidence in the ported
+	// processing stages and the package is made non-internal.
 	JSONConfig         *JSONConfig         `river:"json,block,optional"`
 	LabelsConfig       *LabelsConfig       `river:"labels,block,optional"`
 	LabelAllowConfig   *LabelAllowConfig   `river:"label_keep,block,optional"`
