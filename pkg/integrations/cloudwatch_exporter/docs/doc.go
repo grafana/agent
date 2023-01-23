82c753032f9d09f98274c7243279b2911bebad0c
Pablo Balbi <pablo.l.balbi@gmail.com>
2023-01-23 12:22:28 -0300

    update doc command

--- pkg/integrations/cloudwatch_exporter/docs/doc.go
+++ pkg/integrations/cloudwatch_exporter/docs/doc.go
@@ -4,6 +4,8 @@ import (
 	_ "embed"
 	"fmt"
 	yaceSvc "github.com/nerdswords/yet-another-cloudwatch-exporter/pkg/services"
+	"log"
+	"os"
 	"strings"
 )
 
@@ -15,6 +17,46 @@ const servicesListReplacement = "{{SERVICE_LIST}}"
 // main is used for programmatically generating a documentation section containing all AWS services supported in cloudwatch
 // exporter discovery jobs.
 func main() {
+	argsWithoutProgram := os.Args[1:]
+	if len(argsWithoutProgram) < 1 {
+		log.Println("Missing arguments: generate OR check <file>")
+		os.Exit(1)
+	}
+	doc := generateServicesDocSection()
+	switch argsWithoutProgram[0] {
+	case "generate":
+		fmt.Println(doc)
+		break
+	case "check":
+		if len(argsWithoutProgram) < 2 {
+			log.Println("Missing arguments: check <file>")
+			os.Exit(1)
+		}
+		fileToCheck := argsWithoutProgram[1]
+		if err := checkServicesDocSection(fileToCheck, doc); err != nil {
+			log.Printf("Check failed: %s\n", err)
+			os.Exit(1)
+		}
+		log.Println("Check successful!")
+		break
+	default:
+		log.Printf("Unknown command: %s\n", argsWithoutProgram[0])
+		os.Exit(1)
+	}
+}
+
+func checkServicesDocSection(path string, expectedDoc string) error {
+	contents, err := os.ReadFile(path)
+	if err != nil {
+		return fmt.Errorf("failed to read file to check: %w", err)
+	}
+	if !strings.Contains(string(contents), strings.TrimRight(expectedDoc, "\n")) {
+		return fmt.Errorf("doc has no services section, or is out of date")
+	}
+	return nil
+}
+
+func generateServicesDocSection() string {
 	var sb strings.Builder
 	for _, supportedSvc := range yaceSvc.SupportedServices {
 		sb.WriteString(
@@ -22,5 +64,5 @@ func main() {
 		)
 	}
 	doc := strings.Replace(docTemplate, servicesListReplacement, sb.String(), 1)
-	fmt.Println(doc)
+	return doc
 }
