grafana/agent

commit 3725047fbeafdbb54f2a11618c87a9ae7a545fb7

Bump node_exporter version to v1.5.0 (#2629)

component/prometheus/integration/node_exporter/config.go
@@ -16,7 +16,7 @@ var DefaultConfig = Config{
 	RootFSPath: node_integration.DefaultConfig.RootFSPath,
 	SysFSPath:  node_integration.DefaultConfig.SysFSPath,
 	Disk: DiskStatsConfig{
-		IgnoredDevices: node_integration.DefaultConfig.DiskStatsIgnoredDevices,
+		DeviceExclude: node_integration.DefaultConfig.DiskStatsDeviceExclude,
 	},
 	EthTool: EthToolConfig{
 		MetricsInclude: ".*",
@@ -92,6 +92,7 @@ type Config struct {
 	Powersupply PowersupplyConfig `river:"powersupply,block,optional"`
 	Runit       RunitConfig       `river:"runit,block,optional"`
 	Supervisord SupervisordConfig `river:"supervisord,block,optional"`
+	Sysctl      SysctlConfig      `river:"sysctl,block,option"`
 	Systemd     SystemdConfig     `river:"systemd,block,optional"`
 	Tapestats   TapestatsConfig   `river:"tapestats,block,optional"`
 	Textfile    TextfileConfig    `river:"textfile,block,optional"`
@@ -113,7 +114,8 @@ func (c *Config) Convert() *node_integration.Config {
 		CPUEnableCPUGuest:                c.CPU.EnableCPUGuest,
 		CPUEnableCPUInfo:                 c.CPU.EnableCPUInfo,
 		CPUFlagsInclude:                  c.CPU.FlagsInclude,
-		DiskStatsIgnoredDevices:          c.Disk.IgnoredDevices,
+		DiskStatsDeviceExclude:           c.Disk.DeviceExclude,
+		DiskStatsDeviceInclude:           c.Disk.DeviceInclude,
 		EthtoolDeviceExclude:             c.EthTool.DeviceExclude,
 		EthtoolDeviceInclude:             c.EthTool.DeviceInclude,
 		EthtoolMetricsInclude:            c.EthTool.MetricsInclude,
@@ -138,6 +140,8 @@ func (c *Config) Convert() *node_integration.Config {
 		PowersupplyIgnoredSupplies:       c.Powersupply.IgnoredSupplies,
 		RunitServiceDir:                  c.Runit.ServiceDir,
 		SupervisordURL:                   c.Supervisord.URL,
+		SysctlInclude:                    c.Sysctl.Include,
+		SysctlIncludeInfo:                c.Sysctl.IncludeInfo,
 		SystemdEnableRestartsMetrics:     c.Systemd.EnableRestartsMetrics,
 		SystemdEnableStartTimeMetrics:    c.Systemd.EnableStartTimeMetrics,
 		SystemdEnableTaskMetrics:         c.Systemd.EnableTaskMetrics,
@@ -245,7 +249,8 @@ type CPUConfig struct {
 
 // DiskStatsConfig contains config specific to the diskstats collector.
 type DiskStatsConfig struct {
-	IgnoredDevices string `river:"ignored_devices,attr,optional"`
+	DeviceExclude string `river:"device_exclude,attr,optional"`
+	DeviceInclude string `river:"device_include,attr,optional"`
 }
 
 // NTPConfig contains config specific to the ntp collector.
@@ -265,4 +270,10 @@ type SystemdConfig struct {
 	EnableTaskMetrics      bool   `river:"task_metrics,attr,optional"`
 	UnitExclude            string `river:"unit_exclude,attr,optional"`
 	UnitInclude            string `river:"unit_include,attr,optional"`
+}
+
+// SysctlConfig contains config specific to the sysctl collector.
+type SysctlConfig struct {
+	Include     []string `river:"include,attr,optional"`
+	IncludeInfo []string `river:"include_info,attr,optional"`
 }

pkg/integrations/node_exporter/config.go
@@ -26,7 +26,7 @@ var (
 		ProcFSPath: procfs.DefaultMountPoint,
 		RootFSPath: "/",
 
-		DiskStatsIgnoredDevices: "^(ram|loop|fd|(h|s|v|xv)d[a-z]|nvme\\d+n\\d+p)\\d+$",
+		DiskStatsDeviceExclude: "^(ram|loop|fd|(h|s|v|xv)d[a-z]|nvme\\d+n\\d+p)\\d+$",
 
 		EthtoolMetricsInclude: ".*",
 
@@ -102,7 +102,8 @@ type Config struct {
 	CPUEnableCPUGuest                bool                `yaml:"enable_cpu_guest_seconds_metric,omitempty"`
 	CPUEnableCPUInfo                 bool                `yaml:"enable_cpu_info_metric,omitempty"`
 	CPUFlagsInclude                  string              `yaml:"cpu_flags_include,omitempty"`
-	DiskStatsIgnoredDevices          string              `yaml:"diskstats_ignored_devices,omitempty"`
+	DiskStatsDeviceExclude           string              `yaml:"diskstats_device_exclude,omitempty"`
+	DiskStatsDeviceInclude           string              `yaml:"diskstats_device_include,omitempty"`
 	EthtoolDeviceExclude             string              `yaml:"ethtool_device_exclude,omitempty"`
 	EthtoolDeviceInclude             string              `yaml:"ethtool_device_include,omitempty"`
 	EthtoolMetricsInclude            string              `yaml:"ethtool_metrics_include,omitempty"`
@@ -127,6 +128,8 @@ type Config struct {
 	PowersupplyIgnoredSupplies       string              `yaml:"powersupply_ignored_supplies,omitempty"`
 	RunitServiceDir                  string              `yaml:"runit_service_dir,omitempty"`
 	SupervisordURL                   string              `yaml:"supervisord_url,omitempty"`
+	SysctlInclude                    flagext.StringSlice `yaml:"sysctl_include,omitempty"`
+	SysctlIncludeInfo                flagext.StringSlice `yaml:"sysctl_include_info,omitempty"`
 	SystemdEnableRestartsMetrics     bool                `yaml:"systemd_enable_restarts_metrics,omitempty"`
 	SystemdEnableStartTimeMetrics    bool                `yaml:"systemd_enable_start_time_metrics,omitempty"`
 	SystemdEnableTaskMetrics         bool                `yaml:"systemd_enable_task_metrics,omitempty"`
@@ -154,6 +157,7 @@ func (c *Config) UnmarshalYAML(unmarshal func(interface{}) error) error {
 		SystemdUnitBlacklist         string `yaml:"systemd_unit_blacklist,omitempty"`
 		FilesystemIgnoredMountPoints string `yaml:"filesystem_ignored_mount_points,omitempty"`
 		FilesystemIgnoredFSTypes     string `yaml:"filesystem_ignored_fs_types,omitempty"`
+		DiskStatsIgnoredDevices      string `yaml:"diskstats_ignored_devices,omitempty"`
 	}
 
 	var fc config // our full config (schema + deprecated names)
@@ -189,6 +193,10 @@ func (c *Config) UnmarshalYAML(unmarshal func(interface{}) error) error {
 		{
 			OldName: "filesystem_ignored_fs_types", NewName: "filesystem_fs_types_exclude",
 			OldValue: &fc.FilesystemIgnoredFSTypes, NewValue: &fc.FilesystemFSTypesExclude,
+		},
+		{
+			OldName: "diskstats_ignored_devices", NewName: "diskstats_device_exclude",
+			OldValue: &fc.DiskStatsIgnoredDevices, NewValue: &fc.DiskStatsDeviceExclude,
 		},
 	}
 
@@ -311,7 +319,8 @@ func MapConfigToNodeExporterFlags(c *Config) (accepted []string, ignored []strin
 	}
 
 	if collectors[CollectorDiskstats] {
-		flags.add("--collector.diskstats.ignored-devices", c.DiskStatsIgnoredDevices)
+		flags.add("--collector.diskstats.device-include", c.DiskStatsDeviceInclude)
+		flags.add("--collector.diskstats.device-exclude", c.DiskStatsDeviceExclude)
 	}
 
 	if collectors[CollectorEthtool] {
@@ -387,6 +396,16 @@ func MapConfigToNodeExporterFlags(c *Config) (accepted []string, ignored []strin
 
 	if collectors[CollectorSupervisord] {
 		flags.add("--collector.supervisord.url", c.SupervisordURL)
+	}
+
+	if collectors[CollectorSysctl] {
+		for _, numValue := range c.SysctlInclude {
+			flags.add("--collector.sysctl.include", numValue)
+		}
+
+		for _, stringValue := range c.SysctlIncludeInfo {
+			flags.add("--collector.sysctl.include-info", stringValue)
+		}
 	}
 
 	if collectors[CollectorSystemd] {

go.mod
@@ -75,7 +75,7 @@ require (
 	github.com/prometheus/consul_exporter v0.8.0
 	github.com/prometheus/memcached_exporter v0.10.0
 	github.com/prometheus/mysqld_exporter v0.14.0
-	github.com/prometheus/node_exporter v1.3.1-0.20220127103407-4f27a4fd8efa
+	github.com/prometheus/node_exporter v1.5.0
 	github.com/prometheus/procfs v0.8.0
 	github.com/prometheus/prometheus v1.99.0
 	github.com/prometheus/snmp_exporter v0.20.1-0.20220111173215-83399c23888f
@@ -129,7 +129,7 @@ require (
 	github.com/cespare/xxhash/v2 v2.2.0
 	github.com/cloudflare/cloudflare-go v0.27.0
 	github.com/coreos/go-systemd v0.0.0-20191104093116-d3cd4ed1dbcf
-	github.com/coreos/go-systemd/v22 v22.4.0
+	github.com/coreos/go-systemd/v22 v22.5.0
 	github.com/fatih/color v1.13.0
 	github.com/ghodss/yaml v1.0.1-0.20190212211648-25d852aebe32
 	github.com/google/go-cmp v0.5.9
@@ -250,7 +250,7 @@ require (
 	github.com/census-instrumentation/opencensus-proto v0.4.1 // indirect
 	github.com/cespare/xxhash v1.1.0 // indirect
 	github.com/checkpoint-restore/go-criu/v5 v5.3.0 // indirect
-	github.com/cilium/ebpf v0.7.0 // indirect
+	github.com/cilium/ebpf v0.9.3 // indirect
 	github.com/cncf/xds/go v0.0.0-20220314180256-7f1daf1720fc // indirect
 	github.com/containerd/cgroups v1.0.3 // indirect
 	github.com/containerd/console v1.0.3 // indirect
@@ -261,6 +261,8 @@ require (
 	github.com/cyphar/filepath-securejoin v0.2.3 // indirect
 	github.com/danieljoos/wincred v1.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dennwc/btrfs v0.0.0-20221026161108-3097362dc072 // indirect
+	github.com/dennwc/ioctl v1.0.0 // indirect
 	github.com/dennwc/varint v1.0.0 // indirect
 	github.com/denverdino/aliyungo v0.0.0-20190125010748-a747050bb1ba // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
@@ -315,7 +317,7 @@ require (
 	github.com/go-test/deep v1.1.0 // indirect
 	github.com/go-zookeeper/zk v1.0.3 // indirect
 	github.com/godbus/dbus v0.0.0-20190726142602-4481cbc300e2 // indirect
-	github.com/godbus/dbus/v5 v5.0.6 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/gogo/googleapis v1.4.1 // indirect
 	github.com/gogo/status v1.1.1 // indirect
 	github.com/golang-jwt/jwt/v4 v4.4.1 // indirect
@@ -343,7 +345,7 @@ require (
 	github.com/hairyhenderson/toml v0.4.2-0.20210923231440-40456b8e66cf // indirect
 	github.com/hashicorp/cronexpr v1.1.1 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
-	github.com/hashicorp/go-envparse v0.0.0-20200406174449-d9cfd743a15e // indirect
+	github.com/hashicorp/go-envparse v0.1.0 // indirect
 	github.com/hashicorp/go-hclog v1.3.1 // indirect
 	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
 	github.com/hashicorp/go-msgpack v0.5.5 // indirect
@@ -366,7 +368,7 @@ require (
 	github.com/hashicorp/vic v1.5.1-0.20190403131502-bbfe86ec9443 // indirect
 	github.com/hashicorp/yamux v0.0.0-20190923154419-df201c70410d // indirect
 	github.com/hetznercloud/hcloud-go v1.35.3 // indirect
-	github.com/hodgesds/perf-utils v0.4.0 // indirect
+	github.com/hodgesds/perf-utils v0.5.1 // indirect
 	github.com/huandu/xstrings v1.3.1 // indirect
 	github.com/illumos/go-kstat v0.0.0-20210513183136-173c9b0a9973 // indirect
 	github.com/imdario/mergo v0.3.13 // indirect
@@ -382,10 +384,10 @@ require (
 	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
-	github.com/josharian/native v0.0.0-20200817173448-b6b71def0850 // indirect
+	github.com/josharian/native v1.0.0 // indirect
 	github.com/joyent/triton-go v0.0.0-20180628001255-830d2b111e62 // indirect
 	github.com/jpillora/backoff v1.0.0 // indirect
-	github.com/jsimonetti/rtnetlink v0.0.0-20211022192332-93da33804786 // indirect
+	github.com/jsimonetti/rtnetlink v1.3.0 // indirect
 	github.com/julienschmidt/httprouter v1.3.0 // indirect
 	github.com/karrick/godirwalk v1.16.1 // indirect
 	github.com/kevinburke/ssh_config v1.1.0 // indirect
@@ -407,10 +409,11 @@ require (
 	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/mattn/go-xmlrpc v0.0.3 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
-	github.com/mdlayher/genetlink v1.0.0 // indirect
-	github.com/mdlayher/netlink v1.4.1 // indirect
-	github.com/mdlayher/socket v0.0.0-20210307095302-262dc9984e00 // indirect
-	github.com/mdlayher/wifi v0.0.0-20200527114002-84f0b9457fdd // indirect
+	github.com/mdlayher/ethtool v0.0.0-20220830195143-0e16326d06d1 // indirect
+	github.com/mdlayher/genetlink v1.2.0 // indirect
+	github.com/mdlayher/netlink v1.7.0 // indirect
+	github.com/mdlayher/socket v0.4.0 // indirect
+	github.com/mdlayher/wifi v0.0.0-20220330172155-a44c70b6d3c8 // indirect
 	github.com/miekg/pkcs11 v1.1.1 // indirect
 	github.com/mindprince/gonvml v0.0.0-20190828220739-9ebdce4bb989 // indirect
 	github.com/mistifyio/go-zfs v2.1.2-0.20190413222219-f784269be439+incompatible // indirect
@@ -443,7 +446,7 @@ require (
 	github.com/opencontainers/image-spec v1.0.3-0.20211202183452-c5a74bcca799 // indirect
 	github.com/opencontainers/runc v1.1.2 // indirect
 	github.com/opencontainers/runtime-spec v1.0.3-0.20210326190908-1c3f411f0417 // indirect
-	github.com/opencontainers/selinux v1.10.1 // indirect
+	github.com/opencontainers/selinux v1.10.2 // indirect
 	github.com/openzipkin/zipkin-go v0.4.1 // indirect
 	github.com/ovh/go-ovh v1.1.0 // indirect
 	github.com/packethost/packngo v0.1.1-0.20180711074735-b9cb5096f54c // indirect
@@ -464,7 +467,7 @@ require (
 	github.com/rs/zerolog v1.26.1 // indirect
 	github.com/ryanuber/go-glob v1.0.0 // indirect
 	github.com/ryszard/goskiplist v0.0.0-20150312221310-2dfbae5fcf46 // indirect
-	github.com/safchain/ethtool v0.1.0 // indirect
+	github.com/safchain/ethtool v0.2.0 // indirect
 	github.com/samuel/go-zookeeper v0.0.0-20190923202752-2cc03de413da // indirect
 	github.com/satori/go.uuid v1.2.1-0.20181028125025-b2ce2384e17b // indirect
 	github.com/scaleway/scaleway-sdk-go v1.0.0-beta.9 // indirect

pkg/integrations/node_exporter/collectors.go
@@ -95,6 +95,10 @@ const (
 	CollectorXFS          = "xfs"
 	CollectorZFS          = "zfs"
 	CollectorZoneinfo     = "zoneinfo"
+	CollectorCGroups      = "cgroups"
+	CollectorSELinux      = "selinux"
+	CollectorSlabInfo     = "slabinfo"
+	CollectorSysctl       = "sysctl"
 )
 
 // Collectors holds a map of known collector names to their default
@@ -106,6 +110,7 @@ var Collectors = map[string]CollectorState{
 	CollectorBonding:      CollectorStateEnabled,
 	CollectorBootTime:     CollectorStateEnabled,
 	CollectorBuddyInfo:    CollectorStateDisabled,
+	CollectorCGroups:      CollectorStateDisabled,
 	CollectorCPU:          CollectorStateEnabled,
 	CollectorCPUFreq:      CollectorStateEnabled,
 	CollectorConntrack:    CollectorStateEnabled,
@@ -150,10 +155,13 @@ var Collectors = map[string]CollectorState{
 	CollectorRAPL:         CollectorStateEnabled,
 	CollectorRunit:        CollectorStateDisabled,
 	CollectorSchedstat:    CollectorStateEnabled,
+	CollectorSELinux:      CollectorStateEnabled,
+	CollectorSlabInfo:     CollectorStateDisabled,
 	CollectorSockstat:     CollectorStateEnabled,
 	CollectorSoftnet:      CollectorStateEnabled,
 	CollectorStat:         CollectorStateEnabled,
 	CollectorSupervisord:  CollectorStateDisabled,
+	CollectorSysctl:       CollectorStateDisabled,
 	CollectorSystemd:      CollectorStateDisabled,
 	CollectorTCPStat:      CollectorStateDisabled,
 	CollectorTapestats:    CollectorStateEnabled,

pkg/integrations/node_exporter/config_test.go
@@ -33,6 +33,7 @@ func TestNodeExporter_ConfigMigrate(t *testing.T) {
       systemd_unit_blacklist: systemd_bl
       filesystem_ignored_mount_points: fs_mp
       filesystem_ignored_fs_types: fs_types
+      diskstats_ignored_devices: diskstats_exclude
       `,
 			expectWarnings: []string{
 				`"netdev_device_whitelist" is deprecated by "netdev_device_include" and will be removed in a future version`,
@@ -41,6 +42,7 @@ func TestNodeExporter_ConfigMigrate(t *testing.T) {
 				`"systemd_unit_blacklist" is deprecated by "systemd_unit_exclude" and will be removed in a future version`,
 				`"filesystem_ignored_mount_points" is deprecated by "filesystem_mount_points_exclude" and will be removed in a future version`,
 				`"filesystem_ignored_fs_types" is deprecated by "filesystem_fs_types_exclude" and will be removed in a future version`,
+				`"diskstats_ignored_devices" is deprecated by "diskstats_device_exclude" and will be removed in a future version`,
 			},
 			check: func(t *testing.T, c *Config) {
 				t.Helper()
@@ -51,6 +53,7 @@ func TestNodeExporter_ConfigMigrate(t *testing.T) {
 				require.Equal(t, c.SystemdUnitExclude, "systemd_bl")
 				require.Equal(t, c.FilesystemMountPointsExclude, "fs_mp")
 				require.Equal(t, c.FilesystemFSTypesExclude, "fs_types")
+				require.Equal(t, c.DiskStatsDeviceExclude, "diskstats_exclude")
 			},
 		},
 		{
@@ -62,6 +65,7 @@ func TestNodeExporter_ConfigMigrate(t *testing.T) {
       systemd_unit_exclude: systemd_bl
       filesystem_mount_points_exclude: fs_mp
       filesystem_fs_types_exclude: fs_types
+      diskstats_device_exclude: diskstats_exclude
       `,
 			check: func(t *testing.T, c *Config) {
 				t.Helper()
@@ -72,6 +76,7 @@ func TestNodeExporter_ConfigMigrate(t *testing.T) {
 				require.Equal(t, c.SystemdUnitExclude, "systemd_bl")
 				require.Equal(t, c.FilesystemMountPointsExclude, "fs_mp")
 				require.Equal(t, c.FilesystemFSTypesExclude, "fs_types")
+				require.Equal(t, c.DiskStatsDeviceExclude, "diskstats_exclude")
 			},
 		},
 		{
@@ -103,6 +108,11 @@ func TestNodeExporter_ConfigMigrate(t *testing.T) {
 			name:        `filesystem_ignored_fs_types and filesystem_fs_types_exclude`,
 			in:          illegalConfig("filesystem_ignored_fs_types", "filesystem_fs_types_exclude"),
 			expectError: `only one of "filesystem_ignored_fs_types" and "filesystem_fs_types_exclude" may be specified`,
+		},
+		{
+			name:        `diskstats_ignored_devices and diskstats_device_exclude`,
+			in:          illegalConfig("diskstats_ignored_devices", "diskstats_device_exclude"),
+			expectError: `only one of "diskstats_ignored_devices" and "diskstats_device_exclude" may be specified`,
 		},
 	}
 

pkg/integrations/node_exporter/node_exporter_test.go
@@ -94,7 +94,6 @@ func TestNodeExporter_IgnoredFlags(t *testing.T) {
 			"collector.cpu.guest",
 			"collector.cpu.info.flags-include",
 			"collector.cpu.info.bugs-include",
-			"collector.diskstats.ignored-devices",
 			"collector.filesystem.mount-timeout",
 		}
 	}

commit 752a4f47b9c4aa7f19e6e3dab6af1204a12b0ade

Flow: reload when SIGHUP is sent (#2879)
* Flow: support reloading when receiving a SIGHUP

This matches the behavior of the static mode.

* deb, rpm: respond to `systemctl reload grafana-agent`

Send a SIGHUP signal to Grafana Agent when `systemctl reload
grafana-agent` is invoked.

* update CHANGELOG

* fix broken river stanza in prometheus.integration.node_exporter

Fixes issue introduced by #2629

cmd/grafana-agent/flow_run.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"os/signal"
 	"sync"
+	"syscall"
 
 	"github.com/grafana/agent/web/api"
 	"github.com/grafana/agent/web/ui"
@@ -256,8 +257,22 @@ func (fr *flowRun) Run(configFile string) error {
 		return err
 	}
 
-	<-ctx.Done()
-	return f.Close()
+	reloadSignal := make(chan os.Signal, 1)
+	signal.Notify(reloadSignal, syscall.SIGHUP)
+	defer signal.Stop(reloadSignal)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return f.Close()
+		case <-reloadSignal:
+			if err := reload(); err != nil {
+				level.Error(l).Log("msg", "failed to reload config", "err", err)
+			} else {
+				level.Info(l).Log("msg", "config reloaded")
+			}
+		}
+	}
 }
 
 // getEnabledComponentsFunc returns a function that gets the current enabled components

component/prometheus/integration/node_exporter/config.go
@@ -92,7 +92,7 @@ type Config struct {
 	Powersupply PowersupplyConfig `river:"powersupply,block,optional"`
 	Runit       RunitConfig       `river:"runit,block,optional"`
 	Supervisord SupervisordConfig `river:"supervisord,block,optional"`
-	Sysctl      SysctlConfig      `river:"sysctl,block,option"`
+	Sysctl      SysctlConfig      `river:"sysctl,block,optional"`
 	Systemd     SystemdConfig     `river:"systemd,block,optional"`
 	Tapestats   TapestatsConfig   `river:"tapestats,block,optional"`
 	Textfile    TextfileConfig    `river:"textfile,block,optional"`

packaging/deb/grafana-agent.service
@@ -11,6 +11,7 @@ Environment=HOSTNAME=%H
 EnvironmentFile=/etc/default/grafana-agent
 WorkingDirectory=/var/lib/grafana-agent
 ExecStart=/usr/bin/grafana-agent --config.file $CONFIG_FILE $CUSTOM_ARGS
+ExecReload=/usr/bin/kill -HUP $MAINPID
 # If running the Agent in scraping service mode, you will want to override this value with
 # something larger to allow the Agent to gracefully leave the cluster. 4800s is recommend.
 TimeoutStopSec=20s

packaging/rpm/grafana-agent.service
@@ -11,6 +11,7 @@ Environment=HOSTNAME=%H
 EnvironmentFile=/etc/sysconfig/grafana-agent
 WorkingDirectory=/var/lib/grafana-agent
 ExecStart=/usr/bin/grafana-agent --config.file $CONFIG_FILE $CUSTOM_ARGS
+ExecReload=/usr/bin/kill -HUP $MAINPID
 # If running the Agent in scraping service mode, you will want to override this value with
 # something larger to allow the Agent to gracefully leave the cluster. 4800s is recommend.
 TimeoutStopSec=20s