2ed14a9f515d8157f58b8c9a9c54097a17023965
Mischa Thompson <mischabear@gmail.com>
2023-02-21 07:30:23 -0800

    Add new test cases

--- component/prometheus/integration/process_exporter/process_exporter_test.go
+++ component/prometheus/integration/process_exporter/process_exporter_test.go
@@ -4,15 +4,16 @@ import (
 	"testing"
 
 	"github.com/grafana/agent/pkg/river"
+	"github.com/ncabatoff/process-exporter/config"
 	"github.com/stretchr/testify/require"
 )
 
-func TestRiverConfig(t *testing.T) {
+func TestRiverConfigUnmarshal(t *testing.T) {
 	var exampleRiverConfig = `
 	process_names {
-		comm    = ["process_name_1", "grafana-agent"]
-		exe     = ["/usr/bin/process"]
-		cmdline = ["*flow*"]
+		name    = "flow"
+		comm    = ["grafana-agent"]
+		cmdline = ["*run*"]
 	}
 	track_children    = false
 	track_threads     = false
@@ -23,4 +24,65 @@ func TestRiverConfig(t *testing.T) {
 	var args Arguments
 	err := river.Unmarshal([]byte(exampleRiverConfig), &args)
 	require.NoError(t, err)
+
+	require.False(t, args.Children)
+	require.False(t, args.Threads)
+	require.True(t, args.SMaps)
+	require.True(t, args.Recheck)
+
+	expected := []MatcherGroup{
+		{
+			Name:         "flow",
+			CommRules:    []string{"grafana-agent"},
+			CmdlineRules: []string{"*run*"},
+		},
+	}
+	require.Equal(t, expected, args.ProcessExporter)
+}
+
+func TestRiverConfigConvert(t *testing.T) {
+	var exampleRiverConfig = `
+	process_names {
+		name    = "static"
+		comm    = ["grafana-agent"]
+		cmdline = ["*config.file*"]
+	}
+	track_children    = true
+	track_threads     = true
+	gather_smaps      = false
+	recheck_on_scrape = false
+`
+
+	var args Arguments
+	err := river.Unmarshal([]byte(exampleRiverConfig), &args)
+	require.NoError(t, err)
+
+	require.True(t, args.Children)
+	require.True(t, args.Threads)
+	require.False(t, args.SMaps)
+	require.False(t, args.Recheck)
+
+	expected := []MatcherGroup{
+		{
+			Name:         "static",
+			CommRules:    []string{"grafana-agent"},
+			CmdlineRules: []string{"*config.file*"},
+		},
+	}
+	require.Equal(t, expected, args.ProcessExporter)
+
+	c := args.Convert()
+	require.True(t, c.Children)
+	require.True(t, c.Threads)
+	require.False(t, c.SMaps)
+	require.False(t, c.Recheck)
+
+	e := config.MatcherRules{
+		{
+			Name:         "static",
+			CommRules:    []string{"grafana-agent"},
+			CmdlineRules: []string{"*config.file*"},
+		},
+	}
+	require.Equal(t, e, c.ProcessExporter)
 }
