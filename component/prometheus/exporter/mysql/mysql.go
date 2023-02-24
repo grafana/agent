cea2258ebdeee23481dc235aca558198a40f12db
Mischa Thompson <mischabear@gmail.com>
2023-02-23 22:14:00 -0800

    Add code

--- component/prometheus/exporter/mysql/mysql.go
+++ component/prometheus/exporter/mysql/mysql.go
@@ -1,2 +1,107 @@
 package mysql
 
+import (
+	"github.com/grafana/agent/component"
+	"github.com/grafana/agent/component/prometheus/exporter"
+	"github.com/grafana/agent/pkg/flow/rivertypes"
+	"github.com/grafana/agent/pkg/integrations"
+	"github.com/grafana/agent/pkg/integrations/mysqld_exporter"
+	config_util "github.com/prometheus/common/config"
+)
+
+func init() {
+	component.Register(component.Registration{
+		Name:    "prometheus.exporter.mysql",
+		Args:    Arguments{},
+		Exports: exporter.Exports{},
+		Build:   exporter.New(createExporter, "mysql"),
+	})
+}
+
+func createExporter(opts component.Options, args component.Arguments) (integrations.Integration, error) {
+	cfg := args.(Arguments)
+	return cfg.Convert().NewIntegration(opts.Logger)
+}
+
+// DefaultArguments holds the default settings for the mysqld_exporter integration.
+var DefaultArguments = Arguments{
+	LockWaitTimeout: 2,
+
+	InfoSchemaProcessListProcessesByUser: true,
+	InfoSchemaProcessListProcessesByHost: true,
+	InfoSchemaTablesDatabases:            "*",
+
+	PerfSchemaEventsStatementsLimit:     250,
+	PerfSchemaEventsStatementsTimeLimit: 86400,
+	PerfSchemaEventsStatementsTextLimit: 120,
+	PerfSchemaFileInstancesFilter:       ".*",
+	PerfSchemaFileInstancesRemovePrefix: "/var/lib/mysql",
+
+	HeartbeatDatabase: "heartbeat",
+	HeartbeatTable:    "heartbeat",
+}
+
+// Arguments controls the mysql component.
+type Arguments struct {
+	// DataSourceName to use to connect to MySQL.
+	DataSourceName rivertypes.Secret `river:"data_source_name,attr,optional"`
+
+	// Collectors to mark as enabled in addition to the default.
+	EnableCollectors []string `river:"enable_collectors,attr,optional"`
+	// Collectors to explicitly mark as disabled.
+	DisableCollectors []string `river:"disable_collectors,attr,optional"`
+
+	// Overrides the default set of enabled collectors with the given list.
+	SetCollectors []string `river:"set_collectors,attr,optional"`
+
+	// Collector-wide options
+	LockWaitTimeout int  `river:"lock_wait_timeout,attr,optional"`
+	LogSlowFilter   bool `river:"log_slow_filter,attr,optional"`
+
+	// Collector-specific config options
+	InfoSchemaProcessListMinTime         int    `river:"info_schema_processlist_min_time,attr,optional"`
+	InfoSchemaProcessListProcessesByUser bool   `river:"info_schema_processlist_processes_by_user,attr,optional"`
+	InfoSchemaProcessListProcessesByHost bool   `river:"info_schema_processlist_processes_by_host,attr,optional"`
+	InfoSchemaTablesDatabases            string `river:"info_schema_tables_databases,attr,optional"`
+	PerfSchemaEventsStatementsLimit      int    `river:"perf_schema_eventsstatements_limit,attr,optional"`
+	PerfSchemaEventsStatementsTimeLimit  int    `river:"perf_schema_eventsstatements_time_limit,attr,optional"`
+	PerfSchemaEventsStatementsTextLimit  int    `river:"perf_schema_eventsstatements_digtext_text_limit,attr,optional"`
+	PerfSchemaFileInstancesFilter        string `river:"perf_schema_file_instances_filter,attr,optional"`
+	PerfSchemaFileInstancesRemovePrefix  string `river:"perf_schema_file_instances_remove_prefix,attr,optional"`
+	HeartbeatDatabase                    string `river:"heartbeat_database,attr,optional"`
+	HeartbeatTable                       string `river:"heartbeat_table,attr,optional"`
+	HeartbeatUTC                         bool   `river:"heartbeat_utc,attr,optional"`
+	MySQLUserPrivileges                  bool   `river:"mysql_user_privileges,attr,optional"`
+}
+
+// UnmarshalRiver implements River unmarshalling for Config.
+func (c *Arguments) UnmarshalRiver(f func(interface{}) error) error {
+	*c = DefaultArguments
+
+	type cfg Arguments
+	return f((*cfg)(c))
+}
+
+func (a *Arguments) Convert() *mysqld_exporter.Config {
+	return &mysqld_exporter.Config{
+		DataSourceName:                       config_util.Secret(a.DataSourceName),
+		EnableCollectors:                     a.EnableCollectors,
+		DisableCollectors:                    a.DisableCollectors,
+		SetCollectors:                        a.SetCollectors,
+		LockWaitTimeout:                      a.LockWaitTimeout,
+		LogSlowFilter:                        a.LogSlowFilter,
+		InfoSchemaProcessListMinTime:         a.InfoSchemaProcessListMinTime,
+		InfoSchemaProcessListProcessesByUser: a.InfoSchemaProcessListProcessesByUser,
+		InfoSchemaProcessListProcessesByHost: a.InfoSchemaProcessListProcessesByHost,
+		InfoSchemaTablesDatabases:            a.InfoSchemaTablesDatabases,
+		PerfSchemaEventsStatementsLimit:      a.PerfSchemaEventsStatementsLimit,
+		PerfSchemaEventsStatementsTimeLimit:  a.PerfSchemaEventsStatementsTimeLimit,
+		PerfSchemaEventsStatementsTextLimit:  a.PerfSchemaEventsStatementsTextLimit,
+		PerfSchemaFileInstancesFilter:        a.PerfSchemaFileInstancesFilter,
+		PerfSchemaFileInstancesRemovePrefix:  a.PerfSchemaFileInstancesRemovePrefix,
+		HeartbeatDatabase:                    a.HeartbeatDatabase,
+		HeartbeatTable:                       a.HeartbeatTable,
+		HeartbeatUTC:                         a.HeartbeatUTC,
+		MySQLUserPrivileges:                  a.MySQLUserPrivileges,
+	}
+}
--- component/prometheus/exporter/mysql/mysql_test.go
+++ component/prometheus/exporter/mysql/mysql_test.go
@@ -1,1 +1,116 @@
 package mysql
+
+import (
+	"testing"
+
+	"github.com/grafana/agent/pkg/integrations/mysqld_exporter"
+	"github.com/grafana/agent/pkg/river"
+	"github.com/stretchr/testify/require"
+)
+
+
+
+func TestRiverConfigUnmarshal(t *testing.T) {
+	var exampleRiverConfig = `
+	data_source_name = "DataSourceName"
+	enable_collectors = ["collector1"]
+	disable_collectors = ["collector2"]
+	set_collectors = ["collector3", "collector4"]
+	lock_wait_timeout = 1
+	log_slow_filter = false
+	info_schema_processlist_min_time = 2
+	info_schema_processlist_processes_by_user = true
+	info_schema_processlist_processes_by_host = false
+	info_schema_tables_databases = "schema"
+	perf_schema_eventsstatements_limit = 3
+	perf_schema_eventsstatements_time_limit = 4
+	perf_schema_eventsstatements_digtext_text_limit = 5
+	perf_schema_file_instances_filter = "instances_filter"
+	perf_schema_file_instances_remove_prefix = "instances_remove"
+	heartbeat_database = "heartbeat_database"
+	heartbeat_table = "heartbeat_table"
+	heartbeat_utc = true
+	mysql_user_privileges = false
+`
+
+	var args Arguments
+	err := river.Unmarshal([]byte(exampleRiverConfig), &args)
+	require.NoError(t, err)
+
+	require.Equal(t, "DataSourceName", string(args.DataSourceName))
+	require.Equal(t, []string{"collector1"}, args.EnableCollectors)
+	require.Equal(t, []string{"collector2"}, args.DisableCollectors)
+	require.Equal(t, []string{"collector3", "collector4"}, args.SetCollectors)
+	require.Equal(t, 1, args.LockWaitTimeout)
+	require.False(t, args.LogSlowFilter)
+	require.Equal(t, 2, args.InfoSchemaProcessListMinTime)
+	require.True(t, args.InfoSchemaProcessListProcessesByUser)
+	require.False(t, args.InfoSchemaProcessListProcessesByHost)
+	require.Equal(t, "schema", args.InfoSchemaTablesDatabases)
+	require.Equal(t, 3, args.PerfSchemaEventsStatementsLimit)
+	require.Equal(t, 4, args.PerfSchemaEventsStatementsTimeLimit)
+	require.Equal(t, 5, args.PerfSchemaEventsStatementsTextLimit)
+	require.Equal(t, "instances_filter", args.PerfSchemaFileInstancesFilter)
+	require.Equal(t, "instances_remove", args.PerfSchemaFileInstancesRemovePrefix)
+	require.Equal(t, "heartbeat_database", args.HeartbeatDatabase)
+	require.Equal(t, "heartbeat_table", args.HeartbeatTable)
+	require.True(t, args.HeartbeatUTC)
+	require.False(t, args.MySQLUserPrivileges)
+}
+
+
+
+func TestRiverConfigConvert(t *testing.T) {
+	var exampleRiverConfig = `
+	data_source_name = "DataSourceName"
+	enable_collectors = ["collector1"]
+	disable_collectors = ["collector2"]
+	set_collectors = ["collector3", "collector4"]
+	lock_wait_timeout = 1
+	log_slow_filter = false
+	info_schema_processlist_min_time = 2
+	info_schema_processlist_processes_by_user = true
+	info_schema_processlist_processes_by_host = false
+	info_schema_tables_databases = "schema"
+	perf_schema_eventsstatements_limit = 3
+	perf_schema_eventsstatements_time_limit = 4
+	perf_schema_eventsstatements_digtext_text_limit = 5
+	perf_schema_file_instances_filter = "instances_filter"
+	perf_schema_file_instances_remove_prefix = "instances_remove"
+	heartbeat_database = "heartbeat_database"
+	heartbeat_table = "heartbeat_table"
+	heartbeat_utc = true
+	mysql_user_privileges = false
+`
+
+	var args Arguments
+	err := river.Unmarshal([]byte(exampleRiverConfig), &args)
+	require.NoError(t, err)
+
+	c := args.Convert()
+	require.Equal(t, "DataSourceName", string(c.DataSourceName))
+	require.Equal(t, []string{"collector1"}, c.EnableCollectors)
+	require.Equal(t, []string{"collector2"}, c.DisableCollectors)
+	require.Equal(t, []string{"collector3", "collector4"}, c.SetCollectors)
+	require.Equal(t, 1, c.LockWaitTimeout)
+	require.False(t, c.LogSlowFilter)
+	require.Equal(t, 2, c.InfoSchemaProcessListMinTime)
+	require.True(t, c.InfoSchemaProcessListProcessesByUser)
+	require.False(t, c.InfoSchemaProcessListProcessesByHost)
+	require.Equal(t, "schema", c.InfoSchemaTablesDatabases)
+	require.Equal(t, 3, c.PerfSchemaEventsStatementsLimit)
+	require.Equal(t, 4, c.PerfSchemaEventsStatementsTimeLimit)
+	require.Equal(t, 5, c.PerfSchemaEventsStatementsTextLimit)
+	require.Equal(t, "instances_filter", c.PerfSchemaFileInstancesFilter)
+	require.Equal(t, "instances_remove", c.PerfSchemaFileInstancesRemovePrefix)
+	require.Equal(t, "heartbeat_database", c.HeartbeatDatabase)
+	require.Equal(t, "heartbeat_table", c.HeartbeatTable)
+	require.True(t, c.HeartbeatUTC)
+	require.False(t, c.MySQLUserPrivileges)
+}
+
+// Checks that the flow and static default configs have not drifted
+func TestDefaultsSame(t *testing.T) {
+	convertedDefaults := DefaultArguments.Convert()
+	require.Equal(t, mysqld_exporter.DefaultConfig, *convertedDefaults)
+}
