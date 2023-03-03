0f400bb58f2cc2498eeb821909a31e58810db55d
Craig Peterson <192540+captncraig@users.noreply.github.com>
2023-03-03 13:34:14 -0500

    simplify test a bit

--- component/prometheus/kubernetes/operator/podMonitor_test.go
+++ component/prometheus/kubernetes/operator/podMonitor_test.go
@@ -1,12 +1,17 @@
 package operator
 
 import (
-	"fmt"
-	"os"
 	"testing"
+	"time"
 
 	"github.com/grafana/agent/pkg/util"
 	v1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
+	commonConfig "github.com/prometheus/common/config"
+	"github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/config"
+	"github.com/prometheus/prometheus/discovery"
+	promk8s "github.com/prometheus/prometheus/discovery/kubernetes"
+	"github.com/prometheus/prometheus/model/relabel"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"gopkg.in/yaml.v3"
@@ -14,13 +19,15 @@ import (
 )
 
 func TestGeneratePodMonitorConfig(t *testing.T) {
-	var falseVal = false
+	//var falseVal = false
 	suite := []struct {
-		name   string
-		m      *v1.PodMonitor
-		ep     v1.PodMetricsEndpoint
-		args   Arguments
-		expect string
+		name                   string
+		m                      *v1.PodMonitor
+		ep                     v1.PodMetricsEndpoint
+		args                   Arguments
+		expectedRelabels       string
+		expectedMetricRelabels string
+		expected               *config.ScrapeConfig
 	}{
 		{
 			name: "default",
@@ -33,193 +40,101 @@ func TestGeneratePodMonitorConfig(t *testing.T) {
 			ep: v1.PodMetricsEndpoint{
 				Port: "metrics",
 			},
-			expect: util.Untab(`
-			job_name: podMonitor/operator/podmonitor/0
-			honor_timestamps: true
-			scrape_interval: 1m
-			scrape_timeout: 10s
-			metrics_path: /metrics
-			scheme: http
-			follow_redirects: true
-			enable_http2: true
-			relabel_configs:
+			expectedRelabels: util.Untab(`
 				- source_labels: [job]
-				  separator: ;
-				  regex: (.*)
 				  target_label: __tmp_prometheus_job_name
-				  replacement: $1
-				  action: replace
 				- source_labels: [__meta_kubernetes_pod_phase]
-				  separator: ;
 				  regex: (Failed|Succeeded)
-				  replacement: $1
 				  action: drop
 				- source_labels: [__meta_kubernetes_pod_container_port_name]
-				  separator: ;
 				  regex: metrics
-				  replacement: $1
 				  action: keep
 				- source_labels: [__meta_kubernetes_namespace]
-				  separator: ;
-				  regex: (.*)
 				  target_label: namespace
-				  replacement: $1
-				  action: replace
 				- source_labels: [__meta_kubernetes_pod_container_name]
-				  separator: ;
-				  regex: (.*)
 				  target_label: container
-				  replacement: $1
-				  action: replace
 				- source_labels: [__meta_kubernetes_pod_name]
-				  separator: ;
-				  regex: (.*)
 				  target_label: pod
-				  replacement: $1
-				  action: replace
-				- separator: ;
-				  regex: (.*)
-				  target_label: job
+				- target_label: job
 				  replacement: operator/podmonitor
-				  action: replace
-				- separator: ;
-				  regex: (.*)
-				  target_label: endpoint
+				- target_label: endpoint
 				  replacement: metrics
-				  action: replace
-			kubernetes_sd_configs:
-				- role: pod
-				  kubeconfig_file: ""
-				  follow_redirects: false
-				  enable_http2: false
-				  namespaces:
-				    own_namespace: false
-				    names: [operator]
 			`),
-		},
-		{
-			name: "everything",
-			m: &v1.PodMonitor{
-				ObjectMeta: meta_v1.ObjectMeta{
-					Namespace: "operator",
-					Name:      "podmonitor",
+			expected: &config.ScrapeConfig{
+				JobName:         "podMonitor/operator/podmonitor/0",
+				HonorTimestamps: true,
+				ScrapeInterval:  model.Duration(time.Minute),
+				ScrapeTimeout:   model.Duration(10 * time.Second),
+				MetricsPath:     "/metrics",
+				Scheme:          "http",
+				HTTPClientConfig: commonConfig.HTTPClientConfig{
+					FollowRedirects: true,
+					EnableHTTP2:     true,
 				},
-				Spec: v1.PodMonitorSpec{
-					JobLabel:        "abc",
-					PodTargetLabels: []string{"label_a", "label_b"},
-					Selector: meta_v1.LabelSelector{
-						MatchLabels: map[string]string{"foo": "bar"},
-						// TODO: test a variety of matchexpressions
+				ServiceDiscoveryConfigs: discovery.Configs{
+					&promk8s.SDConfig{
+						Role: "pod",
+
+						NamespaceDiscovery: promk8s.NamespaceDiscovery{
+							IncludeOwnNamespace: false,
+							Names:               []string{"operator"},
+						},
 					},
-					NamespaceSelector: v1.NamespaceSelector{Any: false, MatchNames: []string{"ns_a", "ns_b"}},
 				},
 			},
-			ep: v1.PodMetricsEndpoint{
-				Port:        "metrics",
-				EnableHttp2: &falseVal,
-			},
-			expect: util.Untab(`
-			job_name: podMonitor/operator/podmonitor/1
-			honor_timestamps: true
-			scrape_interval: 1m
-			scrape_timeout: 10s
-			metrics_path: /metrics
-			scheme: http
-			follow_redirects: true
-			enable_http2: false
-			relabel_configs:
-				- source_labels: [job]
-				  separator: ;
-				  regex: (.*)
-				  target_label: __tmp_prometheus_job_name
-				  replacement: $1
-				  action: replace
-				- source_labels: [__meta_kubernetes_pod_phase]
-				  separator: ;
-				  regex: (Failed|Succeeded)
-				  replacement: $1
-				  action: drop
-				- source_labels: [__meta_kubernetes_pod_label_foo, __meta_kubernetes_pod_labelpresent_foo]
-				  separator: ;
-				  regex: (bar);true
-				  replacement: $1
-				  action: keep
-				- source_labels: [__meta_kubernetes_pod_container_port_name]
-				  separator: ;
-				  regex: metrics
-				  replacement: $1
-				  action: keep
-				- source_labels: [__meta_kubernetes_namespace]
-				  separator: ;
-				  regex: (.*)
-				  target_label: namespace
-				  replacement: $1
-				  action: replace
-				- source_labels: [__meta_kubernetes_pod_container_name]
-				  separator: ;
-				  regex: (.*)
-				  target_label: container
-				  replacement: $1
-				  action: replace
-				- source_labels: [__meta_kubernetes_pod_name]
-				  separator: ;
-				  regex: (.*)
-				  target_label: pod
-				  replacement: $1
-				  action: replace
-				- source_labels: [__meta_kubernetes_pod_label_label_a]
-				  separator: ;
-				  regex: (.+)
-				  target_label: label_a
-				  replacement: ${1}
-				  action: replace
-				- source_labels: [__meta_kubernetes_pod_label_label_b]
-				  separator: ;
-				  regex: (.+)
-				  target_label: label_b
-				  replacement: ${1}
-				  action: replace
-				- separator: ;
-				  regex: (.*)
-				  target_label: job
-				  replacement: operator/podmonitor
-				  action: replace
-				- source_labels: [__meta_kubernetes_pod_label_abc]
-				  separator: ;
-				  regex: (.+)
-				  target_label: job
-				  replacement: ${1}
-				  action: replace
-				- separator: ;
-				  regex: (.*)
-				  target_label: endpoint
-				  replacement: metrics
-				  action: replace
-			kubernetes_sd_configs:
-				- role: pod
-				  kubeconfig_file: ""
-				  follow_redirects: false
-				  enable_http2: false
-				  namespaces:
-				    own_namespace: false
-				    names: [ns_a,ns_b]
-			`),
 		},
+		// {
+		// 	name: "everything",
+		// 	m: &v1.PodMonitor{
+		// 		ObjectMeta: meta_v1.ObjectMeta{
+		// 			Namespace: "operator",
+		// 			Name:      "podmonitor",
+		// 		},
+		// 		Spec: v1.PodMonitorSpec{
+		// 			JobLabel:        "abc",
+		// 			PodTargetLabels: []string{"label_a", "label_b"},
+		// 			Selector: meta_v1.LabelSelector{
+		// 				MatchLabels: map[string]string{"foo": "bar"},
+		// 				// TODO: test a variety of matchexpressions
+		// 			},
+		// 			NamespaceSelector: v1.NamespaceSelector{Any: false, MatchNames: []string{"ns_a", "ns_b"}},
+		// 		},
+		// 	},
+		// 	ep: v1.PodMetricsEndpoint{
+		// 		Port:        "metrics",
+		// 		EnableHttp2: &falseVal,
+		// 	},
+		// },
 	}
 	for i, tc := range suite {
 		t.Run(tc.name, func(t *testing.T) {
-
 			cg := &configGenerator{
 				config: &tc.args,
 			}
 			cfg, err := cg.generatePodMonitorConfig(tc.m, tc.ep, i)
+			// check relabel configs seperately
+			rlcs := cfg.RelabelConfigs
+			mrlcs := cfg.MetricRelabelConfigs
+			cfg.RelabelConfigs = nil
+			cfg.MetricRelabelConfigs = nil
 			require.NoError(t, err)
-			actual, err := yaml.Marshal(cfg)
-			require.NoError(t, err)
-			if !assert.YAMLEq(t, tc.expect, string(actual)) {
-				fmt.Fprintln(os.Stderr, string(actual))
-			}
 
+			require.Equal(t, tc.expected, cfg)
+
+			// load expected relabel configs
+			exRlcs := []*relabel.Config{}
+			err = yaml.Unmarshal([]byte(tc.expectedRelabels), &exRlcs)
+			require.NoError(t, err)
+			assert.Equal(t, exRlcs, rlcs)
+
+			if tc.expectedMetricRelabels == "" {
+				assert.Nil(t, mrlcs)
+			} else {
+				exMrlcs := []*relabel.Config{}
+				err = yaml.Unmarshal([]byte(tc.expectedMetricRelabels), &exMrlcs)
+				require.NoError(t, err)
+				assert.Equal(t, exMrlcs, mrlcs)
+			}
 		})
 	}
 }
