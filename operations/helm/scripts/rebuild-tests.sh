b9b4693fd0934248ce336f766c74cd49c7a5f2d0
Carlos Juan Gómez Peñalver <carlosjuangp@gmail.com>
2023-03-07 15:09:06 +0000

    [issue-3173] Fix some values references and add new properties

--- operations/helm/scripts/rebuild-tests.sh
+++ operations/helm/scripts/rebuild-tests.sh
@@ -12,5 +12,6 @@ for FILEPATH in $TESTS; do
   FILENAME=$(basename $FILEPATH)
   TESTNAME=${FILENAME%.values.yaml}
 
+  echo "Render with file ${FILEPATH}"
   helm template --namespace default --debug ${CHART_NAME} ${CHART_PATH} -f ${FILEPATH} --output-dir ${OUTPUT_PATH}/${TESTNAME}
 done
