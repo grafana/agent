c54ba0c8dd9fb90f51ff9e47ba902ca69c461ddc
Carlos Juan Gómez Peñalver <carlosjuangp@gmail.com>
2023-03-09 11:11:05 +0000

    [issue-3173] Helm test generation script location aware

--- operations/helm/scripts/rebuild-tests.sh
+++ operations/helm/scripts/rebuild-tests.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 
-CHART_PATH="operations/helm/charts/grafana-agent"
-OUTPUT_PATH="operations/helm/tests"
+set +x
+
+CHART_PATH="charts/grafana-agent"
+OUTPUT_PATH="tests"
+
+CHART_PATH_FROM_REPO_ROOT="operations/helm/${CHART_PATH}"
+OUTPUT_PATH_FROM_REPO_ROOT="operations/helm/${OUTPUT_PATH}"
+
+CURRENT_DIR_NAME=${PWD##*/}
+
+if [ $CURRENT_DIR_NAME != "helm" ]
+then
+  CHART_PATH=$CHART_PATH_FROM_REPO_ROOT
+  OUTPUT_PATH=$OUTPUT_PATH_FROM_REPO_ROOT
+fi
 
 rm -rf $OUTPUT_PATH
 
