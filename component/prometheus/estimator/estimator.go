cc15641c5dc5948e8425364c6409c7e033b3d046
Ryan J. Geyer <me@ryangeyer.com>
2023-03-17 15:45:50 -0700

    Differentiate between metrics and series properly (I think)

--- component/prometheus/estimator/estimator.go
+++ component/prometheus/estimator/estimator.go
@@ -12,6 +12,7 @@ import (
 	"github.com/grafana/agent/component"
 	"github.com/grafana/agent/component/discovery"
 	"github.com/grafana/agent/component/prometheus"
+	"github.com/grafana/agent/pkg/flow/logging"
 	"github.com/prometheus/prometheus/model/labels"
 	"github.com/prometheus/prometheus/storage"
 )
@@ -34,9 +35,11 @@ type Component struct {
 	// This is maybe a hacky way to track unique combinations of seriesRef (map key), and label hash (map value).
 	// Could consume less memory by combining those two values into a single unique hash, but I'm not sure it's worth the additional effort ¯\_(ツ)_/¯
 	// Also, we may end up storing *actual* values for these, should we wish to report on them individually. I.E. There are x series with y label(s)
-	activeSeries      map[storage.SeriesRef]map[uint64]struct{}
+	activeSeries      map[string]map[storage.SeriesRef]struct{}
 	activeSeriesGauge prom_client.Gauge
+	metricsGauge      prom_client.Gauge
 	reg               prom_client.Registry
+	log               logging.Logger
 }
 
 type Arguments struct{}
@@ -49,31 +52,40 @@ type Exports struct {
 func New(o component.Options, args Arguments) (*Component, error) {
 	c := &Component{
 		opts:         o,
-		activeSeries: make(map[storage.SeriesRef]map[uint64]struct{}),
+		activeSeries: make(map[string]map[storage.SeriesRef]struct{}),
 		activeSeriesGauge: prom_client.NewGauge(
 			prom_client.GaugeOpts{
 				Name: "estimator_active_series",
 				Help: "The last count of active series being sent to the estimator",
 			},
 		),
+		metricsGauge: prom_client.NewGauge(
+			prom_client.GaugeOpts{
+				Name: "estimator_metrics",
+				Help: "The last count of metrics being sent to the estimator",
+			},
+		),
 		reg: *prom_client.NewRegistry(),
+		log: *o.Logger,
 	}
 
 	c.reg.MustRegister(c.activeSeriesGauge)
+	c.reg.MustRegister(c.metricsGauge)
 
 	interceptor := prometheus.NewInterceptor(
 		nil,
 		prometheus.WithAppendHook(func(globalRef storage.SeriesRef, l labels.Labels, t int64, v float64, next storage.Appender) (storage.SeriesRef, error) {
+			metricName := l.Get("__name__")
 
 			// TODO: Is `prometheus.GlobalRefMapping` required here?
-			_, ok := c.activeSeries[globalRef]
+			_, ok := c.activeSeries[metricName]
 			if !ok {
-				c.activeSeries[globalRef] = make(map[uint64]struct{})
+				c.activeSeries[metricName] = make(map[storage.SeriesRef]struct{})
 			}
 
-			_, ok = c.activeSeries[globalRef][l.Hash()]
+			_, ok = c.activeSeries[metricName][globalRef]
 			if !ok {
-				c.activeSeries[globalRef][l.Hash()] = struct{}{}
+				c.activeSeries[metricName][globalRef] = struct{}{}
 			}
 
 			return globalRef, nil
@@ -109,7 +121,7 @@ func (c *Component) Run(ctx context.Context) error {
 
 func (c *Component) Update(newConfig component.Arguments) error {
 	// Reset the tracked active series
-	c.activeSeries = make(map[storage.SeriesRef]map[uint64]struct{})
+	c.activeSeries = make(map[string]map[storage.SeriesRef]struct{})
 	return nil
 }
 
@@ -121,14 +133,20 @@ func (c *Component) currentActiveSeriesCount() uint64 {
 	return uint64(series)
 }
 
+func (c *Component) currentMetricsCount() uint64 {
+	return uint64(len(c.activeSeries))
+}
+
 func (c *Component) DebugInfo() interface{} {
 	return debugInfo{
 		ActiveSeries: c.currentActiveSeriesCount(),
+		Metrics:      c.currentMetricsCount(),
 	}
 }
 
 type debugInfo struct {
 	ActiveSeries uint64 `river:"active_series,attr"`
+	Metrics      uint64 `river:"metrics,attr"`
 }
 
 func (c *Component) Handler() http.Handler {
@@ -137,7 +155,9 @@ func (c *Component) Handler() http.Handler {
 
 func (c *Component) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	series := c.currentActiveSeriesCount()
+	metrics := c.currentMetricsCount()
 	c.activeSeriesGauge.Set(float64(series))
+	c.metricsGauge.Set(float64(metrics))
 	if req.URL.Path == "/metrics" {
 		promhttp.HandlerFor(&c.reg, promhttp.HandlerOpts{Registry: &c.reg}).ServeHTTP(w, req)
 		return
