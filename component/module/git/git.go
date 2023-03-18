d5cd66a7b25c6404d0de1c52bc9b396d3a1de797
Robert Fratto <robertfratto@gmail.com>
2023-03-18 16:12:07 -0400

    module.git: initial commit
    This commit prototypes a `module.git` module loader. `module.git` clones
    and manages a Git repository and loads a modules from a file within the
    managed repository.
    
    The revision of the repository to retrieve can be any recognized
    revision; including branch names, tag names, or individual commit SHAs.
    
    The repository reference can be anything recognized by `git clone`,
    including the local filesystem.
    
    The repository will be fetched with the given pull_frequency argument.
    If pull_frequency is 0, then the repository will only be re-fetched when
    the component is updated. Setting this argument is primarily useful when
    pointing `module.git` at a branch.
    
    I have published a repository at
    https://github.com/rfratto/agent-modules to be used with this PR for
    examples.
    
    This configuration file can load one of the example modules from that
    repository:
    
        logging {
                level = "debug"
        }
    
        module.git "add" {
                repository = "https://github.com/rfratto/agent-modules.git"
                revision   = "main"
                path       = "add/module.river"
    
                arguments = {
                        a = 15,
                        b = 45,
                }
        }

---
+++ component/module/git/git.go
@@ -0,0 +1,301 @@
+// Package git implements the module.git component.
+package git
+
+import (
+	"context"
+	"net/http"
+	"path/filepath"
+	"reflect"
+	"sync"
+	"time"
+
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
+	"github.com/gorilla/mux"
+	"github.com/grafana/agent/component"
+	"github.com/grafana/agent/component/module/git/internal/vcs"
+	"github.com/grafana/agent/pkg/flow"
+	"github.com/grafana/agent/pkg/flow/logging"
+	"github.com/grafana/agent/pkg/flow/tracing"
+	"github.com/grafana/agent/pkg/river"
+	"github.com/grafana/agent/web/api"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+func init() {
+	component.Register(component.Registration{
+		Name:    "module.git",
+		Args:    Arguments{},
+		Exports: Exports{},
+
+		Build: func(opts component.Options, args component.Arguments) (component.Component, error) {
+			return New(opts, args.(Arguments))
+		},
+	})
+}
+
+// Arguments configures the module.git component.
+type Arguments struct {
+	Repository string `river:"repository,attr"`
+	Revision   string `river:"revision,attr"`
+	Path       string `river:"path,attr"`
+
+	PullFrequency time.Duration `river:"pull_frequency,attr,optional"`
+
+	Arguments map[string]any `river:"arguments,attr,optional"`
+}
+
+// DefaultArguments holds default settings for Arguments.
+var DefaultArguments = Arguments{
+	PullFrequency: time.Minute,
+}
+
+var _ river.Unmarshaler = (*Arguments)(nil)
+
+// UnmarshalRiver implements river.Unmarshaler and applies default settings to
+// args.
+func (args *Arguments) UnmarshalRiver(f func(interface{}) error) error {
+	*args = DefaultArguments
+
+	type arguments Arguments
+	return f((*arguments)(args))
+}
+
+// Exports are emitted by the module.git component.
+type Exports struct {
+	Exports map[string]any `river:"exports,attr"`
+}
+
+// Component implements the module.git component.
+type Component struct {
+	opts component.Options
+	log  log.Logger
+	ctrl *flow.Flow
+
+	mut      sync.RWMutex
+	repo     *vcs.GitRepo
+	repoOpts vcs.GitRepoOptions
+	args     Arguments
+
+	argsChanged chan struct{}
+
+	healthMut sync.RWMutex
+	health    component.Health
+}
+
+var (
+	_ component.Component       = (*Component)(nil)
+	_ component.HealthComponent = (*Component)(nil)
+	_ component.HTTPComponent   = (*Component)(nil)
+)
+
+// New creates a new module.git component.
+func New(o component.Options, args Arguments) (*Component, error) {
+	// TODO(rfratto): replace these with a tracer/registry which properly
+	// propagates data back to the parent.
+	flowTracer, _ := tracing.New(tracing.DefaultOptions)
+	flowRegistry := prometheus.NewRegistry()
+
+	c := &Component{
+		opts: o,
+		log:  o.Logger,
+
+		ctrl: flow.New(flow.Options{
+			ControllerID: o.ID,
+			LogSink:      logging.LoggerSink(o.Logger),
+			Tracer:       flowTracer,
+			Reg:          flowRegistry,
+
+			DataPath:       o.DataPath,
+			HTTPPathPrefix: o.HTTPPath,
+			HTTPListenAddr: o.HTTPListenAddr,
+
+			OnExportsChange: func(exports map[string]any) {
+				o.OnStateChange(Exports{Exports: exports})
+			},
+		}),
+
+		argsChanged: make(chan struct{}, 1),
+	}
+
+	if err := c.Update(args); err != nil {
+		return nil, err
+	}
+	return c, nil
+}
+
+// Run implements component.Component.
+func (c *Component) Run(ctx context.Context) error {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	go c.ctrl.Run(ctx)
+
+	var (
+		ticker  *time.Ticker
+		tickerC <-chan time.Time
+	)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+
+		case <-c.argsChanged:
+			c.mut.Lock()
+			{
+				level.Info(c.log).Log("msg", "updating repository pull frequency", "new_frequency", c.args.PullFrequency)
+
+				if c.args.PullFrequency > 0 {
+					if ticker == nil {
+						ticker = time.NewTicker(c.args.PullFrequency)
+						tickerC = ticker.C
+					} else {
+						ticker.Reset(c.args.PullFrequency)
+					}
+				} else {
+					if ticker != nil {
+						ticker.Stop()
+					}
+					ticker = nil
+					tickerC = nil
+				}
+			}
+			c.mut.Unlock()
+
+		case <-tickerC:
+			level.Info(c.log).Log("msg", "updating repository", "new_frequency", c.args.PullFrequency)
+			c.tickPollFile(ctx)
+		}
+	}
+}
+
+func (c *Component) tickPollFile(ctx context.Context) {
+	c.mut.Lock()
+	err := c.pollFile(ctx, c.args)
+	c.mut.Unlock()
+
+	c.updateHealth(err)
+}
+
+func (c *Component) updateHealth(err error) {
+	c.healthMut.Lock()
+	defer c.healthMut.Unlock()
+
+	if err != nil {
+		c.health = component.Health{
+			Health:     component.HealthTypeUnhealthy,
+			Message:    err.Error(),
+			UpdateTime: time.Now(),
+		}
+	} else {
+		c.health = component.Health{
+			Health:     component.HealthTypeHealthy,
+			Message:    "module updated",
+			UpdateTime: time.Now(),
+		}
+	}
+}
+
+// Update implements component.Component.
+func (c *Component) Update(args component.Arguments) (err error) {
+	defer func() {
+		c.updateHealth(err)
+	}()
+
+	c.mut.Lock()
+	defer c.mut.Unlock()
+
+	newArgs := args.(Arguments)
+
+	// TODO(rfratto): store in a repo-specific directory so changing repositories
+	// doesn't break the module loader.
+	repoPath := filepath.Join(c.opts.DataPath, "repo")
+
+	repoOpts := vcs.GitRepoOptions{
+		Repository: newArgs.Repository,
+		Revision:   newArgs.Revision,
+	}
+
+	// Create or update the repo field.
+	if c.repo == nil || !reflect.DeepEqual(repoOpts, c.repoOpts) {
+		r, err := vcs.NewGitRepo(context.Background(), repoPath, repoOpts)
+		if err != nil {
+			return err
+		}
+		c.repo = r
+		c.repoOpts = repoOpts
+	}
+
+	if err := c.pollFile(context.Background(), newArgs); err != nil {
+		return err
+	}
+
+	// Schedule an update for handling the changed arguments.
+	select {
+	case c.argsChanged <- struct{}{}:
+	default:
+	}
+
+	c.args = newArgs
+	return nil
+}
+
+// pollFile fetches the latest content from the repository and updates the
+// controller. pollFile must only be called with c.mut held.
+func (c *Component) pollFile(ctx context.Context, args Arguments) error {
+	// Make sure our repo is up-to-date.
+	if err := c.repo.Update(ctx); err != nil {
+		return err
+	}
+
+	// Finally, configure our controller.
+	bb, err := c.repo.ReadFile(args.Path)
+	if err != nil {
+		return err
+	}
+
+	f, err := flow.ReadFile(c.opts.ID, bb)
+	if err != nil {
+		return err
+	}
+
+	return c.ctrl.LoadFile(f, args.Arguments)
+}
+
+// CurrentHealth implements component.HealthComponent.
+func (c *Component) CurrentHealth() component.Health {
+	c.healthMut.RLock()
+	defer c.healthMut.RUnlock()
+
+	return c.health
+}
+
+// Handler implements component.HTTPComponent.
+func (c *Component) Handler() http.Handler {
+	r := mux.NewRouter()
+
+	fa := api.NewFlowAPI(c.ctrl, r)
+	fa.RegisterRoutes("/", r)
+
+	r.PathPrefix("/{id}/").Handler(c.ctrl.ComponentHandler())
+	return r
+}
+
+// DebugInfo implements component.DebugComponent.
+func (c *Component) DebugInfo() interface{} {
+	type DebugInfo struct {
+		SHA       string `river:"sha,attr"`
+		RepoError string `river:"repo_error,attr,optional"`
+	}
+
+	c.mut.RLock()
+	defer c.mut.RUnlock()
+
+	rev, err := c.repo.CurrentRevision()
+	if err != nil {
+		return DebugInfo{RepoError: err.Error()}
+	} else {
+		return DebugInfo{SHA: rev}
+	}
+}
---
+++ component/module/git/internal/vcs/git.go
@@ -0,0 +1,177 @@
+package vcs
+
+import (
+	"context"
+	"errors"
+	"io"
+	"path/filepath"
+
+	"github.com/go-git/go-billy/v5/osfs"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/cache"
+	"github.com/go-git/go-git/v5/storage/filesystem"
+)
+
+type GitRepoOptions struct {
+	Repository string
+	Revision   string
+}
+
+// GitRepo manages a Git repository for the purposes of retrieving a file from
+// it.
+type GitRepo struct {
+	opts     GitRepoOptions
+	repo     *git.Repository
+	workTree *git.Worktree
+}
+
+// NewGitRepo creates a new instance of a GitRepo, where the Git repository is
+// managed at storagePath.
+//
+// If storagePath is empty on disk, NewGitRepo initializes GitRepo by cloning
+// the repository. Otherwise, NewGitRepo will do a fetch.
+//
+// After GitRepo is initialized, it checks out to the Revision specified in
+// GitRepoOptions.
+func NewGitRepo(ctx context.Context, storagePath string, opts GitRepoOptions) (*GitRepo, error) {
+	var (
+		gitFsys  = osfs.New(filepath.Join(storagePath, ".git"))
+		lruCache = cache.NewObjectLRUDefault()
+		storage  = filesystem.NewStorage(gitFsys, lruCache)
+
+		workFsys = osfs.New(storagePath)
+	)
+
+	var (
+		repo *git.Repository
+		err  error
+	)
+
+	// If the .git directory is empty, we need to clone the repository.
+	if fi, readErr := gitFsys.ReadDir(""); readErr != nil || len(fi) == 0 {
+		repo, err = git.CloneContext(ctx, storage, workFsys, &git.CloneOptions{
+			URL:               opts.Repository,
+			ReferenceName:     plumbing.HEAD,
+			RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
+			Tags:              git.AllTags,
+		})
+	} else {
+		repo, err = git.Open(storage, workFsys)
+	}
+	if err != nil {
+		return nil, DownloadFailedError{
+			Repository: opts.Repository,
+			Inner:      err,
+		}
+	}
+
+	// Fetch the latest contents. This may be a no-op if we just did a clone.
+	err = repo.FetchContext(ctx, &git.FetchOptions{
+		RemoteName: "origin",
+		Force:      true,
+	})
+	if err != nil && !errors.Is(err, git.NoErrAlreadyUpToDate) {
+		return nil, UpdateFailedError{
+			Repository: opts.Repository,
+			Inner:      err,
+		}
+	}
+
+	// Finally, hard reset to our requested revision.
+	hash, err := findRevision(opts.Revision, repo)
+	if err != nil {
+		return nil, InvalidRevisionError{Revision: opts.Revision}
+	}
+
+	workTree, err := repo.Worktree()
+	if err != nil {
+		return nil, err
+	}
+	err = workTree.Reset(&git.ResetOptions{
+		Commit: hash,
+		Mode:   git.HardReset,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &GitRepo{
+		opts:     opts,
+		repo:     repo,
+		workTree: workTree,
+	}, nil
+}
+
+// Update updates the repository by fetching new content and re-checking out to
+// latest version of Revision.
+func (repo *GitRepo) Update(ctx context.Context) error {
+	err := repo.repo.FetchContext(ctx, &git.FetchOptions{
+		RemoteName: "origin",
+		Force:      true,
+	})
+	if err != nil && !errors.Is(err, git.NoErrAlreadyUpToDate) {
+		return UpdateFailedError{
+			Repository: repo.opts.Repository,
+			Inner:      err,
+		}
+	}
+
+	// Find the latest revision being requested and hard-reset to it.
+	hash, err := findRevision(repo.opts.Revision, repo.repo)
+	if err != nil {
+		return InvalidRevisionError{Revision: repo.opts.Revision}
+	}
+
+	err = repo.workTree.Reset(&git.ResetOptions{
+		Commit: hash,
+		Mode:   git.HardReset,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// ReadFile returns a file from the repository specified by path.
+func (repo *GitRepo) ReadFile(path string) ([]byte, error) {
+	f, err := repo.workTree.Filesystem.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	return io.ReadAll(f)
+}
+
+// CurrentRevision returns the current revision of the repository (by SHA).
+func (repo *GitRepo) CurrentRevision() (string, error) {
+	ref, err := repo.repo.Head()
+	if err != nil {
+		return "", err
+	}
+	return ref.Hash().String(), nil
+}
+
+func findRevision(rev string, repo *git.Repository) (plumbing.Hash, error) {
+	// Try looking for the revision in the following order:
+	//
+	// 1. Search by tag name.
+	// 2. Search by remote ref name.
+	// 3. Try to resolve the revision directly.
+
+	if tagRef, err := repo.Tag(rev); err == nil {
+		return tagRef.Hash(), nil
+	}
+
+	if remoteRef, err := repo.Reference(plumbing.NewRemoteReferenceName("origin", rev), true); err == nil {
+		return remoteRef.Hash(), nil
+	}
+
+	if hash, err := repo.ResolveRevision(plumbing.Revision(rev)); err == nil {
+		return *hash, nil
+	}
+
+	return plumbing.ZeroHash, plumbing.ErrReferenceNotFound
+}
