f4f742c929c3b752891f2d1721dcd1f86820901f
Carlos Juan Gómez Peñalver <carlosjuangp@gmail.com>
2023-03-23 09:28:23 +0000

    [issue-3173 1] Rebuild Helm tests scripts outputs always to the same place

--- operations/helm/scripts/rebuild-tests.sh
+++ operations/helm/scripts/rebuild-tests.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 
-CHART_PATH="operations/helm/charts/grafana-agent"
-OUTPUT_PATH="operations/helm/tests"
+set +x
+
+CHART_PATH="charts/grafana-agent"
+OUTPUT_PATH="tests"
+
+CHART_PATH_FROM_REPO_ROOT="operations/helm/${CHART_PATH}"
+OUTPUT_PATH_FROM_REPO_ROOT="operations/helm/${OUTPUT_PATH}"
+
+CURRENT_DIR_NAME=${PWD##*/}
+
+if [ $CURRENT_DIR_NAME != "helm" ]
+then
+  CHART_PATH=$CHART_PATH_FROM_REPO_ROOT
+  OUTPUT_PATH=$OUTPUT_PATH_FROM_REPO_ROOT
+fi
 
 rm -rf $OUTPUT_PATH
 
@@ -12,5 +25,6 @@ for FILEPATH in $TESTS; do
   FILENAME=$(basename $FILEPATH)
   TESTNAME=${FILENAME%.values.yaml}
 
+  echo "Render with file ${FILEPATH}"
   helm template --namespace default --debug ${CHART_NAME} ${CHART_PATH} -f ${FILEPATH} --output-dir ${OUTPUT_PATH}/${TESTNAME} --set '$chart_tests=true'
 done
