1a4871cd45ef4781e618cd88cfcc01c3a46dc537
Stefan Eskelid <stefanes@duck.com>
2023-04-05 23:15:48 +0200

    Update grafanacloud-install.ps1

--- production/grafanacloud-install.ps1
+++ production/grafanacloud-install.ps1
@@ -1,93 +1,88 @@
-# Script to install Grafana agen for Windows
-param ($GCLOUD_STACK_ID, $GCLOUD_API_KEY, $GCLOUD_API_URL)
+#Requires -RunAsAdministrator
+
+# Script to install Grafana agent for Windows
+param ($GCLOUD_HOSTED_METRICS_URL, $GCLOUD_HOSTED_METRICS_ID, $GCLOUD_SCRAPE_INTERVAL, $GCLOUD_HOSTED_LOGS_URL, $GCLOUD_HOSTED_LOGS_ID, $GCLOUD_RW_API_KEY)
 
 Write-Host "Setting up Grafana agent"
 
-if ( -Not [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544") ) {
-  Write-Host "ERROR: The script needs to be run with Administrator privileges"
-  exit
-}
-
 # Check if required parameters are present
-if ($GCLOUD_STACK_ID -eq "") {
-  Write-Host "ERROR: Required argument GCLOUD_STACK_ID missing"
-  exit
+if ($GCLOUD_HOSTED_METRICS_URL -eq "") {
+	Write-Host "ERROR: Required argument GCLOUD_HOSTED_METRICS_URL missing"
+	exit
 }
 
-if ($GCLOUD_API_KEY -eq "") {
-  Write-Host "ERROR:  Required argument GCLOUD_API_KEY missing"
-  exit
+if ($GCLOUD_HOSTED_METRICS_ID -eq "") {
+	Write-Host "ERROR: Required argument GCLOUD_HOSTED_METRICS_ID missing"
+	exit
 }
 
-if ($GCLOUD_API_URL -eq "") {
-  Write-Host "ERROR: Required argument GCLOUD_API_URL missing"
-  exit
+if ($GCLOUD_SCRAPE_INTERVAL -eq "") {
+	Write-Host "ERROR: Required argument GCLOUD_SCRAPE_INTERVAL missing"
+	exit
 }
 
-Write-Host "GCLOUD_STACK_ID:" $GCLOUD_STACK_ID
-Write-Host "GCLOUD_API_KEY:" $GCLOUD_API_KEY
-Write-Host "GCLOUD_API_URL:" $GCLOUD_API_URL
+if ($GCLOUD_HOSTED_LOGS_URL -eq "") {
+	Write-Host "ERROR: Required argument GCLOUD_HOSTED_LOGS_URL missing"
+	exit
+}
 
-# Install Module Powershell-yaml required to convert agent config from json to yaml
-Write-Host "Checking and installing required Powershell-yaml module"
-Install-Module PowerShell-yaml
+if ($GCLOUD_HOSTED_LOGS_ID -eq "") {
+	Write-Host "ERROR: Required argument GCLOUD_HOSTED_LOGS_ID missing"
+	exit
+}
+
+if ($GCLOUD_RW_API_KEY -eq "") {
+	Write-Host "ERROR: Required argument GCLOUD_RW_API_KEY missing"
+	exit
+}
+
+Write-Host "GCLOUD_HOSTED_METRICS_URL:" $GCLOUD_HOSTED_METRICS_URL
+Write-Host "GCLOUD_HOSTED_METRICS_ID:" $GCLOUD_HOSTED_METRICS_ID
+Write-Host "GCLOUD_SCRAPE_INTERVAL:" $GCLOUD_SCRAPE_INTERVAL
+Write-Host "GCLOUD_HOSTED_LOGS_URL:" $GCLOUD_HOSTED_LOGS_URL
+Write-Host "GCLOUD_HOSTED_LOGS_ID:" $GCLOUD_HOSTED_LOGS_ID
+Write-Host "GCLOUD_RW_API_KEY:" $GCLOUD_RW_API_KEY
 
 Write-Host "Downloading Grafana agent Windows Installer"
-$DOWLOAD_URL = "https://github.com/grafana/agent/releases/latest/download/grafana-agent-installer.exe.zip"
-$OUTPUT_ZIP_FILE = ".\grafana-agent-installer.zip"
-$OUTPUT_FILE = ".\grafana-agent-installer"
-Invoke-WebRequest -Uri $DOWLOAD_URL -OutFile $OUTPUT_ZIP_FILE
-Expand-Archive -LiteralPath $OUTPUT_ZIP_FILE -DestinationPath $OUTPUT_FILE
+$DOWNLOAD_URL = "https://github.com/grafana/agent/releases/latest/download/grafana-agent-installer.exe.zip"
+$OUTPUT_ZIP_FILE = ".\grafana-agent-installer.exe.zip"
+$WORKING_DIR = Get-Location
+Invoke-WebRequest -Uri $DOWNLOAD_URL -OutFile $OUTPUT_ZIP_FILE
+Expand-Archive -LiteralPath $OUTPUT_ZIP_FILE -DestinationPath $WORKING_DIR.path
 
 # Install Grafana agent in silent mode
 Write-Host "Installing Grafana agent for Windows"
-.\grafana-agent-installer\grafana-agent-installer.exe /S
+.\grafana-agent-installer.exe /S
 
-Write-Host "Retrieving and updating Grafana agent config"
-$CONFIG_URI = "$GCLOUD_API_URL/stacks/$GCLOUD_STACK_ID/agent_config?platforms=windows"
-$AUTH_TOKEN = "Bearer $GCLOUD_API_KEY"
+Write-Host "Retrieving Grafana agent config"
+$CONFIG_URI = "https://storage.googleapis.com/cloud-onboarding/agent/config/config.yaml"
+$CONFIG_FILE = ".\grafana-agent.yaml"
+Invoke-WebRequest -Uri $CONFIG_URI -Outfile $CONFIG_FILE
 
-$headers = @{
-    Authorization = $AUTH_TOKEN
-}
+Write-Host "Updating agent config file"
+$content = Get-Content $CONFIG_FILE
+$content = $content.Replace("{GCLOUD_HOSTED_METRICS_URL}", $GCLOUD_HOSTED_METRICS_URL)
+$content = $content.Replace("{GCLOUD_HOSTED_METRICS_ID}", $GCLOUD_HOSTED_METRICS_ID)
+$content = $content.Replace("{GCLOUD_SCRAPE_INTERVAL}", $GCLOUD_SCRAPE_INTERVAL)
+$content = $content.Replace("{GCLOUD_HOSTED_LOGS_URL}", $GCLOUD_HOSTED_LOGS_URL)
+$content = $content.Replace("{GCLOUD_HOSTED_LOGS_ID}", $GCLOUD_HOSTED_LOGS_ID)
+$content = $content.Replace("{GCLOUD_RW_API_KEY}", $GCLOUD_RW_API_KEY)
+$content | Set-Content $CONFIG_FILE
 
-$response = Invoke-WebRequest $CONFIG_URI -Method 'GET' -Headers $headers -UseBasicParsing
+Move-Item $config_file "C:\Program Files\Grafana Agent\agent-config.yaml" -force
 
-$jsonObj = $response | ConvertFrom-Json
-if ($jsonObj.status -eq "success") {
-  $config_file = ".\agent-config.yaml"
-  Write-Host "Saving and updating agent configuration file"
-  $yamlConfig = $jsonObj.data | ConvertTo-Yaml
-  Set-Content -Path $config_file -Value ($yamlConfig)
-    # Append APPDATA path to bookmark files
-    $line = Get-Content $config_file | Select-String bookmark_path | Select-Object -ExpandProperty Line
-    if ($line -ne $null) {
-        $content = Get-Content $config_file
-        $line | ForEach-Object {
-            $split_line = $_ -split ": "
-            $prefix = $split_line[0]
-            $bookmark_filename = $split_line[1] -replace "./",""
-            $content = $content -replace $_,"$($prefix): $($env:APPDATA)\$($bookmark_filename)"
-        }
-        $content | Set-Content $config_file
-    }
-    Move-Item $config_file "C:\Program Files\Grafana Agent\agent-config.yaml" -force
+# Wait for service to initialize after first install
+Write-Host "Wait for Grafana agent service to initialize"
+Start-Sleep -s 5
 
-  # Wait for service to initialize after first install
-  Write-Host "Wait for Grafana service to initialize"
-  Start-Sleep -s 5
+# Restart Grafana agent to load new configuration
+Write-Host "Restarting Grafana agent service"
+Stop-Service "Grafana Agent"
+Start-Service "Grafana Agent"
 
-  # Restart Grafana agent to load new configuration
-  Write-Host "Restarting Grafana agent service"
-  Stop-Service "Grafana Agent"
-  Start-Service "Grafana Agent"
+# Wait for service to startup after restart
+Write-Host "Wait for Grafana service to initialize after restart"
+Start-Sleep -s 10
 
-  # Wait for service to startup after restart
-  Write-Host "Wait for Grafana service to initialize after restart"
-  Start-Sleep -s 10
-
-  # Show Grafana agent service status
-  Get-Service "Grafana Agent"
-} else {
-  Write-Host "Failed to retrieve config"
-}
+# Show Grafana agent service status
+Get-Service "Grafana Agent"
