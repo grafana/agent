1ef9c373723fb1d7a847b85f23473c0cbfe57973
Piotr Gwizdala <17101802+thampiotr@users.noreply.github.com>
2023-04-20 10:30:26 +0100

    basics working - wired things together

--- component/loki/source/http/http.go
+++ component/loki/source/http/http.go
@@ -6,17 +6,23 @@ import (
 	"github.com/efficientgo/core/errors"
 	"github.com/grafana/agent/component"
 	"github.com/grafana/agent/component/common/loki"
-	"github.com/grafana/agent/pkg/flow/logging"
-	"io"
-	"net/http"
+	"github.com/grafana/agent/component/common/relabel"
+	"github.com/grafana/agent/component/loki/source/http/internal/lokipush"
+	"github.com/grafana/loki/clients/pkg/promtail/api"
+	"github.com/grafana/loki/clients/pkg/promtail/scrapeconfig"
+	"github.com/prometheus/common/model"
+	"github.com/weaveworks/common/server"
 )
 
 type Arguments struct {
-	HttpEndpoint string              `river:"http_endpoint,attr"`
-	ForwardTo    []loki.LogsReceiver `river:"forward_to,attr"`
+	HttpAddress string              `river:"http_address,attr"`
+	HttpPort    int                 `river:"http_port,attr"`
+	ForwardTo   []loki.LogsReceiver `river:"forward_to,attr"`
 
-	//TODO: add support for additional labels, like loki push api in promtail https://grafana.com/docs/loki/next/clients/promtail/configuration/#loki_push_api
-	//TODO: add support for use_incoming_timestamp like in https://grafana.com/docs/loki/next/clients/promtail/configuration/#loki_push_api
+	Labels               map[string]string `river:"labels,attr,optional"`
+	RelabelRules         relabel.Rules     `river:"relabel_rules,attr,optional"`
+	UseIncomingTimestamp bool              `river:"use_incoming_timestamp,attr,optional"`
+
 	//TODO: add support for http_server_read_timeout like in https://grafana.com/docs/loki/next/clients/promtail/configuration/#loki_push_api
 	//TODO: add support for http_server_write_timeout like in https://grafana.com/docs/loki/next/clients/promtail/configuration/#loki_push_api
 	//TODO: add support for http_server_idle_timeout like in https://grafana.com/docs/loki/next/clients/promtail/configuration/#loki_push_api
@@ -47,26 +53,35 @@ func New(opts component.Options, args Arguments) component.Component {
 func (c *Component) Run(ctx context.Context) error {
 	c.opts.Logger.Log("msg", "starting component")
 
-	server := &http.Server{Addr: c.args.HttpEndpoint, Handler: &httpSinkHandler{log: c.opts.Logger}}
-	serverDone := make(chan struct{})
+	entriesChan := make(chan api.Entry)
+	entryHandler := api.NewEntryHandler(
+		entriesChan,
+		func() {
+			c.opts.Logger.Log("msg", "entry handler stopped")
+		})
 
-	go func() {
-		c.opts.Logger.Log("msg", "server listening")
-		err := server.ListenAndServe()
-		if err != nil {
-			c.opts.Logger.Log("msg", "server failed", "error", err)
-		}
-		serverDone <- struct{}{}
-	}()
+	pushTarget, err := lokipush.NewPushTarget(
+		c.opts.Logger,
+		entryHandler,
+		relabel.ComponentToPromRelabelConfigs(c.args.RelabelRules),
+		// TODO: pick correct metric names
+		"loki_source_http",
+		c.args.toPushTargetConfig(),
+	)
+
+	if err != nil {
+		return errors.Wrapf(err, "failed to create loki push API server: %v", err)
+	}
 
 	for {
 		select {
 		case <-ctx.Done():
 			c.opts.Logger.Log("msg", "finishing due to context done")
-			return nil
-		case <-serverDone:
-			c.opts.Logger.Log("msg", "finishing due to server exit")
-			return nil
+			// When PushTarget is stopped, it will also Stop() the entry handler.
+			return pushTarget.Stop()
+		case entry := <-entriesChan:
+			// TODO: fan out
+			c.opts.Logger.Log("msg", fmt.Sprintf("got log message ====> %v", entry))
 		}
 	}
 }
@@ -77,23 +92,25 @@ func (c *Component) Update(args component.Arguments) error {
 	} else {
 		c.args = newArgs
 	}
+	// TODO: implement update properly...
 	return nil
 }
 
-// ------------- debug -----------
-type httpSinkHandler struct {
-	log *logging.Logger
+func (a *Arguments) toPushTargetConfig() *scrapeconfig.PushTargetConfig {
+	return &scrapeconfig.PushTargetConfig{
+		Server: server.Config{
+			HTTPListenPort:    a.HttpPort,
+			HTTPListenAddress: a.HttpAddress,
+		},
+		Labels:        a.labelSet(),
+		KeepTimestamp: a.UseIncomingTimestamp,
+	}
 }
 
-func (h *httpSinkHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	allBody, _ := io.ReadAll(request.Body)
-	h.log.Log(
-		"msg", "got request!",
-		"url", request.URL.String(),
-		"method", request.Method,
-		"body", string(allBody),
-		"headers", fmt.Sprintf("%+#v", request.Header))
-	_, _ = writer.Write([]byte("thank you for your request!"))
+func (a *Arguments) labelSet() model.LabelSet {
+	labelSet := make(model.LabelSet, len(a.Labels))
+	for k, v := range a.Labels {
+		labelSet[model.LabelName(k)] = model.LabelValue(v)
+	}
+	return labelSet
 }
-
-// -------------------------------
