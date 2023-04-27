172a8eabf35e06b2827d2f756049856741c67518
Pablo Balbi <pablo.l.balbi@gmail.com>
2023-04-27 14:41:32 -0300

    add tests with river raw config

--- component/common/loki/net/config_test.go
+++ component/common/loki/net/config_test.go
@@ -4,33 +4,46 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+
+	"github.com/grafana/agent/pkg/river"
+	weaveworks "github.com/weaveworks/common/server"
 )
 
-func TestConfig_Default(t *testing.T) {
-	cfg := ServerConfig{}
-	serverConfig := cfg.Convert()
-
-	// assert over defaults specified by agent code
-	require.Equal(t, 0, serverConfig.HTTPListenPort)
-	require.Equal(t, 0, serverConfig.GRPCListenPort)
-}
-
-func TestConfig_OverridingDefaults(t *testing.T) {
-	cfg := ServerConfig{
-		HTTP: &HTTPConfig{
-			ListenAddress: "0.0.0.0",
-			ListenPort:    4000,
+func TestConfig(t *testing.T) {
+	type testcase struct {
+		raw         string
+		errExpected bool
+		assert      func(t *testing.T, config weaveworks.Config)
+	}
+	var cases = map[string]testcase{
+		"empty config applies defaults": {
+			raw: ``,
+			assert: func(t *testing.T, config weaveworks.Config) {
+				require.Equal(t, "", config.HTTPListenAddress)
+				require.Equal(t, 0, config.HTTPListenPort)
+				require.Equal(t, "", config.GRPCListenAddress)
+				require.Equal(t, 0, config.GRPCListenPort)
+			},
 		},
-		GRPC: &GRPCConfig{
-			ListenAddress: "0.0.0.0",
-			ListenPort:    4001,
+		"overriding defaults": {
+			raw: `
+			http {
+				listen_port = 8080
+				listen_address = "0.0.0.0"
+			}`,
+			assert: func(t *testing.T, config weaveworks.Config) {
+				require.Equal(t, 8080, config.HTTPListenPort)
+				require.Equal(t, "0.0.0.0", config.HTTPListenAddress)
+			},
 		},
 	}
-	serverConfig := cfg.Convert()
-
-	// assert over defaults specified by agent code
-	require.Equal(t, 4000, serverConfig.HTTPListenPort)
-	require.Equal(t, "0.0.0.0", serverConfig.HTTPListenAddress)
-	require.Equal(t, 4001, serverConfig.GRPCListenPort)
-	require.Equal(t, "0.0.0.0", serverConfig.GRPCListenAddress)
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			cfg := ServerConfig{}
+			err := river.Unmarshal([]byte(tc.raw), &cfg)
+			require.Equal(t, tc.errExpected, err != nil)
+			wConfig := cfg.Convert()
+			tc.assert(t, wConfig)
+		})
+	}
 }
