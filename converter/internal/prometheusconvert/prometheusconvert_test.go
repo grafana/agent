625cfd53b3613dc66da8cfb8a9f3da608c5ca946
Erik Baranowski <39704712+erikbaranowski@users.noreply.github.com>
2023-05-18 13:19:51 -0400

    Config API v1 (#3898)
    * Committing all prometheus prototypes in case we want to reference them again before finalizing the approach.
    
    Signed-off-by: erikbaranowski <39704712+erikbaranowski@users.noreply.github.com>
    
    * Implement first version of config converter.
    
    Signed-off-by: erikbaranowski <39704712+erikbaranowski@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: erikbaranowski <39704712+erikbaranowski@users.noreply.github.com>

---
+++ converter/internal/prometheusconvert/prometheusconvert_test.go
@@ -0,0 +1,60 @@
+package prometheusconvert_test
+
+import (
+	"bytes"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/grafana/agent/converter/internal/prometheusconvert"
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	promSuffix = ".yaml"
+	flowSuffix = ".river"
+)
+
+func TestConvert(t *testing.T) {
+	filepath.WalkDir("testdata", func(path string, d fs.DirEntry, _ error) error {
+		if d.IsDir() {
+			return nil
+		}
+
+		if strings.HasSuffix(path, promSuffix) {
+			inputFile := path
+			expectFile := strings.TrimSuffix(path, promSuffix) + flowSuffix
+
+			inputBytes, err := os.ReadFile(inputFile)
+			require.NoError(t, err)
+			expectBytes, err := os.ReadFile(expectFile)
+			require.NoError(t, err)
+
+			caseName := filepath.Base(path)
+			caseName = strings.TrimSuffix(caseName, promSuffix)
+
+			t.Run(caseName, func(t *testing.T) {
+				testConverter(t, inputBytes, expectBytes)
+			})
+		}
+
+		return nil
+	})
+}
+
+func testConverter(t *testing.T, input, expect []byte) {
+	t.Helper()
+
+	actual, err := prometheusconvert.Convert(input)
+
+	require.NoError(t, err)
+	require.Equal(t, string(normalizeLineEndings(expect)), string(normalizeLineEndings(actual))+"\n")
+}
+
+// Replace '\r\n' with '\n'
+func normalizeLineEndings(data []byte) []byte {
+	normalized := bytes.ReplaceAll(data, []byte{'\r', '\n'}, []byte{'\n'})
+	return normalized
+}
---
+++ converter/internal/prometheusconvert/remote_write.go
@@ -0,0 +1,59 @@
+package prometheusconvert
+
+import (
+	"time"
+
+	"github.com/grafana/agent/component/prometheus/remotewrite"
+	promconfig "github.com/prometheus/prometheus/config"
+)
+
+func toRemotewriteArguments(promConfig *promconfig.Config) *remotewrite.Arguments {
+	return &remotewrite.Arguments{
+		ExternalLabels: promConfig.GlobalConfig.ExternalLabels.Map(),
+		Endpoints:      getEndpointOptions(promConfig.RemoteWriteConfigs),
+		WALOptions:     remotewrite.DefaultWALOptions,
+	}
+}
+
+func getEndpointOptions(remoteWriteConfigs []*promconfig.RemoteWriteConfig) []*remotewrite.EndpointOptions {
+	endpoints := make([]*remotewrite.EndpointOptions, 0)
+
+	for _, remoteWriteConfig := range remoteWriteConfigs {
+		endpoint := &remotewrite.EndpointOptions{
+			Name:                 remoteWriteConfig.Name,
+			URL:                  remoteWriteConfig.URL.String(),
+			RemoteTimeout:        time.Duration(remoteWriteConfig.RemoteTimeout),
+			Headers:              remoteWriteConfig.Headers,
+			SendExemplars:        remoteWriteConfig.SendExemplars,
+			SendNativeHistograms: remoteWriteConfig.SendNativeHistograms,
+			HTTPClientConfig:     toHttpClientConfig(&remoteWriteConfig.HTTPClientConfig),
+			QueueOptions:         toQueueOptions(&remoteWriteConfig.QueueConfig),
+			MetadataOptions:      toMetadataOptions(&remoteWriteConfig.MetadataConfig),
+		}
+
+		endpoints = append(endpoints, endpoint)
+	}
+
+	return endpoints
+}
+
+func toQueueOptions(queueConfig *promconfig.QueueConfig) *remotewrite.QueueOptions {
+	return &remotewrite.QueueOptions{
+		Capacity:          queueConfig.Capacity,
+		MaxShards:         queueConfig.MaxShards,
+		MinShards:         queueConfig.MinShards,
+		MaxSamplesPerSend: queueConfig.MaxSamplesPerSend,
+		BatchSendDeadline: time.Duration(queueConfig.BatchSendDeadline),
+		MinBackoff:        time.Duration(queueConfig.MinBackoff),
+		MaxBackoff:        time.Duration(queueConfig.MaxBackoff),
+		RetryOnHTTP429:    queueConfig.RetryOnRateLimit,
+	}
+}
+
+func toMetadataOptions(metadataConfig *promconfig.MetadataConfig) *remotewrite.MetadataOptions {
+	return &remotewrite.MetadataOptions{
+		Send:              metadataConfig.Send,
+		SendInterval:      time.Duration(metadataConfig.SendInterval),
+		MaxSamplesPerSend: metadataConfig.MaxSamplesPerSend,
+	}
+}
