46172db1b49a578b73ba741ada71dc61ab2d3c4e
erikbaranowski <39704712+erikbaranowski@users.noreply.github.com>
2023-05-18 13:11:56 -0400

    remote flow parsing test for now from converter code
    Signed-off-by: erikbaranowski <39704712+erikbaranowski@users.noreply.github.com>

--- converter/internal/prometheusconvert/prometheusconvert_test.go
+++ converter/internal/prometheusconvert/prometheusconvert_test.go
@@ -8,12 +8,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/grafana/agent/component/prometheus/remotewrite"
-	"github.com/grafana/agent/component/prometheus/scrape"
 	"github.com/grafana/agent/converter/internal/prometheusconvert"
-	"github.com/grafana/agent/pkg/flow"
-	"github.com/grafana/agent/pkg/river/ast"
-	"github.com/grafana/agent/pkg/river/vm"
 	"github.com/stretchr/testify/require"
 )
 
@@ -62,53 +57,4 @@ func testConverter(t *testing.T, input, expect []byte) {
 func normalizeLineEndings(data []byte) []byte {
 	normalized := bytes.ReplaceAll(data, []byte{'\r', '\n'}, []byte{'\n'})
 	return normalized
-}
-
-func TestFlowParsing(t *testing.T) {
-	filepath.WalkDir("testdata", func(path string, d fs.DirEntry, _ error) error {
-		if d.IsDir() {
-			return nil
-		}
-
-		if strings.HasSuffix(path, flowSuffix) {
-			caseName := filepath.Base(path)
-			caseName = strings.TrimSuffix(caseName, flowSuffix)
-
-			t.Run(caseName, func(t *testing.T) {
-				inputBytes, err := os.ReadFile(path)
-				require.NoError(t, err)
-
-				file, err := flow.ReadFile(path, inputBytes)
-				require.NoError(t, err)
-
-				if len(file.ConfigBlocks) > 0 {
-					require.FailNow(t, "This test doesn't support config blocks")
-				}
-
-				for _, node := range file.Components {
-					evalBlock(t, node)
-				}
-			})
-		}
-
-		return nil
-	})
-}
-
-func evalBlock(t *testing.T, block *ast.BlockStmt) {
-	var err error
-	eval := vm.New(block.Body)
-
-	switch block.GetBlockName() {
-	case "prometheus.scrape":
-		var args scrape.Arguments
-		err = eval.Evaluate(nil, &args)
-	case "prometheus.remote_write":
-		var args remotewrite.Arguments
-		err = eval.Evaluate(nil, &args)
-	default:
-		require.FailNow(t, "This test doesn't support "+block.GetBlockName())
-	}
-
-	require.NoError(t, err)
 }
