84cdda9bdee52d8b6ccf2298ada0d53420b68b16
erikbaranowski <39704712+erikbaranowski@users.noreply.github.com>
2023-05-22 11:34:58 -0400

    remove extra comments
    Signed-off-by: erikbaranowski <39704712+erikbaranowski@users.noreply.github.com>

--- converter/internal/prometheusconvert/prometheusconvert.go
+++ converter/internal/prometheusconvert/prometheusconvert.go
@@ -32,22 +32,18 @@ import (
 //	discovery.lightsail
 //	discovery.relabel
 func Convert(in []byte) ([]byte, error) {
-	// Load and validate the prometheus config.
 	promConfig, err := promconfig.Load(string(in), false, log.NewNopLogger())
 	if err != nil {
 		return nil, err
 	}
 
-	// Prep the builder.
 	f := builder.NewFile()
 
-	// prometheus.remote_write
 	remoteWriteArgs := toRemotewriteArguments(promConfig)
 	remoteWriteBlock := builder.NewBlock([]string{"prometheus", "remote_write"}, "default")
 	remoteWriteBlock.Body().AppendFrom(remoteWriteArgs)
 	f.Body().AppendBlock(remoteWriteBlock)
 
-	// prometheus.scrape
 	forwardTo := make([]storage.Appendable, 0)
 	forwardTo = append(forwardTo, common.ConvertAppendable{Expr: "prometheus.remote_write.default.receiver"})
 	for _, scrapeConfig := range promConfig.ScrapeConfigs {
@@ -59,7 +55,6 @@ func Convert(in []byte) ([]byte, error) {
 		f.Body().AppendBlock(scrapeBlock)
 	}
 
-	// Write the final output.
 	var buf bytes.Buffer
 	if _, err := f.WriteTo(&buf); err != nil {
 		return nil, fmt.Errorf("failed to render Flow config: %w", err)
