7e9e56d30898801dc00f0c16b7a419671035c152
erikbaranowski <39704712+erikbaranowski@users.noreply.github.com>
2023-05-24 14:49:36 -0400

    new file
    Signed-off-by: erikbaranowski <39704712+erikbaranowski@users.noreply.github.com>

---
+++ converter/internal/common/diagnostics.go
@@ -0,0 +1,38 @@
+// Package common exposes error types used throughout converter and a method to
+// pretty-print them to the screen.
+package common
+
+// Severity denotes the severity level of a diagnostic. The zero value of
+// severity is invalid.
+type Severity int
+
+// Supported severity levels.
+const (
+	SeverityLevelWarn Severity = iota + 1
+	SeverityLevelError
+)
+
+// Diagnostic is an individual diagnostic message. Diagnostic messages can have
+// different levels of severities.
+type Diagnostic struct {
+	// Severity holds the severity level of this Diagnostic.
+	Severity Severity
+
+	Message string
+}
+
+// Error implements error.
+func (d Diagnostic) Error() string {
+	return d.Message
+}
+
+// Diagnostics is a collection of diagnostic messages.
+type Diagnostics []Diagnostic
+
+// Add adds an individual Diagnostic to the diagnostics list.
+func (ds *Diagnostics) Add(severity Severity, message string) {
+	*ds = append(*ds, Diagnostic{
+		Severity: severity,
+		Message:  message,
+	})
+}
