1393aaeaba4e1af324c18feeb229c8f5bca0fb05
Paschalis Tsilias <paschalis.tsilias@grafana.com>
2023-05-25 17:11:18 +0300

    Add TODO comment about using a copy-on-write approach instead
    Signed-off-by: Paschalis Tsilias <paschalis.tsilias@grafana.com>

--- component/loki/process/process.go
+++ component/loki/process/process.go
@@ -133,6 +133,10 @@ func (c *Component) handleIn(ctx context.Context, wg *sync.WaitGroup) {
 				return
 			case c.processIn <- entry.Clone():
 				// no-op
+				// TODO(@tpaschalis) Instead of calling Clone() at the
+				// component's entrypoint here, we can try a copy-on-write
+				// approach instead, so that the copy only gets made on the
+				// first stage that needs to modify the entry's labels.
 			}
 			c.mut.RUnlock()
 		}
