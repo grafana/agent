ddb6f4b4eb513023a6d9afef59746d87cd5bee85
Tolyan Korniltsev <korniltsev.anatoly@gmail.com>
2023-06-06 08:26:15 +0000

    metrics help

--- component/pyroscope/ebpf/ebpfspy/metrics/metrics.go
+++ component/pyroscope/ebpf/ebpfspy/metrics/metrics.go
@@ -17,35 +17,35 @@ func NewMetrics(reg prometheus.Registerer) *Metrics {
 	m := &Metrics{
 		PidCacheHit: prometheus.NewCounter(prometheus.CounterOpts{
 			Name: "pyroscope_ebpf_pid_cache_hit_total",
-			Help: "",
+			Help: "Total number of ebpf symbolizer pid cache hit.",
 		}),
 		PidCacheMiss: prometheus.NewCounter(prometheus.CounterOpts{
 			Name: "pyroscope_ebpf_pid_cache_miss_total",
-			Help: "",
+			Help: "Total number of ebpf symbolizer pid cache miss.",
 		}),
 		ElfCacheBuildIDHit: prometheus.NewCounter(prometheus.CounterOpts{
 			Name: "pyroscope_ebpf_elf_cache_build_id_hit_total",
-			Help: "",
+			Help: "Total number of ebpf symbolizer elf cache (build-id) hit.",
 		}),
 		ElfCacheBuildIDMiss: prometheus.NewCounter(prometheus.CounterOpts{
 			Name: "pyroscope_ebpf_elf_cache_build_id_miss_total",
-			Help: "",
+			Help: "Total number of ebpf symbolizer elf cache (build-id) miss.",
 		}),
 		ElfCacheStatHit: prometheus.NewCounter(prometheus.CounterOpts{
 			Name: "pyroscope_ebpf_elf_cache_stat_hit_total",
-			Help: "",
+			Help: "Total number of ebpf symbolizer elf cache (stat) hit.",
 		}),
 		ElfCacheStatMiss: prometheus.NewCounter(prometheus.CounterOpts{
 			Name: "pyroscope_ebpf_elf_cache_stat_miss_total",
-			Help: "",
+			Help: "Total number of ebpf symbolizer elf cache (stat) miss.",
 		}),
 		ContainerIDCacheHit: prometheus.NewCounter(prometheus.CounterOpts{
 			Name: "pyroscope_ebpf_container_id_cache_hit_total",
-			Help: "",
+			Help: "Total number of ebpf target finder container id cache hit.",
 		}),
 		ContainerIDCacheMiss: prometheus.NewCounter(prometheus.CounterOpts{
 			Name: "pyroscope_ebpf_container_id_cache_miss_total",
-			Help: "",
+			Help: "Total number of ebpf target finder container id cache miss.",
 		}),
 	}
 
