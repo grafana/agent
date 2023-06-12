fd1583b6b1ac0d295f33bb1eb0946419961e1989
erikbaranowski <39704712+erikbaranowski@users.noreply.github.com>
2023-06-12 14:10:17 -0400

    Address PR feedback
    Signed-off-by: erikbaranowski <39704712+erikbaranowski@users.noreply.github.com>

--- converter/internal/common/convert_targets.go
+++ converter/internal/common/convert_targets.go
@@ -2,6 +2,7 @@ package common
 
 import (
 	"github.com/grafana/agent/component/discovery"
+	"github.com/grafana/agent/pkg/river"
 	"github.com/grafana/agent/pkg/river/token"
 	"github.com/grafana/agent/pkg/river/token/builder"
 )
@@ -13,55 +14,56 @@ type ConvertTargets struct {
 	Targets []discovery.Target
 }
 
+var _ builder.Tokenizer = ConvertTargets{}
+var _ river.Capsule = ConvertTargets{}
+
 func (f ConvertTargets) RiverCapsule() {}
 func (f ConvertTargets) RiverTokenize() []builder.Token {
 	var toks []builder.Token
-	if len(f.Targets) == 0 {
-		toks = append(toks, builder.Token{Tok: token.LBRACK, Lit: ""})
-		toks = append(toks, builder.Token{Tok: token.RBRACK, Lit: ""})
+
+	targetCount := 0
+	for _, targetMap := range f.Targets {
+		targetCount += len(targetMap)
+	}
+
+	if targetCount == 0 {
+		toks = append(toks, builder.Token{Tok: token.LBRACK})
+		toks = append(toks, builder.Token{Tok: token.RBRACK})
 		return toks
 	}
 
-	// We are relying on each targetMap having exactly 1 target which we control
-	// from the upstream converter code. We panic below if not.
-	if len(f.Targets) > 1 {
+	if targetCount > 1 {
 		toks = append(toks, builder.Token{Tok: token.LITERAL, Lit: "concat"})
-		toks = append(toks, builder.Token{Tok: token.LPAREN, Lit: ""})
+		toks = append(toks, builder.Token{Tok: token.LPAREN})
 	}
 
 	for ix, targetMap := range f.Targets {
-		if len(targetMap) != 1 {
-			panic("unexpected number of targets received on a target map")
-		}
-
 		for key, target := range targetMap {
 			if key == "__address__" {
-				toks = append(toks, builder.Token{Tok: token.LBRACK, Lit: ""})
-				toks = append(toks, builder.Token{Tok: token.LCURLY, Lit: ""})
+				toks = append(toks, builder.Token{Tok: token.LBRACK})
+				toks = append(toks, builder.Token{Tok: token.LCURLY})
 				toks = append(toks, builder.Token{Tok: token.LITERAL, Lit: "\n"})
 				toks = append(toks, builder.Token{Tok: token.LITERAL, Lit: key})
-				toks = append(toks, builder.Token{Tok: token.ASSIGN, Lit: ""})
+				toks = append(toks, builder.Token{Tok: token.ASSIGN})
 				toks = append(toks, builder.Token{Tok: token.STRING, Lit: `"` + target + `"`})
-				toks = append(toks, builder.Token{Tok: token.COMMA, Lit: ""})
+				toks = append(toks, builder.Token{Tok: token.COMMA})
 				toks = append(toks, builder.Token{Tok: token.LITERAL, Lit: "\n"})
-				toks = append(toks, builder.Token{Tok: token.RCURLY, Lit: ""})
-				toks = append(toks, builder.Token{Tok: token.RBRACK, Lit: ""})
+				toks = append(toks, builder.Token{Tok: token.RCURLY})
+				toks = append(toks, builder.Token{Tok: token.RBRACK})
 			} else {
 				toks = append(toks, builder.Token{Tok: token.LITERAL, Lit: key})
 			}
 
 			if ix != len(f.Targets)-1 {
-				toks = append(toks, builder.Token{Tok: token.COMMA, Lit: ""})
+				toks = append(toks, builder.Token{Tok: token.COMMA})
 				toks = append(toks, builder.Token{Tok: token.LITERAL, Lit: "\n"})
 			}
 		}
 	}
 
-	if len(f.Targets) > 1 {
-		toks = append(toks, builder.Token{Tok: token.RPAREN, Lit: ""})
+	if targetCount > 1 {
+		toks = append(toks, builder.Token{Tok: token.RPAREN})
 	}
 
 	return toks
 }
-
-var _ builder.Tokenizer = ConvertTargets{}
