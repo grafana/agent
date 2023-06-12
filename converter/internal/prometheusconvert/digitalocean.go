42a2a6ad7e3313d214f924b7122ab797f9e4c76c
erikbaranowski <39704712+erikbaranowski@users.noreply.github.com>
2023-06-12 16:15:31 -0400

    files...
    Signed-off-by: erikbaranowski <39704712+erikbaranowski@users.noreply.github.com>

---
+++ converter/internal/prometheusconvert/digitalocean.go
@@ -0,0 +1,58 @@
+package prometheusconvert
+
+import (
+	"time"
+
+	"github.com/grafana/agent/component/common/config"
+	"github.com/grafana/agent/component/discovery"
+	"github.com/grafana/agent/component/discovery/digitalocean"
+	"github.com/grafana/agent/converter/diag"
+	"github.com/grafana/agent/converter/internal/common"
+	"github.com/grafana/agent/pkg/river/rivertypes"
+	"github.com/grafana/agent/pkg/river/token/builder"
+	promdigitalocean "github.com/prometheus/prometheus/discovery/digitalocean"
+)
+
+func appendDiscoveryDigitalOcean(f *builder.File, jobName string, sdConfig *promdigitalocean.SDConfig) (discovery.Exports, diag.Diagnostics) {
+	discoveryDigitalOceanArgs, diags := toDiscoveryDigitalOcean(sdConfig)
+	common.AppendBlockWithOverride(f, []string{"discovery", "digitalocean"}, jobName, discoveryDigitalOceanArgs)
+	return discovery.Exports{
+		// This target map will utilize a RiverTokenize that results in this
+		// component export rather than the standard discovery.Target RiverTokenize.
+		Targets: []discovery.Target{map[string]string{"discovery.digitalocean." + jobName + ".targets": ""}},
+	}, diags
+}
+
+func toDiscoveryDigitalOcean(sdConfig *promdigitalocean.SDConfig) (*digitalocean.Arguments, diag.Diagnostics) {
+	if sdConfig == nil {
+		return nil, nil
+	}
+
+	return &digitalocean.Arguments{
+		RefreshInterval: time.Duration(sdConfig.RefreshInterval),
+		Port:            sdConfig.Port,
+		BearerToken:     rivertypes.Secret(sdConfig.HTTPClientConfig.BearerToken),
+		BearerTokenFile: sdConfig.HTTPClientConfig.BearerTokenFile,
+		ProxyURL:        config.URL(sdConfig.HTTPClientConfig.ProxyURL),
+		FollowRedirects: sdConfig.HTTPClientConfig.FollowRedirects,
+		EnableHTTP2:     sdConfig.HTTPClientConfig.EnableHTTP2,
+	}, validateDiscoveryDigitalOcean(sdConfig)
+}
+
+func validateDiscoveryDigitalOcean(sdConfig *promdigitalocean.SDConfig) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	if sdConfig.HTTPClientConfig.NoProxy != "" {
+		diags.Add(diag.SeverityLevelWarn, "unsupported digitalocean service discovery config no_proxy was provided")
+	}
+
+	if sdConfig.HTTPClientConfig.ProxyFromEnvironment {
+		diags.Add(diag.SeverityLevelWarn, "unsupported digitalocean service discovery config proxy_from_environment was provided")
+	}
+
+	if len(sdConfig.HTTPClientConfig.ProxyConnectHeader) > 0 {
+		diags.Add(diag.SeverityLevelWarn, "unsupported digitalocean service discovery config proxy_connect_header was provided")
+	}
+
+	return diags
+}
