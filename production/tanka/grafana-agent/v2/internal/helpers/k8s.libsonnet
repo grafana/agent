grafana/agent

commit 12fd2d405c0a2be00ceaa0ba2fadb3911ff40a3f

Kubernetes: retain cAdvisor metrics for system processes (#4048)
Currently, CPU metrics for Kubelet, Containerd, etc. are dropped by a
relabel rule since they don't have a container image.

On most systems they run in their own cgroup called `system.slice`, so
we can add another rule which uses that fact to give the `image` label a
value and then they won't be dropped.

Signed-off-by: Bryan Boreham <bjboreham@gmail.com>
Co-authored-by: Paschalis Tsilias <tpaschalis@users.noreply.github.com>

production/tanka/grafana-agent/v2/internal/helpers/k8s.libsonnet
@@ -355,6 +355,15 @@ local gen_scrape_config(job_name, pod_uid) = {
         ],
 
         metric_relabel_configs: [
+          // Let system processes like kubelet survive the next rule by giving them a fake image.
+          {
+            source_labels: ['__name__', 'id'],
+            regex: 'container_([a-z_]+);/system.slice/(.+)',
+            target_label: 'image',
+            replacement: '$2',
+          },
+
+        metric_relabel_configs: [
           // Drop container_* metrics with no image.
           {
             source_labels: ['__name__', 'image'],

commit a910a7f0971537466ab6a445fa141cc8feeefa9c

Fix typo in helpers/k8s.libsonnet
A rather obvious mistake I introduced in #4048.

production/tanka/grafana-agent/v2/internal/helpers/k8s.libsonnet
@@ -363,7 +363,6 @@ local gen_scrape_config(job_name, pod_uid) = {
             replacement: '$2',
           },
 
-        metric_relabel_configs: [
           // Drop container_* metrics with no image.
           {
             source_labels: ['__name__', 'image'],