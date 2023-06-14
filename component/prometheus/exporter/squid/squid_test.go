d63ffc8fa7c69d84dca727da988c1061f66750ad
armstrmi <mitchell.armstrong@bluemedora.com>
2023-06-14 16:34:49 -0400

    added test for Validate
    Signed-off-by: armstrmi <mitchell.armstrong@bluemedora.com>

--- component/prometheus/exporter/squid/squid_test.go
+++ component/prometheus/exporter/squid/squid_test.go
@@ -1,6 +1,7 @@
 package squid
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/grafana/agent/component/discovery"
@@ -13,7 +14,7 @@ import (
 
 func TestRiverUnmarshal(t *testing.T) {
 	riverConfig := `
-	address = "some_address"
+	address = "some_address:port"
 	username     = "some_user"
 	password     = "some_password"
 	`
@@ -23,7 +24,7 @@ func TestRiverUnmarshal(t *testing.T) {
 	require.NoError(t, err)
 
 	expected := Arguments{
-		SquidAddr:     "some_address",
+		SquidAddr:     "some_address:port",
 		SquidUser:     "some_user",
 		SquidPassword: rivertypes.Secret("some_password"),
 	}
@@ -33,7 +34,7 @@ func TestRiverUnmarshal(t *testing.T) {
 
 func TestConvert(t *testing.T) {
 	riverConfig := `
-	address = "some_address"
+	address = "some_address:port"
 	username     = "some_user"
 	password     = "some_password"
 	`
@@ -44,7 +45,7 @@ func TestConvert(t *testing.T) {
 	res := args.Convert()
 
 	expected := squid_exporter.Config{
-		Address:  "some_address",
+		Address:  "some_address:port",
 		Username: "some_user",
 		Password: config.Secret("some_password"),
 	}
@@ -60,4 +61,73 @@ func TestCustomizeTarget(t *testing.T) {
 	newTargets := customizeTarget(baseTarget, args)
 	require.Equal(t, 1, len(newTargets))
 	require.Equal(t, "some_address", newTargets[0]["instance"])
+}
+
+func TestConfigValidate(t *testing.T) {
+	cases := []struct {
+		name        string
+		getConfig   func() Arguments
+		expectedErr error
+	}{
+		{
+			name: "valid",
+			getConfig: func() Arguments {
+				c := Arguments{
+					SquidAddr: "localhost:3128",
+				}
+				return c
+			},
+		},
+		{
+			name: "no hostname",
+			getConfig: func() Arguments {
+				c := Arguments{}
+				c.SquidAddr = ":3128"
+				return c
+			},
+			expectedErr: errNoHostname,
+		},
+		{
+			name: "no port",
+			getConfig: func() Arguments {
+				c := Arguments{}
+				c.SquidAddr = "localhost:"
+				return c
+			},
+			expectedErr: errNoPort,
+		},
+		{
+			name: "no empty config",
+			getConfig: func() Arguments {
+				cfg := Arguments{
+					SquidAddr:     "",
+					SquidUser:     "",
+					SquidPassword: "",
+				}
+				return cfg
+			},
+			expectedErr: errNoAddress,
+		},
+		{
+			name: "invalid config",
+			getConfig: func() Arguments {
+				cfg := Arguments{}
+				cfg.SquidAddr = "a@#$%:asdf::12312"
+				return cfg
+			},
+			expectedErr: errors.New("address a@#$%:asdf::12312: too many colons in address"),
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			cfg := tc.getConfig()
+			err := cfg.Validate()
+			if tc.expectedErr == nil {
+				require.NoError(t, err)
+				return
+			}
+			require.ErrorContains(t, err, tc.expectedErr.Error())
+		})
+	}
 }
