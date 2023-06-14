8bd18fde2daf284100e8cf9a653bafebe0300fa6
Robert Fratto <robertfratto@gmail.com>
2023-06-14 15:10:39 -0400

    Flow: add new methods to get and list components (#4097)
    * pkg/flow: use consistent receiver names for Flow
    
    This commit changes the receiver names to always be `f` rather than a
    mixture of `c` and `f`.
    
    * pkg/flow: add APIs to list/get component details
    
    This commit adds a Flow.GetComponent and Flow.ListComponents methods to
    allow retrieving detailed information on running components.
    
    Part of the API is unimplemented; there is the intent that these
    endpoints will be able to return all components, including components
    inside of modules, but this is left unimplemented while the work in
    grafana/agent#4027 is ongoing. For now, the controller is limited to
    returning its own components.
    
    * web/api: change listComponentHandler to getComponentHandler
    
    listComponentsHandler and listComponentHandler were named too similarly,
    making it hard to differentiate between the two.
    
    This commit renames the get method so the method pair is now
    listComponentsHandler and getComponentHandler.
    
    * web/api: use new Flow APIs for web API
    
    Change the implementation of web/api to use the new Flow
    ListComponents/GetComponent methods to power the data returned via the
    API.
    
    * module.string: allow tests to run on macOS as well as Linux
    
    * module.string: use new Flow APIs in tests
    
    * cmd/internal/flowmode: use new Flow APIs for getting enabled components
    
    * pkg/flow: remove unused code
    
    Remove unused code from previous implementation of getting component
    information for the HTTP API.
    
    * web/api: move provider definition to component package
    
    This is a bit of a code smell, but it's the best place to handle this
    for now. It should be easier to work around it in the future with the
    services proposal without having to have too many type definitions in a
    single package with no logic.
    
    * web/api/apitypes: remove unused package
    
    The web/api/apitypes package is now unused and can be removed; it's
    replaced with the detailed generic component information which is also
    serializable to JSON.

--- web/api/api.go
+++ web/api/api.go
@@ -5,44 +5,38 @@
 package api
 
 import (
-	"bytes"
 	"encoding/json"
-	"io"
 	"net/http"
 	"path"
 
 	"github.com/gorilla/mux"
-	"github.com/grafana/agent/web/api/apitypes"
+	"github.com/grafana/agent/component"
 	"github.com/prometheus/prometheus/util/httputil"
 )
 
-// ComponentProvider provides the ability to introspect a flow components
-// in a read only manner.
-type ComponentProvider interface {
-	ComponentInfos() []*apitypes.ComponentInfo
-	ComponentJSON(io.Writer, *apitypes.ComponentInfo) error
-}
-
 // FlowAPI is a wrapper around the component API.
 type FlowAPI struct {
-	flow ComponentProvider
+	flow component.Provider
 }
 
 // NewFlowAPI instantiates a new Flow API.
-func NewFlowAPI(flow ComponentProvider) *FlowAPI {
+func NewFlowAPI(flow component.Provider) *FlowAPI {
 	return &FlowAPI{flow: flow}
 }
 
 // RegisterRoutes registers all the API's routes.
 func (f *FlowAPI) RegisterRoutes(urlPrefix string, r *mux.Router) {
 	r.Handle(path.Join(urlPrefix, "/components"), httputil.CompressionHandler{Handler: f.listComponentsHandler()})
-	r.Handle(path.Join(urlPrefix, "/components/{id}"), httputil.CompressionHandler{Handler: f.listComponentHandler()})
+	r.Handle(path.Join(urlPrefix, "/components/{id}"), httputil.CompressionHandler{Handler: f.getComponentHandler()})
 }
 
 func (f *FlowAPI) listComponentsHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, _ *http.Request) {
-		infos := f.flow.ComponentInfos()
-		bb, err := json.Marshal(infos)
+		components := f.flow.ListComponents(component.InfoOptions{
+			GetHealth: true,
+		})
+
+		bb, err := json.Marshal(components)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -51,34 +45,30 @@ func (f *FlowAPI) listComponentsHandler() http.HandlerFunc {
 	}
 }
 
-func (f *FlowAPI) listComponentHandler() http.HandlerFunc {
+func (f *FlowAPI) getComponentHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
-		infos := f.flow.ComponentInfos()
 		requestedComponent := vars["id"]
 
-		for _, info := range infos {
-			if requestedComponent == info.ID {
-				bb, err := f.json(info)
-				if err != nil {
-					http.Error(w, err.Error(), http.StatusInternalServerError)
-					return
-				}
-				_, _ = w.Write(bb)
-				return
-			}
+		component, err := f.flow.GetComponent(component.ID{
+			ModuleID: "", // TODO(rfratto): support getting component from module.
+			LocalID:  requestedComponent,
+		}, component.InfoOptions{
+			GetHealth:    true,
+			GetArguments: true,
+			GetExports:   true,
+			GetDebugInfo: true,
+		})
+		if err != nil {
+			http.NotFound(w, r)
+			return
 		}
 
-		http.NotFound(w, r)
+		bb, err := json.Marshal(component)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		_, _ = w.Write(bb)
 	}
-}
-
-// json returns the JSON representation of c.
-func (f *FlowAPI) json(c *apitypes.ComponentInfo) ([]byte, error) {
-	var buf bytes.Buffer
-	err := f.flow.ComponentJSON(&buf, c)
-	if err != nil {
-		return nil, err
-	}
-	return buf.Bytes(), nil
 }
