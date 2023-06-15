9f0359e95bc4b7e6fc38b915fdaf98b6f26781ce
Aksel Skaar Leirvaag <akselleirv@gmail.com>
2023-06-15 15:04:37 +0200

    underscore unused variable

--- component/loki/process/internal/stages/geoip.go
+++ component/loki/process/internal/stages/geoip.go
@@ -115,7 +115,7 @@ func (g *geoIPStage) Name() string {
 	return StageTypeGeoIP
 }
 
-func (g *geoIPStage) process(labels model.LabelSet, extracted map[string]interface{}) {
+func (g *geoIPStage) process(_ model.LabelSet, extracted map[string]interface{}) {
 	var ip net.IP
 	if g.cfgs.Source != nil {
 		if _, ok := extracted[*g.cfgs.Source]; !ok {
