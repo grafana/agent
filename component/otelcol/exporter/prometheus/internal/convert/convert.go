ceb14e188478824fd8e8c35a55b1895dcff80da9
Nikola Grcevski <grcevski@gmail.com>
2023-06-16 16:27:11 -0400

    Make histogram counts cumulative for prometheus

--- component/otelcol/exporter/prometheus/internal/convert/convert.go
+++ component/otelcol/exporter/prometheus/internal/convert/convert.go
@@ -10,6 +10,8 @@ package convert
 import (
 	"context"
 	"fmt"
+	"math"
+	"sort"
 	"strconv"
 	"sync"
 	"time"
@@ -458,11 +460,34 @@ func (conv *Converter) consumeHistogram(app storage.Appender, memResource *memor
 			}
 		}
 
+		// Sort the histogram by bounds ascending
+		bSize := int(math.Min(float64(dp.ExplicitBounds().Len()), float64(dp.BucketCounts().Len())))
+		buckets := make(map[float64]uint64, bSize)
+		bounds := make([]float64, bSize)
+		for i := 0; i < dp.ExplicitBounds().Len() && i < dp.BucketCounts().Len(); i++ {
+			bound := dp.ExplicitBounds().At(i)
+			buckets[bound] = dp.BucketCounts().At(i)
+			bounds = append(bounds, bound)
+		}
+
+		sort.Float64s(bounds)
+
+		// Calculate cumulative count value as Prometheus wants it
+		var c uint64 = 0
+		for i := 0; i < len(bounds); i++ {
+			bound := bounds[i]
+			c += buckets[bound]
+			buckets[bound] = c
+		}
+
 		// Process the boundaries. The number of buckets = number of explicit
 		// bounds + 1.
 		for i := 0; i < dp.ExplicitBounds().Len() && i < dp.BucketCounts().Len(); i++ {
 			bound := dp.ExplicitBounds().At(i)
-			count := dp.BucketCounts().At(i)
+			count, ok := buckets[bound]
+			if !ok {
+				count = dp.BucketCounts().At(i)
+			}
 
 			bucketLabel := labels.Label{
 				Name:  model.BucketLabel,
