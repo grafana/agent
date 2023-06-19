385e0f4046474b30125b1d91c2e04d407a315797
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2023-06-19 15:54:33 +0700

    lint

---
+++ component/pyroscope/ebpf/ebpfspy/symtab/stat_darwin.go
@@ -0,0 +1,25 @@
+//go:build darwin
+
+package symtab
+
+import (
+	"os"
+	"syscall"
+)
+
+type Stat struct {
+	dev uint64 `river:"dev,block,optional"`
+	ino uint64 `river:"ino,block,optional"`
+}
+
+func statFromFileInfo(file os.FileInfo) Stat {
+	sys := file.Sys()
+	sysStat, ok := sys.(*syscall.Stat_t)
+	if !ok || sysStat == nil {
+		return Stat{}
+	}
+	return Stat{
+		dev: uint64(sysStat.Dev),
+		ino: sysStat.Ino,
+	}
+}
