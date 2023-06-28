7aeb4559b44e818457eeeefcd31977a9ac4cc542
Paschalis Tsilias <tpaschalis@users.noreply.github.com>
2023-06-28 14:08:43 +0300

    clustering: add basic UI page for peer info (#4266)
    Signed-off-by: Paschalis Tsilias <paschalis.tsilias@grafana.com>

--- web/api/api.go
+++ web/api/api.go
@@ -11,23 +11,26 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/grafana/agent/component"
+	"github.com/grafana/agent/pkg/cluster"
 	"github.com/prometheus/prometheus/util/httputil"
 )
 
 // FlowAPI is a wrapper around the component API.
 type FlowAPI struct {
 	flow component.Provider
+	node cluster.Node
 }
 
 // NewFlowAPI instantiates a new Flow API.
-func NewFlowAPI(flow component.Provider) *FlowAPI {
-	return &FlowAPI{flow: flow}
+func NewFlowAPI(flow component.Provider, node cluster.Node) *FlowAPI {
+	return &FlowAPI{flow: flow, node: node}
 }
 
 // RegisterRoutes registers all the API's routes.
 func (f *FlowAPI) RegisterRoutes(urlPrefix string, r *mux.Router) {
 	r.Handle(path.Join(urlPrefix, "/components"), httputil.CompressionHandler{Handler: f.listComponentsHandler()})
 	r.Handle(path.Join(urlPrefix, "/components/{id}"), httputil.CompressionHandler{Handler: f.getComponentHandler()})
+	r.Handle(path.Join(urlPrefix, "/peers"), httputil.CompressionHandler{Handler: f.getClusteringPeersHandler()})
 }
 
 func (f *FlowAPI) listComponentsHandler() http.HandlerFunc {
@@ -65,6 +68,20 @@ func (f *FlowAPI) getComponentHandler() http.HandlerFunc {
 		}
 
 		bb, err := json.Marshal(component)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		_, _ = w.Write(bb)
+	}
+}
+
+func (f *FlowAPI) getClusteringPeersHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, _ *http.Request) {
+		// TODO(@tpaschalis) Detect if clustering is disabled and propagate to
+		// the Typescript code (eg. via the returned status code?).
+		peers := f.node.Peers()
+		bb, err := json.Marshal(peers)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
---
+++ web/ui/src/features/clustering/PeerList.tsx
@@ -0,0 +1,45 @@
+import { PeerInfo } from '../clustering/types';
+
+import Table from './Table';
+
+import styles from './PeerList.module.css';
+
+interface PeerListProps {
+  peers: PeerInfo[];
+}
+
+const TABLEHEADERS = ['Node Name', 'Advertised Address', 'Current State', 'Local Node'];
+
+const PeerList = ({ peers }: PeerListProps) => {
+  const tableStyles = { width: '130px' };
+
+  /**
+   * Custom renderer for table data
+   */
+  const renderTableData = () => {
+    return peers.map(({ name, addr, state, isSelf }) => (
+      <tr key={name} style={{ lineHeight: '2.5' }}>
+        <td>
+          <span className={styles.idName}>{name}</span>
+        </td>
+        <td>
+          <span className={styles.idName}>{addr}</span>
+        </td>
+        <td>
+          <span className={styles.idName}>{state}</span>
+        </td>
+        <td>
+          <span> {isSelf ? '✅' : '🚫'}</span>
+        </td>
+      </tr>
+    ));
+  };
+
+  return (
+    <div className={styles.list}>
+      <Table tableHeaders={TABLEHEADERS} renderTableData={renderTableData} style={tableStyles} />
+    </div>
+  );
+};
+
+export default PeerList;
