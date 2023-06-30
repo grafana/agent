9dd6f837525ed3a3876b6b352798bc3e17277175
Mischa Thompson <mischabear@gmail.com>
2023-06-30 08:50:41 -0700

    Add tests for the generated HTTPPath and DataPath

---
+++ pkg/flow/internal/controller/component_test.go
@@ -0,0 +1,37 @@
+package controller
+
+import (
+	"github.com/grafana/agent/component"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestGlobalID(t *testing.T) {
+	mo := getManagedOptions(ComponentGlobals{
+		DataPath:       "/data/",
+		HTTPPathPrefix: "/http/",
+		ControllerID:   "module.file",
+		NewModuleController: func(id string) component.ModuleController {
+			return nil
+		},
+	}, &ComponentNode{
+		nodeID: "local.id",
+	})
+	require.True(t, mo.HTTPPath == "/http/module.file/local.id/")
+	require.True(t, mo.DataPath == "/data/module.file/local.id")
+}
+
+func TestLocalID(t *testing.T) {
+	mo := getManagedOptions(ComponentGlobals{
+		DataPath:       "/data/",
+		HTTPPathPrefix: "/http/",
+		ControllerID:   "",
+		NewModuleController: func(id string) component.ModuleController {
+			return nil
+		},
+	}, &ComponentNode{
+		nodeID: "local.id",
+	})
+	require.True(t, mo.HTTPPath == "/http/local.id/")
+	require.True(t, mo.DataPath == "/data/local.id")
+}
