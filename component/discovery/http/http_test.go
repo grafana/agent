d3bdcc3e51618ea247e66f3fcb4997100d3552ef
Craig Peterson <192540+captncraig@users.noreply.github.com>
2023-07-11 11:24:59 -0400

    add full test

--- component/discovery/http/http_test.go
+++ component/discovery/http/http_test.go
@@ -1,11 +1,17 @@
 package http
 
 import (
+	"context"
+	"net/http"
+	"net/http/httptest"
 	"net/url"
+	"sync"
 	"testing"
 	"time"
 
+	"github.com/grafana/agent/component"
 	"github.com/grafana/agent/component/common/config"
+	"github.com/grafana/agent/component/discovery"
 	"github.com/grafana/agent/pkg/river"
 	"github.com/prometheus/common/model"
 	"github.com/stretchr/testify/require"
@@ -37,4 +43,73 @@ func TestConvert(t *testing.T) {
 	assert.Equal(t, "https://www.example.com:12345/foo", sd.URL)
 	assert.Equal(t, model.Duration(60*time.Second), sd.RefreshInterval)
 	assert.Equal(t, true, sd.HTTPClientConfig.EnableHTTP2)
+}
+
+func TestComponent(t *testing.T) {
+	discovery.MaxUpdateFrequency = time.Second / 2
+	endpointCalled := false
+	stateChanged := false
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		endpointCalled = true
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(200)
+		// from https://prometheus.io/docs/prometheus/latest/http_sd/
+		w.Write([]byte(`[
+			{
+				"targets": ["10.0.10.2:9100", "10.0.10.3:9100", "10.0.10.4:9100", "10.0.10.5:9100"],
+				"labels": {
+					"__meta_datacenter": "london",
+					"__meta_prometheus_job": "node"
+				}
+			},
+			{
+				"targets": ["10.0.40.2:9100", "10.0.40.3:9100"],
+				"labels": {
+					"__meta_datacenter": "london",
+					"__meta_prometheus_job": "alertmanager"
+				}
+			},
+			{
+				"targets": ["10.0.40.2:9093", "10.0.40.3:9093"],
+				"labels": {
+					"__meta_datacenter": "newyork",
+					"__meta_prometheus_job": "alertmanager"
+				}
+			}
+		]`))
+	}))
+	defer srv.Close()
+	u, _ := url.Parse(srv.URL)
+	var cancel func()
+	component, err := New(
+		component.Options{
+			OnStateChange: func(e component.Exports) {
+				stateChanged = true
+				args, ok := e.(discovery.Exports)
+				assert.Equal(t, true, ok)
+				assert.Equal(t, 8, len(args.Targets))
+				cancel()
+			},
+		},
+		Arguments{
+			RefreshInterval:  time.Second,
+			HTTPClientConfig: config.DefaultHTTPClientConfig,
+			URL: config.URL{
+				URL: u,
+			},
+		})
+	assert.NilError(t, err)
+	wg := sync.WaitGroup{}
+	var ctx context.Context
+	ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
+	wg.Add(1)
+	go func() {
+		err := component.Run(ctx)
+		assert.NilError(t, err)
+		wg.Done()
+	}()
+	wg.Wait()
+	cancel()
+	assert.Equal(t, true, endpointCalled)
+	assert.Equal(t, true, stateChanged)
 }
