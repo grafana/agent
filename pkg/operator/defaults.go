cec3f281386078b25a9c372a7a5b4bdff320036d
matt durham <mattdurham@ppog.org>
2023-07-13 08:51:04 -0400

    Update release versioning

--- pkg/operator/defaults.go
+++ pkg/operator/defaults.go
@@ -2,7 +2,7 @@ package operator
 
 // Supported versions of the Grafana Agent.
 var (
-	DefaultAgentVersion   = "v0.34.3"
+	DefaultAgentVersion   = "v0.35.0-rc.0"
 	DefaultAgentBaseImage = "grafana/agent"
 	DefaultAgentImage     = DefaultAgentBaseImage + ":" + DefaultAgentVersion
 )
