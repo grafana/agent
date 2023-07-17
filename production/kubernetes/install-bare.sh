c0604dddf39e72d87b4cb82c0d079fe6fa06fadf
mattdurham <mattdurham@ppog.org>
2023-07-17 16:06:22 -0400

    Cherry pick version (#4489)
    * trigger doc rebuild
    
    * Update RC numbers (#4485)
    
    * Update RC numbers
    
    * Remove newline so the diff doesnt look wierd.
    
    (cherry picked from commit 8f87ef6dbbf7d43b72b2745f82f3fa30a9582009)
    
    * Remove duplicate header

--- production/kubernetes/install-bare.sh
+++ production/kubernetes/install-bare.sh
@@ -25,7 +25,7 @@ check_installed() {
 check_installed curl
 check_installed envsubst
 
-MANIFEST_BRANCH=v0.35.0-rc.0
+MANIFEST_BRANCH=v0.35.0-rc.1
 MANIFEST_URL=${MANIFEST_URL:-https://raw.githubusercontent.com/grafana/agent/${MANIFEST_BRANCH}/production/kubernetes/agent-bare.yaml}
 NAMESPACE=${NAMESPACE:-default}
 
