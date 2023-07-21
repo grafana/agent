bacbed189b791a8ebb04a95d0173adc88f8ecebf
Craig Peterson <192540+captncraig@users.noreply.github.com>
2023-07-21 11:05:02 -0400

    discovery.nomad

---
+++ component/discovery/nomad/nomad_test.go
@@ -0,0 +1,57 @@
+package nomad
+
+import (
+	"testing"
+	"time"
+
+	"github.com/grafana/agent/pkg/river"
+	"github.com/prometheus/common/model"
+	"github.com/stretchr/testify/require"
+	"gotest.tools/assert"
+)
+
+func TestRiverUnmarshal(t *testing.T) {
+	riverCfg := `
+		allow_stale = false
+		namespace = "foo"
+		refresh_interval = "20s"
+		region = "test"
+		server = "http://foo:4949"
+		tag_separator = ";"
+		enable_http2 = true
+		follow_redirects = false
+		proxy_url = "http://example:8080"`
+
+	var args Arguments
+	err := river.Unmarshal([]byte(riverCfg), &args)
+	require.NoError(t, err)
+
+	assert.Equal(t, false, args.AllowStale)
+	assert.Equal(t, "foo", args.Namespace)
+	assert.Equal(t, 20*time.Second, args.RefreshInterval)
+	assert.Equal(t, "test", args.Region)
+	assert.Equal(t, "http://foo:4949", args.Server)
+	assert.Equal(t, ";", args.TagSeparator)
+	assert.Equal(t, true, args.HTTPClientConfig.EnableHTTP2)
+	assert.Equal(t, false, args.HTTPClientConfig.FollowRedirects)
+	assert.Equal(t, "http://example:8080", args.HTTPClientConfig.ProxyURL.String())
+}
+
+func TestConvert(t *testing.T) {
+	riverArgsOAuth := Arguments{
+		AllowStale:      false,
+		Namespace:       "test",
+		RefreshInterval: time.Minute,
+		Region:          "a",
+		Server:          "http://foo:111",
+		TagSeparator:    ";",
+	}
+
+	promArgs := riverArgsOAuth.Convert()
+	assert.Equal(t, false, promArgs.AllowStale)
+	assert.Equal(t, "test", promArgs.Namespace)
+	assert.Equal(t, "a", promArgs.Region)
+	assert.Equal(t, model.Duration(time.Minute), promArgs.RefreshInterval)
+	assert.Equal(t, "http://foo:111", promArgs.Server)
+	assert.Equal(t, ";", promArgs.TagSeparator)
+}
