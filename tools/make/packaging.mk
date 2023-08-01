3f89af3c831f9a12d381b9f2e2cb1ac7dc4021c5
Robert Fratto <robertfratto@gmail.com>
2023-08-01 08:33:52 -0400

    build: do not produce Windows binaries with netgo build tags
    Go 1.19 adds Windows support for the native Go network stack, but
    doesn't include support for resolving DNS short names.
    
    Other projects, including Prometheus, have updated their build process
    to exclude the netgo build tags when producing Windows binaries to work
    around this behavior.
    
    Fixes grafana/agent#4665.

--- tools/make/packaging.mk
+++ tools/make/packaging.mk
@@ -31,63 +31,65 @@ dist-agent-binaries: dist/grafana-agent-linux-amd64       \
                      dist/grafana-agent-linux-amd64-boringcrypto  \
                      dist/grafana-agent-linux-arm64-boringcrypto
 
-dist/grafana-agent-linux-amd64: GO_TAGS += builtinassets promtail_journal_enabled
+dist/grafana-agent-linux-amd64: GO_TAGS += netgo builtinassets promtail_journal_enabled
 dist/grafana-agent-linux-amd64: GOOS    := linux
 dist/grafana-agent-linux-amd64: GOARCH  := amd64
 dist/grafana-agent-linux-amd64: generate-ui
 	$(PACKAGING_VARS) AGENT_BINARY=$@ $(MAKE) -f $(PARENT_MAKEFILE) agent
 
-dist/grafana-agent-linux-arm64: GO_TAGS += builtinassets promtail_journal_enabled
+dist/grafana-agent-linux-arm64: GO_TAGS += netgo builtinassets promtail_journal_enabled
 dist/grafana-agent-linux-arm64: GOOS    := linux
 dist/grafana-agent-linux-arm64: GOARCH  := arm64
 dist/grafana-agent-linux-arm64: generate-ui
 	$(PACKAGING_VARS) AGENT_BINARY=$@ $(MAKE) -f $(PARENT_MAKEFILE) agent
 
-dist/grafana-agent-linux-ppc64le: GO_TAGS += builtinassets promtail_journal_enabled
+dist/grafana-agent-linux-ppc64le: GO_TAGS += netgo builtinassets promtail_journal_enabled
 dist/grafana-agent-linux-ppc64le: GOOS    := linux
 dist/grafana-agent-linux-ppc64le: GOARCH  := ppc64le
 dist/grafana-agent-linux-ppc64le: generate-ui
 	$(PACKAGING_VARS) AGENT_BINARY=$@ $(MAKE) -f $(PARENT_MAKEFILE) agent
 
-dist/grafana-agent-linux-s390x: GO_TAGS += builtinassets promtail_journal_enabled
+dist/grafana-agent-linux-s390x: GO_TAGS += netgo builtinassets promtail_journal_enabled
 dist/grafana-agent-linux-s390x: GOOS    := linux
 dist/grafana-agent-linux-s390x: GOARCH  := s390x
 dist/grafana-agent-linux-s390x: generate-ui
 	$(PACKAGING_VARS) AGENT_BINARY=$@ $(MAKE) -f $(PARENT_MAKEFILE) agent
 
-dist/grafana-agent-darwin-amd64: GO_TAGS += builtinassets
+dist/grafana-agent-darwin-amd64: GO_TAGS += netgo builtinassets
 dist/grafana-agent-darwin-amd64: GOOS    := darwin
 dist/grafana-agent-darwin-amd64: GOARCH  := amd64
 dist/grafana-agent-darwin-amd64: generate-ui
 	$(PACKAGING_VARS) AGENT_BINARY=$@ $(MAKE) -f $(PARENT_MAKEFILE) agent
 
-dist/grafana-agent-darwin-arm64: GO_TAGS += builtinassets
+dist/grafana-agent-darwin-arm64: GO_TAGS += netgo builtinassets
 dist/grafana-agent-darwin-arm64: GOOS    := darwin
 dist/grafana-agent-darwin-arm64: GOARCH  := arm64
 dist/grafana-agent-darwin-arm64: generate-ui
 	$(PACKAGING_VARS) AGENT_BINARY=$@ $(MAKE) -f $(PARENT_MAKEFILE) agent
 
+# NOTE(rfratto): do not use netgo when building Windows binaries, which
+# prevents DNS short names from being resovable. See grafana/agent#4665.
 dist/grafana-agent-windows-amd64.exe: GO_TAGS += builtinassets
 dist/grafana-agent-windows-amd64.exe: GOOS    := windows
 dist/grafana-agent-windows-amd64.exe: GOARCH  := amd64
 dist/grafana-agent-windows-amd64.exe: generate-ui
 	$(PACKAGING_VARS) AGENT_BINARY=$@ $(MAKE) -f $(PARENT_MAKEFILE) agent
 
-dist/grafana-agent-freebsd-amd64: GO_TAGS += builtinassets
+dist/grafana-agent-freebsd-amd64: GO_TAGS += netgo builtinassets
 dist/grafana-agent-freebsd-amd64: GOOS    := freebsd
 dist/grafana-agent-freebsd-amd64: GOARCH  := amd64
 dist/grafana-agent-freebsd-amd64: generate-ui
 	$(PACKAGING_VARS) AGENT_BINARY=$@ $(MAKE) -f $(PARENT_MAKEFILE) agent
 
 
-dist/grafana-agent-linux-amd64-boringcrypto: GO_TAGS      += builtinassets promtail_journal_enabled
+dist/grafana-agent-linux-amd64-boringcrypto: GO_TAGS      += netgo builtinassets promtail_journal_enabled
 dist/grafana-agent-linux-amd64-boringcrypto: GOOS         := linux
 dist/grafana-agent-linux-amd64-boringcrypto: GOARCH       := amd64
 dist/grafana-agent-linux-amd64-boringcrypto: GOEXPERIMENT := boringcrypto
 dist/grafana-agent-linux-amd64-boringcrypto: generate-ui
 	$(PACKAGING_VARS) AGENT_BINARY=$@ $(MAKE) -f $(PARENT_MAKEFILE) agent
 
-dist/grafana-agent-linux-arm64-boringcrypto: GO_TAGS      += builtinassets promtail_journal_enabled
+dist/grafana-agent-linux-arm64-boringcrypto: GO_TAGS      += netgo builtinassets promtail_journal_enabled
 dist/grafana-agent-linux-arm64-boringcrypto: GOOS         := linux
 dist/grafana-agent-linux-arm64-boringcrypto: GOARCH       := arm64
 dist/grafana-agent-linux-arm64-boringcrypto: GOEXPERIMENT := boringcrypto
@@ -107,37 +109,39 @@ dist-agentctl-binaries: dist/grafana-agentctl-linux-amd64       \
                         dist/grafana-agentctl-windows-amd64.exe \
                         dist/grafana-agentctl-freebsd-amd64
 
-dist/grafana-agentctl-linux-amd64: GO_TAGS += promtail_journal_enabled
+dist/grafana-agentctl-linux-amd64: GO_TAGS += netgo promtail_journal_enabled
 dist/grafana-agentctl-linux-amd64: GOOS    := linux
 dist/grafana-agentctl-linux-amd64: GOARCH  := amd64
 dist/grafana-agentctl-linux-amd64:
 	$(PACKAGING_VARS) AGENTCTL_BINARY=$@ $(MAKE) -f $(PARENT_MAKEFILE) agentctl
 
-dist/grafana-agentctl-linux-arm64: GO_TAGS += promtail_journal_enabled
+dist/grafana-agentctl-linux-arm64: GO_TAGS += netgo promtail_journal_enabled
 dist/grafana-agentctl-linux-arm64: GOOS    := linux
 dist/grafana-agentctl-linux-arm64: GOARCH  := arm64
 dist/grafana-agentctl-linux-arm64:
 	$(PACKAGING_VARS) AGENTCTL_BINARY=$@ $(MAKE) -f $(PARENT_MAKEFILE) agentctl
 
-dist/grafana-agentctl-linux-ppc64le: GO_TAGS += promtail_journal_enabled
+dist/grafana-agentctl-linux-ppc64le: GO_TAGS += netgo promtail_journal_enabled
 dist/grafana-agentctl-linux-ppc64le: GOOS    := linux
 dist/grafana-agentctl-linux-ppc64le: GOARCH  := ppc64le
 dist/grafana-agentctl-linux-ppc64le:
 	$(PACKAGING_VARS) AGENTCTL_BINARY=$@ $(MAKE) -f $(PARENT_MAKEFILE) agentctl
 
-dist/grafana-agentctl-linux-s390x: GO_TAGS += promtail_journal_enabled
+dist/grafana-agentctl-linux-s390x: GO_TAGS += netgo promtail_journal_enabled
 dist/grafana-agentctl-linux-s390x: GOOS    := linux
 dist/grafana-agentctl-linux-s390x: GOARCH  := s390x
 dist/grafana-agentctl-linux-s390x:
 	$(PACKAGING_VARS) AGENTCTL_BINARY=$@ $(MAKE) -f $(PARENT_MAKEFILE) agentctl
 
-dist/grafana-agentctl-darwin-amd64: GOOS   := darwin
-dist/grafana-agentctl-darwin-amd64: GOARCH := amd64
+dist/grafana-agentctl-darwin-amd64: GO_TAGS += netgo
+dist/grafana-agentctl-darwin-amd64: GOOS    := darwin
+dist/grafana-agentctl-darwin-amd64: GOARCH  := amd64
 dist/grafana-agentctl-darwin-amd64:
 	$(PACKAGING_VARS) AGENTCTL_BINARY=$@ $(MAKE) -f $(PARENT_MAKEFILE) agentctl
 
-dist/grafana-agentctl-darwin-arm64: GOOS   := darwin
-dist/grafana-agentctl-darwin-arm64: GOARCH := arm64
+dist/grafana-agentctl-darwin-arm64: GO_TAGS += netgo
+dist/grafana-agentctl-darwin-arm64: GOOS    := darwin
+dist/grafana-agentctl-darwin-arm64: GOARCH  := arm64
 dist/grafana-agentctl-darwin-arm64:
 	$(PACKAGING_VARS) AGENTCTL_BINARY=$@ $(MAKE) -f $(PARENT_MAKEFILE) agentctl
 
@@ -146,6 +150,7 @@ dist/grafana-agentctl-windows-amd64.exe: GOARCH := amd64
 dist/grafana-agentctl-windows-amd64.exe:
 	$(PACKAGING_VARS) AGENTCTL_BINARY=$@ $(MAKE) -f $(PARENT_MAKEFILE) agentctl
 
+dist/grafana-agentctl-freebsd-amd64: GO_TAGS += netgo
 dist/grafana-agentctl-freebsd-amd64: GOOS    := freebsd
 dist/grafana-agentctl-freebsd-amd64: GOARCH  := amd64
 dist/grafana-agentctl-freebsd-amd64:
@@ -167,25 +172,25 @@ dist-agent-flow-binaries: dist.temp/grafana-agent-flow-linux-amd64       \
                           dist.temp/grafana-agent-flow-linux-s390x       \
                           dist.temp/grafana-agent-flow-windows-amd64.exe
 
-dist.temp/grafana-agent-flow-linux-amd64: GO_TAGS += builtinassets promtail_journal_enabled
+dist.temp/grafana-agent-flow-linux-amd64: GO_TAGS += netgo builtinassets promtail_journal_enabled
 dist.temp/grafana-agent-flow-linux-amd64: GOOS    := linux
 dist.temp/grafana-agent-flow-linux-amd64: GOARCH  := amd64
 dist.temp/grafana-agent-flow-linux-amd64: generate-ui
 	$(PACKAGING_VARS) FLOW_BINARY=$@ $(MAKE) -f $(PARENT_MAKEFILE) agent-flow
 
-dist.temp/grafana-agent-flow-linux-arm64: GO_TAGS += builtinassets promtail_journal_enabled
+dist.temp/grafana-agent-flow-linux-arm64: GO_TAGS += netgo builtinassets promtail_journal_enabled
 dist.temp/grafana-agent-flow-linux-arm64: GOOS    := linux
 dist.temp/grafana-agent-flow-linux-arm64: GOARCH  := arm64
 dist.temp/grafana-agent-flow-linux-arm64: generate-ui
 	$(PACKAGING_VARS) FLOW_BINARY=$@ $(MAKE) -f $(PARENT_MAKEFILE) agent-flow
 
-dist.temp/grafana-agent-flow-linux-ppc64le: GO_TAGS += builtinassets promtail_journal_enabled
+dist.temp/grafana-agent-flow-linux-ppc64le: GO_TAGS += netgo builtinassets promtail_journal_enabled
 dist.temp/grafana-agent-flow-linux-ppc64le: GOOS    := linux
 dist.temp/grafana-agent-flow-linux-ppc64le: GOARCH  := ppc64le
 dist.temp/grafana-agent-flow-linux-ppc64le: generate-ui
 	$(PACKAGING_VARS) FLOW_BINARY=$@ $(MAKE) -f $(PARENT_MAKEFILE) agent-flow
 
-dist.temp/grafana-agent-flow-linux-s390x: GO_TAGS += builtinassets promtail_journal_enabled
+dist.temp/grafana-agent-flow-linux-s390x: GO_TAGS += netgo builtinassets promtail_journal_enabled
 dist.temp/grafana-agent-flow-linux-s390x: GOOS    := linux
 dist.temp/grafana-agent-flow-linux-s390x: GOARCH  := s390x
 dist.temp/grafana-agent-flow-linux-s390x: generate-ui
