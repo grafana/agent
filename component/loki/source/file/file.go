154435bbdd55874ffbf0609a6e2c536e9061bd3f
Piotr Gwizdala <17101802+thampiotr@users.noreply.github.com>
2023-08-03 14:07:18 +0100

    Add eager validation

--- component/loki/source/file/file.go
+++ component/loki/source/file/file.go
@@ -15,6 +15,7 @@ import (
 	"github.com/grafana/agent/component/common/loki/positions"
 	"github.com/grafana/agent/component/discovery"
 	"github.com/prometheus/common/model"
+	"golang.org/x/exp/maps"
 )
 
 func init() {
@@ -46,6 +47,22 @@ type DecompressionConfig struct {
 	Enabled      bool          `river:"enabled,attr"`
 	InitialDelay time.Duration `river:"initial_delay,attr,optional"`
 	Format       string        `river:"format,attr"`
+}
+
+func (d DecompressionConfig) Validate() error {
+	if d.Format == "" {
+		return nil
+	}
+	for sf := range supportedCompressedFormats() {
+		if d.Format == sf {
+			return nil
+		}
+	}
+	return fmt.Errorf(
+		"unsupported compression format: %q - please use one of %q",
+		d.Format,
+		strings.Join(maps.Keys(supportedCompressedFormats()), ", "),
+	)
 }
 
 var (
