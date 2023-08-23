48efa0db9a6a6c4e9601ab3a7a68c874f88d082b
Devin Trejo <dtrejo@palantir.com>
2023-08-23 12:38:02 -0400

    Merge latest main branch into dt/async-yace

--- pkg/integrations/cloudwatch_exporter/cloudwatch_exporter_decoupled.go
+++ pkg/integrations/cloudwatch_exporter/cloudwatch_exporter_decoupled.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/go-kit/log"
 	yace "github.com/nerdswords/yet-another-cloudwatch-exporter/pkg"
-	yaceClients "github.com/nerdswords/yet-another-cloudwatch-exporter/pkg/clients"
 	yaceClientsV1 "github.com/nerdswords/yet-another-cloudwatch-exporter/pkg/clients/v1"
 	yaceConf "github.com/nerdswords/yet-another-cloudwatch-exporter/pkg/config"
 	"github.com/prometheus/client_golang/prometheus"
@@ -19,11 +18,11 @@ import (
 
 // asyncExporter wraps YACE entrypoint around an Integration implementation
 type asyncExporter struct {
-	name         string
-	logger       yaceLoggerWrapper
-	sessionCache yaceClients.Factory
-	scrapeConf   yaceConf.ScrapeConf
-	registry     atomic.Pointer[prometheus.Registry]
+	name                 string
+	logger               yaceLoggerWrapper
+	cachingClientFactory cachingFactory
+	scrapeConf           yaceConf.ScrapeConf
+	registry             atomic.Pointer[prometheus.Registry]
 	// scrapeInterval is the frequency in which a background go-routine collects new AWS metrics via YACE.
 	scrapeInterval time.Duration
 }
@@ -37,19 +36,18 @@ func NewDecoupledCloudwatchExporter(name string, logger log.Logger, conf yaceCon
 		log:   logger,
 	}
 	return &asyncExporter{
-		name:           name,
-		logger:         loggerWrapper,
-		sessionCache:   yaceClientsV1.NewFactory(conf, fipsEnabled, loggerWrapper),
-		scrapeConf:     conf,
-		registry:       atomic.Pointer[prometheus.Registry]{},
-		scrapeInterval: scrapeInterval,
+		name:                 name,
+		logger:               loggerWrapper,
+		cachingClientFactory: yaceClientsV1.NewFactory(conf, fipsEnabled, loggerWrapper),
+		scrapeConf:           conf,
+		registry:             atomic.Pointer[prometheus.Registry]{},
+		scrapeInterval:       scrapeInterval,
 	}
 }
 
 func (e *asyncExporter) MetricsHandler() (http.Handler, error) {
 	// Wrapping handler to have logging around handler
 	h := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		e.logger.Debug("Running collect in cloudwatch_exporter")
 		reg := e.registry.Load()
 		if reg == nil {
 			e.logger.Warn("cloudwatch_exporter prometheus metric registry is empty")
@@ -72,32 +70,42 @@ func (e *asyncExporter) Run(ctx context.Context) error {
 	ticker := time.NewTicker(e.scrapeInterval)
 	defer ticker.Stop()
 	for {
-		reg := prometheus.NewRegistry()
-		err := yace.UpdateMetrics(
-			ctx,
-			e.logger,
-			e.scrapeConf,
-			reg,
-			e.sessionCache,
-			yace.MetricsPerQuery(metricsPerQuery),
-			yace.LabelsSnakeCase(labelsSnakeCase),
-			yace.CloudWatchAPIConcurrency(cloudWatchConcurrency),
-			yace.TaggingAPIConcurrency(tagConcurrency),
-			// Enable max-dimension-associator feature flag
-			// https://github.com/nerdswords/yet-another-cloudwatch-exporter/blob/master/docs/feature_flags.md#new-associator-algorithm
-			yace.EnableFeatureFlag(yaceConf.MaxDimensionsAssociator),
-		)
-		if err != nil {
-			e.logger.Error(err, "Error collecting cloudwatch metrics")
-		}
-		// always update the registry even on error, to ensure we don't expose stale metrics from the previous
-		// registry
-		e.registry.Store(reg)
-
+		e.scrape(ctx)
 		select {
 		case <-ctx.Done():
 			return nil
 		case <-ticker.C:
 		}
 	}
+}
+
+func (e *asyncExporter) scrape(ctx context.Context) {
+	e.logger.Debug("Running collect in cloudwatch_exporter")
+	// since we have called refresh, we have loaded all the credentials
+	// into the clients and it is now safe to call concurrently. Defer the
+	// clearing, so we always clear credentials before the next scrape
+	e.cachingClientFactory.Refresh()
+	defer e.cachingClientFactory.Clear()
+
+	reg := prometheus.NewRegistry()
+	err := yace.UpdateMetrics(
+		ctx,
+		e.logger,
+		e.scrapeConf,
+		reg,
+		e.cachingClientFactory,
+		yace.MetricsPerQuery(metricsPerQuery),
+		yace.LabelsSnakeCase(labelsSnakeCase),
+		yace.CloudWatchAPIConcurrency(cloudWatchConcurrency),
+		yace.TaggingAPIConcurrency(tagConcurrency),
+		// Enable max-dimension-associator feature flag
+		// https://github.com/nerdswords/yet-another-cloudwatch-exporter/blob/master/docs/feature_flags.md#new-associator-algorithm
+		yace.EnableFeatureFlag(yaceConf.MaxDimensionsAssociator),
+	)
+	if err != nil {
+		e.logger.Error(err, "Error collecting cloudwatch metrics")
+	}
+	// always update the registry even on error, to ensure we don't expose stale metrics from the previous
+	// registry
+	e.registry.Store(reg)
 }
