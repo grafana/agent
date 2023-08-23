505b4d5e385e1d51208b07507ed843f7cde894b4
Erik Baranowski <39704712+erikbaranowski@users.noreply.github.com>
2023-08-23 15:03:27 -0400

    separate windows tests for windows components in the static converter (#4907)

--- converter/internal/staticconvert/staticconvert_test.go
+++ converter/internal/staticconvert/staticconvert_test.go
@@ -1,6 +1,7 @@
 package staticconvert_test
 
 import (
+	"runtime"
 	"testing"
 
 	"github.com/grafana/agent/converter/internal/staticconvert"
@@ -9,4 +10,8 @@ import (
 
 func TestConvert(t *testing.T) {
 	test_common.TestDirectory(t, "testdata", ".yaml", staticconvert.Convert)
+
+	if runtime.GOOS == "windows" {
+		test_common.TestDirectory(t, "testdata_windows", ".yaml", staticconvert.Convert)
+	}
 }
