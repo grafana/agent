334b853ef2172dca94d748e1e76aeb4b8ab45b93
William Dumont <william.dumont@grafana.com>
2023-09-01 13:13:58 +0200

    first version keeping promtail consul agent version

---
+++ component/discovery/consulagent/consulagent_test.go
@@ -0,0 +1,118 @@
+package consulagent
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/grafana/river"
+	promcfg "github.com/prometheus/common/config"
+	"github.com/prometheus/common/model"
+	"github.com/stretchr/testify/require"
+	"gotest.tools/assert"
+)
+
+func TestConvert(t *testing.T) {
+	var exampleRiverConfig = `
+	server = "localhost:8500"
+	token = "token"
+	datacenter = "dc"
+	tag_separator = ","
+	scheme = "scheme"
+	username = "username"
+	password = "pass"
+	allow_stale = false
+	refresh_interval = "5m"
+	services = ["service1", "service2"]
+	tags = ["tag1", "tag2"]
+	node_meta = {
+		what  = "this",
+		where = "here",
+	}
+	tls_config {
+		ca_file = "/path/to/ca_file"
+	}
+`
+
+	var args Arguments
+	err := river.Unmarshal([]byte(exampleRiverConfig), &args)
+	require.NoError(t, err)
+
+	converted := args.Convert()
+	assert.Equal(t, "localhost:8500", converted.Server)
+	assert.Equal(t, promcfg.Secret("token"), converted.Token)
+	assert.Equal(t, "dc", converted.Datacenter)
+	assert.Equal(t, ",", converted.TagSeparator)
+	assert.Equal(t, "scheme", converted.Scheme)
+	assert.Equal(t, "username", converted.Username)
+	assert.Equal(t, promcfg.Secret("pass"), converted.Password)
+	assert.Equal(t, false, converted.AllowStale)
+	assert.Equal(t, model.Duration(5*time.Minute), converted.RefreshInterval)
+	expectedServices := []string{"service1", "service2"}
+	if !reflect.DeepEqual(expectedServices, converted.Services) {
+		t.Errorf("Services do not match, expected: %v, got: %v", expectedServices, converted.Services)
+	}
+	expectedNodeMeta := map[string]string{"what": "this", "where": "here"}
+	if !reflect.DeepEqual(expectedNodeMeta, converted.NodeMeta) {
+		t.Errorf("Services do not match, expected: %v, got: %v", expectedNodeMeta, converted.NodeMeta)
+	}
+	assert.Equal(t, "username", converted.Username)
+	assert.Equal(t, "/path/to/ca_file", converted.TLSConfig.CAFile)
+}
+
+func TestBadTLSRiverConfig(t *testing.T) {
+	var exampleRiverConfig = `
+	server = "localhost:8500"
+	token = "token"
+	datacenter = "dc"
+	tag_separator = ","
+	scheme = "scheme"
+	username = "username"
+	password = "pass"
+	allow_stale = false
+	refresh_interval = "10s"
+	services = ["service1", "service2"]
+	tags = ["tag1", "tag2"]
+	node_meta = {
+		what  = "this",
+		where = "here",
+	}
+	tls_config {
+		ca_file = "/path/to/ca_file"
+		ca_pem = "not a real pem"
+	}
+`
+
+	// Make sure the TLSConfig Validate function is being utilized correctly.
+	var args Arguments
+	err := river.Unmarshal([]byte(exampleRiverConfig), &args)
+	require.ErrorContains(t, err, "at most one of ca_pem and ca_file must be configured")
+}
+
+func TestBadRefreshIntervalRiverConfig(t *testing.T) {
+	var exampleRiverConfig = `
+	server = "localhost:8500"
+	token = "token"
+	datacenter = "dc"
+	tag_separator = ","
+	scheme = "scheme"
+	username = "username"
+	password = "pass"
+	allow_stale = false
+	refresh_interval = "-1s"
+	services = ["service1", "service2"]
+	tags = ["tag1", "tag2"]
+	node_meta = {
+		what  = "this",
+		where = "here",
+	}
+	tls_config {
+		ca_file = "/path/to/ca_file"
+	}
+`
+
+	// Make sure the Refresh Interval is tested correctly.
+	var args Arguments
+	err := river.Unmarshal([]byte(exampleRiverConfig), &args)
+	require.ErrorContains(t, err, "refresh_interval must be greater than 0")
+}
--- converter/internal/promtailconvert/internal/build/consul_agent.go
+++ converter/internal/promtailconvert/internal/build/consul_agent.go
@@ -1,13 +1,49 @@
 package build
 
-import "github.com/grafana/agent/converter/diag"
+import (
+	"time"
+
+	"github.com/grafana/agent/component/discovery/consulagent"
+	"github.com/grafana/agent/converter/internal/common"
+	"github.com/grafana/agent/converter/internal/prometheusconvert"
+	promtail_consulagent "github.com/grafana/loki/clients/pkg/promtail/discovery/consulagent"
+	"github.com/grafana/river/rivertypes"
+)
 
 func (s *ScrapeConfigBuilder) AppendConsulAgentSDs() {
-	// TODO: implement this
-	if s.cfg.ServiceDiscoveryConfig.ConsulAgentSDConfigs != nil {
-		s.diags.Add(
-			diag.SeverityLevelError,
-			"consul_agent SDs are not currently supported in Grafana Agent Flow - see https://github.com/grafana/agent/issues/2261",
-		)
+	if len(s.cfg.ServiceDiscoveryConfig.ConsulAgentSDConfigs) == 0 {
+		return
+	}
+
+	for i, sd := range s.cfg.ServiceDiscoveryConfig.ConsulAgentSDConfigs {
+		args := toDiscoveryAgentConsul(sd)
+		compLabel := common.LabelWithIndex(i, s.globalCtx.LabelPrefix, s.cfg.JobName)
+		s.f.Body().AppendBlock(common.NewBlockWithOverride(
+			[]string{"discovery", "consulagent"},
+			compLabel,
+			args,
+		))
+		s.allTargetsExps = append(s.allTargetsExps, "discovery.consulagent."+compLabel+".targets")
+	}
+}
+
+func toDiscoveryAgentConsul(sdConfig *promtail_consulagent.SDConfig) *consulagent.Arguments {
+	if sdConfig == nil {
+		return nil
+	}
+	return &consulagent.Arguments{
+		RefreshInterval: time.Duration(sdConfig.RefreshInterval),
+		Server:          sdConfig.Server,
+		Token:           rivertypes.Secret(sdConfig.Token),
+		Datacenter:      sdConfig.Datacenter,
+		TagSeparator:    sdConfig.TagSeparator,
+		Scheme:          sdConfig.Scheme,
+		Username:        sdConfig.Username,
+		Password:        rivertypes.Secret(sdConfig.Password),
+		AllowStale:      sdConfig.AllowStale,
+		Services:        sdConfig.Services,
+		ServiceTags:     sdConfig.ServiceTags,
+		NodeMeta:        sdConfig.NodeMeta,
+		TLSConfig:       *prometheusconvert.ToTLSConfig(&sdConfig.TLSConfig),
 	}
 }
