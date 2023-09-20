bcde5101b218b1dd5654668a8bd029be2a54ef3f
erikbaranowski <39704712+erikbaranowski@users.noreply.github.com>
2023-09-20 15:57:05 -0400

    simple test demo'ing loki parse escapes
    Signed-off-by: erikbaranowski <39704712+erikbaranowski@users.noreply.github.com>

--- component/loki/process/stages/match_test.go
+++ component/loki/process/stages/match_test.go
@@ -33,7 +33,7 @@ stage.match {
 
 stage.match {
 		pipeline_name = "app2"
-		selector = "{app=\"poki\"}"
+		selector = "{log_type=\"\", logs_agent_grafana_com_log_format=~\"(?i).*((generic-?)?json).*\"} |~ \"^\\\\s*\\\\{.+\\\\}\\\\s*$\""
 		stage.json {
 				expressions = { "msg" = "msg" }
 		}
@@ -89,7 +89,7 @@ func TestMatchStage(t *testing.T) {
 	e.Extracted = map[string]interface{}{}
 	in <- e
 	e = <-out
-	assert.Equal(t, "app2 log line", e.Line)
+	assert.Equal(t, testMatchLogLineApp2, e.Line)
 	close(in)
 }
 
