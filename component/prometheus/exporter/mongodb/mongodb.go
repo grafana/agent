da008f8f1127cf5447aa3d4dd29b084b612eac70
Paschalis Tsilias <tpaschalis@users.noreply.github.com>
2023-09-26 11:03:10 +0300

    prometheus.exporter: standardize minimum set of labels, instance key (#5169)
    Signed-off-by: Paschalis Tsilias <paschalis.tsilias@grafana.com>

--- component/prometheus/exporter/mongodb/mongodb.go
+++ component/prometheus/exporter/mongodb/mongodb.go
@@ -1,10 +1,7 @@
 package mongodb
 
 import (
-	"net/url"
-
 	"github.com/grafana/agent/component"
-	"github.com/grafana/agent/component/discovery"
 	"github.com/grafana/agent/component/prometheus/exporter"
 	"github.com/grafana/agent/pkg/integrations"
 	"github.com/grafana/agent/pkg/integrations/mongodb_exporter"
@@ -18,22 +15,13 @@ func init() {
 		Args:          Arguments{},
 		Exports:       exporter.Exports{},
 		NeedsServices: exporter.RequiredServices(),
-		Build:         exporter.NewWithTargetBuilder(createExporter, "mongodb", customizeTarget),
+		Build:         exporter.New(createExporter, "mongodb"),
 	})
 }
 
-func createExporter(opts component.Options, args component.Arguments) (integrations.Integration, error) {
+func createExporter(opts component.Options, args component.Arguments, defaultInstanceKey string) (integrations.Integration, string, error) {
 	a := args.(Arguments)
-	return a.Convert().NewIntegration(opts.Logger)
-}
-
-func customizeTarget(baseTarget discovery.Target, args component.Arguments) []discovery.Target {
-	a := args.(Arguments)
-	target := baseTarget
-
-	u, _ := url.Parse(string(a.URI))
-	target["instance"] = u.Host
-	return []discovery.Target{target}
+	return integrations.NewIntegrationWithInstanceKey(opts.Logger, a.Convert(), defaultInstanceKey)
 }
 
 type Arguments struct {
