7454935bbf833e2fed1f0361a96f2527019045a8
Mischa Thompson <mischabear@gmail.com>
2023-09-27 00:30:42 -0700

    cAdvisor component tests and docs (#5310)
    Co-authored-by: Clayton Cornell <131809008+clayton-cornell@users.noreply.github.com>

--- component/prometheus/exporter/cadvisor/cadvisor_test.go
+++ component/prometheus/exporter/cadvisor/cadvisor_test.go
@@ -1,51 +1,95 @@
 package cadvisor
 
-/*
+import (
+	"testing"
+	"time"
+
+	"github.com/grafana/agent/pkg/integrations/cadvisor"
+	"github.com/grafana/river"
+	"github.com/stretchr/testify/require"
+)
+
 func TestUnmarshalRiver(t *testing.T) {
 	riverCfg := `
-		api_token_file = "/etc/github-api-token"
-		repositories = ["grafana/agent"]
-		organizations = ["grafana", "prometheus"]
-		users = ["jcreixell"]
-		api_url = "https://some-other-api.github.com"
+store_container_labels = true
+allowlisted_container_labels = ["label1", "label2"]
+env_metadata_allowlist = ["env1", "env2"]
+raw_cgroup_prefix_allowlist = ["prefix1", "prefix2"]
+perf_events_config = "perf_events_config"
+resctrl_interval = "1s"
+disabled_metrics = ["metric1", "metric2"]
+enabled_metrics = ["metric3", "metric4"]
+storage_duration = "2s"
+containerd_host = "containerd_host"
+containerd_namespace = "containerd_namespace"
+docker_host = "docker_host"
+use_docker_tls = true
+docker_tls_cert = "docker_tls_cert"
+docker_tls_key = "docker_tls_key"
+docker_tls_ca = "docker_tls_ca"
 `
 	var args Arguments
 	err := river.Unmarshal([]byte(riverCfg), &args)
 	require.NoError(t, err)
-	require.Equal(t, "/etc/github-api-token", args.APITokenFile)
-	require.Equal(t, []string{"grafana/agent"}, args.Repositories)
-	require.Contains(t, args.Organizations, "grafana")
-	require.Contains(t, args.Organizations, "prometheus")
-	require.Equal(t, []string{"jcreixell"}, args.Users)
-	require.Equal(t, "https://some-other-api.github.com", args.APIURL)
+	expected := Arguments{
+		StoreContainerLabels:       true,
+		AllowlistedContainerLabels: []string{"label1", "label2"},
+		EnvMetadataAllowlist:       []string{"env1", "env2"},
+		RawCgroupPrefixAllowlist:   []string{"prefix1", "prefix2"},
+		PerfEventsConfig:           "perf_events_config",
+		ResctrlInterval:            1 * time.Second,
+		DisabledMetrics:            []string{"metric1", "metric2"},
+		EnabledMetrics:             []string{"metric3", "metric4"},
+		StorageDuration:            2 * time.Second,
+		ContainerdHost:             "containerd_host",
+		ContainerdNamespace:        "containerd_namespace",
+		DockerHost:                 "docker_host",
+		UseDockerTLS:               true,
+		DockerTLSCert:              "docker_tls_cert",
+		DockerTLSKey:               "docker_tls_key",
+		DockerTLSCA:                "docker_tls_ca",
+	}
+	require.Equal(t, expected, args)
 }
 
 func TestConvert(t *testing.T) {
 	args := Arguments{
-		APITokenFile:  "/etc/github-api-token",
-		Repositories:  []string{"grafana/agent"},
-		Organizations: []string{"grafana", "prometheus"},
-		Users:         []string{"jcreixell"},
-		APIURL:        "https://some-other-api.github.com",
+		StoreContainerLabels:       true,
+		AllowlistedContainerLabels: []string{"label1", "label2"},
+		EnvMetadataAllowlist:       []string{"env1", "env2"},
+		RawCgroupPrefixAllowlist:   []string{"prefix1", "prefix2"},
+		PerfEventsConfig:           "perf_events_config",
+		ResctrlInterval:            1 * time.Second,
+		DisabledMetrics:            []string{"metric1", "metric2"},
+		EnabledMetrics:             []string{"metric3", "metric4"},
+		StorageDuration:            2 * time.Second,
+		ContainerdHost:             "containerd_host",
+		ContainerdNamespace:        "containerd_namespace",
+		DockerHost:                 "docker_host",
+		UseDockerTLS:               true,
+		DockerTLSCert:              "docker_tls_cert",
+		DockerTLSKey:               "docker_tls_key",
+		DockerTLSCA:                "docker_tls_ca",
 	}
 
 	res := args.Convert()
-	require.Equal(t, "/etc/github-api-token", res.APITokenFile)
-	require.Equal(t, []string{"grafana/agent"}, res.Repositories)
-	require.Contains(t, res.Organizations, "grafana")
-	require.Contains(t, res.Organizations, "prometheus")
-	require.Equal(t, []string{"jcreixell"}, res.Users)
-	require.Equal(t, "https://some-other-api.github.com", res.APIURL)
-}
-
-func TestCustomizeTarget_Valid(t *testing.T) {
-	args := Arguments{
-		APIURL: "https://some-other-api.github.com",
+	expected := &cadvisor.Config{
+		StoreContainerLabels:       true,
+		AllowlistedContainerLabels: []string{"label1", "label2"},
+		EnvMetadataAllowlist:       []string{"env1", "env2"},
+		RawCgroupPrefixAllowlist:   []string{"prefix1", "prefix2"},
+		PerfEventsConfig:           "perf_events_config",
+		ResctrlInterval:            int64(1 * time.Second),
+		DisabledMetrics:            []string{"metric1", "metric2"},
+		EnabledMetrics:             []string{"metric3", "metric4"},
+		StorageDuration:            2 * time.Second,
+		Containerd:                 "containerd_host",
+		ContainerdNamespace:        "containerd_namespace",
+		Docker:                     "docker_host",
+		DockerTLS:                  true,
+		DockerTLSCert:              "docker_tls_cert",
+		DockerTLSKey:               "docker_tls_key",
+		DockerTLSCA:                "docker_tls_ca",
 	}
-
-	baseTarget := discovery.Target{}
-	newTargets := customizeTarget(baseTarget, args)
-	require.Equal(t, 1, len(newTargets))
-	require.Equal(t, "some-other-api.github.com", newTargets[0]["instance"])
+	require.Equal(t, expected, res)
 }
-*/
---
+++ docs/sources/flow/reference/components/prometheus.exporter.cadvisor.md
@@ -0,0 +1,115 @@
+---
+aliases:
+- /docs/grafana-cloud/agent/flow/reference/components/prometheus.exporter.cadvisor/
+- /docs/grafana-cloud/monitor-infrastructure/agent/flow/reference/components/prometheus.exporter.cadvisor/
+- /docs/grafana-cloud/monitor-infrastructure/integrations/agent/flow/reference/components/prometheus.exporter.cadvisor/
+canonical: https://grafana.com/docs/agent/latest/flow/reference/components/prometheus.exporter.cadvisor/
+title: prometheus.exporter.cadvisor
+description: Learn about the prometheus.exporter.cadvisor
+---
+
+# prometheus.exporter.cadvisor
+The `prometheus.exporter.cadvisor` component collects container metrics using
+[cAdvisor](https://github.com/google/cadvisor).
+
+## Usage
+
+```river
+prometheus.exporter.cadvisor "LABEL" {
+}
+```
+
+## Arguments
+The following arguments can be used to configure the exporter's behavior.
+All arguments are optional. Omitted fields take their default values.
+
+Name | Type | Description | Default | Required
+---- | ---- | ----------- | ------- | --------
+`store_container_labels` | `bool` | Whether to convert container labels and environment variables into labels on Prometheus metrics for each container. | `true` | no
+`allowlisted_container_labels` | `list(string)` | Allowlist of container labels to convert to Prometheus labels. | | no
+`env_metadata_allowlist` | `list(string)` | Allowlist of environment variable keys matched with specified prefix that needs to be collected for containers. | | no
+`raw_cgroup_prefix_allowlist` | `list(string)` | List of cgroup path prefix that needs to be collected, even when docker_only is specified. | | no
+`perf_events_config` | `string` | Path to a JSON file containing configuration of perf events to measure. | | no
+`resctrl_interval` | `duration` | Interval to update resctrl mon groups. | `0` | no
+`disabled_metrics` | `list(string)` | List of metrics to be disabled which, if set, overrides the default disabled metrics. | | no
+`enabled_metrics` | `list(string)` | List of metrics to be enabled which, if set, overrides disabled_metrics. | | no
+`storage_duration` | `duration` | Length of time to keep data stored in memory. | `2m` | no
+`containerd_host` | `string` | Containerd endpoint. | `/run/containerd/containerd.sock` | no
+`containerd_namespace` | `string` | Containerd namespace. | `k8s.io` | no
+`docker_host` | `string` | Docker endpoint. | `unix:///var/run/docker.sock` | no
+`use_docker_tls` | `bool` | Use TLS to connect to docker. | `false` | no
+`docker_tls_cert` | `string` | Path to client certificate for TLS connection to docker. | `cert.pem` | no
+`docker_tls_key` | `string` | Path to private key for TLS connection to docker. | `key.pem` | no
+`docker_tls_ca` | `string` | Path to a trusted CA for TLS connection to docker. | `ca.pem` | no
+`docker_only` | `bool` | Only report docker containers in addition to root stats. | `false` | no
+
+For `allowlisted_container_labels` to take effect, `store_container_labels` must be set to `false`.
+
+`env_metadata_allowlist` is only supported for containerd and Docker runtimes.
+
+If `perf_events_config` is not set, measurement of perf events is disabled.
+
+A `resctrl_interval` of `0` disables updating mon groups.
+
+## Blocks
+
+The `prometheus.exporter.cadvisor` component does not support any blocks, and is configured
+fully through arguments.
+
+## Exported fields
+
+{{< docs/shared lookup="flow/reference/components/exporter-component-exports.md" source="agent" version="<AGENT VERSION>" >}}
+
+## Component health
+
+`prometheus.exporter.cadvisor` is only reported as unhealthy if given
+an invalid configuration. In those cases, exported fields retain their last
+healthy values.
+
+## Debug information
+
+`prometheus.exporter.cadvisor` does not expose any component-specific
+debug information.
+
+## Debug metrics
+
+`prometheus.exporter.cadvisor` does not expose any component-specific
+debug metrics.
+
+## Example
+
+This example uses a [`prometheus.scrape` component][scrape] to collect metrics
+from `prometheus.exporter.cadvisor`:
+
+```river
+prometheus.exporter.cadvisor "example" {
+  docker     = "unix:///var/run/docker.sock"
+
+  storage_duration = "5m"
+}
+
+// Configure a prometheus.scrape component to collect cadvisor metrics.
+prometheus.scrape "scraper" {
+  targets    = prometheus.exporter.cadvisor.example.targets
+  forward_to = [ prometheus.remote_write.demo.receiver ]
+}
+
+prometheus.remote_write "demo" {
+  endpoint {
+    url = PROMETHEUS_REMOTE_WRITE_URL
+
+    basic_auth {
+      username = USERNAME
+      password = PASSWORD
+    }
+  }
+}
+```
+
+Replace the following:
+
+- `PROMETHEUS_REMOTE_WRITE_URL`: The URL of the Prometheus remote_write-compatible server to send metrics to.
+- `USERNAME`: The username to use for authentication to the remote_write API.
+- `PASSWORD`: The password to use for authentication to the remote_write API.
+
+[scrape]: {{< relref "./prometheus.scrape.md" >}}
