f0d10785feec97f1a95602aa6747320c9122efa0
Mischa Thompson <mischabear@gmail.com>
2023-09-28 13:27:50 -0700

    Fix condition

--- tools/generate-version-files.sh
+++ tools/generate-version-files.sh
@@ -7,7 +7,7 @@ fi
 
 versionMatcher='^v[0-9]+\.[0-9]+\.[0-9]+(-rc[0-9]+)?$'
 
-if echo "$AGENT_VERSION" | grep -Eq "$versionMatcher"; then
+if ! echo "$AGENT_VERSION" | grep -Eq "$versionMatcher"; then
     echo "AGENT_VERSION env var is not in the correct format. It should be in the format of vX.Y.Z or vX.Y.Z-rcN"
     exit 1
 fi
