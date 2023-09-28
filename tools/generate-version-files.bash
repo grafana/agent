3e2ced80591a44619872c7b19a07815dc206c9af
Mischa Thompson <mischabear@gmail.com>
2023-09-28 07:34:16 -0700

    Add pattern matching and use sed

--- tools/generate-version-files.bash
+++ tools/generate-version-files.bash
@@ -5,8 +5,15 @@ if [ -z "$AGENT_VERSION" ]; then
     exit 1
 fi
 
+versionMatcher='^v[0-9]+\.[0-9]+\.[0-9]+(-rc[0-9]+)?$'
+
+if [[ ! $AGENT_VERSION =~ $versionMatcher ]]; then
+    echo "AGENT_VERSION env var is not in the correct format. It should be in the format of vX.Y.Z or vX.Y.Z-rcN"
+    exit 1
+fi
+
 templates=$(find . -type f -name "*.templ" -not -path "./.git/*")
 for template in $templates; do
     echo "Generating ${template%.templ}"
-    envsubst < $template > ${template%.templ}
+    sed -e "s/\$AGENT_VERSION/$AGENT_VERSION/g" < $template > ${template%.templ}
 done
