6487b2326efdb748e51aec0bb00015f04efa932f
Paulin Todev <paulin.todev@gmail.com>
2023-10-04 09:52:23 +0100

    Add otel service as a dependency to module loaders.
    This is necessary so that modules can successfully load
    otelcol components which depend on the otel service.
    Those are almost all receivers and exporters.

--- component/module/git/git.go
+++ component/module/git/git.go
@@ -15,6 +15,7 @@ import (
 	"github.com/grafana/agent/component/module/git/internal/vcs"
 	"github.com/grafana/agent/service/cluster"
 	"github.com/grafana/agent/service/http"
+	otel_service "github.com/grafana/agent/service/otel"
 )
 
 func init() {
@@ -22,7 +23,7 @@ func init() {
 		Name:          "module.git",
 		Args:          Arguments{},
 		Exports:       module.Exports{},
-		NeedsServices: []string{http.ServiceName, cluster.ServiceName},
+		NeedsServices: []string{http.ServiceName, cluster.ServiceName, otel_service.ServiceName},
 
 		Build: func(opts component.Options, args component.Arguments) (component.Component, error) {
 			return New(opts, args.(Arguments))
