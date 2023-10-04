2670c2d2bfe831770ab54bc40d7e311da3131111
Paulin Todev <paulin.todev@gmail.com>
2023-10-04 20:02:40 +0100

    Add otel service as a dependency to module loaders. (#5362)
    * Add otel service as a dependency to module loaders.
    
    This is necessary so that modules can successfully load
    otelcol components which depend on the otel service.
    Those are almost all receivers and exporters.

--- component/module/http/http.go
+++ component/module/http/http.go
@@ -11,6 +11,7 @@ import (
 	remote_http "github.com/grafana/agent/component/remote/http"
 	"github.com/grafana/agent/service/cluster"
 	http_service "github.com/grafana/agent/service/http"
+	otel_service "github.com/grafana/agent/service/otel"
 	"github.com/grafana/river/rivertypes"
 )
 
@@ -19,7 +20,7 @@ func init() {
 		Name:          "module.http",
 		Args:          Arguments{},
 		Exports:       module.Exports{},
-		NeedsServices: []string{http_service.ServiceName, cluster.ServiceName},
+		NeedsServices: []string{http_service.ServiceName, cluster.ServiceName, otel_service.ServiceName},
 
 		Build: func(opts component.Options, args component.Arguments) (component.Component, error) {
 			return New(opts, args.(Arguments))
