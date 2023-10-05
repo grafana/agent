84665d8ff526dc449159acaf4c75035a6406b91a
Mischa Thompson <mischabear@gmail.com>
2023-10-05 12:50:58 -0700

    Update version and regenerate

--- pkg/operator/defaults.go
+++ pkg/operator/defaults.go
@@ -2,7 +2,7 @@ package operator
 
 // Supported versions of the Grafana Agent.
 var (
-	DefaultAgentVersion   = "v0.37.0-rc.0"
+	DefaultAgentVersion   = "v0.37.0-rc0"
 	DefaultAgentBaseImage = "grafana/agent"
 	DefaultAgentImage     = DefaultAgentBaseImage + ":" + DefaultAgentVersion
 )
