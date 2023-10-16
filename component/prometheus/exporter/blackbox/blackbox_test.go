be397c3dddf2620685856413fad00031383e8466
Paschalis Tsilias <tpaschalis@users.noreply.github.com>
2023-10-16 16:14:59 +0300

    Cherry picks for v0.37.2 (#5484)
    Co-authored-by: Erik Baranowski <39704712+erikbaranowski@users.noreply.github.com>
    Co-authored-by: Craig Peterson <192540+captncraig@users.noreply.github.com>
    Co-authored-by: Paulin Todev <paulin.todev@gmail.com>
    Signed-off-by: Paschalis Tsilias <paschalis.tsilias@grafana.com>

--- component/prometheus/exporter/blackbox/blackbox_test.go
+++ component/prometheus/exporter/blackbox/blackbox_test.go
@@ -7,8 +7,10 @@ import (
 	"github.com/grafana/agent/component"
 	"github.com/grafana/agent/component/discovery"
 	"github.com/grafana/river"
+	blackbox_config "github.com/prometheus/blackbox_exporter/config"
 	"github.com/prometheus/common/model"
 	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v2"
 )
 
 func TestUnmarshalRiver(t *testing.T) {
@@ -56,8 +58,11 @@ func TestUnmarshalRiverWithInlineConfig(t *testing.T) {
 	err := river.Unmarshal([]byte(riverCfg), &args)
 	require.NoError(t, err)
 	require.Equal(t, "", args.ConfigFile)
-	require.Equal(t, args.ConfigStruct.Modules["http_2xx"].Prober, "http")
-	require.Equal(t, args.ConfigStruct.Modules["http_2xx"].Timeout, 5*time.Second)
+	var blackboxConfig blackbox_config.Config
+	err = yaml.UnmarshalStrict([]byte(args.Config.Value), &blackboxConfig)
+	require.NoError(t, err)
+	require.Equal(t, blackboxConfig.Modules["http_2xx"].Prober, "http")
+	require.Equal(t, blackboxConfig.Modules["http_2xx"].Timeout, 5*time.Second)
 	require.Equal(t, 2, len(args.Targets))
 	require.Equal(t, 500*time.Millisecond, args.ProbeTimeoutOffset)
 	require.Contains(t, "target_a", args.Targets[0].Name)
@@ -67,6 +72,40 @@ func TestUnmarshalRiverWithInlineConfig(t *testing.T) {
 	require.Contains(t, "http://grafana.com", args.Targets[1].Target)
 	require.Contains(t, "http_2xx", args.Targets[1].Module)
 }
+
+func TestUnmarshalRiverWithInlineConfigYaml(t *testing.T) {
+	riverCfg := `
+		config = "modules:\n  http_2xx:\n    prober: http\n    timeout: 5s\n"
+
+		target "target_a" {
+			address = "http://example.com"
+			module = "http_2xx"
+		}
+		target "target_b" {
+			address = "http://grafana.com"
+			module = "http_2xx"
+		}
+		probe_timeout_offset = "0.5s"
+`
+	var args Arguments
+	err := river.Unmarshal([]byte(riverCfg), &args)
+	require.NoError(t, err)
+	require.Equal(t, "", args.ConfigFile)
+	var blackboxConfig blackbox_config.Config
+	err = yaml.UnmarshalStrict([]byte(args.Config.Value), &blackboxConfig)
+	require.NoError(t, err)
+	require.Equal(t, blackboxConfig.Modules["http_2xx"].Prober, "http")
+	require.Equal(t, blackboxConfig.Modules["http_2xx"].Timeout, 5*time.Second)
+	require.Equal(t, 2, len(args.Targets))
+	require.Equal(t, 500*time.Millisecond, args.ProbeTimeoutOffset)
+	require.Contains(t, "target_a", args.Targets[0].Name)
+	require.Contains(t, "http://example.com", args.Targets[0].Target)
+	require.Contains(t, "http_2xx", args.Targets[0].Module)
+	require.Contains(t, "target_b", args.Targets[1].Name)
+	require.Contains(t, "http://grafana.com", args.Targets[1].Target)
+	require.Contains(t, "http_2xx", args.Targets[1].Module)
+}
+
 func TestUnmarshalRiverWithInvalidInlineConfig(t *testing.T) {
 	var tests = []struct {
 		testname      string
