f3fc02569b76f62b17bfa9f08fe271aae78b2155
Piotr Gwizdala <17101802+thampiotr@users.noreply.github.com>
2023-10-20 15:06:48 +0100

    More prom tests

---
+++ cmd/internal/pipelinetests/internal/framework/testtarget.go
@@ -0,0 +1,31 @@
+package framework
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http/httptest"
+)
+
+type TestTarget struct {
+	registry *prometheus.Registry
+	server   *httptest.Server
+}
+
+func newTestTarget() *TestTarget {
+	target := &TestTarget{
+		registry: prometheus.NewRegistry(),
+	}
+
+	server := httptest.NewServer(promhttp.InstrumentMetricHandler(
+		target.registry, promhttp.HandlerFor(target.registry, promhttp.HandlerOpts{
+			Registry: target.registry,
+		}),
+	))
+	target.server = server
+
+	return target
+}
+
+func (t *TestTarget) Register(collector prometheus.Collector) {
+	t.registry.MustRegister(collector)
+}
---
+++ cmd/internal/pipelinetests/prometheus_test.go
@@ -0,0 +1,131 @@
+package pipelinetests
+
+import (
+	"testing"
+
+	"github.com/grafana/agent/cmd/internal/pipelinetests/internal/framework"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPipeline_Prometheus_SelfScrapeAndWrite(topT *testing.T) {
+	framework.RunPipelineTest(topT, framework.PipelineTest{
+		ConfigFile: "testdata/self_scrape_and_write.river",
+		EventuallyAssert: func(t *assert.CollectT, context *framework.RuntimeContext) {
+			assert.NotEmptyf(t, context.DataSentToProm.WritesCount(), "must receive at least one prom write request")
+			// One target expected
+			assert.Equal(t, float64(1), context.DataSentToProm.FindLastSampleMatching("agent_prometheus_scrape_targets_gauge"))
+			// Fanned out at least one target
+			assert.GreaterOrEqual(t, context.DataSentToProm.FindLastSampleMatching(
+				"agent_prometheus_fanout_latency_count",
+				"component_id",
+				"prometheus.scrape.agent_self",
+			), float64(1))
+
+			// Received at least `count` samples
+			count := 1000
+			assert.Greater(t, context.DataSentToProm.FindLastSampleMatching(
+				"agent_prometheus_forwarded_samples_total",
+				"component_id",
+				"prometheus.scrape.agent_self",
+			), float64(count))
+			assert.Greater(t, context.DataSentToProm.FindLastSampleMatching(
+				"agent_wal_samples_appended_total",
+				"component_id",
+				"prometheus.remote_write.default",
+			), float64(count))
+
+			// At least 100 active series should be present
+			assert.Greater(t, context.DataSentToProm.FindLastSampleMatching(
+				"agent_wal_storage_active_series",
+				"component_id", "prometheus.remote_write.default",
+				"job", "agent",
+			), float64(100))
+		},
+	})
+}
+
+func TestPipeline_Prometheus_TargetScrapeAndWrite(topT *testing.T) {
+	framework.RunPipelineTest(topT, framework.PipelineTest{
+		ConfigFile:       "testdata/target_scrape_and_write.river",
+		EventuallyAssert: verifyDifferentTypesOfMetricsWithTestTarget(),
+	})
+}
+
+func TestPipeline_Prometheus_TargetScrapeAndWrite_WithOTELConversion(topT *testing.T) {
+	framework.RunPipelineTest(topT, framework.PipelineTest{
+		ConfigFile:       "testdata/target_scrape_and_write_otel_conversion.river",
+		EventuallyAssert: verifyDifferentTypesOfMetricsWithTestTarget(),
+	})
+}
+
+// verifyDifferentTypesOfMetricsWithTestTarget exposes different metrics using the context.TestTarget and then
+// verifies that they all arrived eventually to context.DataSentToProm. This test can be used to verify various
+// pipelines that are expected to ship the metrics from a test target to the prometheus remote write endpoint.
+func verifyDifferentTypesOfMetricsWithTestTarget() func(t *assert.CollectT, context *framework.RuntimeContext) {
+	registered := false
+	gauge := prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "test_target_gauge",
+		ConstLabels: map[string]string{
+			"foo": "bar",
+		},
+	})
+	counter := prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "test_target_counter",
+		ConstLabels: map[string]string{
+			"owner": "count_von_count",
+		},
+	})
+	hist := prometheus.NewHistogram(prometheus.HistogramOpts{
+		Name: "test_target_histogram",
+		ConstLabels: map[string]string{
+			"type": "histogram",
+		},
+	})
+
+	return func(t *assert.CollectT, context *framework.RuntimeContext) {
+		if !registered {
+			// Register and set the test gauge only once
+			context.TestTarget.Register(gauge)
+			gauge.Set(123)
+
+			context.TestTarget.Register(counter)
+			counter.Add(321)
+
+			context.TestTarget.Register(hist)
+			for i := 0; i < 100; i++ {
+				hist.Observe(float64(i))
+			}
+
+			registered = true
+		}
+
+		assert.NotEmptyf(t, context.DataSentToProm.WritesCount(), "must receive at least one prom write request")
+		// Check the gauge as expected value and has const labels
+		assert.Equal(t, float64(123), context.DataSentToProm.FindLastSampleMatching(
+			"test_target_gauge",
+			"foo", "bar",
+		))
+
+		// Check the counter labels
+		assert.Equal(t, float64(321), context.DataSentToProm.FindLastSampleMatching(
+			"test_target_counter",
+			"owner", "count_von_count",
+		))
+
+		// Check the histogram metrics
+		assert.Equal(t, float64(100), context.DataSentToProm.FindLastSampleMatching(
+			"test_target_histogram_count",
+			"type", "histogram",
+		))
+		assert.Equal(t, float64(4950), context.DataSentToProm.FindLastSampleMatching(
+			"test_target_histogram_sum",
+			"type", "histogram",
+		))
+		assert.Equal(t, float64(11), context.DataSentToProm.FindLastSampleMatching(
+			"test_target_histogram_bucket",
+			"type", "histogram",
+			"le", "10",
+		))
+	}
+}
