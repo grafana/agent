469284b99b8e12e930b86503425eb58faa16e259
Pablo Balbi <pablo.l.balbi@gmail.com>
2023-10-25 18:06:14 -0300

    fixing benchmark

--- component/common/loki/client/queue_client_test.go
+++ component/common/loki/client/queue_client_test.go
@@ -9,11 +9,13 @@ import (
 
 	"github.com/alecthomas/units"
 	"github.com/go-kit/log"
+	"github.com/grafana/agent/component/common/loki"
 	"github.com/grafana/agent/component/common/loki/utils"
 	"github.com/grafana/dskit/backoff"
 	"github.com/grafana/dskit/flagext"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/common/config"
+	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/model/labels"
 	"github.com/prometheus/prometheus/tsdb/chunks"
 	"github.com/prometheus/prometheus/tsdb/record"
@@ -168,7 +170,7 @@ func TestQueueClient(t *testing.T) {
 	}
 }
 
-func BenchmarkQueueClient(b *testing.B) {
+func BenchmarkClientImplementations(b *testing.B) {
 	for name, bc := range map[string]testCase{
 		"100 entries, single series, no batching": {
 			numLines:  100,
@@ -192,12 +194,18 @@ func BenchmarkQueueClient(b *testing.B) {
 		},
 	} {
 		b.Run(name, func(b *testing.B) {
-			runSingleBenchCase(b, bc)
+			b.Run("implementation=queue", func(b *testing.B) {
+				runQueueClientBenchCase(b, bc)
+			})
+
+			b.Run("implementation=regular", func(b *testing.B) {
+				runRegularClientBenchCase(b, bc)
+			})
 		})
 	}
 }
 
-func runSingleBenchCase(b *testing.B, bc testCase) {
+func runQueueClientBenchCase(b *testing.B, bc testCase) {
 	reg := prometheus.NewRegistry()
 
 	// Create a buffer channel where we do enqueue received requests
@@ -205,6 +213,9 @@ func runSingleBenchCase(b *testing.B, bc testCase) {
 	// count the number for remote-write requests received (which should correlated with the number of sent batches),
 	// and the total number of entries.
 	var receivedEntriesCount atomic.Int64
+	reset := func() {
+		receivedEntriesCount.Store(0)
+	}
 
 	go func() {
 		for req := range receivedReqsChan {
@@ -280,6 +291,97 @@ func runSingleBenchCase(b *testing.B, bc testCase) {
 		require.Eventually(b, func() bool {
 			return receivedEntriesCount.Load() == int64(len(lines))
 		}, time.Second*10, time.Second, "timed out waiting for entries to arrive")
+
+		// reset counters
+		reset()
+	}
+
+	// Stop the client: it waits until the current batch is sent
+	qc.Stop()
+	close(receivedReqsChan)
+}
+
+func runRegularClientBenchCase(b *testing.B, bc testCase) {
+	reg := prometheus.NewRegistry()
+
+	// Create a buffer channel where we do enqueue received requests
+	receivedReqsChan := make(chan utils.RemoteWriteRequest, 10)
+	// count the number for remote-write requests received (which should correlated with the number of sent batches),
+	// and the total number of entries.
+	var receivedEntriesCount atomic.Int64
+	reset := func() {
+		receivedEntriesCount.Store(0)
+	}
+
+	go func() {
+		for req := range receivedReqsChan {
+			for _, s := range req.Request.Streams {
+				receivedEntriesCount.Add(int64(len(s.Entries)))
+			}
+		}
+	}()
+
+	// Start a local HTTP server
+	server := utils.NewRemoteWriteServer(receivedReqsChan, 200)
+	require.NotNil(b, server)
+	defer server.Close()
+
+	// Get the URL at which the local test server is listening to
+	serverURL := flagext.URLValue{}
+	err := serverURL.Set(server.URL)
+	require.NoError(b, err)
+
+	// Instance the client
+	cfg := Config{
+		URL:            serverURL,
+		BatchWait:      time.Millisecond * 50,
+		BatchSize:      10,
+		Client:         config.HTTPClientConfig{},
+		BackoffConfig:  backoff.Config{MinBackoff: 5 * time.Second, MaxBackoff: 10 * time.Second, MaxRetries: 1},
+		ExternalLabels: lokiflag.LabelSet{},
+		Timeout:        1 * time.Second,
+		TenantID:       "",
+		Queue: QueueConfig{
+			Capacity:     1000, // queue size of 100
+			DrainTimeout: time.Second * 10,
+		},
+	}
+
+	logger := log.NewLogfmtLogger(os.Stdout)
+
+	m := NewMetrics(reg)
+	qc, err := New(m, cfg, 0, 0, false, logger)
+	require.NoError(b, err)
+
+	//labels := model.LabelSet{"app": "test"}
+	var lines []string
+	for i := 0; i < bc.numLines; i++ {
+		lines = append(lines, fmt.Sprintf("hola %d", i))
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		// Send all the input log entries
+		for j, l := range lines {
+			seriesId := j % bc.numSeries
+			qc.Chan() <- loki.Entry{
+				Labels: model.LabelSet{
+					// take j module bc.numSeries to evenly distribute those numSeries across all sent entries
+					"app": model.LabelValue(fmt.Sprintf("series-%d", seriesId)),
+				},
+				Entry: logproto.Entry{
+					Timestamp: time.Now(),
+					Line:      l,
+				},
+			}
+		}
+
+		require.Eventually(b, func() bool {
+			return receivedEntriesCount.Load() == int64(len(lines))
+		}, time.Second*10, time.Second, "timed out waiting for entries to arrive")
+
+		// reset counters
+		reset()
 	}
 
 	// Stop the client: it waits until the current batch is sent
