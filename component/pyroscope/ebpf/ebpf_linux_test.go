9ccd438b94abc2838de77fe3d94adf5521ecb2c0
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2023-10-25 16:25:30 +0700

    fix tests

--- component/pyroscope/ebpf/ebpf_linux_test.go
+++ component/pyroscope/ebpf/ebpf_linux_test.go
@@ -41,6 +41,10 @@ func (m *mockSession) Update(options ebpfspy.SessionOptions) error {
 	return nil
 }
 
+func (m *mockSession) UpdateTargets(_ sd.TargetsOptions) {
+
+}
+
 func (m *mockSession) CollectProfiles(f func(target *sd.Target, stack []string, value uint64, pid uint32)) error {
 	m.collected++
 	if m.collectError != nil {
@@ -109,7 +113,7 @@ func TestContextShutdown(t *testing.T) {
 		{"a", "b", "c"},
 		{"q", "w", "e"},
 	}
-	session.dataTarget, _ = sd.NewTarget("cid", map[string]string{"service_name": "foo"})
+	session.dataTarget = sd.NewTarget("cid", 0, map[string]string{"service_name": "foo"})
 	var g run.Group
 	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(time.Second*1))
 	defer cancel()
