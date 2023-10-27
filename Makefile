6acb80f5d418390adb412020c8e77ecbda5fe27b
William Dumont <william.dumont@grafana.com>
2023-10-27 16:27:36 +0200

    Integration tests proposal (#5475)
    * add integration tests
    
    * add readme
    
    * use otlp mimir endpoint
    
    * remove docker cleanup to test cache
    
    * cleanup docker images when using docker compose down
    
    * try running integration tests only on PRs

--- Makefile
+++ Makefile
@@ -15,8 +15,9 @@
 ##
 ## Targets for running tests:
 ##
-##   test  Run tests
-##   lint  Lint code
+##   test              Run tests
+##   lint              Lint code
+##   integration-tests Run integration tests
 ##
 ## Targets for building binaries:
 ##
@@ -172,12 +173,16 @@ lint: agentlint
 # We have to run test twice: once for all packages with -race and then once
 # more without -race for packages that have known race detection issues.
 test:
-	$(GO_ENV) go test $(GO_FLAGS) -race ./...
+	$(GO_ENV) go test $(GO_FLAGS) -race $(shell go list ./... | grep -v /integration-tests/)
 	$(GO_ENV) go test $(GO_FLAGS) ./pkg/integrations/node_exporter ./pkg/logs ./pkg/operator ./pkg/util/k8s ./component/otelcol/processor/tail_sampling ./component/loki/source/file
 
 test-packages:
 	docker pull $(BUILD_IMAGE)
 	go test -tags=packaging  ./packaging
+
+.PHONY: integration-tests
+integration-test:
+	cd integration-tests && $(GO_ENV) go run .
 
 #
 # Targets for building binaries
---
+++ integration-tests/utils.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"sync"
+)
+
+const (
+	agentBinaryPath  = "../../../build/grafana-agent-flow"
+	dockerComposeCmd = "docker-compose"
+	makeCmd          = "make"
+)
+
+type TestLog struct {
+	TestDir    string
+	AgentLog   string
+	TestOutput string
+}
+
+var logChan chan TestLog
+
+func buildAgent() {
+	fmt.Println("Building agent...")
+	cmd := exec.Command(makeCmd, "-C", "..", "agent-flow")
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
+}
+
+func setupEnvironment() {
+	fmt.Println("Setting up environment with Docker Compose...")
+	cmd := exec.Command(dockerComposeCmd, "up", "-d")
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
+}
+
+func runSingleTest(testDir string) {
+	info, err := os.Stat(testDir)
+	if err != nil {
+		panic(err)
+	}
+	if !info.IsDir() {
+		return
+	}
+
+	dirName := filepath.Base(testDir)
+
+	var agentLogBuffer bytes.Buffer
+	cmd := exec.Command(agentBinaryPath, "run", "config.river")
+	cmd.Dir = testDir
+	cmd.Stdout = &agentLogBuffer
+	cmd.Stderr = &agentLogBuffer
+
+	if err := cmd.Start(); err != nil {
+		logChan <- TestLog{
+			TestDir:  dirName,
+			AgentLog: fmt.Sprintf("Failed to start agent: %v", err),
+		}
+		return
+	}
+
+	testCmd := exec.Command("go", "test")
+	testCmd.Dir = testDir
+	testOutput, errTest := testCmd.CombinedOutput()
+
+	err = cmd.Process.Kill()
+	if err != nil {
+		panic(err)
+	}
+
+	agentLog := agentLogBuffer.String()
+
+	if errTest != nil {
+		logChan <- TestLog{
+			TestDir:    dirName,
+			AgentLog:   agentLog,
+			TestOutput: string(testOutput),
+		}
+	}
+
+	err = os.RemoveAll(filepath.Join(testDir, "data-agent"))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func runAllTests() {
+	testDirs, err := filepath.Glob("./tests/*")
+	if err != nil {
+		panic(err)
+	}
+	var wg sync.WaitGroup
+
+	for _, testDir := range testDirs {
+		fmt.Println("Running", testDir)
+		wg.Add(1)
+		go func(td string) {
+			defer wg.Done()
+			runSingleTest(td)
+		}(testDir)
+	}
+	wg.Wait()
+}
+
+func cleanUpEnvironment() {
+	fmt.Println("Cleaning up Docker environment...")
+	err := exec.Command(dockerComposeCmd, "down", "--volumes", "--rmi", "all").Run()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func reportResults() {
+	close(logChan)
+	testsFailed := 0
+	for log := range logChan {
+		fmt.Printf("Failure detected in %s:\n", log.TestDir)
+		fmt.Println("Test output:", log.TestOutput)
+		fmt.Println("Agent logs:", log.AgentLog)
+		testsFailed++
+	}
+
+	if testsFailed > 0 {
+		fmt.Printf("%d tests failed!\n", testsFailed)
+		os.Exit(1)
+	} else {
+		fmt.Println("All integration tests passed!")
+	}
+}
