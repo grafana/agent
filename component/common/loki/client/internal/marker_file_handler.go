d0dfe5d327d5a75d6df067ac684aae260ebd9bf1
Pablo Balbi <pablo.l.balbi@gmail.com>
2023-10-30 16:28:37 -0300

    encoding wip

--- component/common/loki/client/internal/marker_file_handler.go
+++ component/common/loki/client/internal/marker_file_handler.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
-	"github.com/prometheus/prometheus/tsdb/fileutil"
 )
 
 const (
@@ -28,6 +27,7 @@ type MarkerFileHandler interface {
 
 type markerFileHandler struct {
 	logger                    log.Logger
+	lastMarkedSegmentDir      string
 	lastMarkedSegmentFilePath string
 }
 
@@ -45,6 +45,7 @@ func NewMarkerFileHandler(logger log.Logger, walDir string) (MarkerFileHandler, 
 
 	mfh := &markerFileHandler{
 		logger:                    logger,
+		lastMarkedSegmentDir:      filepath.Join(markerDir),
 		lastMarkedSegmentFilePath: filepath.Join(markerDir, MarkerFileName),
 	}
 
@@ -78,19 +79,48 @@ func (mfh *markerFileHandler) LastMarkedSegment() int {
 
 // MarkSegment implements MarkerHandler.
 func (mfh *markerFileHandler) MarkSegment(segment int) {
-	var (
-		segmentText = strconv.Itoa(segment)
-		tmp         = mfh.lastMarkedSegmentFilePath + ".tmp"
-	)
-
-	if err := os.WriteFile(tmp, []byte(segmentText), 0o666); err != nil {
-		level.Error(mfh.logger).Log("msg", "could not create segment marker file", "file", tmp, "err", err)
+	encodedMarker, err := EncodeMarkerV1(uint64(segment))
+	if err != nil {
+		level.Error(mfh.logger).Log("msg", "failed to encode marker when marking segment", "err", err)
 		return
 	}
-	if err := fileutil.Replace(tmp, mfh.lastMarkedSegmentFilePath); err != nil {
+
+	if err := mfh.swapMarkerFile(encodedMarker); err != nil {
 		level.Error(mfh.logger).Log("msg", "could not replace segment marker file", "file", mfh.lastMarkedSegmentFilePath, "err", err)
 		return
 	}
 
 	level.Debug(mfh.logger).Log("msg", "updated segment marker file", "file", mfh.lastMarkedSegmentFilePath, "segment", segment)
+}
+
+func (mfh *markerFileHandler) swapMarkerFile(bs []byte) error {
+	tempFile, err := os.CreateTemp(mfh.lastMarkedSegmentDir, "segment*")
+	if err != nil {
+		return err
+	}
+
+	// upon exit attempt to close the temporal file handler and remove the file
+	defer func() {
+		if err := tempFile.Close(); err != nil {
+			level.Warn(mfh.logger).Log("msg", "failed to close temporary marker file handler")
+			// TODO: should we try to remove the temporal file either way
+			return
+		}
+		if err := os.Remove(tempFile.Name()); err != nil {
+			level.Warn(mfh.logger).Log("msg", "failed to remove temporary marker file")
+		}
+	}()
+
+	written, err := tempFile.Write(bs)
+	if err != nil {
+		return fmt.Errorf("failed to write to temporal marker file: %w", err)
+	} else if written != len(bs) {
+		return fmt.Errorf("failed to write whole marker. written %d, expected %d", written, len(bs))
+	}
+
+	if err := os.Rename(tempFile.Name(), mfh.lastMarkedSegmentFilePath); err != nil {
+		return fmt.Errorf("failed to swap marker files: %w", err)
+	}
+
+	return nil
 }
