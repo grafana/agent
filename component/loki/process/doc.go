4bd83513236c7f37f7de1624e7ec0ab44d8e3253
Paschalis Tsilias <paschalis.tsilias@grafana.com>
2023-11-01 11:48:51 +0200

    Move component-level docs to process.go
    Signed-off-by: Paschalis Tsilias <paschalis.tsilias@grafana.com>

--- component/loki/process/doc.go
+++
@@ -1,4 +0,0 @@
-// The code in this package is adapted/ported over from grafana/loki/clients/pkg/logentry.
-//
-// The last Loki commit scanned for upstream changes was 7d5475541c66a819f6f456a45f8c143a084e6831.
-package stages
