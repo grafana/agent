180f36ef34c40049f7e03aa8f4b1fa304dc1ce17
Das, Barun <bdas31@gatech.edu>
2023-12-02 20:02:59 -0500

    improve debug exporter tests

--- component/otelcol/exporter/debug/debug_test.go
+++ component/otelcol/exporter/debug/debug_test.go
@@ -5,28 +5,29 @@ import (
 
 	"github.com/grafana/agent/component/otelcol/exporter/debug"
 	"github.com/stretchr/testify/require"
+	otelcomponent "go.opentelemetry.io/collector/component"
 )
 
 func Test(t *testing.T) {
-    happyArgs := debug.Arguments{
-        Verbosity: "detailed",
-        SamplingInitial: 5,
-        SamplingThereafter: 20,
-    }
-    // Check no errors on converting to exporter args
-    otelcomp, err := happyArgs.Convert()
-    require.NoError(t, err)
-    
-    // Check that exporter is created correctly
-    err = &otelcomp.Validate()
-    require.NoError(t, err, "error on creating debug exporter config")
+	happyArgs := debug.Arguments{
+		Verbosity:          "detailed",
+		SamplingInitial:    5,
+		SamplingThereafter: 20,
+	}
+	// Check no errors on converting to exporter args
+	otelconf, err := happyArgs.Convert()
+	require.NoError(t, err)
 
-    invalidArgs := debug.Arguments{
-        Verbosity: "test",
-        SamplingInitial: 5,
-        SamplingThereafter: 20,
-    }
-    // Check error on converting invalid args
-    _, err = invalidArgs.Convert()
-    require.NotNil(t, err, "no error on invalid arguments")
+	// Check that exporter config is created correctly
+	err = otelcomponent.ValidateConfig(otelconf)
+	require.NoError(t, err, "error on creating debug exporter config")
+
+	invalidArgs := debug.Arguments{
+		Verbosity:          "test",
+		SamplingInitial:    5,
+		SamplingThereafter: 20,
+	}
+	// Check error on converting invalid args
+	_, err = invalidArgs.Convert()
+	require.NotNil(t, err, "no error on invalid arguments")
 }
