03f83c9f32043552a25a6e9db25acbe6a6094c65
matt durham <mattdurham@ppog.org>
2023-12-13 11:33:08 -0500

    Add code to check for staleness which touched a ton of tests since I also added metrics.

--- component/prometheus/interceptor.go
+++ component/prometheus/interceptor.go
@@ -8,6 +8,7 @@ import (
 	"github.com/prometheus/prometheus/model/histogram"
 	"github.com/prometheus/prometheus/model/labels"
 	"github.com/prometheus/prometheus/model/metadata"
+	"github.com/prometheus/prometheus/model/value"
 	"github.com/prometheus/prometheus/storage"
 )
 
@@ -100,6 +101,13 @@ var _ storage.Appender = (*interceptappender)(nil)
 func (a *interceptappender) Append(ref storage.SeriesRef, l labels.Labels, t int64, v float64) (storage.SeriesRef, error) {
 	if ref == 0 {
 		ref = storage.SeriesRef(a.ls.GetOrAddGlobalRefID(l))
+	}
+
+	if value.IsStaleNaN(v) {
+		a.ls.AddStaleMarker(uint64(ref), l)
+	} else {
+		// This feels excessive to call this each time.
+		a.ls.RemoveStaleMarker(uint64(ref))
 	}
 
 	if a.interceptor.onAppend != nil {
