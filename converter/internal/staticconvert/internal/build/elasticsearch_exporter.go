6ef7b8b1fd84236f8e01878249b2fc01029587a0
Đỗ Trọng Hải <41283691+hainenber@users.noreply.github.com>
2024-01-02 20:35:00 +0700

    feat(exporter/elasticsearch): add Basic Auth support (#5814)
    * feat(exporter/elasticsearch): add Basic Auth support
    
    Signed-off-by: hainenber <dotronghai96@gmail.com>
    
    * misc: fix changelog entry (#5812)
    
    PR #5802 accidentally added the changelog entry to a published release.
    
    * fix(exporter/elasticsearch): only add Auth header when needed
    
    Signed-off-by: hainenber <dotronghai96@gmail.com>
    
    * doc(exporter/elasticsearch): add Basic Auth block desc
    
    Signed-off-by: hainenber <dotronghai96@gmail.com>
    
    * feat(exporter/elasticsearch): add Basic Auth support
    
    Signed-off-by: hainenber <dotronghai96@gmail.com>
    
    * feat(exporter/elasticsearch): support Basic Auth for static config
    
    Signed-off-by: hainenber <dotronghai96@gmail.com>
    
    * chore(CHANGELOG): remove merge conflict artifact
    
    Signed-off-by: hainenber <dotronghai96@gmail.com>
    
    * misc: fix changelog entry
    
    Signed-off-by: hainenber <dotronghai96@gmail.com>
    
    * feat(converter): map ES exporter's BasicAuth flow from static to flow
    
    Signed-off-by: hainenber <dotronghai96@gmail.com>
    
    * fix(converter): nil check for ES exporter's Basic Auth
    
    Signed-off-by: hainenber <dotronghai96@gmail.com>
    
    * lint
    
    Signed-off-by: erikbaranowski <39704712+erikbaranowski@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: hainenber <dotronghai96@gmail.com>
    Signed-off-by: erikbaranowski <39704712+erikbaranowski@users.noreply.github.com>
    Co-authored-by: Robert Fratto <robertfratto@gmail.com>
    Co-authored-by: erikbaranowski <39704712+erikbaranowski@users.noreply.github.com>

--- converter/internal/staticconvert/internal/build/elasticsearch_exporter.go
+++ converter/internal/staticconvert/internal/build/elasticsearch_exporter.go
@@ -1,9 +1,11 @@
 package build
 
 import (
+	commonCfg "github.com/grafana/agent/component/common/config"
 	"github.com/grafana/agent/component/discovery"
 	"github.com/grafana/agent/component/prometheus/exporter/elasticsearch"
 	"github.com/grafana/agent/pkg/integrations/elasticsearch_exporter"
+	"github.com/grafana/river/rivertypes"
 )
 
 func (b *IntegrationsConfigBuilder) appendElasticsearchExporter(config *elasticsearch_exporter.Config, instanceKey *string) discovery.Exports {
@@ -12,7 +14,7 @@ func (b *IntegrationsConfigBuilder) appendElasticsearchExporter(config *elastics
 }
 
 func toElasticsearchExporter(config *elasticsearch_exporter.Config) *elasticsearch.Arguments {
-	return &elasticsearch.Arguments{
+	arg := &elasticsearch.Arguments{
 		Address:                   config.Address,
 		Timeout:                   config.Timeout,
 		AllNodes:                  config.AllNodes,
@@ -31,4 +33,14 @@ func toElasticsearchExporter(config *elasticsearch_exporter.Config) *elasticsear
 		ExportDataStreams:         config.ExportDataStreams,
 		ExportSLM:                 config.ExportSLM,
 	}
+
+	if config.BasicAuth != nil {
+		arg.BasicAuth = &commonCfg.BasicAuth{
+			Username:     config.BasicAuth.Username,
+			Password:     rivertypes.Secret(config.BasicAuth.Password),
+			PasswordFile: config.BasicAuth.PasswordFile,
+		}
+	}
+
+	return arg
 }
