8354554d6e1e345bd0bbaae18fe822c7578b08fc
matt durham <mattdurham@ppog.org>
2024-01-04 09:03:13 -0500

    remove shell scripts

--- cmd/benchmark/logs.sh
+++
@@ -1,5 +0,0 @@
-go build -o main  
-
-# each test is ran with the first argument being the name , the second whether the endpoint accepts metrics, the third for the duration and the last being the discovery
-# endpont. See test.river for details on each endpoint.
-./main logs 1h
