812392ec9151fda1dcfa01efd124cdb50111aa62
Paschalis Tsilias <paschalis.tsilias@grafana.com>
2024-01-05 14:22:17 +0200

    loki.source.docker: filter out duplicate container IDs
    Signed-off-by: Paschalis Tsilias <paschalis.tsilias@grafana.com>

--- component/loki/source/docker/docker_test.go
+++ component/loki/source/docker/docker_test.go
@@ -5,9 +5,11 @@ import (
 	"testing"
 	"time"
 
+	"github.com/grafana/agent/component"
 	"github.com/grafana/agent/pkg/flow/componenttest"
 	"github.com/grafana/agent/pkg/util"
 	"github.com/grafana/river"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/stretchr/testify/require"
 )
 
@@ -32,4 +34,31 @@ func Test(t *testing.T) {
 	}()
 
 	require.NoError(t, ctrl.WaitRunning(time.Minute))
+}
+
+func TestDuplicateTargets(t *testing.T) {
+	// Use host that works on all platforms (including Windows).
+	var cfg = `
+		host       = "tcp://127.0.0.1:9375"
+		targets    = [
+			{__meta_docker_container_id = "foo", __meta_docker_port_private = "8080"},
+			{__meta_docker_container_id = "foo", __meta_docker_port_private = "8080"},
+			{__meta_docker_container_id = "bar", __meta_docker_port_private = "8080"},
+		]
+		forward_to = []
+	`
+
+	var args Arguments
+	err := river.Unmarshal([]byte(cfg), &args)
+	require.NoError(t, err)
+
+	cmp, err := New(component.Options{
+		ID:         "loki.source.docker.test",
+		Logger:     util.TestFlowLogger(t),
+		Registerer: prometheus.NewRegistry(),
+		DataPath:   t.TempDir(),
+	}, args)
+	require.NoError(t, err)
+
+	require.Len(t, cmp.manager.tasks, 2)
 }
