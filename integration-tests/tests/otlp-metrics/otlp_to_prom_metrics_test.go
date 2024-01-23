c3e27588480bb67b9aa58fcd09f2568b870d4516
William Dumont <william.dumont@grafana.com>
2024-01-23 15:31:44 +0100

    refactor integration tests to reduce redundancy (#6221)

--- integration-tests/tests/otlp-metrics/otlp_to_prom_metrics_test.go
+++ integration-tests/tests/otlp-metrics/otlp_to_prom_metrics_test.go
@@ -4,39 +4,20 @@ package main
 
 import (
 	"testing"
+
+	"github.com/grafana/agent/integration-tests/common"
 )
 
 func TestOTLPToPromMetrics(t *testing.T) {
-	const testName = "otlp_to_prom_metrics"
-	tests := []struct {
-		metric string
-	}{
-		{"example_counter_total"},
-		{"example_float_counter_total"},
-		{"example_updowncounter"},
-		{"example_float_updowncounter"},
-		{"example_histogram_bucket"},
-		{"example_float_histogram_bucket"},
+	// Not using the default here because some metric names change during the conversion.
+	metrics := []string{
+		"example_counter_total",       // Change from example_counter to example_counter_total.
+		"example_float_counter_total", // Change from example_float_counter to example_float_counter_total.
+		"example_updowncounter",
+		"example_float_updowncounter",
+		"example_histogram_bucket",
+		"example_float_histogram_bucket",
 	}
 
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.metric, func(t *testing.T) {
-			t.Parallel()
-			assertMetricData(t, metricQuery(tt.metric, testName), tt.metric, testName)
-		})
-	}
-
-	histogramTests := []string{
-		"example_exponential_histogram",
-		"example_exponential_float_histogram",
-	}
-
-	for _, metric := range histogramTests {
-		metric := metric
-		t.Run(metric, func(t *testing.T) {
-			t.Parallel()
-			assertHistogramData(t, metricQuery(metric, testName), metric, testName)
-		})
-	}
+	common.MimirMetricsTest(t, metrics, common.OtelDefaultHistogramMetrics, "otlp_to_prom_metrics")
 }
