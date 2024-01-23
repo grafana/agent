2c9fb2403ac1beb0b279be8836853332d3ed87db
William Dumont <william.dumont@grafana.com>
2024-01-23 20:49:48 +0100

    add comments to the func interface

--- pkg/flow/internal/controller/node_with_dependants.go
+++ pkg/flow/internal/controller/node_with_dependants.go
@@ -10,9 +10,12 @@ import (
 type NodeWithDependants interface {
 	BlockNode
 
+	// LastUpdateTime returns the time corresponding to the last time where the node changed its exports.
 	LastUpdateTime() time.Time
 
+	// Exports returns the current set of exports from the managed component.
 	Exports() component.Exports
 
+	// ID returns the component ID of the managed component from its River block.
 	ID() ComponentID
 }
