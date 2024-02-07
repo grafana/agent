0cda952178be046f171ee9800e259c84620da006
William Dumont <william.dumont@grafana.com>
2024-02-07 16:23:09 +0100

    Declare new modules (second attempt) (#6291)
    * Add declare node and import config node skeleton
    
    DeclareNode is a new node which represents a `declare` block.
    Another node `ImportConfigNode` is added as an empty shell to help understanding the design choices of the PR.
    Its implementation will come later. For now it is not populated and has no impact.
    
    * Add custom component registry
    
    CustomComponentRegistry holds custom component definitions that are available in the loaded config.
    It's structured as a tree that maps the hierarchy between the declare and import blocks.
    
    * Add CustomComponent interface
    
    CustomComponent is defined in the controller and aimed to be managed by CustomComponentNodes.
    This replaces the component.Module interface which will be removed with the old modules.
    This change aligns with the new modules terminology and keeps the custom components within the controller package.
    A custom component can load a config via LoadBody and run via Run.
    LoadOptions are added to allow users to pass additional options to the loader with the new config.
    For now, the LoadOptions contain a CustomComponentRegistry which provides custom component templates that the custom component can use to instantiate other custom components.
    
    * Add CustomComponentNode
    
    CustomComponentNode is a controller node which manages a custom component.
    On evaluation, it fetches its config(template+customComponentRegistry) via a callback and starts a new CustomComponent.
    
    * Add modulesIDs to the ComponentNode interface
    
    This change is necessary to show the running components inside of a custom component in the UI.
    
    * Add ComponentNodeManager
    
    The ComponentNodeManager is responsible for creating new components nodes and obtaining the necessary information to run them.
    The data is stored in a builtinComponentRegistry and a customComponentRegistry.
    The created custom components must be wired to the declare and import nodes that they depend on to ensure that their config is loaded
    before they evaluate and to trigger a re-evaluation when an import node they depend on received new content.
    
    * Add test declare
    
    Add tests to check that the declare functionality with custom components works as intended in various scenarios (from basic to several levels of nesting)
    Test also that cycle dependencies are prevented.
    
    * forbid using 'declare' as a label for declare blocks
    
    * Add tests for declare
    
    Shadow namespace - name a declare block with an existing namespace (prometheus for example) (correct)
    Shadow declare - redeclare a declare block (correct)
    
    OutOfScopeReference - trying to reference the exports of a node outside of the declare (error)
    OutOfScopeDefinition - trying to instantiate a custom component via an out of scope template (error)
    ForbiddenDeclareLabel - using the label "declare" for a declare block (error)
    
    UpdateDeclare - reload the config with a different declare definition (correct)
    
    * remove import logic
    
    * add examples to circle and self dependencies
    
    * change how apply options are passed to the loader
    
    * refactor collectCustomComponentReferences to reduce nesting
    
    * remove stale comment
    
    * reduce redundancy
    
    * add missing error check with test
    
    * change error message
    
    * change error msg for unrecognized component

--- pkg/flow/internal/controller/module.go
+++ pkg/flow/internal/controller/module.go
@@ -1,6 +1,11 @@
 package controller
 
-import "github.com/grafana/agent/component"
+import (
+	"context"
+
+	"github.com/grafana/agent/component"
+	"github.com/grafana/river/ast"
+)
 
 // ModuleController is a lower-level interface for module controllers which
 // allows probing for the list of managed modules.
@@ -9,4 +14,22 @@ type ModuleController interface {
 
 	// ModuleIDs returns the list of managed modules in unspecified order.
 	ModuleIDs() []string
+
+	// Creates a new custom component.
+	NewCustomComponent(id string, export component.ExportFunc) (CustomComponent, error)
+}
+
+// CustomComponent is a controller for running components within a CustomComponent.
+type CustomComponent interface {
+	// LoadBody loads a River AST body into the CustomComponent. LoadBody can be called
+	// multiple times, and called prior to [CustomComponent.Run].
+	// customComponentRegistry provides custom component definitions for the loaded config.
+	LoadBody(body ast.Body, args map[string]any, customComponentRegistry *CustomComponentRegistry) error
+
+	// Run starts the CustomComponent. No components within the CustomComponent
+	// will be run until Run is called.
+	//
+	// Run blocks until the provided context is canceled. The ID of a CustomComponent as defined in
+	// ModuleController.NewCustomComponent will not be released until Run returns.
+	Run(context.Context) error
 }
