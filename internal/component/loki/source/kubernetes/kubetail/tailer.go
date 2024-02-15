6abb1c3837a1f794ae956d9ac2b5920c4d0ed200
hainenber <dotronghai96@gmail.com>
2024-02-15 16:41:21 +0700

    fix(loki/src/k8s): only restart tailer for K8s < v1.29.0
    Signed-off-by: hainenber <dotronghai96@gmail.com>

--- internal/component/loki/source/kubernetes/kubetail/tailer.go
+++ internal/component/loki/source/kubernetes/kubetail/tailer.go
@@ -201,7 +201,7 @@ func (t *tailer) tail(ctx context.Context, handler loki.EntryHandler) error {
 	// False positives here are acceptable, but false negatives mean that
 	// we'll have a larger spike of missing logs until we detect a rolled
 	// file.
-	if k8sComparableServerVersion.LE(semver.Version{Major: 1, Minor: 29, Patch: 0}) {
+	if k8sComparableServerVersion.LT(semver.Version{Major: 1, Minor: 29, Patch: 0}) {
 		go func() {
 			rolledFileTicker := time.NewTicker(1 * time.Second)
 			defer func() {
