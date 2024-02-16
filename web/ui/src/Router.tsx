601374f888cae175f71270f7147f389a85afa2ea
hainenber <dotronghai96@gmail.com>
2024-02-16 22:10:52 +0700

    feat(web/ui): finalize graph for modular components
    Signed-off-by: hainenber <dotronghai96@gmail.com>

--- web/ui/src/Router.tsx
+++ web/ui/src/Router.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './features/layout/Navbar';
 import PageClusteringPeers from './pages/Clustering';
 import ComponentDetailPage from './pages/ComponentDetailPage';
-import Graph from './pages/Graph';
+import { Graph, ModuleGraph } from './pages/Graph';
 import PageComponentList from './pages/PageComponentList';
 
 interface Props {
@@ -19,6 +19,7 @@ const Router = ({ basePath }: Props) => {
           <Route path="/" element={<PageComponentList />} />
           <Route path="/component/*" element={<ComponentDetailPage />} />
           <Route path="/graph" element={<Graph />} />
+          <Route path="/graph/*" element={<ModuleGraph />} />
           <Route path="/clustering" element={<PageClusteringPeers />} />
         </Routes>
       </main>
--- web/ui/src/pages/Graph.tsx
+++ web/ui/src/pages/Graph.tsx
@@ -1,11 +1,13 @@
+import { useParams } from 'react-router-dom';
 import { faDiagramProject } from '@fortawesome/free-solid-svg-icons';
 
 import { ComponentGraph } from '../features/graph/ComponentGraph';
 import Page from '../features/layout/Page';
-import { useAllComponentInfo } from '../hooks/componentInfo';
+import { useComponentInfo } from '../hooks/componentInfo';
+import { parseID } from '../utils/id';
 
-function Graph() {
-  const [components] = useAllComponentInfo('');
+export function Graph() {
+  const [components] = useComponentInfo('');
 
   return (
     <Page name="Graph" desc="Relationships between defined components" icon={faDiagramProject}>
@@ -14,4 +16,15 @@ function Graph() {
   );
 }
 
-export default Graph;
+export function ModuleGraph() {
+  const { '*': id } = useParams();
+
+  const { localID } = parseID(id || '');
+  const [components] = useComponentInfo(localID);
+
+  return (
+    <Page name="Graph" desc={`Relationships between defined components in ${localID}`} icon={faDiagramProject}>
+      {components.length > 0 && <ComponentGraph components={components} />}
+    </Page>
+  );
+}
