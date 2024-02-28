7462a5ba0ed2afd9db2a2a088c2707dee1d54393
William Dumont <william.dumont@grafana.com>
2024-02-28 11:39:27 +0100

    use flow logging level instead of go-kit logging level

--- component/loki/rules/kubernetes/rules.go
+++ component/loki/rules/kubernetes/rules.go
@@ -7,8 +7,8 @@ import (
 	"time"
 
 	"github.com/go-kit/log"
-	"github.com/go-kit/log/level"
 	"github.com/grafana/agent/component"
+	"github.com/grafana/agent/pkg/flow/logging/level"
 	lokiClient "github.com/grafana/agent/pkg/loki/client"
 	"github.com/grafana/dskit/instrument"
 	promListers "github.com/prometheus-operator/prometheus-operator/pkg/client/listers/monitoring/v1"
