cb93188fb1b2feb90ef14b6361de5f372a8cd65e
Robert Fratto <robertfratto@gmail.com>
2024-03-05 15:14:38 -0500

    fix test which failed due to bad merge conflict resolution

--- pkg/flow/internal/controller/loader_test.go
+++ pkg/flow/internal/controller/loader_test.go
@@ -145,7 +145,7 @@ func TestLoader(t *testing.T) {
 		`
 		l := controller.NewLoader(newLoaderOptions())
 		diags := applyFromContent(t, l, []byte(invalidFile), nil, nil)
-		require.ErrorContains(t, diags.ErrorOrNil(), `cannot find the definition of component name "doesnotexist`)
+		require.ErrorContains(t, diags.ErrorOrNil(), `cannot retrieve the definition of component name "doesnotexist`)
 	})
 
 	t.Run("Load with component with empty label", func(t *testing.T) {
