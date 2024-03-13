e6940007a798c3e1bf94ec40f8169212ea6e0211
Jake Wise <wise.jake@live.co.uk>
2024-03-13 10:25:11 +0000

    issue #6667 fix wrong yaml tag and add test

--- internal/mimir/client/types.go
+++ internal/mimir/client/types.go
@@ -3,6 +3,6 @@ package client
 import "github.com/prometheus/prometheus/model/rulefmt"
 
 type RuleGroup struct {
-	rulefmt.RuleGroup `yaml:"embedded,omitempty"`
+	rulefmt.RuleGroup `yaml:",inline"`
 	SourceTenants     []string `yaml:"source_tenants,omitempty"`
 }
