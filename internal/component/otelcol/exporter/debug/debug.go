90e9834590b86b83414c2b36878c7123e54cb950
Barun Das <barun.das.23@gmail.com>
2024-03-20 22:46:07 -0400

    rebasing

---
+++ internal/component/otelcol/exporter/debug/debug.go
@@ -0,0 +1,96 @@
+package debug
+
+import (
+	"fmt"
+
+	"github.com/grafana/agent/internal/component"
+	"github.com/grafana/agent/internal/component/otelcol"
+	"github.com/grafana/agent/internal/component/otelcol/exporter"
+  "github.com/grafana/agent/internal/featuregate"
+	otelcomponent "go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/config/configtelemetry"
+	debugexporter "go.opentelemetry.io/collector/exporter/debugexporter"
+  otelextension "go.opentelemetry.io/collector/extension"
+)
+
+func init() {
+	component.Register(component.Registration{
+		Name:    "otelcol.exporter.debug",
+		Args:    Arguments{},
+		Exports: otelcol.ConsumerExports{},
+    Stability: featuregate.StabilityExperimental,
+
+		Build: func(opts component.Options, args component.Arguments) (component.Component, error) {
+			fact := debugexporter.NewFactory()
+			return exporter.New(opts, fact, args.(Arguments), exporter.TypeAll)
+		},
+	})
+}
+
+type Arguments struct {
+	Verbosity          string `river:"verbosity,attr,optional"`
+	SamplingInitial    int    `river:"sampling_initial,attr,optional"`
+	SamplingThereafter int    `river:"sampling_thereafter,attr,optional"`
+}
+
+func (args Arguments) convertVerbosity() (configtelemetry.Level, error) {
+	var verbosity configtelemetry.Level
+	switch args.Verbosity {
+	case "basic":
+		verbosity = configtelemetry.LevelBasic
+	case "normal":
+		verbosity = configtelemetry.LevelNormal
+	case "detailed":
+		verbosity = configtelemetry.LevelDetailed
+	default:
+		// Invalid verbosity
+		// debugexporter only supports basic, normal and detailed levels
+		return verbosity, fmt.Errorf("invalid verbosity %q", args.Verbosity)
+	}
+
+	return verbosity, nil
+}
+
+var _ exporter.Arguments = Arguments{}
+
+// DefaultArguments holds default values for Arguments.
+var DefaultArguments = Arguments{
+	Verbosity:          "basic",
+	SamplingInitial:    2,
+	SamplingThereafter: 500,
+}
+
+// SetToDefault implements river.Defaulter.
+func (args *Arguments) SetToDefault() {
+	*args = DefaultArguments
+}
+
+// Convert implements exporter.Arguments.
+func (args Arguments) Convert() (otelcomponent.Config, error) {
+	verbosity, err := args.convertVerbosity()
+	if err != nil {
+		return nil, fmt.Errorf("error in conversion to config arguments, %v", err)
+	}
+
+	return &debugexporter.Config{
+		Verbosity:          verbosity,
+		SamplingInitial:    args.SamplingInitial,
+		SamplingThereafter: args.SamplingThereafter,
+	}, nil
+}
+
+// Extensions implements exporter.Arguments.
+func (args Arguments) Extensions() map[otelcomponent.ID]otelextension.Extension {
+	return nil
+}
+
+// Exporters implements exporter.Arguments.
+func (args Arguments) Exporters() map[otelcomponent.DataType]map[otelcomponent.ID]otelcomponent.Component {
+	return nil
+}
+
+// DebugMetricsConfig implements receiver.Arguments.
+func (args Arguments) DebugMetricsConfig() otelcol.DebugMetricsArguments {
+	var debugMetrics otelcol.DebugMetricsArguments
+	return debugMetrics
+}
---
+++ internal/component/otelcol/exporter/debug/debug_test.go
@@ -0,0 +1,83 @@
+package debug_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/grafana/agent/internal/component/otelcol/exporter/debug"
+	"github.com/grafana/river"
+	"github.com/stretchr/testify/require"
+	otelcomponent "go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/config/configtelemetry"
+	debugexporter "go.opentelemetry.io/collector/exporter/debugexporter"
+)
+
+func Test(t *testing.T) {
+	tests := []struct {
+		testName       string
+		args           string
+		expectedReturn debugexporter.Config
+		errorMsg       string
+	}{
+		{
+			testName: "defaultConfig",
+			args: `
+			verbosity = "basic"
+			sampling_initial = 2
+			sampling_thereafter = 500
+			`,
+			expectedReturn: debugexporter.Config{
+				Verbosity:          configtelemetry.LevelBasic,
+				SamplingInitial:    2,
+				SamplingThereafter: 500,
+			},
+		},
+
+		{
+			testName: "validConfig",
+			args: ` 
+				verbosity = "detailed"
+				sampling_initial = 5
+				sampling_thereafter = 20
+			`,
+			expectedReturn: debugexporter.Config{
+				Verbosity:          configtelemetry.LevelDetailed,
+				SamplingInitial:    5,
+				SamplingThereafter: 20,
+			},
+		},
+
+		{
+			testName: "invalidConfig",
+			args: `
+				verbosity = "test"
+				sampling_initial = 5
+				sampling_thereafter = 20
+			`,
+			errorMsg: "error in conversion to config arguments",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.testName, func(t *testing.T) {
+			var args debug.Arguments
+			err := river.Unmarshal([]byte(tc.args), &args)
+			require.NoError(t, err)
+
+			actualPtr, err := args.Convert()
+			if tc.errorMsg != "" {
+				require.ErrorContains(t, err, tc.errorMsg)
+				return
+			}
+
+			require.NoError(t, err)
+
+			actual := actualPtr.(*debugexporter.Config)
+			fmt.Printf("Passed conversion")
+
+			require.NoError(t, otelcomponent.ValidateConfig(actual))
+
+			require.Equal(t, tc.expectedReturn, *actual)
+		})
+	}
+}
