scottatron/grafana-agent

Upgrade OTel from v0.87.0 to v0.96.0

internal/component/otelcol/exporter/loadbalancing/loadbalancing.go
@@ -133,9 +133,9 @@ func (otlpConfig OtlpConfig) Convert() otlpexporter.Config {
 		TimeoutSettings: exporterhelper.TimeoutSettings{
 			Timeout: otlpConfig.Timeout,
 		},
-		QueueSettings:      *otlpConfig.Queue.Convert(),
-		RetrySettings:      *otlpConfig.Retry.Convert(),
-		GRPCClientSettings: *otlpConfig.Client.Convert(),
+		QueueConfig:  *otlpConfig.Queue.Convert(),
+		RetryConfig:  *otlpConfig.Retry.Convert(),
+		ClientConfig: *otlpConfig.Client.Convert(),
 	}
 }
 
@@ -269,7 +269,7 @@ type GRPCClientArguments struct {
 var _ river.Defaulter = &GRPCClientArguments{}
 
 // Convert converts args into the upstream type.
-func (args *GRPCClientArguments) Convert() *otelconfiggrpc.GRPCClientSettings {
+func (args *GRPCClientArguments) Convert() *otelconfiggrpc.ClientConfig {
 	if args == nil {
 		return nil
 	}
@@ -285,7 +285,7 @@ func (args *GRPCClientArguments) Convert() *otelconfiggrpc.GRPCClientSettings {
 		auth = &otelconfigauth.Authentication{AuthenticatorID: args.Auth.ID}
 	}
 
-	return &otelconfiggrpc.GRPCClientSettings{
+	return &otelconfiggrpc.ClientConfig{
 		Compression: args.Compression.Convert(),
 
 		TLSSetting: *args.TLS.Convert(),

grafana/agent

internal/component/otelcol/exporter/loadbalancing/loadbalancing.go
@@ -124,9 +124,9 @@ func (oc OtlpConfig) Convert() otlpexporter.Config {
 		TimeoutSettings: exporterhelper.TimeoutSettings{
 			Timeout: oc.Timeout,
 		},
-		QueueSettings:      *oc.Queue.Convert(),
-		RetrySettings:      *oc.Retry.Convert(),
-		GRPCClientSettings: *oc.Client.Convert(),
+		QueueConfig:  *oc.Queue.Convert(),
+		RetryConfig:  *oc.Retry.Convert(),
+		ClientConfig: *oc.Client.Convert(),
 	}
 }
 
@@ -260,7 +260,7 @@ type GRPCClientArguments struct {
 var _ river.Defaulter = &GRPCClientArguments{}
 
 // Convert converts args into the upstream type.
-func (args *GRPCClientArguments) Convert() *otelconfiggrpc.GRPCClientSettings {
+func (args *GRPCClientArguments) Convert() *otelconfiggrpc.ClientConfig {
 	if args == nil {
 		return nil
 	}
@@ -281,7 +281,7 @@ func (args *GRPCClientArguments) Convert() *otelconfiggrpc.GRPCClientSettings {
 		balancerName = otelcol.DefaultBalancerName
 	}
 
-	return &otelconfiggrpc.GRPCClientSettings{
+	return &otelconfiggrpc.ClientConfig{
 		Compression: args.Compression.Convert(),
 
 		TLSSetting: *args.TLS.Convert(),