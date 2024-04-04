d3e4e88d29c68c1ffae9920b1156ff83227f7a55
Aaron Sanders <aaron.sanders@grafana.com>
2024-04-04 14:45:51 -0500

    Fixup after rebase

--- internal/component/all/all.go
+++ internal/component/all/all.go
@@ -3,8 +3,6 @@ package all
 
 import (
 	_ "github.com/grafana/agent/internal/component/beyla/ebpf"                               // Import beyla.ebpf
-	_ "github.com/grafana/agent/internal/component/discovery/aws"                            // Import discovery.aws.ec2 and discovery.aws.lightsail
-	_ "github.com/grafana/agent/internal/component/discovery/azure"                          // Import discovery.azure
 	_ "github.com/grafana/agent/internal/component/discovery/consul"                         // Import discovery.consul
 	_ "github.com/grafana/agent/internal/component/discovery/consulagent"                    // Import discovery.consulagent
 	_ "github.com/grafana/agent/internal/component/discovery/dns"                            // Import discovery.dns
