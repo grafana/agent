grafana/alloy

Update pyroscope/ebpf to v0.4.6 (#678)
* Update pyroscope/ebpf to v0.4.4

Note: Pyroscope updated its prometheus/common dependecy and hence this
required further updates.

* Also update windows exporter

* Allow pid maps to be configured

This also modifieds the way we do defaulting and valides those two new
fields.

* Update pyroscope/ebpf v0.4.5

This updates the pyroscope/ebpf annotations to both have alloy and
river ones.

* Add some testing for argument validation

* Update to v0.4.6 to fix debug data

This also includes a test to make sure debug data is marshalled
correctly.

internal/component/pyroscope/ebpf/args.go
@@ -1,6 +1,7 @@
 package ebpf
 
 import (
+	"errors"
 	"time"
 
 	"github.com/grafana/alloy/internal/component/discovery"
@@ -21,4 +22,18 @@ type Arguments struct {
 	CollectKernelProfile bool                   `alloy:"collect_kernel_profile,attr,optional"`
 	Demangle             string                 `alloy:"demangle,attr,optional"`
 	PythonEnabled        bool                   `alloy:"python_enabled,attr,optional"`
+	SymbolsMapSize       int                    `alloy:"symbols_map_size,attr,optional"`
+	PIDMapSize           int                    `alloy:"pid_map_size,attr,optional"`
+}
+
+// Validate implements syntax.Validator.
+func (arg *Arguments) Validate() error {
+	var errs []error
+	if arg.SymbolsMapSize <= 0 {
+		errs = append(errs, errors.New("symbols_map_size must be greater than 0"))
+	}
+	if arg.PIDMapSize <= 0 {
+		errs = append(errs, errors.New("pid_map_size must be greater than 0"))
+	}
+	return errors.Join(errs...)
 }

internal/component/pyroscope/ebpf/ebpf_linux_test.go
@@ -9,16 +9,20 @@ import (
 	"testing"
 	"time"
 
-	"github.com/grafana/alloy/internal/component"
-	"github.com/grafana/alloy/internal/component/pyroscope"
-	"github.com/grafana/alloy/internal/util"
-	"github.com/grafana/alloy/syntax"
 	ebpfspy "github.com/grafana/pyroscope/ebpf"
 	"github.com/grafana/pyroscope/ebpf/pprof"
 	"github.com/grafana/pyroscope/ebpf/sd"
+	"github.com/grafana/pyroscope/ebpf/symtab"
+	"github.com/grafana/pyroscope/ebpf/symtab/elf"
 	"github.com/oklog/run"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/stretchr/testify/require"
+
+	"github.com/grafana/alloy/internal/component"
+	"github.com/grafana/alloy/internal/component/discovery"
+	"github.com/grafana/alloy/internal/component/pyroscope"
+	"github.com/grafana/alloy/internal/util"
+	"github.com/grafana/alloy/syntax"
 )
 
 type mockSession struct {
@@ -67,7 +71,36 @@ func (m *mockSession) CollectProfiles(f pprof.CollectProfilesCallback) error {
 }
 
 func (m *mockSession) DebugInfo() interface{} {
-	return nil
+	return ebpfspy.SessionDebugInfo{
+		ElfCache: symtab.ElfCacheDebugInfo{
+			BuildIDCache: symtab.GCacheDebugInfo[elf.SymTabDebugInfo]{},
+			SameFileCache: symtab.GCacheDebugInfo[elf.SymTabDebugInfo]{
+				LRUSize:      10,
+				RoundSize:    10,
+				CurrentRound: 1,
+				LRUDump: []elf.SymTabDebugInfo{
+					{
+						Name:          "X",
+						Size:          123,
+						LastUsedRound: 1,
+					},
+				},
+			},
+		},
+		PidCache: symtab.GCacheDebugInfo[symtab.ProcTableDebugInfo]{
+			LRUSize:      10,
+			RoundSize:    10,
+			CurrentRound: 1,
+			LRUDump: []symtab.ProcTableDebugInfo{
+				{
+					Pid:  666,
+					Size: 123,
+				},
+			},
+		},
+		Arch:   "my-arch",
+		Kernel: "my-kernel",
+	}
 }
 
 func TestShutdownOnError(t *testing.T) {
@@ -78,7 +111,7 @@ func TestShutdownOnError(t *testing.T) {
 	})
 	require.NoError(t, err)
 	session := &mockSession{}
-	arguments := defaultArguments()
+	arguments := NewDefaultArguments()
 	arguments.CollectInterval = time.Millisecond * 100
 	c := newTestComponent(
 		component.Options{
@@ -105,7 +138,7 @@ func TestContextShutdown(t *testing.T) {
 	})
 	require.NoError(t, err)
 	session := &mockSession{}
-	arguments := defaultArguments()
+	arguments := NewDefaultArguments()
 	arguments.CollectInterval = time.Millisecond * 100
 	c := newTestComponent(
 		component.Options{
@@ -150,8 +183,34 @@ func TestContextShutdown(t *testing.T) {
 }
 
 func TestUnmarshalConfig(t *testing.T) {
-	var arg Arguments
-	err := syntax.Unmarshal([]byte(`targets = [{"service_name" = "foo", "container_id"= "cid"}]
+	for _, tt := range []struct {
+		name        string
+		in          string
+		expected    func() Arguments
+		expectedErr string
+	}{
+		{
+			name: "required-params-only",
+			in: `
+targets = [{"service_name" = "foo", "container_id"= "cid"}]
+forward_to = []
+`,
+			expected: func() Arguments {
+				x := NewDefaultArguments()
+				x.Targets = []discovery.Target{
+					map[string]string{
+						"container_id": "cid",
+						"service_name": "foo",
+					},
+				}
+				x.ForwardTo = []pyroscope.Appendable{}
+				return x
+			},
+		},
+		{
+			name: "full-config",
+			in: `
+targets = [{"service_name" = "foo", "container_id"= "cid"}]
 forward_to = []
 collect_interval = "3s"
 sample_rate = 239
@@ -161,34 +220,131 @@ same_file_cache_size = 3000
 container_id_cache_size = 4000
 cache_rounds = 4
 collect_user_profile = true
-collect_kernel_profile = false`), &arg)
-	require.NoError(t, err)
-	require.Empty(t, arg.ForwardTo)
-	require.Equal(t, time.Second*3, arg.CollectInterval)
-	require.Equal(t, 239, arg.SampleRate)
-	require.Equal(t, 1000, arg.PidCacheSize)
-	require.Equal(t, 2000, arg.BuildIDCacheSize)
-	require.Equal(t, 3000, arg.SameFileCacheSize)
-	require.Equal(t, 4000, arg.ContainerIDCacheSize)
-	require.Equal(t, 4, arg.CacheRounds)
-	require.Equal(t, true, arg.CollectUserProfile)
-	require.Equal(t, false, arg.CollectKernelProfile)
-}
-
-func TestUnmarshalBadConfig(t *testing.T) {
-	var arg Arguments
-	err := syntax.Unmarshal([]byte(`targets = [{"service_name" = "foo", "container_id"= "cid"}]
+collect_kernel_profile = false`,
+			expected: func() Arguments {
+				x := NewDefaultArguments()
+				x.Targets = []discovery.Target{
+					map[string]string{
+						"container_id": "cid",
+						"service_name": "foo",
+					},
+				}
+				x.ForwardTo = []pyroscope.Appendable{}
+				x.CollectInterval = time.Second * 3
+				x.SampleRate = 239
+				x.PidCacheSize = 1000
+				x.BuildIDCacheSize = 2000
+				x.SameFileCacheSize = 3000
+				x.ContainerIDCacheSize = 4000
+				x.CacheRounds = 4
+				x.CollectUserProfile = true
+				x.CollectKernelProfile = false
+				return x
+			},
+		},
+		{
+			name: "syntax-problem",
+			in: `
+targets = [{"service_name" = "foo", "container_id"= "cid"}]
 forward_to = []
 collect_interval = 3s"
-sample_rate = 239
-pid_cache_size = 1000
-build_id_cache_size = 2000
-same_file_cache_size = 3000
-container_id_cache_size = 4000
-cache_rounds = 4
-collect_user_profile = true
-collect_kernel_profile = false`), &arg)
-	require.Error(t, err)
+`,
+			expectedErr: "4:21: expected TERMINATOR, got IDENT (and 1 more diagnostics)",
+		},
+		{
+			name: "incorrect-map-sizes",
+			in: `
+targets = [{"service_name" = "foo", "container_id"= "cid"}]
+forward_to = []
+symbols_map_size = -1
+pid_map_size = 0
+`,
+			expectedErr: "symbols_map_size must be greater than 0\npid_map_size must be greater than 0",
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			arg := Arguments{}
+			if tt.expectedErr != "" {
+				err := syntax.Unmarshal([]byte(tt.in), &arg)
+				require.Error(t, err)
+				require.Equal(t, tt.expectedErr, err.Error())
+				return
+			}
+			require.NoError(t, syntax.Unmarshal([]byte(tt.in), &arg))
+			require.Equal(t, tt.expected(), arg)
+		})
+	}
+}
+
+type mockTargetFinder struct {
+	sd.TargetFinder
+}
+
+func (m *mockTargetFinder) DebugInfo() []map[string]string {
+	return []map[string]string{
+		{"__container_id__": "foo", "__name__": "process_cpu", "container": "kube-proxy"},
+		{"__container_id__": "baz", "__name__": "process_cpu", "container": "kube-proxy"},
+	}
+}
+
+func TestDebugInfo(t *testing.T) {
+	c := &Component{
+		session:      &mockSession{},
+		targetFinder: &mockTargetFinder{},
+	}
+
+	c.updateDebugInfo()
+	di := c.DebugInfo()
+
+	v, err := syntax.Marshal(di)
+	require.NoError(t, err)
+
+	require.Equal(t, `targets = [{
+	__container_id__ = "foo",
+	__name__         = "process_cpu",
+	container        = "kube-proxy",
+}, {
+	__container_id__ = "baz",
+	__name__         = "process_cpu",
+	container        = "kube-proxy",
+}]
+session = {
+	elf_cache = {
+		build_id_cache = {
+			lru_size      = 0,
+			round_size    = 0,
+			current_round = 0,
+			lru_dump      = [],
+			round_dump    = [],
+		},
+		same_file_cache = {
+			lru_size      = 10,
+			round_size    = 10,
+			current_round = 1,
+			lru_dump      = [{
+				name            = "X",
+				symbol_count    = 123,
+				file            = "",
+				last_used_round = 1,
+			}],
+			round_dump = [],
+		},
+	},
+	pid_cache = {
+		lru_size      = 10,
+		round_size    = 10,
+		current_round = 1,
+		lru_dump      = [{
+			elfs = {},
+			size            = 123,
+			pid             = 666,
+			last_used_round = 0,
+		}],
+		round_dump = [],
+	},
+	arch   = "my-arch",
+	kernel = "my-kernel",
+}`, string(v))
 }
 
 func newTestComponent(opts component.Options, args Arguments, session *mockSession, targetFinder sd.TargetFinder, ms *metrics) *Component {

grafana/agent

internal/component/pyroscope/ebpf/args.go
@@ -1,6 +1,7 @@
 package ebpf
 
 import (
+	"errors"
 	"time"
 
 	"github.com/grafana/agent/internal/component/discovery"
@@ -21,4 +22,18 @@ type Arguments struct {
 	CollectKernelProfile bool                   `river:"collect_kernel_profile,attr,optional"`
 	Demangle             string                 `river:"demangle,attr,optional"`
 	PythonEnabled        bool                   `river:"python_enabled,attr,optional"`
+	SymbolsMapSize       int                    `river:"symbols_map_size,attr,optional"`
+	PIDMapSize           int                    `river:"pid_map_size,attr,optional"`
+}
+
+// Validate implements syntax.Validator.
+func (arg *Arguments) Validate() error {
+	var errs []error
+	if arg.SymbolsMapSize <= 0 {
+		errs = append(errs, errors.New("symbols_map_size must be greater than 0"))
+	}
+	if arg.PIDMapSize <= 0 {
+		errs = append(errs, errors.New("pid_map_size must be greater than 0"))
+	}
+	return errors.Join(errs...)
 }

internal/component/pyroscope/ebpf/ebpf_linux_test.go
@@ -9,16 +9,20 @@ import (
 	"testing"
 	"time"
 
-	"github.com/grafana/agent/internal/component"
-	"github.com/grafana/agent/internal/component/pyroscope"
-	"github.com/grafana/agent/internal/util"
 	ebpfspy "github.com/grafana/pyroscope/ebpf"
 	"github.com/grafana/pyroscope/ebpf/pprof"
 	"github.com/grafana/pyroscope/ebpf/sd"
-	"github.com/grafana/river"
+	"github.com/grafana/pyroscope/ebpf/symtab"
+	"github.com/grafana/pyroscope/ebpf/symtab/elf"
+	syntax "github.com/grafana/river"
 	"github.com/oklog/run"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/stretchr/testify/require"
+
+	"github.com/grafana/agent/internal/component"
+	"github.com/grafana/agent/internal/component/discovery"
+	"github.com/grafana/agent/internal/component/pyroscope"
+	"github.com/grafana/agent/internal/util"
 )
 
 type mockSession struct {
@@ -67,7 +71,36 @@ func (m *mockSession) CollectProfiles(f pprof.CollectProfilesCallback) error {
 }
 
 func (m *mockSession) DebugInfo() interface{} {
-	return nil
+	return ebpfspy.SessionDebugInfo{
+		ElfCache: symtab.ElfCacheDebugInfo{
+			BuildIDCache: symtab.GCacheDebugInfo[elf.SymTabDebugInfo]{},
+			SameFileCache: symtab.GCacheDebugInfo[elf.SymTabDebugInfo]{
+				LRUSize:      10,
+				RoundSize:    10,
+				CurrentRound: 1,
+				LRUDump: []elf.SymTabDebugInfo{
+					{
+						Name:          "X",
+						Size:          123,
+						LastUsedRound: 1,
+					},
+				},
+			},
+		},
+		PidCache: symtab.GCacheDebugInfo[symtab.ProcTableDebugInfo]{
+			LRUSize:      10,
+			RoundSize:    10,
+			CurrentRound: 1,
+			LRUDump: []symtab.ProcTableDebugInfo{
+				{
+					Pid:  666,
+					Size: 123,
+				},
+			},
+		},
+		Arch:   "my-arch",
+		Kernel: "my-kernel",
+	}
 }
 
 func TestShutdownOnError(t *testing.T) {
@@ -78,7 +111,7 @@ func TestShutdownOnError(t *testing.T) {
 	})
 	require.NoError(t, err)
 	session := &mockSession{}
-	arguments := defaultArguments()
+	arguments := NewDefaultArguments()
 	arguments.CollectInterval = time.Millisecond * 100
 	c := newTestComponent(
 		component.Options{
@@ -105,7 +138,7 @@ func TestContextShutdown(t *testing.T) {
 	})
 	require.NoError(t, err)
 	session := &mockSession{}
-	arguments := defaultArguments()
+	arguments := NewDefaultArguments()
 	arguments.CollectInterval = time.Millisecond * 100
 	c := newTestComponent(
 		component.Options{
@@ -150,8 +183,34 @@ func TestContextShutdown(t *testing.T) {
 }
 
 func TestUnmarshalConfig(t *testing.T) {
-	var arg Arguments
-	err := river.Unmarshal([]byte(`targets = [{"service_name" = "foo", "container_id"= "cid"}]
+	for _, tt := range []struct {
+		name        string
+		in          string
+		expected    func() Arguments
+		expectedErr string
+	}{
+		{
+			name: "required-params-only",
+			in: `
+targets = [{"service_name" = "foo", "container_id"= "cid"}]
+forward_to = []
+`,
+			expected: func() Arguments {
+				x := NewDefaultArguments()
+				x.Targets = []discovery.Target{
+					map[string]string{
+						"container_id": "cid",
+						"service_name": "foo",
+					},
+				}
+				x.ForwardTo = []pyroscope.Appendable{}
+				return x
+			},
+		},
+		{
+			name: "full-config",
+			in: `
+targets = [{"service_name" = "foo", "container_id"= "cid"}]
 forward_to = []
 collect_interval = "3s"
 sample_rate = 239
@@ -161,34 +220,131 @@ same_file_cache_size = 3000
 container_id_cache_size = 4000
 cache_rounds = 4
 collect_user_profile = true
-collect_kernel_profile = false`), &arg)
-	require.NoError(t, err)
-	require.Empty(t, arg.ForwardTo)
-	require.Equal(t, time.Second*3, arg.CollectInterval)
-	require.Equal(t, 239, arg.SampleRate)
-	require.Equal(t, 1000, arg.PidCacheSize)
-	require.Equal(t, 2000, arg.BuildIDCacheSize)
-	require.Equal(t, 3000, arg.SameFileCacheSize)
-	require.Equal(t, 4000, arg.ContainerIDCacheSize)
-	require.Equal(t, 4, arg.CacheRounds)
-	require.Equal(t, true, arg.CollectUserProfile)
-	require.Equal(t, false, arg.CollectKernelProfile)
-}
-
-func TestUnmarshalBadConfig(t *testing.T) {
-	var arg Arguments
-	err := river.Unmarshal([]byte(`targets = [{"service_name" = "foo", "container_id"= "cid"}]
+collect_kernel_profile = false`,
+			expected: func() Arguments {
+				x := NewDefaultArguments()
+				x.Targets = []discovery.Target{
+					map[string]string{
+						"container_id": "cid",
+						"service_name": "foo",
+					},
+				}
+				x.ForwardTo = []pyroscope.Appendable{}
+				x.CollectInterval = time.Second * 3
+				x.SampleRate = 239
+				x.PidCacheSize = 1000
+				x.BuildIDCacheSize = 2000
+				x.SameFileCacheSize = 3000
+				x.ContainerIDCacheSize = 4000
+				x.CacheRounds = 4
+				x.CollectUserProfile = true
+				x.CollectKernelProfile = false
+				return x
+			},
+		},
+		{
+			name: "syntax-problem",
+			in: `
+targets = [{"service_name" = "foo", "container_id"= "cid"}]
 forward_to = []
 collect_interval = 3s"
-sample_rate = 239
-pid_cache_size = 1000
-build_id_cache_size = 2000
-same_file_cache_size = 3000
-container_id_cache_size = 4000
-cache_rounds = 4
-collect_user_profile = true
-collect_kernel_profile = false`), &arg)
-	require.Error(t, err)
+`,
+			expectedErr: "4:21: expected TERMINATOR, got IDENT (and 1 more diagnostics)",
+		},
+		{
+			name: "incorrect-map-sizes",
+			in: `
+targets = [{"service_name" = "foo", "container_id"= "cid"}]
+forward_to = []
+symbols_map_size = -1
+pid_map_size = 0
+`,
+			expectedErr: "symbols_map_size must be greater than 0\npid_map_size must be greater than 0",
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			arg := Arguments{}
+			if tt.expectedErr != "" {
+				err := syntax.Unmarshal([]byte(tt.in), &arg)
+				require.Error(t, err)
+				require.Equal(t, tt.expectedErr, err.Error())
+				return
+			}
+			require.NoError(t, syntax.Unmarshal([]byte(tt.in), &arg))
+			require.Equal(t, tt.expected(), arg)
+		})
+	}
+}
+
+type mockTargetFinder struct {
+	sd.TargetFinder
+}
+
+func (m *mockTargetFinder) DebugInfo() []map[string]string {
+	return []map[string]string{
+		{"__container_id__": "foo", "__name__": "process_cpu", "container": "kube-proxy"},
+		{"__container_id__": "baz", "__name__": "process_cpu", "container": "kube-proxy"},
+	}
+}
+
+func TestDebugInfo(t *testing.T) {
+	c := &Component{
+		session:      &mockSession{},
+		targetFinder: &mockTargetFinder{},
+	}
+
+	c.updateDebugInfo()
+	di := c.DebugInfo()
+
+	v, err := syntax.Marshal(di)
+	require.NoError(t, err)
+
+	require.Equal(t, `targets = [{
+	__container_id__ = "foo",
+	__name__         = "process_cpu",
+	container        = "kube-proxy",
+}, {
+	__container_id__ = "baz",
+	__name__         = "process_cpu",
+	container        = "kube-proxy",
+}]
+session = {
+	elf_cache = {
+		build_id_cache = {
+			lru_size      = 0,
+			round_size    = 0,
+			current_round = 0,
+			lru_dump      = [],
+			round_dump    = [],
+		},
+		same_file_cache = {
+			lru_size      = 10,
+			round_size    = 10,
+			current_round = 1,
+			lru_dump      = [{
+				name            = "X",
+				symbol_count    = 123,
+				file            = "",
+				last_used_round = 1,
+			}],
+			round_dump = [],
+		},
+	},
+	pid_cache = {
+		lru_size      = 10,
+		round_size    = 10,
+		current_round = 1,
+		lru_dump      = [{
+			elfs = {},
+			size            = 123,
+			pid             = 666,
+			last_used_round = 0,
+		}],
+		round_dump = [],
+	},
+	arch   = "my-arch",
+	kernel = "my-kernel",
+}`, string(v))
 }
 
 func newTestComponent(opts component.Options, args Arguments, session *mockSession, targetFinder sd.TargetFinder, ms *metrics) *Component {