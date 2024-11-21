610d49872a96c616fe4b1594bb20a7682cbd8842
Matt Durham <mattdurham@ppog.org>
2024-11-21 12:11:18 -0500

    make versioned

--- static/operator/defaults.go
+++ static/operator/defaults.go
@@ -2,7 +2,7 @@ package operator
 
 // Supported versions of the Grafana Agent.
 var (
-	DefaultAgentVersion   = "v0.42.0"
+	DefaultAgentVersion   = "v0.43.4"
 	DefaultAgentBaseImage = "grafana/agent"
 	DefaultAgentImage     = DefaultAgentBaseImage + ":" + DefaultAgentVersion
 )
