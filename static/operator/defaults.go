1be16dcbd101b5c345baa1cfe0d11b9f9dc4f225
Joe Harvey <51208233+jharvey10@users.noreply.github.com>
2026-01-23 10:31:01 -0500

    chore: update agent version to v0.44.7 (#7248)

--- static/operator/defaults.go
+++ static/operator/defaults.go
@@ -2,7 +2,7 @@ package operator
 
 // Supported versions of the Grafana Agent.
 var (
-	DefaultAgentVersion   = "v0.44.6"
+	DefaultAgentVersion   = "v0.44.7"
 	DefaultAgentBaseImage = "grafana/agent"
 	DefaultAgentImage     = DefaultAgentBaseImage + ":" + DefaultAgentVersion
 )
